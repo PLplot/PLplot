9b9d51b7954c14ad25cee41f4aaad9b1d5a985da
Maurice LeBrun <mjl@users.sourceforge.net>
1993-01-23 05:53:31 +0000

    Formatting changes only (everything got run through indent).
    svn path=/trunk/; revision=75

--- src/pldtik.c
+++ src/pldtik.c
@@ -1,8 +1,11 @@
 /* $Id$
    $Log$
-   Revision 1.2  1992/09/29 04:45:52  furnish
-   Massive clean up effort to remove support for garbage compilers (K&R).
+   Revision 1.3  1993/01/23 05:53:31  mjl
+   Formatting changes only (everything got run through indent).
 
+ * Revision 1.2  1992/09/29  04:45:52  furnish
+ * Massive clean up effort to remove support for garbage compilers (K&R).
+ *
  * Revision 1.1  1992/05/20  21:34:21  furnish
  * Initial checkin of the whole PLPLOT project.
  *
@@ -10,7 +13,7 @@
 
 /*	pldtik.c
 
-	Determines tick spacing and mode (fixed or floating) of 
+	Determines tick spacing and mode (fixed or floating) of
 	numeric axis labels.
 */
 
@@ -43,34 +46,36 @@
 * of digits a label may occupy.  If digmax<0, it is disregarded, and if
 * digmax=0 the default value is used.  For digmax>0, mode=1 is chosen if
 * there is insufficient room for the label within the specified # of digits.
-* 
-* In the case of mode=0, the actual # of digits will become too large when 
+*
+* In the case of mode=0, the actual # of digits will become too large when
 * the magnitude of the labels become too large.  The mode=1 case offers the
-* greatest precision for the smallest field length. 
-* 
+* greatest precision for the smallest field length.
+*
 * The determination of maximum length for fixed point quantities is
 * complicated by the fact that very long fixed point representations look
 * much worse than the same sized floating point representation.  Further, a
 * fixed point number with a large negative exponent will actually gain in
 * precision when written as floating point.  Thus we use certain fuzz
 * factors to get 'digfix' from 'digmax', however it will always be true
-* that digfix<=digmax. 
-* 
+* that digfix<=digmax.
+*
 * Finally, if 'digmax' is set, 'prec' is reduced in size if necessary so
 * that the labels fit the requested field length.
 \*----------------------------------------------------------------------*/
 
 void
-pldtik(PLFLT vmin, PLFLT vmax, PLFLT * tick, PLINT * nsubt,
-       PLINT * mode, PLINT * prec, PLINT digmax, PLINT * scale)
+pldtik(PLFLT vmin, PLFLT vmax, PLFLT *tick, PLINT *nsubt,
+       PLINT *mode, PLINT *prec, PLINT digmax, PLINT *scale)
 {
     PLFLT t0, t1, t2, vmod;
-    PLINT msd, np, ns, digmin, digfix, autoscale=0;
+    PLINT msd, np, ns, digmin, digfix, autoscale = 0;
 
     *mode = 0;
     *scale = 0;
-    if (*tick == 0.0) autoscale = 1;
-    if (digmax == 0) digmax = DIGMAX_DEF;
+    if (*tick == 0.0)
+	autoscale = 1;
+    if (digmax == 0)
+	digmax = DIGMAX_DEF;
 
 /* Magnitute of min/max to get number of significant digits */
 
@@ -118,25 +123,27 @@ pldtik(PLFLT vmin, PLFLT vmax, PLFLT * tick, PLINT * nsubt,
     else if (t1 > 0.1760912591) {
 	t2 = 5.0;
 	ns = 5;
-	np = np-1;
+	np = np - 1;
     }
-    else  {
+    else {
 	t2 = 2.0;
 	ns = 4;
-	np = np-1;
+	np = np - 1;
     }
 
 /* Now compute tick spacing */
 
     if (autoscale) {
-	*tick = t2 * pow(10.0,(double)np);
-	if (vmin > vmax) *tick = -*tick;
+	*tick = t2 * pow(10.0, (double) np);
+	if (vmin > vmax)
+	    *tick = -*tick;
     }
-    else  {
+    else {
 	t1 = log10(ABS(*tick));
-	np = (PLINT)floor(t1);
+	np = (PLINT) floor(t1);
     }
-    if (*nsubt == 0) *nsubt = ns;
+    if (*nsubt == 0)
+	*nsubt = ns;
 
 /* Establish precision.  */
 
@@ -166,4 +173,3 @@ pldtik(PLFLT vmin, PLFLT vmax, PLFLT * tick, PLINT * nsubt,
 	}
     }
 }
-
