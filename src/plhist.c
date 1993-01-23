fded2ef88fe8dab2a4ec4317e511e24984b642f6
Maurice LeBrun <mjl@users.sourceforge.net>
1993-01-23 05:55:21 +0000

    Formatting changes only, I think.
    svn path=/trunk/; revision=78

--- src/plhist.c
+++ src/plhist.c
@@ -1,10 +1,13 @@
 /* $Id$
    $Log$
-   Revision 1.4  1992/10/12 17:08:04  mjl
-   Added PL_NEED_SIZE_T define to those files that need to know the value
-   of (size_t) for non-POSIX systems (in this case the Amiga) that require you
-   to include <stddef.h> to get it.
+   Revision 1.5  1993/01/23 05:55:21  mjl
+   Formatting changes only, I think.
 
+ * Revision 1.4  1992/10/12  17:08:04  mjl
+ * Added PL_NEED_SIZE_T define to those files that need to know the value
+ * of (size_t) for non-POSIX systems (in this case the Amiga) that require you
+ * to include <stddef.h> to get it.
+ *
  * Revision 1.3  1992/09/30  18:25:50  furnish
  * Massive cleanup to irradicate garbage code.  Almost everything is now
  * prototyped correctly.  Builds on HPUX, SUNOS (gcc), AIX, and UNICOS.
@@ -35,12 +38,12 @@
 * Draws a histogram of n values of a variable in array data[0..n-1] in
 * the range datmin to datmax using nbin bins. If "oldwin" is 1, the
 * histogram is plotted in the current window. If not, the routine calls
-* "plenv" to set up the graphics environment. 
+* "plenv" to set up the graphics environment.
 \*----------------------------------------------------------------------*/
 
 void
-c_plhist (PLINT n, PLFLT * data, PLFLT datmin, PLFLT datmax,
-	  PLINT nbin, PLINT oldwin)
+c_plhist(PLINT n, PLFLT *data, PLFLT datmin, PLFLT datmax,
+	 PLINT nbin, PLINT oldwin)
 {
     PLINT bin, level;
     PLFLT *x, *y, dx, ymax;
@@ -48,16 +51,19 @@ c_plhist (PLINT n, PLFLT * data, PLFLT datmin, PLFLT datmax,
 
     glev(&level);
     if (level < 1)
-	plexit("Please call plstar before plhist.");
+	plexit("plhist: Please call plstar first.");
+
     if (level < 3 && oldwin)
-	plexit("Please set up window before calling plhist.");
+	plexit("plhist: Please set up window first.");
+
     if (datmin >= datmax)
-	plexit("Data range invalid in plhist.");
+	plexit("plhist: Data range invalid.");
 
     if (!(x = (PLFLT *) malloc((size_t) nbin * sizeof(PLFLT))))
-	plexit("Out of memory in plhist");
+	 plexit("plhist: Out of memory");
+
     if (!(y = (PLFLT *) malloc((size_t) nbin * sizeof(PLFLT))))
-	plexit("Out of memory in plhist");
+	 plexit("plhist: Out of memory");
 
     dx = (datmax - datmin) / nbin;
     for (i = 0; i < nbin; i++) {
@@ -82,4 +88,70 @@ c_plhist (PLINT n, PLFLT * data, PLFLT datmin, PLFLT datmax,
     plbin(nbin, x, y, 0);
     free((VOID *) x);
     free((VOID *) y);
+}
+
+/*----------------------------------------------------------------------*\
+* void plbin()
+*
+* Plot a histogram using the arrays x and y to represent data values
+* and frequencies respectively. If center is false, x values denote the
+* lower edge of the bin, and if center is true, they denote the center
+* of the bin.
+\*----------------------------------------------------------------------*/
+
+void
+c_plbin(PLINT nbin, PLFLT *x, PLFLT *y, PLINT center)
+{
+    PLINT i;
+    PLFLT xmin, xmax, vpwxmi, vpwxma, vpwymi, vpwyma;
+    PLINT level;
+
+    glev(&level);
+    if (level < 3)
+	plexit("plbin: Please set up window first.");
+
+    /* Check x[i] are in ascending order */
+
+    for (i = 0; i < nbin - 1; i++)
+	if (x[i] >= x[i + 1])
+	    plexit("plbin: Elements of x[] must be increasing.");
+
+    gvpw(&vpwxmi, &vpwxma, &vpwymi, &vpwyma);
+    if (!center) {
+	for (i = 0; i < nbin - 1; i++) {
+	    pljoin(x[i], vpwymi, x[i], y[i]);
+	    pljoin(x[i], y[i], x[i + 1], y[i]);
+	    pljoin(x[i + 1], y[i], x[i + 1], vpwymi);
+	}
+	if (x[nbin - 1] < vpwxma) {
+	    pljoin(x[nbin - 1], vpwymi, x[nbin - 1], y[nbin - 1]);
+	    pljoin(x[nbin - 1], y[nbin - 1], vpwxma, y[nbin - 1]);
+	    pljoin(vpwxma, y[nbin - 1], vpwxma, vpwymi);
+	}
+    }
+    else {
+	if (nbin < 2)
+	    return;
+	xmin = vpwxmi;
+	xmax = MAX(0.5 * (x[0] + x[2]), vpwxmi);
+	if (xmin < xmax) {
+	    pljoin(xmin, vpwymi, xmin, y[0]);
+	    pljoin(xmin, y[0], xmax, y[0]);
+	    pljoin(xmax, y[0], xmax, vpwymi);
+	}
+	for (i = 1; i < nbin - 1; i++) {
+	    xmin = xmax;
+	    xmax = MIN(0.5 * (x[i] + x[i + 1]), vpwxma);
+	    pljoin(xmin, vpwymi, xmin, y[i]);
+	    pljoin(xmin, y[i], xmax, y[i]);
+	    pljoin(xmax, y[i], xmax, vpwymi);
+	}
+	xmin = xmax;
+	xmax = vpwxma;
+	if (xmin < xmax) {
+	    pljoin(xmin, vpwymi, xmin, y[nbin - 1]);
+	    pljoin(xmin, y[nbin - 1], xmax, y[nbin - 1]);
+	    pljoin(xmax, y[nbin - 1], xmax, vpwymi);
+	}
+    }
 }
