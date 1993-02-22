415b005bc8c6781ba7c66b3dd3d282aaeabd2947
Maurice LeBrun <mjl@users.sourceforge.net>
1993-02-22 23:15:12 +0000

    Eliminated the gradv() driver calls, as these were made obsolete by recent changes to plmeta and plrender.  Also eliminated page clear commands from grtidy() -- plend now calls grclr() and grtidy() explicitly.  Eliminated the (atend) specification for BoundingBox, since this isn't recognized by some programs; instead enough space is left for it and a rewind and subsequent write is done from ps_tidy().  Familying is no longer directly supported by the PS driver as a result.  The output done by the @end function was eliminated to reduce aggravation when viewing with ghostview.
    svn path=/trunk/; revision=102

--- drivers/ps.c
+++ drivers/ps.c
@@ -1,9 +1,19 @@
 /* $Id$
    $Log$
-   Revision 1.6  1993/01/23 05:41:51  mjl
-   Changes to support new color model, polylines, and event handler support
-   (interactive devices only).
+   Revision 1.7  1993/02/22 23:15:12  mjl
+   Eliminated the gradv() driver calls, as these were made obsolete by recent
+   changes to plmeta and plrender.  Also eliminated page clear commands from
+   grtidy() -- plend now calls grclr() and grtidy() explicitly.  Eliminated
+   the (atend) specification for BoundingBox, since this isn't recognized by
+   some programs; instead enough space is left for it and a rewind and
+   subsequent write is done from ps_tidy().  Familying is no longer directly
+   supported by the PS driver as a result.  The output done by the @end
+   function was eliminated to reduce aggravation when viewing with ghostview.
 
+ * Revision 1.6  1993/01/23  05:41:51  mjl
+ * Changes to support new color model, polylines, and event handler support
+ * (interactive devices only).
+ *
  * Revision 1.5  1992/11/07  07:48:47  mjl
  * Fixed orientation operation in several files and standardized certain startup
  * operations. Fixed bugs in various drivers.
@@ -58,6 +68,8 @@ static char *getdate(void);
 #define PSX             XPSSIZE-1
 #define PSY             YPSSIZE-1
 
+#define OF		pls->OutFile
+
 static char outbuf[128];
 static int llx = XPSSIZE, lly = YPSSIZE, urx = 0, ury = 0, ptcnt;
 static PLINT orient;
@@ -82,6 +94,7 @@ ps_init(PLStream *pls)
     pls->icol0 = 1;
     pls->bytecnt = 0;
     pls->page = 0;
+    pls->family = 0;		/* I don't want to support familying here */
 
     if (!pls->colorset)
 	pls->color = 0;		/* no color by default: user can override */
@@ -95,10 +108,6 @@ ps_init(PLStream *pls)
     }
     else
 	pls->width = 1;
-
-/* Initialize family file info */
-
-    plFamInit(pls);
 
 /* Prompt for a file name if not already set */
 
@@ -139,120 +148,118 @@ ps_init(PLStream *pls)
 
     /* Header comments into PostScript file */
 
-    fprintf(pls->OutFile, "%%!PS-Adobe-2.0 EPSF-2.0\n");
-    fprintf(pls->OutFile, "%%%%BoundingBox: (atend)\n");
-    fprintf(pls->OutFile, "%%%%Title: PLPLOT Graph\n");
-    fprintf(pls->OutFile, "%%%%Creator: PLPLOT Version 4.0\n");
-    fprintf(pls->OutFile, "%%%%CreationDate: %s\n", getdate());
-    fprintf(pls->OutFile, "%%%%Pages: (atend)\n");
-    fprintf(pls->OutFile, "%%%%EndComments\n\n");
+    fprintf(OF, "%%!PS-Adobe-2.0 EPSF-2.0\n");
+    fprintf(OF, "%%%%BoundingBox:         \n");
+    fprintf(OF, "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+
+    fprintf(OF, "%%%%Title: PLPLOT Graph\n");
+    fprintf(OF, "%%%%Creator: PLPLOT Version 4.0\n");
+    fprintf(OF, "%%%%CreationDate: %s\n", getdate());
+    fprintf(OF, "%%%%Pages: (atend)\n");
+    fprintf(OF, "%%%%EndComments\n\n");
 
     /* Definitions */
 
-    fprintf(pls->OutFile, "/PSSave save def\n");	/* save VM state */
-    fprintf(pls->OutFile, "/PSDict 200 dict def\n");	/* define a dictionary */
-    fprintf(pls->OutFile, "PSDict begin\n");	/* start using it */
-    fprintf(pls->OutFile, "/@restore /restore load def\n");
-    fprintf(pls->OutFile, "/restore\n");
-    fprintf(pls->OutFile, "   {vmstatus pop\n");
-    fprintf(pls->OutFile, "    dup @VMused lt {pop @VMused} if\n");
-    fprintf(pls->OutFile, "    exch pop exch @restore /@VMused exch def\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/@pri\n");
-    fprintf(pls->OutFile, "   {\n");
-    fprintf(pls->OutFile, "    ( ) print\n");
-    fprintf(pls->OutFile, "    (                                       ) cvs print\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/@copies\n");	/* n @copies - */
-    fprintf(pls->OutFile, "   {\n");
-    fprintf(pls->OutFile, "    /#copies exch def\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/@start\n");	/* - @start -  -- start everything */
-    fprintf(pls->OutFile, "   {\n");
-    fprintf(pls->OutFile, "    vmstatus pop /@VMused exch def pop\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/@end\n");	/* - @end -  -- finished */
-    fprintf(pls->OutFile, "   {(VM Used: ) print @VMused @pri\n");
-    fprintf(pls->OutFile, "    (. Unused: ) print vmstatus @VMused sub @pri pop pop\n");
-    fprintf(pls->OutFile, "    (\\n) print flush\n");
-    fprintf(pls->OutFile, "    end\n");
-    fprintf(pls->OutFile, "    PSSave restore\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/bop\n");	/* bop -  -- begin a new page */
-    fprintf(pls->OutFile, "   {\n");
-    fprintf(pls->OutFile, "    /SaveImage save def\n");
+    fprintf(OF, "/PSSave save def\n");	/* save VM state */
+    fprintf(OF, "/PSDict 200 dict def\n");	/* define a dictionary */
+    fprintf(OF, "PSDict begin\n");	/* start using it */
+    fprintf(OF, "/@restore /restore load def\n");
+    fprintf(OF, "/restore\n");
+    fprintf(OF, "   {vmstatus pop\n");
+    fprintf(OF, "    dup @VMused lt {pop @VMused} if\n");
+    fprintf(OF, "    exch pop exch @restore /@VMused exch def\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/@pri\n");
+    fprintf(OF, "   {\n");
+    fprintf(OF, "    ( ) print\n");
+    fprintf(OF, "    (                                       ) cvs print\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/@copies\n");	/* n @copies - */
+    fprintf(OF, "   {\n");
+    fprintf(OF, "    /#copies exch def\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/@start\n");	/* - @start -  -- start everything */
+    fprintf(OF, "   {\n");
+    fprintf(OF, "    vmstatus pop /@VMused exch def pop\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/@end\n");	/* - @end -  -- finished */
+    fprintf(OF, "   {flush\n");
+    fprintf(OF, "    end\n");
+    fprintf(OF, "    PSSave restore\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/bop\n");	/* bop -  -- begin a new page */
+    fprintf(OF, "   {\n");
+    fprintf(OF, "    /SaveImage save def\n");
     if (pls->color) {
-	fprintf(pls->OutFile, "    Z %d %d M %d %d D %d %d D %d %d D %d %d",
+	fprintf(OF, "    Z %d %d M %d %d D %d %d D %d %d D %d %d",
 		0, 0, 0, PSY, PSX, PSY, PSX, 0, 0, 0);
-	r = pls->cmap0[0].r / 255.;
-	g = pls->cmap0[0].g / 255.;
-	b = pls->cmap0[0].b / 255.;
-	fprintf(pls->OutFile, "    closepath %f %f %f setrgbcolor fill\n",
+	r = ((float) pls->cmap0[0].r) / 255.;
+	g = ((float) pls->cmap0[0].g) / 255.;
+	b = ((float) pls->cmap0[0].b) / 255.;
+	fprintf(OF, "    closepath %f %f %f setrgbcolor fill\n",
 		r, g, b);
     }
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/eop\n");	/* - eop -  -- end a page */
-    fprintf(pls->OutFile, "   {\n");
-    fprintf(pls->OutFile, "    showpage\n");
-    fprintf(pls->OutFile, "    SaveImage restore\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/@line\n");	/* set line parameters */
-    fprintf(pls->OutFile, "   {0 setlinecap\n");
-    fprintf(pls->OutFile, "    0 setlinejoin\n");
-    fprintf(pls->OutFile, "    1 setmiterlimit\n");
-    fprintf(pls->OutFile, "   } def\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/eop\n");	/* - eop -  -- end a page */
+    fprintf(OF, "   {\n");
+    fprintf(OF, "    showpage\n");
+    fprintf(OF, "    SaveImage restore\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/@line\n");	/* set line parameters */
+    fprintf(OF, "   {0 setlinecap\n");
+    fprintf(OF, "    0 setlinejoin\n");
+    fprintf(OF, "    1 setmiterlimit\n");
+    fprintf(OF, "   } def\n");
     /* d @hsize -  horizontal clipping dimension */
-    fprintf(pls->OutFile, "/@hsize   {/hs exch def} def\n");
-    fprintf(pls->OutFile, "/@vsize   {/vs exch def} def\n");
+    fprintf(OF, "/@hsize   {/hs exch def} def\n");
+    fprintf(OF, "/@vsize   {/vs exch def} def\n");
     /* d @hoffset - shift for the plots */
-    fprintf(pls->OutFile, "/@hoffset {/ho exch def} def\n");
-    fprintf(pls->OutFile, "/@voffset {/vo exch def} def\n");
+    fprintf(OF, "/@hoffset {/ho exch def} def\n");
+    fprintf(OF, "/@voffset {/vo exch def} def\n");
     /* s @hscale - scale factors */
-    fprintf(pls->OutFile, "/@hscale  {100 div /hsc exch def} def\n");
-    fprintf(pls->OutFile, "/@vscale  {100 div /vsc exch def} def\n");
+    fprintf(OF, "/@hscale  {100 div /hsc exch def} def\n");
+    fprintf(OF, "/@vscale  {100 div /vsc exch def} def\n");
     /* s @lscale - linewidth scale factor */
-    fprintf(pls->OutFile, "/@lscale  {100 div /lin exch def} def\n");
-    fprintf(pls->OutFile, "/@lwidth  {lin lw mul setlinewidth} def\n");
-    fprintf(pls->OutFile, "/@SetPlot\n");	/* setup user specified
+    fprintf(OF, "/@lscale  {100 div /lin exch def} def\n");
+    fprintf(OF, "/@lwidth  {lin lw mul setlinewidth} def\n");
+    fprintf(OF, "/@SetPlot\n");	/* setup user specified
 						   offsets, */
-    fprintf(pls->OutFile, "   {\n");	/* scales, sizes for clipping    */
-    fprintf(pls->OutFile, "    ho vo translate\n");
-    fprintf(pls->OutFile, "    XScale YScale scale\n");
-    fprintf(pls->OutFile, "    lin lw mul setlinewidth\n");
-    fprintf(pls->OutFile, "   } def\n");
-    fprintf(pls->OutFile, "/XScale\n");	/* setup x scale */
-    fprintf(pls->OutFile, "   {hsc hs mul %d div} def\n", YPSSIZE);
-    fprintf(pls->OutFile, "/YScale\n");	/* setup y scale */
-    fprintf(pls->OutFile, "   {vsc vs mul %d div} def\n", XPSSIZE);
+    fprintf(OF, "   {\n");	/* scales, sizes for clipping    */
+    fprintf(OF, "    ho vo translate\n");
+    fprintf(OF, "    XScale YScale scale\n");
+    fprintf(OF, "    lin lw mul setlinewidth\n");
+    fprintf(OF, "   } def\n");
+    fprintf(OF, "/XScale\n");	/* setup x scale */
+    fprintf(OF, "   {hsc hs mul %d div} def\n", YPSSIZE);
+    fprintf(OF, "/YScale\n");	/* setup y scale */
+    fprintf(OF, "   {vsc vs mul %d div} def\n", XPSSIZE);
 
     /* Default line width */
 
-    fprintf(pls->OutFile, "/lw %d def\n", pls->width);
+    fprintf(OF, "/lw %d def\n", pls->width);
 
     /* Stroke definitions, to keep output file as small as possible */
 
-    fprintf(pls->OutFile, "/M {moveto} def\n");
-    fprintf(pls->OutFile, "/D {lineto} def\n");
-    fprintf(pls->OutFile, "/S {stroke} def\n");
-    fprintf(pls->OutFile, "/Z {stroke newpath} def\n");
-    fprintf(pls->OutFile, "end\n\n");	/* end of dictionary definition */
+    fprintf(OF, "/M {moveto} def\n");
+    fprintf(OF, "/D {lineto} def\n");
+    fprintf(OF, "/S {stroke} def\n");
+    fprintf(OF, "/Z {stroke newpath} def\n");
+    fprintf(OF, "end\n\n");	/* end of dictionary definition */
 
     /* Set up the plots */
 
-    fprintf(pls->OutFile, "PSDict begin\n");
-    fprintf(pls->OutFile, "@start\n");
-    fprintf(pls->OutFile, "%d @copies\n", COPIES);
-    fprintf(pls->OutFile, "@line\n");
-    fprintf(pls->OutFile, "%d @hsize\n", YSIZE);
-    fprintf(pls->OutFile, "%d @vsize\n", XSIZE);
-    fprintf(pls->OutFile, "%d @hoffset\n", YOFFSET);
-    fprintf(pls->OutFile, "%d @voffset\n", XOFFSET);
-    fprintf(pls->OutFile, "%d @hscale\n", YSCALE);
-    fprintf(pls->OutFile, "%d @vscale\n", XSCALE);
-    fprintf(pls->OutFile, "%d @lscale\n", LINESCALE);
-    fprintf(pls->OutFile, "@SetPlot\n\n");
-
-    fprintf(pls->OutFile, "bop\n");
+    fprintf(OF, "PSDict begin\n");
+    fprintf(OF, "@start\n");
+    fprintf(OF, "%d @copies\n", COPIES);
+    fprintf(OF, "@line\n");
+    fprintf(OF, "%d @hsize\n", YSIZE);
+    fprintf(OF, "%d @vsize\n", XSIZE);
+    fprintf(OF, "%d @hoffset\n", YOFFSET);
+    fprintf(OF, "%d @voffset\n", XOFFSET);
+    fprintf(OF, "%d @hscale\n", YSCALE);
+    fprintf(OF, "%d @vscale\n", XSCALE);
+    fprintf(OF, "%d @lscale\n", LINESCALE);
+    fprintf(OF, "@SetPlot\n\n");
 }
 
 /*----------------------------------------------------------------------*\
@@ -267,11 +274,11 @@ ps_line(PLStream *pls, PLSHORT x1a, PLSHORT y1a, PLSHORT x2a, PLSHORT y2a)
     int x1 = x1a, y1 = y1a, x2 = x2a, y2 = y2a;
 
     if (pls->linepos + 21 > LINELENGTH) {
-	putc('\n', pls->OutFile);
+	putc('\n', OF);
 	pls->linepos = 0;
     }
     else
-	putc(' ', pls->OutFile);
+	putc(' ', OF);
 
     pls->bytecnt++;
 
@@ -301,7 +308,7 @@ ps_line(PLStream *pls, PLSHORT x1a, PLSHORT y1a, PLSHORT x2a, PLSHORT y2a)
     urx = MAX(urx, x2);
     ury = MAX(ury, y2);
 
-    fprintf(pls->OutFile, "%s", outbuf);
+    fprintf(OF, "%s", outbuf);
     pls->bytecnt += strlen(outbuf);
     dev->xold = x2;
     dev->yold = y2;
@@ -332,14 +339,13 @@ ps_polyline(PLStream *pls, PLSHORT *xa, PLSHORT *ya, PLINT npts)
 void
 ps_clear(PLStream *pls)
 {
-    fprintf(pls->OutFile, " S\neop\nbop\n");
+    fprintf(OF, " S\neop\n");
 }
 
 /*----------------------------------------------------------------------*\
 * ps_page()
 *
 * Set up for the next page.
-* Advance to next family file if necessary (file output).
 \*----------------------------------------------------------------------*/
 
 void
@@ -348,25 +354,10 @@ ps_page(PLStream *pls)
     dev->xold = UNDEFINED;
     dev->yold = UNDEFINED;
 
-    if (!pls->termin)
-	plGetFam(pls);
-
     pls->page++;
-    fprintf(pls->OutFile, "%%%%Page: %d %d\n", pls->page, pls->page);
+    fprintf(OF, " bop\n");
+    fprintf(OF, "%%%%Page: %d %d\n", pls->page, pls->page);
     pls->linepos = 0;
-}
-
-/*----------------------------------------------------------------------*\
-* ps_adv()
-*
-* Advance to the next page.
-\*----------------------------------------------------------------------*/
-
-void
-ps_adv(PLStream *pls)
-{
-    ps_clear(pls);
-    ps_page(pls);
 }
 
 /*----------------------------------------------------------------------*\
@@ -378,9 +369,8 @@ ps_adv(PLStream *pls)
 void
 ps_tidy(PLStream *pls)
 {
-    fprintf(pls->OutFile, " S\neop\n");
-    fprintf(pls->OutFile, "@end\n\n");
-    fprintf(pls->OutFile, "%%%%Trailer\n");
+    fprintf(OF, "@end\n\n");
+    fprintf(OF, "%%%%Trailer\n");
 
     llx /= ENLARGE;
     lly /= ENLARGE;
@@ -390,14 +380,19 @@ ps_tidy(PLStream *pls)
     lly += YOFFSET;
     urx += XOFFSET;
     ury += YOFFSET;
-    fprintf(pls->OutFile, "%%%%BoundingBox: %d %d %d %d\n", llx, lly, urx, ury);
-    fprintf(pls->OutFile, "%%%%Pages: %d\n", pls->page);
+    fprintf(OF, "%%%%Pages: %d\n", pls->page);
 
-    fclose(pls->OutFile);
+/* Backtrack to write the BoundingBox at the beginning */
+/* Some applications don't like it atend */
+
+    rewind(OF);
+    fprintf(OF, "%%!PS-Adobe-2.0 EPSF-2.0\n");
+    fprintf(OF, "%%%%BoundingBox: %d %d %d %d\n", llx, lly, urx, ury);
+    fclose(OF);
     pls->fileset = 0;
     pls->page = 0;
     pls->linepos = 0;
-    pls->OutFile = NULL;
+    OF = NULL;
 }
 
 /*----------------------------------------------------------------------*\
@@ -412,11 +407,11 @@ ps_color(PLStream *pls)
     float r, g, b;
 
     if (pls->color) {
-	r = pls->curcolor.r / 255.0;
-	g = pls->curcolor.g / 255.0;
-	b = pls->curcolor.b / 255.0;
+	r = ((float) pls->curcolor.r) / 255.0;
+	g = ((float) pls->curcolor.g) / 255.0;
+	b = ((float) pls->curcolor.b) / 255.0;
 
-	fprintf(pls->OutFile, " S %f %f %f setrgbcolor\n", r, g, b);
+	fprintf(OF, " S %f %f %f setrgbcolor\n", r, g, b);
     }
 }
 
@@ -454,7 +449,7 @@ ps_width(PLStream *pls)
     if (pls->width < 1 || pls->width > 10)
 	fprintf(stderr, "\nInvalid pen width selection.");
     else {
-	fprintf(pls->OutFile, " S\n/lw %d def\n@lwidth\n", pls->width);
+	fprintf(OF, " S\n/lw %d def\n@lwidth\n", pls->width);
     }
 }
 
