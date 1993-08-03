6ae56cb24e142f05e32df80bc821464ecec84039
Maurice LeBrun <mjl@users.sourceforge.net>
1993-08-03 01:50:15 +0000

    Minor changes to text.
    svn path=/trunk/; revision=348

--- bindings/tk/help_gui.tcl
+++ bindings/tk/help_gui.tcl
@@ -18,20 +18,20 @@ proc help_gui {{w .help_gui}} {
     normal_text_setup $w 
 
     insertWithTags $w.t {\
-The PLPLOT/TK user interface is constructed from TK widgets, using Tcl as 
-a (dynamic) specification language.  The Tcl scripts provided with PLPLOT
+      The PLPLOT/TK user interface is constructed from TK widgets, using Tcl 
+as a (dynamic) specification language.  The Tcl scripts provided with PLPLOT
 control the layout of the user interface, initialization of the widgets, and
 some of the binding of actions to events.  Widgets, on the other hand, are
 all coded in C for speed.  
 
-The fundamental addition to PLPLOT to allow it to use Tcl/TK effectively
+      The fundamental addition to PLPLOT to allow it to use Tcl/TK effectively
 has been the creation of a PLPLOT widget, called a "plframe".  It was
 originally based on the TK frame widget but has many enhancements, as well
 as close ties to the underlying graphics library.  The plframe widget can
 be created, mapped, and destroyed basically just like a TK frame widget.
 In addition, it supports numerous widget commands.
 
-The default PLPLOT/TK interface looks like a window with a top-level menu
+      The default PLPLOT/TK interface looks like a window with a top-level menu
 bar, containing any number of frames devoted to plotting.  These child
 windows contain a bar with an end-of-plot indicator, a "Plot" menu, a forward
 page button (also a back page button when plrender is being used), and a
@@ -41,7 +41,7 @@ of the plframe when in zoom mode.  The actual plframe has no decoration, but
 interacts with all of the surrounding widgets.  The plframe and surrounding
 widgets should be considered as one entity.
 
-There are several ways in which the plframe widget can be utilized, but
+      There are several ways in which the plframe widget can be utilized, but
 the most usual is via selection of the TK driver when running an
 application linked to PLPLOT.  The structure of the code then can be
 illustrated as follows:
@@ -61,21 +61,21 @@ illustrated as follows:
 					     child X window
 
 
-The user-code and the renderer (called } normal
+      The user-code and the renderer (called } normal
     insertWithTags $w.t {plserver} bold
     insertWithTags $w.t {) form a client/server
 relationship, and are separate processes.  Under typical usage, where the
 user-code starts first, it forks (vfork on systems that support it) and execs
 the renderer.  Tcl/TK communication (via X properties) is used between their
-embedded interpreters to send commands back and forth (note: an extended
-version of Tcl called Tcl-DP allows sockets to be used for this, and the
-model will eventually be extended to allow distribution across a network).
-The existence of an interpreter in each process (actually each plplot stream
-that uses the TK driver) allows for a very easy and robust method of sending
-commands between processes.  The data goes through via an alternate channel
-(fifo for now, maybe socket later) for maximum speed.
+embedded interpreters to send commands back and forth.  An extended version
+of Tcl called Tcl-DP allows sockets to be used for this, and the model will
+eventually be extended to allow the user code and the renderer to be running
+on different machines.  The existence of an interpreter in each process
+(actually each plplot stream that uses the TK driver) allows for a very easy
+and robust method of sending commands between processes.  The data goes
+through via an alternate channel for maximum speed.
 
-Also supported (but as of yet untested) are some alternate configurations:
+      Also supported are some alternate configurations:
 
 (a) the server creates widgets, then launches applications passing the
       name of the plframe widget to plot to.  You could launch multiple
@@ -84,16 +84,15 @@ Also supported (but as of yet untested) are some alternate configurations:
 (b) the server creates widgets, then calls functions to do the actual
       plotting.  This is in fact the easiest configuration since all the
       work being done in one process, but has the disadvantage that the
-      user interface goes "dead" while the calculation is proceeding (not a
-      problem in some cases).
+      user interface goes "dead" while the calculation is proceeding.
 
-By offloading the GUI-building code into a simple script language, it becomes
+      By offloading the GUI-building code into a simple script language, it becomes
 easy to build menus, buttons, scrollbars, etc.  All of these are configurable
 by the user, as well as the ability to map events (such as button presses) to
-functions calls in your own program.  (Note: these capabilities have not been
-fully implemented and tested yet.)
+functions calls in your own program. Note: the alternate configurations as
+well as the customization options are not yet fully tested.
 
-Most of the currently supported operations of the PLPLOT/TK user interface
+      Most of the currently supported operations of the PLPLOT/TK user interface
 are self-explanatory, and can be understood with a bit of experimentation.
 Better documentation will eventually follow.  Some points to remember: 
 
