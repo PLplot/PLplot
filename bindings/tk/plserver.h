90ee5cf7a4fc99f55e8e7f2bf7410ca82f964e1f
Maurice LeBrun <mjl@users.sourceforge.net>
1993-08-03 01:48:08 +0000

    Eliminated dependence on internal Tcl/TK header files.
    svn path=/trunk/; revision=346

--- bindings/tk/plserver.h
+++ bindings/tk/plserver.h
@@ -1,6 +1,9 @@
 /* $Id$
  * $Log$
- * Revision 1.2  1993/07/16 22:01:29  mjl
+ * Revision 1.3  1993/08/03 01:48:08  mjl
+ * Eliminated dependence on internal Tcl/TK header files.
+ *
+ * Revision 1.2  1993/07/16  22:01:29  mjl
  * Eliminated obsolete variables from renderer state struct.
  *
  * Revision 1.1  1993/07/02  06:58:32  mjl
@@ -16,15 +19,36 @@
  * Declarations for plserver and associated files.  
  */
 
+/* This is always included first */
+
 #include "plplotP.h"
+
+/* System headers */
+
 #include <stdio.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <math.h>
+#include <pwd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/file.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include "plstream.h"
+#include <sys/time.h>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
+#include <X11/Xatom.h>
+#include <X11/Xproto.h>
+#include <X11/Xresource.h>
+#include <X11/Xutil.h>
 
 #include <tk.h>
+
+#include "plstream.h"
 
 /* Macro settings */
 
