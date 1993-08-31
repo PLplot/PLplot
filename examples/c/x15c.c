e8b9f1a90a0c150e7bbb44c9b756fa55aa4f76d8
Maurice LeBrun <mjl@users.sourceforge.net>
1993-08-31 17:57:11 +0000

    Actually works now.  Makes three plots, shading a single or multiple regions.
    svn path=/trunk/; revision=441

--- examples/c/x15c.c
+++ examples/c/x15c.c
@@ -1,11 +1,21 @@
-/* Test of plshade function */
-
 /* $Id$
  * $Log$
- * Revision 1.1  1993/08/18 19:06:44  mjl
+ * Revision 1.2  1993/08/31 17:57:11  mjl
+ * Actually works now.  Makes three plots, shading a single or multiple
+ * regions.
+ *
+ * Revision 1.1  1993/08/18  19:06:44  mjl
  * This file for testing plshade capability.  Just sketched out at this
  * point.
- *
+*/
+
+/*	x15c.c
+
+	plshade test functions.
+
+	Maurice LeBrun
+	IFS, University of Texas at Austin
+	31 Aug 1993
 */
 
 #define PL_NEED_MALLOC
@@ -13,38 +23,43 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define XPTS    35
-#define YPTS    46
-#define XSPA    2./(XPTS-1)
-#define YSPA    2./(YPTS-1)
+/* Utility macros */
+
 #ifndef PI
 #define PI	3.1415926535897932384
 #endif
+#ifndef MAX
+#define MAX(a,b)    (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
+#define MIN(a,b)    (((a) < (b)) ? (a) : (b))
+#endif
 
-PLFLT tr[6] =
-{XSPA, 0.0, -1.0, 0.0, YSPA, -1.0};
+/* Function prototypes */
 
-void
-mypltr(PLFLT x, PLFLT y, PLFLT *tx, PLFLT *ty, void *pltr_data)
-{
-    *tx = tr[0] * x + tr[1] * y + tr[2];
-    *ty = tr[3] * x + tr[4] * y + tr[5];
-}
+void plot1	(void);
+void plot2	(void);
+void plot3	(void);
+void f2mnmx	(PLFLT *, PLINT, PLINT, PLFLT *, PLFLT *);
 
-static PLFLT clevel[11] =
-{-1., -.8, -.6, -.4, -.2, 0, .2, .4, .6, .8, 1.};
+/* Data to plot */
+
+#define XPTS    35
+#define YPTS    46
+
+PLFLT z[XPTS][YPTS], zmin, zmax;
+
+/*----------------------------------------------------------------------*\
+* main
+*
+* Does a variety of shade plots.
+\*----------------------------------------------------------------------*/
 
 int
 main(int argc, char *argv[])
 {
     int i, j;
-    PLFLT xx, yy, argx, argy, distort;
-    static PLINT mark = 1500, space = 1500;
-
-    PLFLT z[XPTS][YPTS], w[XPTS][YPTS];
-    PLFLT xg1[XPTS], yg1[YPTS];
-    PLcGrid  cgrid1;
-    PLcGrid2 cgrid2;
+    PLFLT xx, yy;
 
 /* Parse and process command line arguments */
 
@@ -54,78 +69,153 @@ main(int argc, char *argv[])
 
     plinit();
 
-/* Set up function arrays */
+/* Set up data array */
 
     for (i = 0; i < XPTS; i++) {
 	xx = (double) (i - (XPTS / 2)) / (double) (XPTS / 2);
 	for (j = 0; j < YPTS; j++) {
 	    yy = (double) (j - (YPTS / 2)) / (double) (YPTS / 2) - 1.0;
-	    z[i][j] = xx * xx - yy * yy;
-	    w[i][j] = 2 * xx * yy;
+
+	    z[i][j] = xx*xx - yy*yy + (xx - yy)/(xx*xx+yy*yy + 0.1);
 	}
     }
+    f2mnmx(&z[0][0], XPTS, YPTS, &zmin, &zmax);
 
-/* Set up grids */
+    plot1();
+    plot2();
+    plot3();
 
-    cgrid1.xg = xg1;
-    cgrid1.yg = yg1;
-    cgrid1.nx = XPTS;
-    cgrid1.ny = YPTS;
+    plend();
+    exit(0);
+}
 
-    Alloc2dGrid(&cgrid2.xg, XPTS, YPTS);
-    Alloc2dGrid(&cgrid2.yg, XPTS, YPTS);
-    cgrid2.nx = XPTS;
-    cgrid2.ny = YPTS;
+/*----------------------------------------------------------------------*\
+* plot1
+*
+* Illustrates a single shaded region.
+\*----------------------------------------------------------------------*/
 
-    for (i = 0; i < XPTS; i++) {
-	for (j = 0; j < YPTS; j++) {
-	    mypltr((PLFLT) i, (PLFLT) j, &xx, &yy, NULL);
+void 
+plot1(void)
+{
+    PLFLT shade_min, shade_max;
+    PLINT sh_color, sh_width;
+    PLINT min_color = 0, min_width = 0, max_color = 0, max_width = 0;
 
-	    argx = xx * PI/2;
-	    argy = yy * PI/2;
-	    distort = 0.4;
+    plcol(1);
+    plenv(-1.0, 1.0, -1.0, 1.0, 0, 0);
 
-	    cgrid1.xg[i] = xx + distort * cos(argx);
-	    cgrid1.yg[j] = yy - distort * cos(argy);
-
-	    cgrid2.xg[i][j] = xx + distort * cos(argx) * cos(argy);
-	    cgrid2.yg[i][j] = yy - distort * cos(argx) * cos(argy);
-	}
-    }
+    shade_min = zmin + (zmax-zmin)*0.4;
+    shade_max = zmin + (zmax-zmin)*0.6;
+    sh_color = 7;
+    sh_width = 2;
+    min_color = 9;
+    max_color = 2;
+    min_width = 2;
+    max_width = 2;
 
 /* Plot using identity transform */
 
-    plenv((PLFLT) -1.0, (PLFLT) 1.0, (PLFLT) -1.0, (PLFLT) 1.0, 0, 0);
-    plcol(2);
-{
-    PLFLT shade_min, shade_max;	/* Defines the interval to be shaded. If */
-				/* shade_max <= shade_min, plshade does */
-				/* nothing. */
-
-    PLINT sh_color, sh_width;	/* defines pen color, width used by the */
-				/* fill pattern */
-    PLINT min_color, min_width;
-    PLINT max_color, max_width;	/* defines pen color, width used by the */
-				/* boundary of shaded region. The min */
-				/* values are used for the shade_min */
-				/* boundary, and the max values are used */
-				/* on the shade_max boundary. Set color */
-				/* and width to zero for no plotted */
-				/* boundaries. */
-
-    shade_min = -0.2;
-    shade_max = 0.2;
-    min_color = 1;
-    max_color = 8;
-    min_width = 0;
-    max_width = 0;
-
-    plshade(&z, XPTS, YPTS, NULL, -1., 1., -1., 1., 
+    plpsty(8);
+    plshade(&z[0][0], XPTS, YPTS, NULL, -1., 1., -1., 1., 
 	    NULL, shade_min, shade_max, 
 	    sh_color, sh_width, min_color, min_width,
 	    max_color, max_width, plfill, 1);
 
+    plcol(2);
+    pllab("distance", "altitude", "Bogon flux");
 }
+
+/*----------------------------------------------------------------------*\
+* plot2
+*
+* Illustrates multiple adjacent shaded regions.
+\*----------------------------------------------------------------------*/
+
+void 
+plot2(void)
+{
+    PLFLT shade_min, shade_max;
+    PLINT sh_color, sh_width;
+    PLINT min_color = 0, min_width = 0, max_color = 0, max_width = 0;
+    int i;
+
     plcol(1);
-    pllab("X Coordinate", "Y Coordinate", "Streamlines of flow");
+    plenv(-1.0, 1.0, -1.0, 1.0, 0, 0);
+
+/* Plot using identity transform */
+    
+    for (i = 0; i < 10; i++) {
+	shade_min = zmin + (zmax - zmin) * i / 10.0;
+	shade_max = zmin + (zmax - zmin) * (i +1) / 10.0;
+	sh_color = i+6;
+	sh_width = 2;
+	plpsty(8);
+
+	plshade(&z[0][0], XPTS, YPTS, NULL, -1., 1., -1., 1., 
+		NULL, shade_min, shade_max, 
+		sh_color, sh_width, min_color, min_width,
+		max_color, max_width, plfill, 1);
+    }
+    plcol(2);
+    pllab("distance", "altitude", "Bogon flux");
+}
+
+/*----------------------------------------------------------------------*\
+* plot3
+*
+* Same as plot2, but uses different fill patterns for each region.
+\*----------------------------------------------------------------------*/
+
+void 
+plot3(void)
+{
+    PLFLT shade_min, shade_max;
+    PLINT sh_color, sh_width;
+    PLINT min_color = 0, min_width = 0, max_color = 0, max_width = 0;
+    int i;
+
+    plcol(1);
+    plenv(-1.0, 1.0, -1.0, 1.0, 0, 0);
+
+/* Plot using identity transform */
+    
+    for (i = 0; i < 10; i++) {
+	shade_min = zmin + (zmax - zmin) * i / 10.0;
+	shade_max = zmin + (zmax - zmin) * (i +1) / 10.0;
+	sh_color = i+6;
+	sh_width = 2;
+	plpsty((i + 2) % 8 + 1);
+
+	plshade(&z[0][0], XPTS, YPTS, NULL, -1., 1., -1., 1., 
+		NULL, shade_min, shade_max, 
+		sh_color, sh_width, min_color, min_width,
+		max_color, max_width, plfill, 1);
+    }
+    plcol(2);
+    pllab("distance", "altitude", "Bogon flux");
+}
+
+/*----------------------------------------------------------------------*\
+* f2mnmx
+*
+* Returns min & max of input 2d array.
+\*----------------------------------------------------------------------*/
+
+#define F(a,b) (f[a*ny+b])
+
+void
+f2mnmx(PLFLT *f, PLINT nx, PLINT ny, PLFLT *fmin, PLFLT *fmax)
+{
+    int i, j;
+
+    *fmax = F(0,0);
+    *fmin = *fmax;
+
+    for (i = 0; i < nx; i++) {
+	for (j = 0; j < ny; j++) {
+            *fmax = MAX(*fmax, F(i, j));
+            *fmin = MIN(*fmin, F(i, j));
+	}
+    }
 }
