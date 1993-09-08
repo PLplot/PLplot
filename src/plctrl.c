3b1b47f20d2fd70f19525875983cdf3e785b1bcf
Maurice LeBrun <mjl@users.sourceforge.net>
1993-09-08 02:37:48 +0000

    Added plGetName for building up file name specs out of path components. Also moved some utility functions from plstream.c here.
    svn path=/trunk/; revision=461

--- src/plctrl.c
+++ src/plctrl.c
@@ -1,8 +1,12 @@
 /* $Id$
    $Log$
-   Revision 1.4  1993/07/31 08:18:10  mjl
-   Changes to text/graph screen switching commands to reflect reorganization.
+   Revision 1.5  1993/09/08 02:37:48  mjl
+   Added plGetName for building up file name specs out of path components.
+   Also moved some utility functions from plstream.c here.
 
+ * Revision 1.4  1993/07/31  08:18:10  mjl
+ * Changes to text/graph screen switching commands to reflect reorganization.
+ *
  * Revision 1.3  1993/07/01  22:20:19  mjl
  * Changed all plplot source files to include plplotP.h (private) rather than
  * plplot.h.  Rationalized namespace -- all externally-visible internal
@@ -24,13 +28,16 @@
 /*	plctrl.c
 
 	Misc. control routines, like begin, end, exit, change graphics/text
-	mode, change color.
+	mode, change color.  If you don't know where it should go, put
+	it here.
 */
 
 #include "plplotP.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+
+static void	strcat_delim	(char *);
 
 /*----------------------------------------------------------------------*\
 * void plexit()
@@ -183,4 +190,121 @@ plHLS_RGB(PLFLT h, PLFLT l, PLFLT s, PLFLT *p_r, PLFLT *p_g, PLFLT *p_b)
 void pl_cmd(PLINT op, void *ptr)
 {
     plP_esc(op, ptr);
+}
+
+/*----------------------------------------------------------------------*\
+* void plGetName()
+*
+* Gets search name for file by concatenating the dir, subdir, and file
+* name, allocating memory as needed.  The appropriate delimiter is added
+* after the dir specification as necessary.  The caller is responsible
+* for freeing the malloc'ed memory.
+\*----------------------------------------------------------------------*/
+
+void
+plGetName(char *dir, char *subdir, char *filename, char **filespec)
+{
+    int lfilespec;
+
+/* Malloc space for filespec */
+
+    free_mem(*filespec);
+    lfilespec = 10;
+    lfilespec = strlen(dir) + strlen(subdir) + strlen(filename) + 10;
+    *filespec = (char *) malloc(lfilespec);
+
+    strcpy(*filespec, dir);
+
+    if (*subdir != '\0') {
+	strcat_delim(*filespec);
+	strcat(*filespec, subdir);
+    }
+    if (*filename != '\0') {
+	strcat_delim(*filespec);
+	strcat(*filespec, filename);
+    }
+}
+
+/*----------------------------------------------------------------------*\
+* void strcat_delim()
+*
+* Append path name deliminator if necessary (does not add one if one's
+* there already, or if dealing with a colon-terminated device name as
+* used on the Amiga).
+\*----------------------------------------------------------------------*/
+
+static void
+strcat_delim(char *dirspec)
+{
+    int ldirspec = strlen(dirspec);
+#ifdef MSDOS
+    if (dirspec[ldirspec-1] != '\\')
+	strcat(dirspec, "\\");
+#endif
+#ifdef __unix
+    if (dirspec[ldirspec-1] != '/')
+	strcat(dirspec, "/");
+#endif
+#ifdef AMIGA
+    if (dirspec[ldirspec-1] != '/' && dirspec[ldirspec-1] != ':')
+	strcat(dirspec, "/");
+#endif
+}
+
+/*----------------------------------------------------------------------*\
+* plGetInt()
+*
+* Prompts human to input an integer in response to given message.
+\*----------------------------------------------------------------------*/
+
+PLINT
+plGetInt(char *s)
+{
+    PLINT m;
+    int i = 0;
+    char line[256];
+
+    while (i++ < 10) {
+	printf(s);
+	fgets(line, sizeof(line), stdin);
+#ifdef MSDOS
+	m = atoi(line);
+	return (m);
+#else
+	if (sscanf(line, "%d", &m) == 1)
+	    return (m);
+	printf("No value or value out of range; please try again\n");
+#endif
+    }
+    plexit("Too many tries.");
+    return (0);
+}
+
+/*----------------------------------------------------------------------*\
+* plGetFlt()
+*
+* Prompts human to input a float in response to given message.
+\*----------------------------------------------------------------------*/
+
+PLFLT
+plGetFlt(char *s)
+{
+    PLFLT m;
+    int i = 0;
+    char line[256];
+
+    while (i++ < 10) {
+	printf(s);
+	fgets(line, sizeof(line), stdin);
+#ifdef MSDOS
+	m = atof(line);
+	return (m);
+#else
+	if (sscanf(line, "%f", &m) == 1)
+	    return (m);
+	printf("No value or value out of range; please try again\n");
+#endif
+    }
+    plexit("Too many tries.");
+    return (0.);
 }
