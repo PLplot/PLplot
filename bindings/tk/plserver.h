6d8bff5d55eb5d60040bdc7bbd9553886ea06bcf
Maurice LeBrun <mjl@users.sourceforge.net>
1993-12-09 20:33:41 +0000

    Eliminated unneccessary system header file inclusions.
    svn path=/trunk/; revision=611

--- bindings/tk/plserver.h
+++ bindings/tk/plserver.h
@@ -1,26 +1,13 @@
 /* $Id$
  * $Log$
- * Revision 1.7  1993/12/08 06:18:08  mjl
+ * Revision 1.8  1993/12/09 20:33:41  mjl
+ * Eliminated unneccessary system header file inclusions.
+ *
+ * Revision 1.7  1993/12/08  06:18:08  mjl
  * Changed to include new plplotX.h header file.
  *
  * Revision 1.6  1993/11/19  07:31:20  mjl
  * Fixed the prototype for tk_toplevel().
- *
- * Revision 1.5  1993/09/08  02:32:02  mjl
- * Added include of <errno.h>.
- *
- * Revision 1.4  1993/08/18  19:04:08  mjl
- * Added include of file "plplotio.h".
- *
- * Revision 1.3  1993/08/03  01:48:08  mjl
- * Eliminated dependence on internal Tcl/TK header files.
- *
- * Revision 1.2  1993/07/16  22:01:29  mjl
- * Eliminated obsolete variables from renderer state struct.
- *
- * Revision 1.1  1993/07/02  06:58:32  mjl
- * The new TCL/TK driver!  Yes it's finally here!  YAAAAAAAAYYYYYYY!!!
- *
  */
 
 /* 
@@ -31,31 +18,21 @@
  * Declarations for plserver and associated files.  
  */
 
-/* This is always included first */
-
 #include "plplotP.h"
 #include "plplotX.h"
-
-/* System headers */
-
-#include <ctype.h>
-#include <fcntl.h>
-#include <math.h>
-#include <pwd.h>
-#include <sys/file.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <errno.h>
-
-#include <X11/Xatom.h>
-#include <X11/Xproto.h>
-#include <X11/Xresource.h>
-
-#include <tk.h>
-
 #include "plstream.h"
 #include "plplotio.h"
+
+#include <tcl.h>
+#include <tk.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
 
 /* Macro settings */
 
