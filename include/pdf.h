ade9e45a4c352979060089cda0bae3fdb399fb9a
Maurice LeBrun <mjl@users.sourceforge.net>
1994-01-15 17:33:16 +0000

    Added typedef of PDFstrm and wrapped function prototypes in a macro so the header can be read without problems by K&R compilers (cc).
    svn path=/trunk/; revision=649

--- include/pdf.h
+++ include/pdf.h
@@ -1,8 +1,12 @@
 /* $Id$
    $Log$
-   Revision 1.5  1993/07/02 07:21:41  mjl
-   All PDF routines now start with "pdf_".
+   Revision 1.6  1994/01/15 17:33:16  mjl
+   Added typedef of PDFstrm and wrapped function prototypes in a macro so
+   the header can be read without problems by K&R compilers (cc).
 
+ * Revision 1.5  1993/07/02  07:21:41  mjl
+ * All PDF routines now start with "pdf_".
+ *
  * Revision 1.4  1993/01/23  05:31:46  mjl
  * Added prototypes for functions that support polyline read/writes.
  *
@@ -15,11 +19,9 @@
  *
  * Revision 1.1  1992/05/20  21:33:40  furnish
  * Initial checkin of the whole PLPLOT project.
- *
 */
 
-/*--------------------------------------------------------------------------*\
-
+/*
     pdf.h
 
     Copyright (C) 1992 by Maurice J. LeBrun
@@ -34,11 +36,20 @@
     It is provided solely "as is". The author(s) disclaim(s) all
     responsibility and liability with respect to this software's usage or
     its effect upon hardware or computer systems. 
+*/
 
-\*--------------------------------------------------------------------------*/
+#ifndef _PDF_INCLUDED
+#define _PDF_INCLUDED
 
-#ifndef INCLUDED_PDF
-#define INCLUDED_PDF
+/* PDFstrm definition */
+/* The low level routines use the transfer method appropriate for the */
+/* first non-null type below */
+
+typedef struct {
+    FILE *file;				/* Filesystem */
+    unsigned char *buffer;		/* Memory buffer */
+    long bp, bufmax;			/* Buffer pointer and max size */
+} PDFstrm;
 
 /* Error numbers */
 
@@ -51,17 +62,29 @@
 #define PDF_WRERR		7	/* Write error		*/
 #define PDF_NOTPDF		8	/* Not a valid PDF file */
 
-int  pdf_rd_header	(FILE *file, char *header);
-int  pdf_wr_header	(FILE *file, char *header);
-int  pdf_wr_1byte	(FILE *file, U_CHAR s);
-int  pdf_rd_1byte	(FILE *file, U_CHAR *ps);
-int  pdf_wr_2bytes	(FILE *file, U_SHORT s);
-int  pdf_rd_2bytes 	(FILE *file, U_SHORT *ps);
-int  pdf_wr_2nbytes	(FILE *file, U_SHORT *s, PLINT n);
-int  pdf_rd_2nbytes 	(FILE *file, U_SHORT *s, PLINT n);
-int  pdf_wr_4bytes	(FILE *file, U_LONG s);
-int  pdf_rd_4bytes 	(FILE *file, U_LONG *ps);
-int  pdf_wr_ieeef	(FILE *file, float f);
-int  pdf_rd_ieeef	(FILE *file, float *pf);
+/* Prototypes */
+/* Use a wrapper for the prototypes for use from cc */
 
-#endif	/* INCLUDED_PDF */
+PDFstrm *pdf_fopen	PLARGS((char *filename, char *mode));
+PDFstrm *pdf_bopen	PLARGS((U_CHAR *buffer, long bufmax));
+PDFstrm *pdf_finit	PLARGS((FILE *file));
+int  pdf_close		PLARGS((PDFstrm *pdfs));
+
+int  pdf_putc		PLARGS((int c, PDFstrm *pdfs));
+int  pdf_getc		PLARGS((PDFstrm *pdfs));
+int  pdf_ungetc		PLARGS((int c, PDFstrm *pdfs));
+
+int  pdf_rd_header	PLARGS((PDFstrm *pdfs, char *header));
+int  pdf_wr_header	PLARGS((PDFstrm *pdfs, char *header));
+int  pdf_wr_1byte	PLARGS((PDFstrm *pdfs, U_CHAR s));
+int  pdf_rd_1byte	PLARGS((PDFstrm *pdfs, U_CHAR *ps));
+int  pdf_wr_2bytes	PLARGS((PDFstrm *pdfs, U_SHORT s));
+int  pdf_rd_2bytes 	PLARGS((PDFstrm *pdfs, U_SHORT *ps));
+int  pdf_wr_2nbytes	PLARGS((PDFstrm *pdfs, U_SHORT *s, PLINT n));
+int  pdf_rd_2nbytes 	PLARGS((PDFstrm *pdfs, U_SHORT *s, PLINT n));
+int  pdf_wr_4bytes	PLARGS((PDFstrm *pdfs, U_LONG s));
+int  pdf_rd_4bytes 	PLARGS((PDFstrm *pdfs, U_LONG *ps));
+int  pdf_wr_ieeef	PLARGS((PDFstrm *pdfs, float f));
+int  pdf_rd_ieeef	PLARGS((PDFstrm *pdfs, float *pf));
+
+#endif	/* _PDF_INCLUDED */
