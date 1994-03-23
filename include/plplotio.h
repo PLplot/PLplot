576416b5972afd18bf8a9e371694e881b6d2eda5
Maurice LeBrun <mjl@users.sourceforge.net>
1994-03-23 07:06:57 +0000

    Guts torn out.  Will be deleted at any time.
    svn path=/trunk/; revision=723

--- include/plplotio.h
+++ include/plplotio.h
@@ -1,31 +1,21 @@
 /* $Id$
    $Log$
-   Revision 1.1  1993/02/23 04:55:47  mjl
-   Added to handle any data structure definitions or function prototypes that
-   need access to stdio.h, so that plplot.h could remain free of this.
+   Revision 1.2  1994/03/23 07:06:57  mjl
+   Guts torn out.  Will be deleted at any time.
 
+ * Revision 1.1  1993/02/23  04:55:47  mjl
+ * Added to handle any data structure definitions or function prototypes that
+ * need access to stdio.h, so that plplot.h could remain free of this.
+ *
 */
 
 /*
     plplotio.h
 
-    Header file for all stdio-dependent data structures and prototypes.
-    Should be included AFTER plplot.h and stdio.h.
+    Obsolete.  May be deleted at any time, so eliminate from your code.
 */
 
 #ifndef __PLPLOTIO_H__
 #define __PLPLOTIO_H__
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void  plgfile	(FILE *);
-
-void  plsfile	(FILE *);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif	/* __PLPLOTIO_H__ */
