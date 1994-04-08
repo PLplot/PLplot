f07d785dada08e77b99db0fb951eccffd6800a71
Maurice LeBrun <mjl@users.sourceforge.net>
1994-04-08 11:47:17 +0000

    Now just a demo file, fully intended for customization by the user.
    svn path=/trunk/; revision=771

--- bindings/tk/plconfig.tcl
+++ bindings/tk/plconfig.tcl
@@ -2,105 +2,21 @@
 # plconfig.tcl
 #
 # Sets configuration options for plplot/TK driver.
-# Maurice LeBrun
-# IFS, University of Texas
-#
-# This is largely based on options.motif.tk from Ioi K Lam's Tix package.
-# The TK options are set to be fairly Motif-like.
-#
-# Aside: one way to be friendly to user preferences is to first check the
-# options database to see if an appropriate resource specification already
-# exists.  The problem with this is that if you have multiple plservers
-# running, you need to specify either (a) a resource for each one (for some
-# reason, under HP/UX at least, plserver*font is not matched if the main
-# window name is "plserver #2"), or (b) an unqualified resource i.e. *font.
-# Neither is a particularly attractive solution.
-#
-# The alternative is to just provide a way for the user to substitute
-# a different plconfig proc, and I have chosen this approach.  Just copy
-# this file, modify it to your tastes, and put it in plserver's search
-# path for TCL procs:
-#
-#	 user-specified directory(s) (set by -auto_load argument)
-#	 Current directory
-#	 $PL_LIBRARY 
-#	 $HOME/tcl
-#	 /usr/local/plplot/tcl 
+# This file contains a dummy proc.  You can customize plplot/TK by copying
+# settings from pldefaults.tcl into this file and modifying them to taste.  
+# See the notes in pldefaults.tcl for more info.
 #----------------------------------------------------------------------------
 
 proc plconfig {} {
-    global dialog_font
-    global dialog_bold_font
 
-# Font-related resources.
+# Uncomment the next line to enable my_key_filter to receive key info.
 
-    set gen_font	-*-helvetica-medium-r-normal-*-*-180-*-*-*-*-*-*
-    set gen_bold_font	-*-helvetica-bold-r-normal-*-*-180-*-*-*-*-*-*
-    set gen_menu_font	-*-helvetica-medium-o-normal-*-*-180-*-*-*-*-*-*
-    set gen_italic_font	-*-helvetica-bold-o-normal-*-*-180-*-*-*-*-*-*
+#    global user_key_filter;	set user_key_filter "my_key_filter"
+}
 
-    set dialog_font	 -*-times-medium-r-normal-*-*-180-*-*-*-*-*-*
-    set dialog_bold_font -*-times-bold-r-normal-*-*-180-*-*-*-*-*-*
+# Demo user key filter.
 
-    option add *font		$gen_font
-    option add *Entry.font	$gen_font
-    option add *Menu*font	$gen_menu_font
-    option add *Menubutton*font	$gen_menu_font
-    option add *Scale.font	$gen_italic_font
+proc my_key_filter {w client k n a} {
 
-#----------------------------------------------------------------------------
-# Color-related resources. 
-# Sets colors in a Motif-like way, but only if on a color system.
-
-    if {[tk colormode .] == "color"} {
-
-	set gen_bg		lightgray
-	set gen_fg		black
-	set gen_darker_bg	gray
-	set gen_darker_fg	black
-	set gen_active_bg	$gen_bg
-	set gen_active_fg	$gen_fg
-
-	option add *background			$gen_bg
-	option add *foreground			$gen_fg
-	option add *activeBackground      	$gen_active_bg
-	option add *activeForeground      	$gen_active_fg
-	option add *disabledForeground		gray45
-	option add *Checkbutton.selector	yellow
-	option add *Radiobutton.selector	yellow
-	option add *Entry.background		#c07070
-	option add *Entry.foreground		black
-	option add *Entry.insertBackground	black
-	option add *Listbox.background		$gen_darker_bg
-	option add *Scale.foreground		$gen_fg
-	option add *Scale.activeForeground	$gen_bg
-	option add *Scale.background		$gen_bg
-	option add *Scale.sliderForeground	$gen_bg
-	option add *Scale.sliderBackground	$gen_darker_bg
-	option add *Scrollbar.foreground	$gen_bg
-	option add *Scrollbar.activeForeground	$gen_bg
-	option add *Scrollbar.background	$gen_darker_bg
-
-# This specifies the default plplot widget background color.
-
-	option add *plwin.background		black
-
-    } else {
-
-	option add *plwin.background		white
-
-    }
-
-#----------------------------------------------------------------------------
-# Miscellaneous 
-
-    option add *anchor				w
-    option add *Button.borderWidth    		2
-    option add *Button.anchor			c
-    option add *Checkbutton.borderWidth    	2
-    option add *Radiobutton.borderWidth    	2
-    option add *Label.anchor			w
-    option add *Labelframe.borderWidth		2
-    option add *Entry.relief			sunken
-    option add *Scrollbar.relief		sunken
+    puts "keypress: $k $n $a"
 }
