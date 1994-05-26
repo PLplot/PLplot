0c343e92a889226b06a7662f294ddce5868fd584
Maurice LeBrun <mjl@users.sourceforge.net>
1994-05-26 19:24:47 +0000

    Inserted missing CVS Id and Log fields.
    svn path=/trunk/; revision=940

--- bindings/tk/help_keys.tcl
+++ bindings/tk/help_keys.tcl
@@ -1,3 +1,8 @@
+# $Id$
+# $Log$
+# Revision 1.4  1994/05/26 19:23:20  mjl
+# Inserted missing CVS Id and Log fields.
+#
 #----------------------------------------------------------------------------
 # help_keys w
 #
