4fc03e58c92f73a0ce6ab0cf45475dc8d95b9081
Maurice LeBrun <mjl@users.sourceforge.net>
1994-06-09 20:07:13 +0000

    Cleaned up and switched to plplot direct commands embedded in plframe widget (through "<widget> cmd <command> <args>" syntax).
    svn path=/trunk/; revision=952

--- bindings/tk/PLWin.tcl
+++ bindings/tk/PLWin.tcl
@@ -7,7 +7,11 @@
 # $Id$
 #
 # $Log$
-# Revision 1.2  1994/05/11 08:07:30  furnish
+# Revision 1.3  1994/06/09 20:07:13  mjl
+# Cleaned up and switched to plplot direct commands embedded in plframe
+# widget (through "<widget> cmd <command> <args>" syntax).
+#
+# Revision 1.2  1994/05/11  08:07:30  furnish
 # Debugging.  Needs to do own toprow for some reason.
 #
 # Revision 1.1  1994/05/09  17:59:19  furnish
@@ -18,57 +22,33 @@
 itcl_class PLWin {
 
     constructor {config} {
-	puts stdout "make a PLWin w/ name $name"
-
 	set class [$this info class]
 	::rename $this $this-tmp-
 	::frame $this -class $class
 	::rename $this $this-win-
 	::rename $this-tmp- $this
 
-#	frame $this
-
-#	puts "did all the renaming stuff"
-
 	plframe $this.plwin -relief sunken
-
-#	puts "made the plframe."
-
 	pack append $this $this.plwin {left expand fill}
-
-	puts "packed it in"
-
-#	plw_configure_TopRow $this
-
-	puts "configured toprow"
     }
 
     method configure {config} {}
 
-    method init {} {
-	$this.plwin cmd init
-    }
-
     method plcol {color} {
-	# Should set the plstream here.
-	::plcol $color
+	$this.plwin cmd plcol0 $color
     }
 
     method plenv {xmin xmax ymin ymax just axis} {
-	# Should set the plstream here.
-	::plenv $xmin $xmax $ymin $ymax $just $axis
+	$this.plwin cmd plenv $xmin $xmax $ymin $ymax $just $axis
     }
 
     method pllab {xlab ylab toplab} {
-	# Should set the plstream here.
-	::pllab $xlab $ylab $toplab
+	$this.plwin cmd pllab $xlab $ylab $toplab
     }
 
     method plline {pairs} {
-	# Should set the plstream here.
-	::plline $pairs
+	$this.plwin cmd plline $pairs
     }
 
-# Should have a stream ID here.
     public name
 }
