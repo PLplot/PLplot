524bcc9ad9bba3fbf2015e0bd12667b176b6c0c3
Maurice LeBrun <mjl@users.sourceforge.net>
1994-06-25 20:33:54 +0000

    Moved stuff corresponding to first demo into separate file.
    svn path=/trunk/; revision=1012

--- examples/tcl/demos.tcl
+++ examples/tcl/demos.tcl
@@ -1,6 +1,9 @@
 # $Id$
 # $Log$
-# Revision 1.1  1994/06/23 22:39:30  mjl
+# Revision 1.2  1994/06/25 20:33:54  mjl
+# Moved stuff corresponding to first demo into separate file.
+#
+# Revision 1.1  1994/06/23  22:39:30  mjl
 # New demo file for "pltcl" interpreter.  Needs fleshing out.
 #
 #----------------------------------------------------------------------------
@@ -15,24 +18,4 @@
 # second, and so on.
 #----------------------------------------------------------------------------
 
-proc 1 {} {
-    1a
-}
-
-proc 1a {} {
-    plcol 1
-    plenv 0 1 0 1 0 0
-    plcol 6
-    pllab "(x)" "(y)" "#frPLplot Example 1 - y=1-2x+2x#u2"
-
-    matrix x1 float 101
-    matrix y1 float 101
-
-    for {set i 0} {$i < 101} {incr i} {
-	set x [expr $i * .01]
-	$x1 $i = $x
-	$y1 $i = [expr 1 - 2 * $x + 2 * $x * $x]
-    }
-
-    plline 101 $x1 $y1
-}
+source x01.tcl
