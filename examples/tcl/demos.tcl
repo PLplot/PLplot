df13e340d5bc4c56b7c494ff08d07219c28d9320
Maurice LeBrun <mjl@users.sourceforge.net>
1994-06-30 18:47:34 +0000

    Now sources all the tcl demo files.
    svn path=/trunk/; revision=1037

--- examples/tcl/demos.tcl
+++ examples/tcl/demos.tcl
@@ -1,6 +1,9 @@
 # $Id$
 # $Log$
-# Revision 1.2  1994/06/25 20:33:54  mjl
+# Revision 1.3  1994/06/30 18:47:34  mjl
+# Now sources all the tcl demo files.
+#
+# Revision 1.2  1994/06/25  20:33:54  mjl
 # Moved stuff corresponding to first demo into separate file.
 #
 # Revision 1.1  1994/06/23  22:39:30  mjl
@@ -19,3 +22,18 @@
 #----------------------------------------------------------------------------
 
 source x01.tcl
+source x02.tcl
+source x03.tcl
+source x04.tcl
+source x05.tcl
+source x06.tcl
+source x07.tcl
+source x08.tcl
+source x09.tcl
+source x10.tcl
+source x11.tcl
+source x12.tcl
+source x13.tcl
+source x14.tcl
+source x15.tcl
+source x16.tcl
