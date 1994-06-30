6198df739f07323aa0e293ff989310edab6a3d6a
Maurice LeBrun <mjl@users.sourceforge.net>
1994-06-30 18:36:37 +0000

    Minor change to what header files are included.
    svn path=/trunk/; revision=1029

--- bindings/tk/plplotTK.h
+++ bindings/tk/plplotTK.h
@@ -1,6 +1,9 @@
 /* $Id$
  * $Log$
- * Revision 1.1  1994/04/08 12:21:18  mjl
+ * Revision 1.2  1994/06/30 18:36:37  mjl
+ * Minor change to what header files are included.
+ *
+ * Revision 1.1  1994/04/08  12:21:18  mjl
  * Added to allow access to TK driver internals by determined users.
  *
 */
@@ -18,9 +21,13 @@
 #include <plplotX.h>
 #include <pdf.h>
 #include <plstream.h>
+#include <pltcl.h>
 
-#include <tcl.h>
 #include <tk.h>
+
+#ifdef TCL_DP
+#include <dp.h>
+#endif
 
 /* One of these holds the TK driver state information */
 
