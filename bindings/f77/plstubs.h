b9f6bb06ea78732ca2de6903163cb3fb3cbfc64b
Maurice LeBrun <mjl@users.sourceforge.net>
1994-07-20 06:07:44 +0000

    Added definitions for Fortran interface routines to handle the new 3d functions plline3(), plpoin3(), and plpoly3().
    svn path=/trunk/; revision=1106

--- bindings/f77/plstubs.h
+++ bindings/f77/plstubs.h
@@ -1,17 +1,11 @@
 /* $Id$
  * $Log$
- * Revision 1.21  1994/03/23 07:11:49  mjl
+ * Revision 1.22  1994/07/20 06:07:44  mjl
+ * Added definitions for Fortran interface routines to handle the new 3d
+ * functions plline3(), plpoin3(), and plpoly3().
+ *
+ * Revision 1.21  1994/03/23  07:11:49  mjl
  * Added stub name definitions for cmap1 selectors and plshade front-ends.
- *
- * Revision 1.20  1993/12/08  06:21:19  mjl
- * Fix for dos386/djgpp; added definition for plspause (should now work from
- * fortran).
- *
- * Revision 1.19  1993/12/06  07:45:25  mjl
- * Modifications for new color model.
- *
- * Revision 1.18  1993/11/15  08:35:22  mjl
- * Name definitions added for new stubs -- PLEOP, PLBOP, PLSCOLBG.
 */
 
 /*
@@ -32,37 +26,37 @@
 #include "plplotP.h"
 
 /*----------------------------------------------------------------------*\
-* Select name transformation based on system type.
-*
-* Define the STUB_LINKAGE flag to get proper C<->Fortran linkage on your
-* system.  This flag describes what the compiler does to Fortran routine
-* names, which we must duplicate on the C stubs so that the Fortran
-* routines may call them.  You can often set STUB_LINKAGE by the
-* construct -DSTUB_LINKAGE=<value> on the C compiler command line, but
-* it is best to either rely on the default or explicitly handle your
-* system below.
-*
-* Current choices for STUB_LINKAGE:
-*
-* 	STUB_LAU	lower-case, append underscore
-* 	STUB_L		lower-case
-*	STUB_U		upper-case
-*	STUB_FORTRAN	use "fortran" keyword (MS-DOS convention)
-*
-* If no choice is made, the default is set to STUB_LAU.  This should
-* handle most generic Unix boxes not already accounted for.  
-*
-* ** Namespace collision **
-*
-* If you use the STUB_L option, the C & Fortran namespaces will collide
-* if the Fortran compiler does lower case folding (they usually do).
-* The problem is then that the stub names and actual function names will
-* be exactly the same, if we insist on the Fortran and C bindings to be
-* similar.  The solution is to give the externally callable C routines
-* unique names, and provide macros to turn the documented entry names in
-* to the real ones.  This is a horrible kludge, but the alternatives are
-* worse.  Fortunately it has no effect on the user program, and you can
-* forget that you ever read about it here.
+ * Select name transformation based on system type.
+ *
+ * Define the STUB_LINKAGE flag to get proper C<->Fortran linkage on your
+ * system.  This flag describes what the compiler does to Fortran routine
+ * names, which we must duplicate on the C stubs so that the Fortran
+ * routines may call them.  You can often set STUB_LINKAGE by the
+ * construct -DSTUB_LINKAGE=<value> on the C compiler command line, but
+ * it is best to either rely on the default or explicitly handle your
+ * system below.
+ *
+ * Current choices for STUB_LINKAGE:
+ *
+ * 	STUB_LAU	lower-case, append underscore
+ * 	STUB_L		lower-case
+ *	STUB_U		upper-case
+ *	STUB_FORTRAN	use "fortran" keyword (MS-DOS convention)
+ *
+ * If no choice is made, the default is set to STUB_LAU.  This should
+ * handle most generic Unix boxes not already accounted for.  
+ *
+ * ** Namespace collision **
+ *
+ * If you use the STUB_L option, the C & Fortran namespaces will collide
+ * if the Fortran compiler does lower case folding (they usually do).
+ * The problem is then that the stub names and actual function names will
+ * be exactly the same, if we insist on the Fortran and C bindings to be
+ * similar.  The solution is to give the externally callable C routines
+ * unique names, and provide macros to turn the documented entry names in
+ * to the real ones.  This is a horrible kludge, but the alternatives are
+ * worse.  Fortunately it has no effect on the user program, and you can
+ * forget that you ever read about it here.
 \*----------------------------------------------------------------------*/
 
 #define STUB_LAU	1
@@ -117,9 +111,9 @@
 #endif	/* ifndef STUB_LINKAGE */
 
 /*----------------------------------------------------------------------*\
-* Define name-translation macro.
-* To use, define with x the upper case routine name, y the lower case.
-* Should be adoptable to virtually any system.
+ * Define name-translation macro.
+ * To use, define with x the upper case routine name, y the lower case.
+ * Should be adoptable to virtually any system.
 \*----------------------------------------------------------------------*/
 
 #if STUB_LINKAGE==STUB_LAU
@@ -139,8 +133,8 @@
 #endif
 
 /*----------------------------------------------------------------------*\
-* Now to actually define the stub names.
-* Each stub must have an entry here.
+ * Now to actually define the stub names.
+ * Each stub must have an entry here.
 \*----------------------------------------------------------------------*/
 
 #define    PLADV	FNAME(PLADV,pladv)
@@ -185,12 +179,15 @@
 #define    PLJOIN	FNAME(PLJOIN,pljoin)
 #define    PLLAB7	FNAME(PLLAB7,pllab7)
 #define    PLLINE	FNAME(PLLINE,plline)
+#define    PLLINE3	FNAME(PLLINE3,plline3)
 #define    PLLSTY	FNAME(PLLSTY,pllsty)
 #define    PLMESH	FNAME(PLMESH,plmesh)
 #define    PLMTEX7	FNAME(PLMTEX7,plmtex7)
 #define    PLOT3D	FNAME(PLOT3D,plot3d)
 #define    PLPAT	FNAME(PLPAT,plpat)
 #define    PLPOIN	FNAME(PLPOIN,plpoin)
+#define    PLPOIN3	FNAME(PLPOIN3,plpoin3)
+#define    PLPOLY3	FNAME(PLPOLY3,plpoly3)
 #define    PLPREC	FNAME(PLPREC,plprec)
 #define    PLPSTY	FNAME(PLPSTY,plpsty)
 #define    PLPTEX7	FNAME(PLPTEX7,plptex7)
