c4d73a893de0109d5d272693cfd58837f4f503c3
Maurice LeBrun <mjl@users.sourceforge.net>
1994-08-26 19:25:40 +0000

    Now checks for the terminal type and provides some rudimentary decisions based on the TERM setting.  The xterm is unaffected, but with a terminal type of "tekterm", the 'Page >' prompt is printed on the graphics screen so that the plot can be seen without having pltek flash back to the text screen immediately.  Other minor cleaning up as well.  Contributed by Mark Olesen.
    svn path=/trunk/; revision=1293

--- utils/pltek.c
+++ utils/pltek.c
@@ -1,6 +1,14 @@
 /* $Id$
  * $Log$
- * Revision 1.6  1994/08/10 01:15:11  mjl
+ * Revision 1.7  1994/08/26 19:25:40  mjl
+ * Now checks for the terminal type and provides some rudimentary decisions
+ * based on the TERM setting.  The xterm is unaffected, but with a terminal
+ * type of "tekterm", the 'Page >' prompt is printed on the graphics screen
+ * so that the plot can be seen without having pltek flash back to the text
+ * screen immediately.  Other minor cleaning up as well.  Contributed by Mark
+ * Olesen.
+ *
+ * Revision 1.6  1994/08/10  01:15:11  mjl
  * Changed/improved page prompt, enabled both forward and backward relative
  * seeking, eliminated system dependent input code.
  *
@@ -19,51 +27,58 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
 
-static long start[1000];
-void describe();
+static void describe(void);
+static void tek_mode(int mode);
 
-/* Define graphics control characters. */
+#define ESC  27				/* ESCape character */
 
-#define FF   12
-#define CAN  24
-#define ESC  27
-#define GS   29
-#define US   31
-#define ETX  003
+#define ALPHA_MODE	0		/* switch to alpha mode */
+#define GRAPH_MODE	1 		/* switch to graph mode */
+#define BUFSZ	1024
+#define MAXPAGES 1024
+
+enum { unknown, xterm, tekterm } termtype = unknown;
+
 
 int
 main(int argc, char *argv[])
 {
     FILE *fd;
-    char infile[128];
     int i, j, nb, npage, ipage, nextpage, ifirst, oldpage;
     int istop;
-    char xtra;
-    char buf[1024], lastchar;
-    char ibuf[128];
-    char c;
+    long start[MAXPAGES];		/* start (offset) of each page */
+    char buf[BUFSZ], xtra, lastchar;
+    char c, ibuf[128], *t;
 
     if (argc < 2) {
 	describe();
 	exit(1);
     }
-    else
-	strcpy(infile, argv[1]);
-
-    if ((fd = fopen(infile, "r")) == NULL) {
+    if ((fd = fopen(argv[1], "r")) == NULL) {
 	printf("Cannot open %s\n", argv[1]);
 	exit(1);
+    }
+   
+    if ( (t = getenv("TERM")) != NULL ) {      
+	if ( strcmp("xterm", t) == 0 ) { 
+	    termtype = xterm;	 
+	} else if (!strncmp("tek",t,3) ||
+		   !strncmp("401",t,3) ||
+		   !strncmp("410",t,3) ) {
+	    termtype = tekterm;
+	}
     }
 
 /* Find out how many pages there are in file. */
 
     ipage = 0;
     start[0] = 0;
-    for (i = 0; i < 10000; i++) {
-	nb = fread(buf, 1, 1024, fd);
+    for (i = 0; i < MAXPAGES; i++) {
+	nb = fread(buf, 1, BUFSZ, fd);
 	if (nb <= 0)
 	    break;
 	ifirst = 0;
@@ -71,7 +86,7 @@ main(int argc, char *argv[])
 	    if ((lastchar = buf[j]) == '\f') {
 		ifirst = j - 1;
 		ipage++;
-		start[ipage] = 1024 * i + ifirst + 2;
+		start[ipage] = BUFSZ * i + ifirst + 2;
 	    }
 	}
     }
@@ -82,14 +97,13 @@ main(int argc, char *argv[])
 	ipage--;
 
     npage = ipage + 1;
-    printf("found %d pages\n", npage);
 
 /* Loop until the user quits */
 
     ipage = 0;
     while (1) {
 	oldpage = ipage;
-	printf("Page %d> ", ipage);
+	printf("Page %d/%d> ", ipage, npage);
 
 	gets(ibuf);
 	c = ibuf[0];
@@ -97,60 +111,48 @@ main(int argc, char *argv[])
 /* User input a page number or a return */
 /* A carriage return in response to the prompt proceeds to the next page. */
 
-	if (c == '\0')
+	if (c == '\0') {	 	/* CR = increment by one page */
 	    ipage++;
-
-	else if (c == '+') 
+	} else if (c == '+') {		/* +<n> = increment by <n> pages */
 	    ipage += atoi(ibuf+1);
-
-	else if (c == '-') 
+	} else if (c == '-') {		/* -<n> = decrement by <n> pages */
 	    ipage -= atoi(ibuf+1);
-
-	else if (isdigit(c)) 
+	} else if (isdigit(c)) {	/* <n> = goto page <n> */
 	    ipage = atoi(ibuf);
-
-/* User input a command or garbage */
-
-	else {
-	    if (c == 'h' || c == '?') {
-		describe();
-		continue;
-	    }
-	    else if (c == 'q')
-		break;
-	    else {
-		describe();
-		continue;
-	    }
+	} else if (c == '\f') {
+	    ;				/* FF = redraw the last plot */
+	} else if (c == 'q') {		/* q = quit */
+	    break;
+	} else {			/* h, ? or garbage = give help */
+	    tek_mode(ALPHA_MODE);
+	    describe();    
+	    continue;
 	}
 
 /* Bounds checking */
 
 	if (ipage > npage) {
-	    printf("  Last page = %d.\n", npage);
 	    ipage = npage;
 	    if (ipage == oldpage)
 		continue;
-	}
-	else if (ipage < 0) {
+	} else if (ipage < 0) {
 	    ipage = 1;
+	} else if (ipage == 0) {
+	    continue;			/* page 0 does not exist */
 	}
 
 /* Time to actually plot something. */
 
+	tek_mode(GRAPH_MODE);
 	istop = fseek(fd, start[ipage - 1], 0);
 	xtra = '\0';
 	istop = 0;
-	printf("%c[?38h", ESC);		/* open graphics window */
-	printf("%c", GS);		/* set to vector mode */
-	printf("%c%c", ESC, FF);	/* clear screen */
-
-	for (i = 0; i < 10000; i++) {
-	    if (xtra != '\0') {
+	for (i = 0; i < 8196; i++) {	/* less than 8MB per page! */
+	    if (xtra) {
 		fwrite(&xtra, 1, 1, stdout);
 		xtra = '\0';
 	    }
-	    nb = fread(buf, 1, 1024, fd);
+	    nb = fread(buf, 1, BUFSZ, fd);
 	    if (nb <= 0)
 		break;
 	    ifirst = 0;
@@ -162,20 +164,62 @@ main(int argc, char *argv[])
 		    break;
 		}
 	    }
-	    if (istop != 0)
+	    if (istop)
 		break;
 	    if (buf[nb] == ESC) {
-		j = j - 1;
 		xtra = ESC;
+		j--;
 	    }
 	    fwrite(&buf[ifirst], 1, j - ifirst, stdout);
 	}
-	printf("%c%c", US, CAN);
-	printf("%c%c", ESC, ETX);
-	fflush(stdout);
+	if ( termtype == xterm ) 
+	    tek_mode(ALPHA_MODE);
     }
+    tek_mode(ALPHA_MODE);		/* how to kill an xterm Tek window? */
     fclose(fd);
     exit(0);
+}
+
+/*----------------------------------------------------------------------*\
+ *  tek_mode()
+ *
+ *  switch to alpha/graph mode
+\*----------------------------------------------------------------------*/
+
+static int currentmode = ALPHA_MODE;
+static void 
+tek_mode (int mode)
+{
+    if ( mode == GRAPH_MODE ) {
+	if ( currentmode == ALPHA_MODE ) {
+	    switch(termtype) {
+	    case tekterm:
+		break;
+	    case xterm:
+		printf("\033[?38h");		/* open graphics window */
+		break;
+	    default:
+		printf("\033[?38h");		/* open graphics window */
+	    }
+	    printf("\035");			/* Enter Vector mode = GS */
+	}
+	printf("\033\f");			/* clear screen = ESC FF */
+	fflush(stdout);      
+	currentmode == mode;
+    } else if ( mode == ALPHA_MODE ) {
+	switch(termtype) {
+	case tekterm:
+	    printf("\037\030");		/* Enter Alpha mode = US CAN */
+	    break;
+	case xterm:
+	    printf("\033\003");		/* VT mode (xterm) = ESC ETX */
+	    break;
+	default:
+	    printf("\033\003");		/* VT mode (xterm) = ESC ETX */
+	}
+	fflush(stdout);      
+	currentmode == mode;
+    }
 }
 
 /*----------------------------------------------------------------------*\
@@ -186,21 +230,17 @@ main(int argc, char *argv[])
  *  since some compilers can't handle strings that long.
 \*----------------------------------------------------------------------*/
 
-void 
+static void 
 describe (void)
 {
     fputs("\
-\n\
 Usage: pltek filename \n\
 At the prompt, the following replies are recognized:\n\
-\n\
-    h	  Gives this help message.\n\
-    ?     As above.\n\
-    q	  Quits program.\n\
-   <n>	  Goes to the specified page number (surprise!).\n\
-   -<n>   Goes back <n> pages.\n\
-   +<n>   Goes forward <n> pages.\n\
- <Return> Goes to the next page (first page, if initial reply).\n\
-\n\
-", stdout);
+   h,?	  Give this help message.\n\
+    q	  Quit program.\n\
+   <n>	  Go to the specified page number.\n\
+   -<n>   Go back <n> pages.\n\
+   +<n>   Go forward <n> pages.\n\
+ <Return> Go to the next page.\n\n",
+	  stdout);
 }
