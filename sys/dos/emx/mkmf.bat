e92a851370687a988b57935102dafffa5727e8a7
Maurice LeBrun <mjl@users.sourceforge.net>
1994-09-16 03:55:12 +0000

    Files for DOS/EMX driver, sent by Mark Olesen.
    svn path=/trunk/; revision=1327

--- sys/dos/emx/mkmf.bat
+++ sys/dos/emx/mkmf.bat
@@ -1,20 +1,26 @@
 @echo off
-:# -----------------------------------------------------------------------
-:# Example Makefile build script for DOS.
-:# -----------------------------------------------------------------------
-
-echo creating tmp\Makefile
+:# --------------------------------------------------------------------
+:# mkmf - a Makefile build batch for DOS
+:# it *must* be run from the emx directory
+:# --------------------------------------------------------------------
 
 set enable_f77="no"
 set plcf=..\..\..\cf
 
+:# --------------------------------------------------------------------
+if exist mkmf.bat goto okstart
+echo "ERROR: mkmk *must* be run from the emx directory"
+goto unset
+
 :# Makefile initialization
+:okstart
+echo "creating tmp\Makefile"
 
 type cf\init.in		>tmp\Makefile
 
 :# Default target, core source and object file lists
 
-type %plcf%\dist.in	>>tmp\Makefile 
+type %plcf%\dist.in	>>tmp\Makefile
 
 :# Copy source file lists and add compiler specific source to makefile
 
@@ -31,32 +37,32 @@ type cf\lib.in		>>tmp\Makefile
 
 :# Program and demo file dependencies, targets
 
-type %plcf%\exes.in	>>tmp\Makefile 
-type %plcf%\demos.in	>>tmp\Makefile 
+type %plcf%\exes.in	>>tmp\Makefile
+type %plcf%\demos.in	>>tmp\Makefile
 
 :# Installation and miscellaneous.
 
-type cf\install.in	>>tmp\Makefile 
-type cf\Misc.in		>>tmp\Makefile 
+type cf\install.in	>>tmp\Makefile
+type cf\Misc.in		>>tmp\Makefile
 
 :#  Object file dependencies
 
-type %plcf%\objs.in	>>tmp\Makefile 
+type %plcf%\objs.in	>>tmp\Makefile
 
 :# make list of objects for the response file
 
-echo creating tmp\libfiles.tmp
+echo "creating tmp\libfiles.tmp"
 
 type cf\all_o.rsp >tmp\libfiles.tmp
 if %enable_f77% == "yes" type cf\fstubs_o.rsp >>tmp\libfiles.tmp
 
-:# -----------------------------------------------------------------------
+:# --------------------------------------------------------------------
 :# Now build Makedemo.in.
 :# Makedemo is a stand-alone makefile for the demo programs.
 :# Note: it links against the installed PLplot library.
-:# -----------------------------------------------------------------------
+:# --------------------------------------------------------------------
 
-echo creating tmp\Makedemo
+echo "creating tmp\Makedemo"
 
 type cf\init.in		>tmp\Makedemo
 type cf\initdemo.in	>>tmp\Makedemo
@@ -64,6 +70,20 @@ type cf\initdemo.in	>>tmp\Makedemo
 type %plcf%\demos.in	>>tmp\Makedemo
 type cf\Miscdemo.in	>>tmp\Makedemo
 
+
+:# give some reminders of what is to be done
+
+echo.
+echo.
+echo To Compile:
+echo.
+echo %cd tmp
+echo %make links			(only done once)
+echo %make
+echo %make install
+
 :# unset local variables
+:unset
 set enable_f77=
 set plcf=
+:# -------------------------------------------------------- end of file
