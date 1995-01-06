b5f3ff34971c9edb0762cf51f5e14dab1fe0a24a
Maurice LeBrun <mjl@users.sourceforge.net>
1995-01-06 07:59:17 +0000

    Added a second plot which illustrates the new options ('f' -- to force fixed point labels even in a log plot, and 'h' -- to print a grid at the subtick intervals) to plbox.
    svn path=/trunk/; revision=1431

--- examples/c/x04c.c
+++ examples/c/x04c.c
@@ -1,6 +1,11 @@
 /* $Id$
  * $Log$
- * Revision 1.7  1994/06/30 17:57:14  mjl
+ * Revision 1.8  1995/01/06 07:59:17  mjl
+ * Added a second plot which illustrates the new options ('f' -- to force
+ * fixed point labels even in a log plot, and 'h' -- to print a grid at the
+ * subtick intervals) to plbox.
+ *
+ * Revision 1.7  1994/06/30  17:57:14  mjl
  * All C example programs: made another pass to eliminate warnings when using
  * gcc -Wall.  Lots of cleaning up: got rid of includes of math.h or string.h
  * (now included by plplot.h), eliminated redundant casts, put in more
@@ -20,6 +25,8 @@
 
 #include <plplot.h>
 
+void plot1(int type);
+
 /*----------------------------------------------------------------------*\
  * main
  *
@@ -29,10 +36,6 @@
 int
 main(int argc, char *argv[])
 {
-    int i;
-    static PLFLT freql[101], ampl[101], phase[101];
-    PLFLT f0, freq;
-
 /* Parse and process command line arguments */
 
     (void) plParseInternalOpts(&argc, argv, PL_PARSE_FULL);
@@ -40,40 +43,79 @@ main(int argc, char *argv[])
 /* Initialize plplot */
 
     plinit();
-
-    pladv(0);
     plfont(2);
 
-    f0 = 1000.0;
+    plot1(0);
+    plot1(1);
+
+    plend();
+    exit(0);
+}
+
+/*----------------------------------------------------------------------*\
+ * plot1
+ *
+ * Log-linear plot.
+\*----------------------------------------------------------------------*/
+
+void
+plot1(int type)
+{
+    int i;
+    static PLFLT freql[101], ampl[101], phase[101];
+    PLFLT f0, freq;
+
+    pladv(0);
+
+/* Set up data for log plot */
+
+    f0 = 1.0;
     for (i = 0; i <= 100; i++) {
-	freql[i] = 1.0 + i / 20.0;
+	freql[i] = -2.0 + i / 20.0;
 	freq = pow(10.0, freql[i]);
 	ampl[i] = 20.0 * log10(1.0 / sqrt(1.0 + pow((freq / f0), 2.)));
 	phase[i] = -(180.0 / 3.141592654) * atan(freq / f0);
     }
 
     plvpor(0.15, 0.85, 0.1, 0.9);
-    plwind(1.0, 6.0, -80.0, 0.0);
+    plwind(-2.0, 3.0, -80.0, 0.0);
+
+/* Try different axis and labelling styles. */
+
     plcol(1);
-    plbox("bclnst", 0.0, 0, "bnstv", 0.0, 0);
+    switch (type) {
+    case 0:
+	plbox("bclnst", 0.0, 0, "bnstv", 0.0, 0);
+	break;
+    case 1:
+	plbox("bcfghlnst", 0.0, 0, "bcghnstv", 0.0, 0);
+	break;
+    }
+
+/* Plot ampl vs freq */
+
     plcol(2);
     plline(101, freql, ampl);
     plcol(1);
     plptex(5.0, -30.0, 1.0, -20.0, 0.5, "-20 dB/decade");
 
-    plwind(1.0, 6.0, -100.0, 0.0);
-    plbox("", 0.0, 0, "cmstv", 30.0, 3);
-    plcol(3);
-    plline(101, freql, phase);
+/* Put labels on */
 
     plcol(1);
     plmtex("b", 3.2, 0.5, 0.5, "Frequency");
     plmtex("t", 2.0, 0.5, 0.5, "Single Pole Low-Pass Filter");
     plcol(2);
     plmtex("l", 5.0, 0.5, 0.5, "Amplitude (dB)");
-    plcol(3);
-    plmtex("r", 5.0, 0.5, 0.5, "Phase shift (degrees)");
 
-    plend();
-    exit(0);
+/* For the gridless case, put phase vs freq on same plot */
+
+    if (type == 0) {
+	plcol(1);
+	plwind(-2.0, 3.0, -100.0, 0.0);
+	plbox("", 0.0, 0, "cmstv", 30.0, 3);
+	plcol(3);
+	plline(101, freql, phase);
+	plcol(3);
+	plmtex("r", 5.0, 0.5, 0.5, "Phase shift (degrees)");
+    }
 }
