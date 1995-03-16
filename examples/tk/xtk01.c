5bee32effd7d707f5d6ae1274e77d44e621a77e8
Maurice LeBrun <mjl@users.sourceforge.net>
1995-03-16 23:23:14 +0000

    Changed one of the plots to a plshade demo, in order to make it easier to investigate cmap1-related behavior when the widget is being driven directly.
    svn path=/trunk/; revision=1491

--- examples/tk/xtk01.c
+++ examples/tk/xtk01.c
@@ -1,20 +1,11 @@
 /* $Id$
  * $Log$
- * Revision 1.7  1994/09/23 07:47:29  mjl
+ * Revision 1.8  1995/03/16 23:23:14  mjl
+ * Changed one of the plots to a plshade demo, in order to make it easier to
+ * investigate cmap1-related behavior when the widget is being driven directly.
+ *
+ * Revision 1.7  1994/09/23  07:47:29  mjl
  * Modified to use the new syntax for pltkMain().
- *
- * Revision 1.6  1994/06/23  22:40:06  mjl
- * Fix to get prototype of pltkMain() correct.
- *
- * Revision 1.5  1994/06/16  19:30:14  mjl
- * Changes to use pltkMain() for creating extended wish.  Should be more
- * portable and robust than old method.
- *
- * Revision 1.4  1994/06/09  20:29:54  mjl
- * Minor cleaning up.
- *
- * Revision 1.3  1994/05/26  22:38:07  mjl
- * Added missing CVS Id and Log fields.
  */
 
 /* Before including plplot.h you must define TK to get all prototypes */
@@ -27,7 +18,7 @@
 static int
 AppInit(Tcl_Interp *interp);
 
-/*----------------------------------------------------------------------*\
+/*--------------------------------------------------------------------------*\
  * main --
  *
  * Just a stub routine to call pltkMain.  The latter is nice to have
@@ -36,7 +27,7 @@ AppInit(Tcl_Interp *interp);
  * systems/compilers/linkers/etc).  Hopefully in the future Tk will
  * supply a sufficiently capable tkMain() type function that can be used
  * instead. 
-\*----------------------------------------------------------------------*/
+\*--------------------------------------------------------------------------*/
 
 int
 main(int argc, char **argv)
@@ -45,7 +36,7 @@ main(int argc, char **argv)
 }
 
 /*
- *----------------------------------------------------------------------
+ *--------------------------------------------------------------------------
  *
  * AppInit --
  *
@@ -81,7 +72,7 @@ main(int argc, char **argv)
  * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
  * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
  * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- *----------------------------------------------------------------------
+ *--------------------------------------------------------------------------
  */
 
 int   myplotCmd        (ClientData, Tcl_Interp *, int, char **);
@@ -125,14 +116,13 @@ AppInit(Tcl_Interp *interp)
     return TCL_OK;
 }
 
-void myplot1();
-void myplot2();
-void myplot3();
-void myplot4();
+ /* =============================================================== */
 
-/* Plots several simple functions */
-/* Note the compiler should automatically convert all non-pointer arguments
-   to satisfy the prototype, but some have problems with constants. */
+/*	Taken from:
+	x01c.c
+
+	Simple line plot and multiple windows demo.
+*/
 
 static PLFLT xs[6] =
 {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
@@ -219,7 +209,7 @@ plot1(void)
     plcol(1);
     plenv(xmin, xmax, ymin, ymax, 0, 0);
     plcol(6);
-    pllab("(x)", "(y)", "#frPLPLOT Example 1 - y=x#u2");
+    pllab("(x)", "(y)", "#frPLplot Example 1 - y=x#u2");
 
 /* Plot the data points */
 
@@ -246,7 +236,7 @@ plot2(void)
     plcol(1);
     plenv((PLFLT) -2.0, (PLFLT) 10.0, (PLFLT) -0.4, (PLFLT) 1.2, 0, 1);
     plcol(2);
-    pllab("(x)", "sin(x)/x", "#frPLPLOT Example 1 - Sinc Function");
+    pllab("(x)", "sin(x)/x", "#frPLplot Example 1 - Sinc Function");
 
 /* Fill up the arrays */
 
@@ -296,7 +286,7 @@ plot3(void)
     plstyl(0, &mark0, &space0);
 
     plcol(3);
-    pllab("Angle (degrees)", "sine", "#frPLPLOT Example 1 - Sine function");
+    pllab("Angle (degrees)", "sine", "#frPLplot Example 1 - Sine function");
 
     for (i = 0; i < 101; i++) {
 	x[i] = 3.6 * i;
@@ -306,6 +296,182 @@ plot3(void)
     plcol(4);
     plline(101, x, y);
 }
+
+ /* =============================================================== */
+
+/*	Taken from:
+	x16c.c
+
+	plshade demo, using color fill.
+
+	Maurice LeBrun
+	IFS, University of Texas at Austin
+	20 Mar 1994
+*/
+
+#define NCONTR	30		/* Number of contours */
+#define XPTS    35		/* Data points in x */
+#define YPTS    46		/* Datat points in y */
+
+#define XSPA    2./(XPTS-1)
+#define YSPA    2./(YPTS-1)
+
+static PLFLT clevel[NCONTR];
+
+/* Utility macros */
+
+#ifndef PI
+#define PI	3.1415926535897932384
+#endif
+#ifndef MAX
+#define MAX(a,b)    (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
+#define MIN(a,b)    (((a) < (b)) ? (a) : (b))
+#endif
+
+/* Transformation function */
+
+PLFLT tr[6] =
+{XSPA, 0.0, -1.0, 0.0, YSPA, -1.0};
+
+static void
+mypltr(PLFLT x, PLFLT y, PLFLT *tx, PLFLT *ty, void *pltr_data)
+{
+    *tx = tr[0] * x + tr[1] * y + tr[2];
+    *ty = tr[3] * x + tr[4] * y + tr[5];
+}
+
+/* Function prototypes */
+
+static void
+f2mnmx(PLFLT **f, PLINT nx, PLINT ny, PLFLT *fmin, PLFLT *fmax);
+
+/*--------------------------------------------------------------------------*\
+ * shade
+ *
+ * Does several shade plots using different coordinate mappings.
+\*--------------------------------------------------------------------------*/
+
+int
+shade(void)
+{
+    int i, j;
+    PLFLT x, y, argx, argy, distort;
+
+    PLFLT **z, **w, zmin, zmax;
+    PLFLT xg1[XPTS], yg1[YPTS];
+    PLcGrid  cgrid1;
+    PLcGrid2 cgrid2;
+
+    PLFLT shade_min, shade_max, sh_color;
+    PLINT sh_cmap = 1, sh_width;
+    PLINT min_color = 1, min_width = 0, max_color = 0, max_width = 0;
+
+/* Set up function arrays */
+
+    plAlloc2dGrid(&z, XPTS, YPTS);
+    plAlloc2dGrid(&w, XPTS, YPTS);
+
+/* Set up data array */
+
+    for (i = 0; i < XPTS; i++) {
+	x = (double) (i - (XPTS / 2)) / (double) (XPTS / 2);
+	for (j = 0; j < YPTS; j++) {
+	    y = (double) (j - (YPTS / 2)) / (double) (YPTS / 2) - 1.0;
+
+	    z[i][j] = - sin(7.*x) * cos(7.*y) + x*x - y*y;
+	    w[i][j] = - cos(7.*x) * sin(7.*y) + 2 * x * y;
+	}
+    }
+    f2mnmx(z, XPTS, YPTS, &zmin, &zmax);
+    for (i = 0; i < NCONTR; i++)
+	clevel[i] = zmin + (zmax - zmin) * (i + 0.5) / (float) NCONTR;
+
+/* Set up coordinate grids */
+
+    cgrid1.xg = xg1;
+    cgrid1.yg = yg1;
+    cgrid1.nx = XPTS;
+    cgrid1.ny = YPTS;
+
+    plAlloc2dGrid(&cgrid2.xg, XPTS, YPTS);
+    plAlloc2dGrid(&cgrid2.yg, XPTS, YPTS);
+    cgrid2.nx = XPTS;
+    cgrid2.ny = YPTS;
+
+    for (i = 0; i < XPTS; i++) {
+	for (j = 0; j < YPTS; j++) {
+	    mypltr((PLFLT) i, (PLFLT) j, &x, &y, NULL);
+
+	    argx = x * PI/2;
+	    argy = y * PI/2;
+	    distort = 0.4;
+
+	    cgrid1.xg[i] = x + distort * cos(argx);
+	    cgrid1.yg[j] = y - distort * cos(argy);
+
+	    cgrid2.xg[i][j] = x + distort * cos(argx) * cos(argy);
+	    cgrid2.yg[i][j] = y - distort * cos(argx) * cos(argy);
+	}
+    }
+
+/* Plot using identity transform */
+
+    pladv(0);
+    plvpor(0.1, 0.9, 0.1, 0.9);
+    plwind(-1.0, 1.0, -1.0, 1.0);
+
+    for (i = 0; i < NCONTR; i++) {
+	shade_min = zmin + (zmax - zmin) * i / (float) NCONTR;
+	shade_max = zmin + (zmax - zmin) * (i +1) / (float) NCONTR;
+	sh_color = i / (float) (NCONTR-1);
+	sh_width = 2;
+	plpsty(0);
+
+	plshade(z, XPTS, YPTS, NULL, -1., 1., -1., 1., 
+		shade_min, shade_max, 
+		sh_cmap, sh_color, sh_width,
+		min_color, min_width, max_color, max_width,
+		plfill, 1, NULL, NULL);
+    }
+
+    plcol(1);
+    plbox("bcnst", 0.0, 0, "bcnstv", 0.0, 0);
+    plcol(2);
+/*
+    plcont(w, XPTS, YPTS, 1, XPTS, 1, YPTS, clevel, NCONTR, mypltr, NULL);
+    */
+    pllab("distance", "altitude", "Bogon density");
+
+/* Clean up */
+
+    free((void *) w);
+    free((void *) z);
+}
+
+/*--------------------------------------------------------------------------*\
+ * f2mnmx
+ *
+ * Returns min & max of input 2d array.
+\*--------------------------------------------------------------------------*/
+
+static void
+f2mnmx(PLFLT **f, PLINT nx, PLINT ny, PLFLT *fmin, PLFLT *fmax)
+{
+    int i, j;
+
+    *fmax = f[0][0];
+    *fmin = *fmax;
+
+    for (i = 0; i < nx; i++) {
+	for (j = 0; j < ny; j++) {
+            *fmax = MAX(*fmax, f[i][j]);
+            *fmin = MIN(*fmin, f[i][j]);
+	}
+    }
+}
+ /* =============================================================== */
 
 int   myplotCmd( ClientData cd, Tcl_Interp *interp, int argc, char **argv )
 {
@@ -319,7 +485,7 @@ int   myplotCmd( ClientData cd, Tcl_Interp *interp, int argc, char **argv )
       myplot3();
 
     if (!strcmp(argv[1],"4"))
-      myplot4();
+      shade();
 
     plflush();
     return TCL_OK;
