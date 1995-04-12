74d97c8376f91f79b72312dd32359fe3341c41c8
Maurice LeBrun <mjl@users.sourceforge.net>
1995-04-12 08:00:30 +0000

    Eliminated use of client variable and fixed plw_create_TopRow syntax.
    svn path=/trunk/; revision=1535

--- bindings/tk/PLXWin.tcl
+++ bindings/tk/PLXWin.tcl
@@ -7,7 +7,10 @@
 # $Id$
 #
 # $Log$
-# Revision 1.4  1995/03/23 21:07:21  mjl
+# Revision 1.5  1995/04/12 08:00:30  mjl
+# Eliminated use of client variable and fixed plw_create_TopRow syntax.
+#
+# Revision 1.4  1995/03/23  21:07:21  mjl
 # Fix to fix of key bindings.
 #
 # Revision 1.3  1995/03/23  20:46:09  mjl
@@ -32,17 +35,14 @@ itcl_class PLXWin {
 	#puts "running PLXWin ctor"
 
 	# The following code copied over from plxframe in
-	# plwidget.tcl.  Now if I could just get Maurice to forget
-	# aobut non itcl stuff altogether ...
-
-	set client {}
+	# plwidget.tcl.
 
 	# Make frame for top row widgets.
 	# plframe widget must already have been created (the plframe
 	# is queried for a list of the valid output devices for page
 	# dumps).
 
-	plw_create_TopRow $this $client
+	plw_create_TopRow $this
 	pack append $this $this.ftop {top fill}
 
 	# Initialize plplot widget
@@ -64,22 +64,12 @@ itcl_class PLXWin {
 	set zyr($this,0) 1.0
 
 	# Bindings
-	#
-	# Note: it is necessary here to protect the $client variable
-	# from becoming two tokens if it has embedded spaces, such as
-	# occurs when you have multiple copies running.  The [list
-	# $client] construct will enclose $client with a pair of
-	# braces if necessary (can't do it directly since braces
-	# prevent variable expansion).  The reason this is necessary
-	# is because binding the command to a key causes it to be
-	# parsed by the interpreter twice -- once during the bind and
-	# once during its execution.
 
 	bind $this.plwin <Any-KeyPress> \
-	    "plw_key_filter $this [list $client] %N %s %x %y %K %A"
+	    "plw_key_filter $this %N %s %x %y %K %A"
 
 	bind $this.plwin <Any-ButtonPress> \
-	    "plw_user_mouse $this [list $client] %b %s %x %y"
+	    "plw_user_mouse $this %b %s %x %y"
 
 	bind $this.plwin <Any-Enter> \
 	    "focus $this.plwin"
