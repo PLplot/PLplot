e6d5e5ae70f231d00211e6c9a436dc30f8f9388c
Maurice LeBrun <mjl@users.sourceforge.net>
1995-05-19 22:21:26 +0000

    Fixes for Tk 4.0.  The default options in the options database changed in some cases so that apps look the same as before.  Users can change these to taste.  The test for mono system using the line      if {[tk colormodel $w] == "color"} (this command was eliminated) has been replaced by      if {[winfo depth $w] == 1} which has the same effect.
    svn path=/trunk/; revision=1618

--- bindings/tk/pldefaults.tcl
+++ bindings/tk/pldefaults.tcl
@@ -1,6 +1,15 @@
 # $Id$
 # $Log$
-# Revision 1.6  1995/03/16 22:59:30  mjl
+# Revision 1.7  1995/05/19 22:21:26  mjl
+# Fixes for Tk 4.0.  The default options in the options database changed in
+# some cases so that apps look the same as before.  Users can change these
+# to taste.  The test for mono system using the line
+#      if {[tk colormodel $w] == "color"}
+# (this command was eliminated) has been replaced by
+#      if {[winfo depth $w] == 1}
+# which has the same effect.
+#
+# Revision 1.6  1995/03/16  22:59:30  mjl
 # Revamped modifier key acceleration scheme.  Now controlled by two global
 # variables: "global key_scroll_mag", which defaults to 5 and is the
 # magnification for each modifier key added, and "key_scroll_speed", which
@@ -66,6 +75,7 @@
 #----------------------------------------------------------------------------
 
 proc pldefaults {} {
+    global tk_version
     global gen_font
     global gen_bold_font
     global gen_menu_font
@@ -111,8 +121,8 @@ proc pldefaults {} {
 
     option add *background			$gen_bg
     option add *foreground			$gen_fg
-    option add *activeBackground      		$gen_active_bg
-    option add *activeForeground      		$gen_active_fg
+    option add *activeBackground		$gen_active_bg
+    option add *activeForeground		$gen_active_fg
     option add *disabledForeground		gray45
     option add *Checkbutton.selector		yellow
     option add *Radiobutton.selector		yellow
@@ -125,9 +135,16 @@ proc pldefaults {} {
     option add *Scale.background		$gen_bg
     option add *Scale.sliderForeground		$gen_bg
     option add *Scale.sliderBackground		$gen_darker_bg
-    option add *Scrollbar.foreground		$gen_bg
-    option add *Scrollbar.activeForeground	$gen_bg
-    option add *Scrollbar.background		$gen_darker_bg
+
+    if {$tk_version < 4.0} then {
+	option add *Scrollbar.foreground	$gen_bg
+	option add *Scrollbar.activeForeground	$gen_bg
+	option add *Scrollbar.background	$gen_darker_bg
+
+    } else {
+	option add *Scrollbar.background	$gen_bg
+	option add *Scrollbar.troughColor	$gen_darker_bg
+    }
 
 # End of page indicator
 
@@ -137,24 +154,29 @@ proc pldefaults {} {
 # This specifies the default plplot widget background color.
 # A white background looks better on grayscale or mono.
 
-    if {[tk colormode .] == "color"} {
-	option add *plwin.background		black
-    } else {
+    if {[winfo depth .] == 1} {
 	option add *plwin.background		white
+    } else {
+	option add *plwin.background		black
     }
 
 #----------------------------------------------------------------------------
 # Miscellaneous 
 
     option add *anchor				w
-    option add *Button.borderWidth    		2
+    option add *Button.borderWidth		2
     option add *Button.anchor			c
-    option add *Checkbutton.borderWidth    	2
-    option add *Radiobutton.borderWidth    	2
+    option add *Checkbutton.borderWidth		2
+    option add *Radiobutton.borderWidth		2
     option add *Label.anchor			w
     option add *Labelframe.borderWidth		2
     option add *Entry.relief			sunken
     option add *Scrollbar.relief		sunken
+
+# I have this in here so that applications written before Tk 4.0 still
+# look the same.  More selectivity might be better.
+
+    option add *highlightThickness		0
 
 # Key shortcut definitions -- change them if you want!
 # Turn them into an empty string to disable.
