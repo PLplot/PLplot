65ef4ece461e1a349469f2f11c8ea1b83612b5a0
Geoffrey Furnish <furnish@users.sourceforge.net>
1995-07-01 21:43:44 +0000

    Initial implementation, converted C example program code to Tcl.
    svn path=/trunk/; revision=1794

--- examples/tcl/x12.tcl
+++ examples/tcl/x12.tcl
@@ -3,7 +3,10 @@
 #
 # $Id$
 # $Log$
-# Revision 1.3  1995/04/24 21:28:20  mjl
+# Revision 1.4  1995/07/01 21:43:43  furnish
+# Initial implementation, converted C example program code to Tcl.
+#
+# Revision 1.3  1995/04/24  21:28:20  mjl
 # Made the widget name default to "loopback" for easier use from pltcl.
 #
 # Revision 1.2  1995/01/27  02:49:55  mjl
@@ -15,5 +18,52 @@
 # Tcl demo programs, just a template for now.
 #----------------------------------------------------------------------------
 
+# Does a simple bar chart, using color fill.  If color fill is
+# unavailable, pattern fill is used instead (automatic).
+
 proc x12 {{w loopback}} {
+
+    matrix y0 f 10 = {5., 15., 12., 24., 28., 30., 20., 8., 12., 3.}
+
+    $w cmd pladv 0
+    $w cmd plvsta
+    $w cmd plwind 1980.0 1990.0 0.0 35.0
+    $w cmd plbox "bc" 1.0 0 "bcnv" 10.0 0
+    $w cmd plcol 2
+    $w cmd pllab "Year" "Widget Sales (millions)" "#frPLplot Example 12"
+
+    for {set i 0} {$i < 10} {incr i} {
+	$w cmd plcol [expr $i+1]
+
+#	$w cmd plcol1 [expr ($i + 1.)/10.0 ]
+
+	$w cmd plpsty 0
+
+	plfbox $w [expr 1980. + $i] [y0 $i]
+
+	set string [format "%.0f" [y0 $i] ]
+	$w cmd plptex [expr 1980. + $i + .5] [expr [y0 $i] + 1.] \
+	    1.0 0.0 .5 $string
+
+	set string [format "%d" [expr 1980 + $i] ]
+	$w cmd plmtex "b" 1.0 [expr ($i + 1) * .1 - .05] 0.5 $string
+    }
+}
+
+proc plfbox  {w x0 y0} {
+
+    matrix x f 4; matrix y f 4
+
+    x 0 = $x0;
+    y 0 = 0.;
+    x 1 = $x0;
+    y 1 = $y0;
+    x 2 = [expr $x0 + 1.];
+    y 2 = $y0;
+    x 3 = [expr $x0 + 1.];
+    y 3 = 0.;
+    $w cmd plfill 4 x y
+    $w cmd plcol 1
+    $w cmd pllsty 1
+    $w cmd plline 4 x y
 }
--- examples/tcl/x13.tcl
+++ examples/tcl/x13.tcl
@@ -3,7 +3,10 @@
 #
 # $Id$
 # $Log$
-# Revision 1.3  1995/04/24 21:28:21  mjl
+# Revision 1.4  1995/07/01 21:43:44  furnish
+# Initial implementation, converted C example program code to Tcl.
+#
+# Revision 1.3  1995/04/24  21:28:21  mjl
 # Made the widget name default to "loopback" for easier use from pltcl.
 #
 # Revision 1.2  1995/01/27  02:49:56  mjl
@@ -15,5 +18,68 @@
 # Tcl demo programs, just a template for now.
 #----------------------------------------------------------------------------
 
+# Does a simple pie chart.
+
 proc x13 {{w loopback}} {
+
+    set text0 "Maurice"
+    set text1 "Randy"
+    set text2 "Mark"
+    set text3 "Steve"
+    set text4 "Warner"
+
+#    int i, j;
+#    PLFLT dthet, theta0, theta1, theta, just, dx, dy;
+#    static PLFLT x[500], y[500], per[5];
+
+    matrix x f 500
+    matrix y f 500
+    matrix per f 5 = {10., 32., 12., 30., 16.}
+
+    $w cmd plenv 0. 10. 0. 10. 1 -2
+    $w cmd plcol 2
+
+    set theta0 0.;
+    set pi 3.1415927
+
+    set dthet [expr 2. * $pi / 500.]
+    for {set i 0} {$i <= 4} {incr i} {
+	set j 0;
+	x $j = 5.;
+	y $j = 5.;
+	incr j
+
+	set theta1 [expr $theta0 + 2. * $pi * [per $i] / 100.]
+	if {$i == 4} {
+	    set theta1 [expr 2. * $pi]
+	}
+
+	for {set theta $theta0} {$theta <= $theta1} {
+	    set theta [expr $theta + $dthet]} {
+	    x $j = [expr 5. + 3. * cos($theta) ]
+	    y $j = [expr 5. + 3. * sin($theta) ]
+	    incr j
+	}
+
+	$w cmd plcol [expr $i + 1]
+	$w cmd plpsty [expr (($i + 3) % 8 + 1)]
+	$w cmd plfill $j x y
+	$w cmd plcol 1
+	$w cmd plline $j x y
+	set just [expr ($theta0 + $theta1) / 2.]
+	set dx [expr .25 * cos($just)]
+	set dy [expr .25 * sin($just)]
+	if {$just < $pi / 2. || $just > 3. * $pi / 2.} {
+	    set just 0.
+	} else {
+	    set just 1.
+	}
+
+	$w cmd plptex [expr [x [expr $j / 2]] + $dx] \
+	    [expr [y [expr $j / 2]] + $dy] 1.0 0.0 $just [set text$i]
+	set theta0 [expr $theta - $dthet]
+    }
+    $w cmd plfont 2
+    $w cmd plschr 0. 1.3
+    $w cmd plptex 5.0 9.0 1.0 0.0 0.5 "Percentage of Sales"
 }
