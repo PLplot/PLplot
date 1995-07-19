d94079adcbd7d82668896e824574082483b6a8e1
Geoffrey Furnish <furnish@users.sourceforge.net>
1995-07-19 18:02:16 +0000

    Completely overhauled.  Now mirrors all Tcl API commands which make any sense on a widget, 83 in all, plus a "cmd" proxy so that code written for a bare Tk plframe can work unmodified on a PLWin.
    svn path=/trunk/; revision=1807

--- bindings/tk/PLWin.tcl
+++ bindings/tk/PLWin.tcl
@@ -7,7 +7,12 @@
 # $Id$
 #
 # $Log$
-# Revision 1.9  1995/07/04 21:53:43  furnish
+# Revision 1.10  1995/07/19 18:02:16  furnish
+# Completely overhauled.  Now mirrors all Tcl API commands which make
+# any sense on a widget, 83 in all, plus a "cmd" proxy so that code
+# written for a bare Tk plframe can work unmodified on a PLWin.
+#
+# Revision 1.9  1995/07/04  21:53:43  furnish
 # plcont and plshade now have long, complex argument lists.  Moreover,
 # since they support defaulting of arguments, not all args even have to
 # be specified.  So we convert to using the [incr Tcl] args metaphor so
@@ -44,6 +49,8 @@
 
 itcl_class PLWin {
 
+    public name
+
     constructor {config} {
 	set class [$this info class]
 	::rename $this $this-tmp-
@@ -57,153 +64,350 @@ itcl_class PLWin {
 
     method configure {config} {}
 
-    method pladv {{subwindow {}}} {
-	$this.plwin cmd pladv $subwindow
+# We add this one just so that user code can be oblivious as to
+# whether or not it is driving a PLWin object, or just a plframe.  In
+# other words, Tcl code which works fine on a plframe or the "loopback
+# widget", can work unmodified with the PLWin [incr Tcl] object.
+
+    method cmd {args} {
+	eval $this.plwin cmd $args
     }
 
-    method plbop {} {
-	$this.plwin cmd plbop
+# From here on down, we just insert what comes out of plitclgen (file
+# gen.itcl).  If you don't like the handling of one of these for some
+# reason, then add the method name to the ignore list in plitclgen,
+# and provide an explicit coding, ABOVE THIS LINE.
+
+    method pladv {args} {
+        eval $this.plwin cmd pladv $args
     }
 
-    method plbox {xopt xtick nxsub yopt ytick nysub} {
-	$this.plwin cmd plbox $xopt $xtick $nxsub $yopt $ytick $nysub
+    method plaxes {args} {
+        eval $this.plwin cmd plaxes $args
     }
 
-    method plcol0 {col0} {
-	$this.plwin cmd plcol0 $col0
+    method plbin {args} {
+        eval $this.plwin cmd plbin $args
     }
 
-    method plcol {col0} {
-	$this.plwin cmd plcol0 $col0
+    method plbop {args} {
+        eval $this.plwin cmd plbop $args
     }
 
-    method plcont {args} {
-	eval $this.plwin cmd plcont $args
+    method plbox {args} {
+        eval $this.plwin cmd plbox $args
     }
 
-    method plend {} {
-	$this.plwin cmd plend 
+    method plbox3 {args} {
+        eval $this.plwin cmd plbox3 $args
     }
 
-    method plend1 {} {
-	$this.plwin cmd plend1 
+    method plcol0 {args} {
+        eval $this.plwin cmd plcol0 $args
     }
 
-    method plenv {xmin xmax ymin ymax just axis} {
-	$this.plwin cmd plenv $xmin $xmax $ymin $ymax $just $axis
+    method plcol1 {args} {
+        eval $this.plwin cmd plcol1 $args
     }
 
-    method pleop {} {
-	$this.plwin cmd pleop 
+    method pldid2pc {args} {
+        eval $this.plwin cmd pldid2pc $args
     }
 
-    method plfont {font} {
-	$this.plwin cmd plfont $font
+    method pldip2dc {args} {
+        eval $this.plwin cmd pldip2dc $args
     }
 
-    method plfontld {font} {
-	$this.plwin cmd plfontld $font
+    method plenv {args} {
+        eval $this.plwin cmd plenv $args
     }
 
-    method plgra {} {
-	$this.plwin cmd plgra 
+    method pleop {args} {
+        eval $this.plwin cmd pleop $args
     }
 
-    method plhist {n data datmin datmax nbin oldwin} {
-	$this.plwin cmd plhist $n $data $datmin $datmax $nbin $oldwin
+    method plerrx {args} {
+        eval $this.plwin cmd plerrx $args
     }
 
-    method plinit {} {
-	$this.plwin cmd plinit 
+    method plerry {args} {
+        eval $this.plwin cmd plerry $args
     }
 
-    method pljoin {x1 y1 x2 y2} {
-	$this.plwin cmd pljoin $x1 $y1 $x2 $y2
+    method plfill {args} {
+        eval $this.plwin cmd plfill $args
     }
 
-    method pllab {xlabel ylabel tlabel} {
-	$this.plwin cmd pllab $xlabel $ylabel $tlabel
+    method plfont {args} {
+        eval $this.plwin cmd plfont $args
     }
 
-    method plline {n xarray yarray} {
-	$this.plwin cmd plline $n $xarray $yarray
+    method plfontld {args} {
+        eval $this.plwin cmd plfontld $args
     }
 
-    method plmtex {side disp pos just text} {
-	$this.plwin cmd plmtex $side $disp $pos $just $text
+    method plgchr {args} {
+        eval $this.plwin cmd plgchr $args
     }
 
-    method plpoin {n xarray yarray code} {
-	$this.plwin cmd plpoin $n $xarray $yarray $code
+    method plgcol0 {args} {
+        eval $this.plwin cmd plgcol0 $args
     }
 
-    method plptex {x y dx dy just text} {
-	$this.plwin cmd plptex $x $y $dx $dy $just $text
+    method plgdidev {args} {
+        eval $this.plwin cmd plgdidev $args
     }
 
-    method plschr {def scale} {
-	$this.plwin cmd plschr $def $scale
+    method plgdiori {args} {
+        eval $this.plwin cmd plgdiori $args
     }
 
-    method plscol0 {index r g b} {
-	$this.plwin cmd plscol0 $index $r $g $b
+    method plgdiplt {args} {
+        eval $this.plwin cmd plgdiplt $args
     }
 
-    method plscolbg {r g b} {
-	$this.plwin cmd plscolbg $r $g $b
+    method plgpage {args} {
+        eval $this.plwin cmd plgpage $args
     }
 
-    method plsetopt {option arg} {
-	$this.plwin cmd plsetopt $option $arg
+    method plgra {args} {
+        eval $this.plwin cmd plgra $args
     }
 
-    method plshade {args} {
-	eval $this.plwin cmd plshade $args
+    method plgspa {args} {
+        eval $this.plwin cmd plgspa $args
     }
 
-    method plsstrm {stream} {
-	$this.plwin cmd plsstrm $stream
+    method plgstrm {args} {
+        eval $this.plwin cmd plgstrm $args
     }
 
-    method plssub {nsubx nsuby} {
-	$this.plwin cmd plssub $nsubx $nsuby
+    method plgxax {args} {
+        eval $this.plwin cmd plgxax $args
     }
 
-    method plstyl {nels mark space} {
-	$this.plwin cmd plstyl $nels $mark $space
+    method plgyax {args} {
+        eval $this.plwin cmd plgyax $args
     }
 
-    method plsxax {digmax} {
-	$this.plwin cmd plsxax $digmax
+    method plgzax {args} {
+        eval $this.plwin cmd plgzax $args
     }
 
-    method plsyax {digmax} {
-	$this.plwin cmd plsyax $digmax
+    method plhist {args} {
+        eval $this.plwin cmd plhist $args
     }
 
-    method plsym {n xarray yarray code} {
-	$this.plwin cmd plsym $n $xarray $yarray $code
+    method plhls {args} {
+        eval $this.plwin cmd plhls $args
     }
 
-    method pltext {} {
-	$this.plwin cmd pltext 
+    method pljoin {args} {
+        eval $this.plwin cmd pljoin $args
     }
 
-    method plvpor {xmin xmax ymin ymax} {
-	$this.plwin cmd plvpor $xmin $xmax $ymin $ymax
+    method pllab {args} {
+        eval $this.plwin cmd pllab $args
     }
 
-    method plvsta {} {
-	$this.plwin cmd plvsta 
+    method plline {args} {
+        eval $this.plwin cmd plline $args
     }
 
-    method plwid {width} {
-	$this.plwin cmd plwid $width
+    method plline3 {args} {
+        eval $this.plwin cmd plline3 $args
     }
 
-    method plwind {xmin xmax ymin ymax} {
-	$this.plwin cmd plwind $xmin $xmax $ymin $ymax
+    method pllsty {args} {
+        eval $this.plwin cmd pllsty $args
     }
 
-    public name
+    method plmtex {args} {
+        eval $this.plwin cmd plmtex $args
+    }
+
+    method plpat {args} {
+        eval $this.plwin cmd plpat $args
+    }
+
+    method plpoin {args} {
+        eval $this.plwin cmd plpoin $args
+    }
+
+    method plpoin3 {args} {
+        eval $this.plwin cmd plpoin3 $args
+    }
+
+    method plpoly3 {args} {
+        eval $this.plwin cmd plpoly3 $args
+    }
+
+    method plprec {args} {
+        eval $this.plwin cmd plprec $args
+    }
+
+    method plpsty {args} {
+        eval $this.plwin cmd plpsty $args
+    }
+
+    method plptex {args} {
+        eval $this.plwin cmd plptex $args
+    }
+
+    method plreplot {args} {
+        eval $this.plwin cmd plreplot $args
+    }
+
+    method plrgb {args} {
+        eval $this.plwin cmd plrgb $args
+    }
+
+    method plrgb1 {args} {
+        eval $this.plwin cmd plrgb1 $args
+    }
+
+    method plschr {args} {
+        eval $this.plwin cmd plschr $args
+    }
+
+    method plscmap0n {args} {
+        eval $this.plwin cmd plscmap0n $args
+    }
+
+    method plscmap1n {args} {
+        eval $this.plwin cmd plscmap1n $args
+    }
+
+    method plscmap0 {args} {
+        eval $this.plwin cmd plscmap0 $args
+    }
+
+    method plscmap1 {args} {
+        eval $this.plwin cmd plscmap1 $args
+    }
+
+    method plscmap1l {args} {
+        eval $this.plwin cmd plscmap1l $args
+    }
+
+    method plscol0 {args} {
+        eval $this.plwin cmd plscol0 $args
+    }
+
+    method plscolbg {args} {
+        eval $this.plwin cmd plscolbg $args
+    }
+
+    method plscolor {args} {
+        eval $this.plwin cmd plscolor $args
+    }
+
+    method plsdidev {args} {
+        eval $this.plwin cmd plsdidev $args
+    }
+
+    method plsdimap {args} {
+        eval $this.plwin cmd plsdimap $args
+    }
+
+    method plsdiori {args} {
+        eval $this.plwin cmd plsdiori $args
+    }
+
+    method plsdiplt {args} {
+        eval $this.plwin cmd plsdiplt $args
+    }
+
+    method plsdiplz {args} {
+        eval $this.plwin cmd plsdiplz $args
+    }
+
+    method plsmaj {args} {
+        eval $this.plwin cmd plsmaj $args
+    }
+
+    method plsmin {args} {
+        eval $this.plwin cmd plsmin $args
+    }
+
+    method plsori {args} {
+        eval $this.plwin cmd plsori $args
+    }
+
+    method plspage {args} {
+        eval $this.plwin cmd plspage $args
+    }
+
+    method plspause {args} {
+        eval $this.plwin cmd plspause $args
+    }
+
+    method plssub {args} {
+        eval $this.plwin cmd plssub $args
+    }
+
+    method plssym {args} {
+        eval $this.plwin cmd plssym $args
+    }
+
+    method plstyl {args} {
+        eval $this.plwin cmd plstyl $args
+    }
+
+    method plsvpa {args} {
+        eval $this.plwin cmd plsvpa $args
+    }
+
+    method plsxax {args} {
+        eval $this.plwin cmd plsxax $args
+    }
+
+    method plsxwin {args} {
+        eval $this.plwin cmd plsxwin $args
+    }
+
+    method plsyax {args} {
+        eval $this.plwin cmd plsyax $args
+    }
+
+    method plsym {args} {
+        eval $this.plwin cmd plsym $args
+    }
+
+    method plszax {args} {
+        eval $this.plwin cmd plszax $args
+    }
+
+    method pltext {args} {
+        eval $this.plwin cmd pltext $args
+    }
+
+    method plvasp {args} {
+        eval $this.plwin cmd plvasp $args
+    }
+
+    method plvpas {args} {
+        eval $this.plwin cmd plvpas $args
+    }
+
+    method plvpor {args} {
+        eval $this.plwin cmd plvpor $args
+    }
+
+    method plvsta {args} {
+        eval $this.plwin cmd plvsta $args
+    }
+
+    method plw3d {args} {
+        eval $this.plwin cmd plw3d $args
+    }
+
+    method plwid {args} {
+        eval $this.plwin cmd plwid $args
+    }
+
+    method plwind {args} {
+        eval $this.plwin cmd plwind $args
+    }
+
 }
