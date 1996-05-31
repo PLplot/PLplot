5b3a5b7f69dead0f7818f1e62e5fac11fed82108
Maurice LeBrun <mjl@users.sourceforge.net>
1996-05-31 22:57:08 +0000

    Updated to reflect some old changes to sfstubs.fm4.
    svn path=/trunk/; revision=1939

--- bindings/f77/sfstubs.f
+++ bindings/f77/sfstubs.f
@@ -35,6 +35,34 @@
 
 !***********************************************************************
 
+	subroutine plgdev(dnam)
+
+        implicit character(a-z)
+	character*(*) dnam
+        
+	integer maxlen
+	parameter (maxlen = 320)
+	character*(maxlen) string1, string2, string3
+	character*(maxlen) string4, string5, string6
+	integer s1(80), s2(80), s3(80), s4(80), s5(80), s6(80)
+	equivalence ( s1, string1 ), ( s2, string2 )
+	equivalence ( s3, string3 ), ( s4, string4 )
+	equivalence ( s5, string5 ), ( s6, string6 )
+	common /zzplstr1/ string1
+	common /zzplstr2/ string2
+	common /zzplstr3/ string3
+	common /zzplstr4/ string4
+	common /zzplstr5/ string5
+	common /zzplstr6/ string6
+
+
+	call plgdev7(string1)
+        call plstrc2f(string1, dnam)
+
+	end
+
+!***********************************************************************
+
 	subroutine plsfnam(fnam)
 
         implicit character(a-z)
