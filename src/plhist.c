ad2268f94220289947102c924e1234ce6493a31a
Vince Darley <vincentdarley@users.sourceforge.net>
2000-08-04 04:12:08 +0000

    patch 100406
    svn path=/trunk/; revision=2138

--- src/plhist.c
+++ src/plhist.c
@@ -1,5 +1,8 @@
 /* $Id$
  * $Log$
+ * Revision 1.10  2000/08/04 04:12:08  vincentdarley
+ * patch 100406
+ *
  * Revision 1.9  1994/06/30 18:22:10  mjl
  * All core source files: made another pass to eliminate warnings when using
  * gcc -Wall.  Lots of cleaning up: got rid of includes of math.h or string.h
@@ -27,14 +30,21 @@
  * void plhist()
  *
  * Draws a histogram of n values of a variable in array data[0..n-1] in
- * the range datmin to datmax using nbin bins. If "oldwin" is 1, the
+ * the range datmin to datmax using nbin bins. If "flags"'s first bit is 1, the
  * histogram is plotted in the current window. If not, the routine calls
  * "plenv" to set up the graphics environment.
+ * 
+ * If flags's second bit is set, then items which fall outside the bin
+ * range are ignored.
+ * 
+ * If flags's third bit is set, the outside bars are the same size
+ * as the rest.  The default old behaviour was for the first and last
+ * bars to expand visually to fill the entire available space.
 \*----------------------------------------------------------------------*/
 
 void
 c_plhist(PLINT n, PLFLT *data, PLFLT datmin, PLFLT datmax,
-	 PLINT nbin, PLINT oldwin)
+	 PLINT nbin, PLINT flags)
 {
     PLINT i, bin;
     PLFLT *x, *y, dx, ymax;
@@ -43,7 +53,7 @@ c_plhist(PLINT n, PLFLT *data, PLFLT datmin, PLFLT datmax,
 	plabort("plhist: Please call plinit first");
 	return;
     }
-    if (plsc->level < 3 && oldwin) {
+    if (plsc->level < 3 && (flags & 1)) {
 	plabort("plhist: Please set up window first");
 	return;
     }
@@ -69,20 +79,26 @@ c_plhist(PLINT n, PLFLT *data, PLFLT datmin, PLFLT datmax,
 
     for (i = 0; i < n; i++) {
 	bin = (data[i] - datmin) / dx;
+	if (flags & 2 == 0) {
 	bin = bin > 0 ? bin : 0;
 	bin = bin < nbin ? bin : nbin - 1;
 	y[bin]++;
+	} else {
+	    if(bin >= 0 && bin < nbin) {
+		y[bin]++;
+	    }
+	}
     }
 
-    if ( ! oldwin) {
+    if (!(flags & 1)) {
 	ymax = 0.0;
 	for (i = 0; i < nbin; i++)
 	    ymax = MAX(ymax, y[i]);
 
 	plenv(datmin, datmax, (PLFLT) 0.0, (PLFLT) (1.1 * ymax), 0, 0);
     }
-
-    plbin(nbin, x, y, 0);
+    /* We pass on the highest couple of bits to the 'plbin' routine */
+    plbin(nbin, x, y, (flags & (4+8+16+32)) >> 2);
     free((void *) x);
     free((void *) y);
 }
@@ -91,13 +107,15 @@ c_plhist(PLINT n, PLFLT *data, PLFLT datmin, PLFLT datmax,
  * void plbin()
  *
  * Plot a histogram using the arrays x and y to represent data values
- * and frequencies respectively. If center is false, x values denote the
- * lower edge of the bin, and if center is true, they denote the center
- * of the bin.
+ * and frequencies respectively. If flags first bit is false, x values 
+ * denote the lower edge of the bin, and if it is true, they denote 
+ * the center of the bin.  If flags second bit is true, then we assume 
+ * the edge bins are the same size as the rest (i.e. the edge bins 
+ * needn't go as far as the variables vpwxmi, vpwxma below).
 \*----------------------------------------------------------------------*/
 
 void
-c_plbin(PLINT nbin, PLFLT *x, PLFLT *y, PLINT center)
+c_plbin(PLINT nbin, PLFLT *x, PLFLT *y, PLINT flags)
 {
     PLINT i;
     PLFLT xmin, xmax, vpwxmi, vpwxma, vpwymi, vpwyma;
@@ -117,23 +135,40 @@ c_plbin(PLINT nbin, PLFLT *x, PLFLT *y, PLINT center)
     }
 
     plP_gvpw(&vpwxmi, &vpwxma, &vpwymi, &vpwyma);
-    if ( ! center) {
+    if (!(flags & 1)) {
 	for (i = 0; i < nbin - 1; i++) {
+	    if (!(flags & 4) || (y[i] != vpwymi)) {
 	    pljoin(x[i], vpwymi, x[i], y[i]);
 	    pljoin(x[i], y[i], x[i + 1], y[i]);
 	    pljoin(x[i + 1], y[i], x[i + 1], vpwymi);
 	}
-	if (x[nbin - 1] < vpwxma) {
-	    pljoin(x[nbin - 1], vpwymi, x[nbin - 1], y[nbin - 1]);
-	    pljoin(x[nbin - 1], y[nbin - 1], vpwxma, y[nbin - 1]);
-	    pljoin(vpwxma, y[nbin - 1], vpwxma, vpwymi);
+	}
+	if (flags & 2) {
+	    if (!(flags & 4) || (y[i] != vpwymi)) {
+		int xm = x[i] + (x[i] - x[i-1]);
+		pljoin(x[i], vpwymi, x[i], y[i]);
+		pljoin(x[i], y[i], xm, y[i]);
+		pljoin(xm, y[i], xm, vpwymi);
+	    }
+	} else {
+	    if (x[i] < vpwxma) {
+		if (!(flags & 4) || (y[i] != vpwymi)) {
+		    pljoin(x[i], vpwymi, x[i], y[i]);
+		    pljoin(x[i], y[i], vpwxma, y[i]);
+		    pljoin(vpwxma, y[i], vpwxma, vpwymi);
+		}
 	}
     }
-    else {
+    } else {
 	if (nbin < 2)
 	    return;
+	if (flags & 2) {
+	    xmin = MAX(vpwxmi, 0.5 * (3 * x[0] - x[1]));
+	} else {
 	xmin = vpwxmi;
-	xmax = MAX(0.5 * (x[0] + x[2]), vpwxmi);
+	}
+	/* Vince fixed bug May 1998 */
+	xmax = MAX(0.5 * (x[0] + x[1]), vpwxmi);
 	if (xmin < xmax) {
 	    pljoin(xmin, vpwymi, xmin, y[0]);
 	    pljoin(xmin, y[0], xmax, y[0]);
@@ -142,16 +177,25 @@ c_plbin(PLINT nbin, PLFLT *x, PLFLT *y, PLINT center)
 	for (i = 1; i < nbin - 1; i++) {
 	    xmin = xmax;
 	    xmax = MIN(0.5 * (x[i] + x[i + 1]), vpwxma);
+	    if (!(flags & 4) || (y[i] != vpwymi)) {
 	    pljoin(xmin, vpwymi, xmin, y[i]);
 	    pljoin(xmin, y[i], xmax, y[i]);
 	    pljoin(xmax, y[i], xmax, vpwymi);
 	}
+	}
 	xmin = xmax;
 	xmax = vpwxma;
+	if (flags & 2) {
+	    xmax = MIN(vpwxma, 0.5 * (3 * x[i] - x[i-1]));
+	} else {
+	    xmax = vpwxma;
+	}
 	if (xmin < xmax) {
-	    pljoin(xmin, vpwymi, xmin, y[nbin - 1]);
-	    pljoin(xmin, y[nbin - 1], xmax, y[nbin - 1]);
-	    pljoin(xmax, y[nbin - 1], xmax, vpwymi);
+	    if (!(flags & 4) || (y[i] != vpwymi)) {
+		pljoin(xmin, vpwymi, xmin, y[i]);
+		pljoin(xmin, y[i], xmax, y[i]);
+		pljoin(xmax, y[i], xmax, vpwymi);
+	    }
 	}
     }
 }
