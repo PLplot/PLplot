932604b6e7e2619e8234a15b8d751ea98e450619
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2001-02-12 17:14:30 +0000

    Script for saving/restoring CVS dirs.  Useful for making Debian packages.
    svn path=/branches/DEBIAN/; revision=2616

---
+++ debian/cvsdir.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# File: cvsdir.sh
+# Description: Save and restore CVS dirs under current dir.  This is
+#   useful for genrating a Debian package without the CVS
+#   administrative directories, but getting them restored later.
+# Author: Rafael Laboissière <rafael@icp.inpg.fr>
+# Created on: Mon Feb 12 18:06:37 CET 2001
+# Last modified on: Mon Feb 12 18:07:48 CET 2001
+# $Id$
+
+
+cvsdirs=`find . -type d -and -name CVS`
+cvstar=../cvs.tar
+
+case "$1" in
+  save) 
+    if [ -n "$cvsdirs" ] ; then 
+      tar cf $cvstar $cvsdirs
+      rm -rf $cvsdirs
+    else
+      echo "W: No CVS dirs under ." 1>&2
+    fi
+  ;;
+  restore)
+    if [ -f $cvstar ] ; then
+      tar xf $cvstar
+    else
+      echo "E: File $cvstar not found" 1>&2
+    fi
+  ;;
+  *)
+    cat 1>&2 <<EOF
+Usage $0 <save|restore>
+EOF
+  ;;
+esac
+		    
