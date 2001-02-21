6e0869b559525e828447e3ed5f499465e3aee858
Alan W. Irwin <airwin@users.sourceforge.net>
2001-02-21 18:53:00 +0000

    Replace "non-implemented" versions with working versions contributed by Dean Clamons Code 7420 Naval Research Lab Washington, DC 20375 202-767-2732
    with some important subsequent changes by Alan W. Irwin to eliminate
    a segfault, and debug other problems.
    
    svn path=/trunk/; revision=2660

--- examples/tcl/x10.tcl
+++ examples/tcl/x10.tcl
@@ -3,5 +3,17 @@
 #----------------------------------------------------------------------------
 
 proc x10 {{w loopback}} {
-puts "x10 not yet implemented"
+  $w cmd pladv 0
+# use color index 1 in cmap0.  Should be red unless cmap0 has been
+# fiddled with by another tcl example.
+  $w cmd plcol0 1
+  $w cmd pladv 0
+  $w cmd plvpor 0.0 1.0 0.0 1.0
+  $w cmd plwind 0.0 1.0 0.0 1.0
+  $w cmd plbox bc 0.0 0 bc 0.0 0
+  $w cmd plsvpa 50.0 150.0 100.0 150.0
+  $w cmd plwind 0.0 1.0 0.0 1.0
+  $w cmd plbox bc 0.0 0 bc 0.0 0
+  $w cmd plptex 0.5 0.5 1.0 0.0 0.5 "BOX at (50,150,100,150)"
+  $w cmd pleop
 }
