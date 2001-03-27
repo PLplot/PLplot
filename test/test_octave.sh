189501a710de14dc8d3a949d892f0743eef8f330
Alan W. Irwin <airwin@users.sourceforge.net>
2001-03-27 16:59:14 +0000

    Put in infrastructure for octave generation of files as part of test suite. For now, test_octave.sh is just a placeholder, but as soon as some bugs are fixed in octave file generation, octave will be contributing some significant demo results to the test examples.
    Also for plplot-test.sh.in the --tmp-test option is replaced by --flat
    (which is more descriptive of why there has to be such an option for the
    invocation of this script from tmp)
    
    svn path=/trunk/; revision=2927

---
+++ test/test_octave.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Test suite for octave demos.  
+# This is called from plplot-test.sh with $octavedir, $driver, and 
+# $dsuffix defined.
+# Currently this file is just a placeholder until the generation of
+# postscript files (at least, and hopefully the rest of the file drivers
+# as well) becomes more reliable under octave.
