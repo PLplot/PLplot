7ed7431d5b59a3463e8f34fb4635cc08efac2163
Joao Cardoso <jcard@users.sourceforge.net>
2001-04-01 01:36:15 +0000

    Support for octave testing. Checked with plmeta, ps and psc drivers. Alan, check it please.
    svn path=/trunk/; revision=2955

--- test/test_octave.sh
+++ test/test_octave.sh
@@ -2,6 +2,20 @@
 # Test suite for octave demos.  
 # This is called from plplot-test.sh with $octavedir, $driver, and 
 # $dsuffix defined.
-# Currently this file is just a placeholder until the generation of
-# postscript files (at least, and hopefully the rest of the file drivers
-# as well) becomes more reliable under octave.
+
+octave -f -q -p $octavedir//: <<EOF
+figure(1, "$driver", "octave.$dsuffix");
+p1
+p2
+p3
+p4
+p5
+p6
+#p7 # p7 works OK with plmeta, e.g., but not ps or psc. pleop/plbop issue?
+p8
+p9
+p13
+p15
+p16
+closefig
+EOF
