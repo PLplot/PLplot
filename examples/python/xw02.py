84ad3018a9f057a2d53e69635672aa067f8eef5a
Alan W. Irwin <airwin@users.sourceforge.net>
2001-04-08 19:39:02 +0000

    reorder function calls to do exactly what x02c.c does.  In particular drop extra plbop at end that was generating an extra empty file for the png driver in familying mode.  Also use same plwid argument as in the C example and move two commands out of inner loop.  Result: no extra empty file in familying mode, exact agreement between xw02.py and x02c for the psc driver.
    svn path=/trunk/; revision=2993

--- examples/python/xw02.py
+++ examples/python/xw02.py
@@ -20,43 +20,34 @@ from pl import *
 
 def main():
 
-    # Parse and process command line arguments
+    # Divide screen into 16 regions
+    plssub(4, 4)
 
+    # Parse and process command line arguments
     plParseOpts(sys.argv, PARSE_FULL)
 
     # Initialize plplot
-
     plinit()
-    # Divide screen into 16 regions
-
-    plssub(4, 4)
 
     plschr(0.0, 3.5)
     plfont(4)
 
     for i in range(16):
-	plcol(i)
+	plcol0(i)
 	pladv(0)
 	vmin = 0.1
 	vmax = 0.9
 	for j in range(3):
-	    plwid(j + 3)
+	    plwid(j + 1)
 	    plvpor(vmin, vmax, vmin, vmax)
 	    plwind(0.0, 1.0, 0.0, 1.0)
 	    plbox("bc", 0.0, 0, "bc", 0.0, 0)
 	    vmin = vmin + 0.1
 	    vmax = vmax - 0.1
-	    plwid(1)
-	    plptex(0.5, 0.5, 1.0, 0.0, 0.5, `i`)
+	plwid(1)
+	plptex(0.5, 0.5, 1.0, 0.0, 0.5, `i`)
 
     # Now finish up.
-
-    pleop()
-
-    # And revert the screen to one plot per page.
-
-    plssub(1,1)
-
     plend()
 
 main()
