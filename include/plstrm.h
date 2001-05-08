a1588c9808dac384a22ebf71984eb2f5b9303e1a
Geoffrey Furnish <furnish@users.sourceforge.net>
2001-05-08 20:48:53 +0000

    Add a dispatch_table member to PLStream.  Now the PLplot API functions can just dereference the function pointers in the dispatch_table entry for the stream itself, without getting involved with indexing into the global dispatch_table monstrosity.  This paves the way for expunging the global dispatch_table altogether, if we decide to do that.
    svn path=/branches/dyndrv_1/; revision=3091

--- include/plstrm.h
+++ include/plstrm.h
@@ -9,6 +9,8 @@
 #ifndef __PLSTRM_H__
 #define __PLSTRM_H__
 
+#include "plplot/dispatch.h"
+
 /*--------------------------------------------------------------------------*\
  * Define the PLDev data structure.
  *
@@ -407,6 +409,10 @@ typedef struct {
 
     PLINT width;
     PLINT widthset, widthlock;
+
+/* Driver dispatch table, obsoletes "device" member below. */
+
+    PLDispatchTable *dispatch_table;
 
 /* Variables used for interacting with or by device driver */
 
