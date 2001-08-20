98eb39296be42f3232e6e13076a4376c8c40333b
Geoffrey Furnish <furnish@users.sourceforge.net>
2001-08-20 20:01:23 +0000

    Beginnings of a Java class to encapsulate a PLplot stream.
    svn path=/trunk/; revision=3162

---
+++ bindings/java/PLStream.java
@@ -0,0 +1,36 @@
+//---------------------------------------------------------------------------//
+// $Id$
+//
+// This file implements the core Java side of the Java->PLplot interface, by
+// providing a Java class PLStream, which is intended to mirror the C
+// PLStream of PLplot.  However, we go further here in Java than we were able
+// to do in C, by actually making the PLplot API entry points methods of this
+// stream class.  Thus, to plot to a stream in Java, you just instantiate the
+// stream, and invoke PLplot API services upon it.  This is basically what we
+// have tried to do in the bindings to other object oriented environments,
+// such as C++ and the Tcl/Tk and Python/Tk widget contexts.  A stream
+// represents sort of a "drawable" abstraction, and the PLplot API gets the
+// drawing done on the identified stream.  In C you have to swtich the stream
+// explicitly with the plsstrm function, but in Java you just invoke the API
+// function on the approrpiate stream object.
+//---------------------------------------------------------------------------//
+
+package plplot.core;
+
+public class PLStream {
+
+    static {
+        System.loadLibrary( "plplot" );
+    }
+
+    public PLStream() {
+        System.out.println( "PLStream constructed." );
+    }
+
+    public void xyz() { System.out.println( "In xyz()" ); }
+}
+
+//---------------------------------------------------------------------------//
+//                              End of PLStream.java
+//---------------------------------------------------------------------------//
+
