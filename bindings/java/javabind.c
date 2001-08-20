01129daaad301e6d9880207a3e808236fbb611cd
Geoffrey Furnish <furnish@users.sourceforge.net>
2001-08-20 23:43:48 +0000

    Implement native wrappers for several commonly used PLplot API entry points.  These are currently quite naive, and do no error checking, but it is enough to obtain initial-grade visual gratification from the Java binding effort.
    Things to do:
    1) Add gobs of remaining API entry points.
    2) Beef up error analysis to check for propper array lenghts, etc.
       Basically do argument validity checking.
    3) Do something about float/double conversions.
    
    svn path=/trunk/; revision=3168

--- bindings/java/javabind.c
+++ bindings/java/javabind.c
@@ -1,3 +1,117 @@
 #include <jni.h>
 
-static void jdummy() {}
+#include "plplot/plplot.h"
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    col0
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_col0( JNIEnv *env, jobject jthis, jint icol )
+{
+    plcol0(icol);
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    end
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_end( JNIEnv *env, jobject jthis )
+{
+    plend();
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    env
+ * Signature: (FFFFII)V
+ */
+
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_env( JNIEnv *env, jobject jthis,
+                               jfloat xmin, jfloat xmax,
+                               jfloat ymin, jfloat ymax,
+                               jint just, jint axis )
+{
+    plenv( xmin, xmax, ymin, ymax, just, axis );
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    fontld
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_fontld( JNIEnv *env, jobject jthis, jint fnt )
+{
+    plfontld(fnt);
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    init
+ * Signature: ()V
+ */
+
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_init( JNIEnv *env, jobject jthis )
+{
+    plinit();
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    lab
+ * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V
+ */
+
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_lab( JNIEnv *env, jobject jthis,
+                               jstring jxlab, jstring jylab, jstring jtlab )
+{
+    const char *xlab = (*env)->GetStringUTFChars( env, jxlab, 0 );
+    const char *ylab = (*env)->GetStringUTFChars( env, jylab, 0 );
+    const char *tlab = (*env)->GetStringUTFChars( env, jtlab, 0 );
+
+    pllab( xlab, ylab, tlab );
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    line
+ * Signature: (I[F[F)V
+ */
+
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_line( JNIEnv *env, jobject jthis,
+                                jint n, jfloatArray jx, jfloatArray jy )
+{
+    jsize len = (*env)->GetArrayLength( env, jx );
+    jfloat *x = (*env)->GetFloatArrayElements( env, jx, 0 );
+    jfloat *y = (*env)->GetFloatArrayElements( env, jy, 0 );
+
+    plline( n, x, y );
+}
+
+/*
+ * Class:     plplot_0002fcore_0002fPLStream
+ * Method:    poin
+ * Signature: (I[F[FI)V
+ */
+
+JNIEXPORT void JNICALL
+Java_plplot_core_PLStream_poin( JNIEnv *env, jobject jthis,
+                                jint n, jfloatArray jx, jfloatArray jy,
+                                jint code )
+{
+    jsize len = (*env)->GetArrayLength( env, jx );
+    jfloat *x = (*env)->GetFloatArrayElements( env, jx, 0 );
+    jfloat *y = (*env)->GetFloatArrayElements( env, jy, 0 );
+
+    plpoin( n, x, y, code );
+}
+
+
