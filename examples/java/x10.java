54c678476d0f02666da64a20a7672f1cef93e643
Geoffrey Furnish <furnish@users.sourceforge.net>
2001-08-28 17:27:13 +0000

    Implemented.
    svn path=/trunk/; revision=3192

--- examples/java/x10.java
+++ examples/java/x10.java
@@ -15,6 +15,32 @@ class x10 {
     public static void main( String[] args ) 
     {
         System.out.println( "x10 not implemented yet." );
+
+        x10 x = new x10( args );
+    }
+
+    public x10( String[] args )
+    {
+        PLStream pls = new PLStream();
+
+    // Parse and process command line arguments.
+
+        pls.ParseOpts( args, pls.PL_PARSE_FULL );
+
+    // Initialize plplot.
+
+        pls.init();
+
+        pls.adv(0);
+        pls.vpor(0.0, 1.0, 0.0, 1.0);
+        pls.wind(0.0, 1.0, 0.0, 1.0);
+        pls.box("bc", 0.0, 0, "bc", 0.0, 0);
+
+        pls.svpa(50.0, 150.0, 50.0, 100.0);
+        pls.wind(0.0, 1.0, 0.0, 1.0);
+        pls.box("bc", 0.0, 0, "bc", 0.0, 0);
+        pls.ptex(0.5, 0.5, 1.0, 0.0, 0.5, "BOX at (50,150,50,100)");
+        pls.end();
     }
 }
 
