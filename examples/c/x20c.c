81bd95d62e21844f28d15a27db495c7a56b676c0
Joao Cardoso <jcard@users.sourceforge.net>
2001-11-27 23:54:32 +0000

    Add an all zeros, except at the border elements, non-square matrix image, to help detect bugs. Correct a bug while reading the image. Correctly read and transpose a non-square image. Change M_PI for PI. Change the image reading mode to binary.
    svn path=/trunk/; revision=3329

--- examples/c/x20c.c
+++ examples/c/x20c.c
@@ -24,11 +24,11 @@ main(int argc, char *argv[])
   FILE *fp;
   int width, height, num_col;
   char ver[80];
-  char *img;
+  unsigned char *img;
   PLFLT *img_f;
 
   PLINT n_col = 255;
-  PLINT rr[256], gg[256], bb[256];
+  PLINT rr[n_col], gg[n_col], bb[n_col];
 
 /* Parse and process command line arguments */
 
@@ -37,13 +37,30 @@ main(int argc, char *argv[])
 /* Initialize plplot */
 
   plinit();
-  plcol0(2); /* draw a yellow border box, useful for diagnostics! */
+  plenv(-1., 1., -1., 1., 1, -2); /* no plot box */
+
+  /* build a square -- diagnostics */
+  for (i=0; i<XDIM; i++)
+    z[i*YDIM+(YDIM-1)] = 1.; /* right */
+  for (i=0; i<XDIM; i++)
+    z[i*YDIM] = 1.; /* left */
+  
+  for (i=0; i<YDIM; i++)
+    z[i] = 1.; /* top */
+  for (i=0; i<YDIM; i++)
+    z[(XDIM-1)*YDIM+i] = 1.; /* botton */
+
+  plimage(z, XDIM, YDIM, -1., 1., -1., 1., -1., 1., -1., 1.);
+  pllab(" "," ","Should appear a red Square");
+  pladv(0);
+
+  plcol0(2); /* draw a yellow plot box, useful for diagnostics! :( */
   plenv(-1., 1., -1., 1., 1, -1);
 
   for (i=0; i<XDIM; i++)
-    x[i] = cos(i*4.*M_PI/XDIM);
+    x[i] = cos(i*4.*PI/XDIM);
   for (i=0; i<YDIM; i++)
-    y[i] = sin(i*4.*M_PI/YDIM);
+    y[i] = sin(i*4.*PI/YDIM);
 
   for (i=0; i<XDIM; i++)
     for (j=0; j<YDIM; j++)
@@ -53,37 +70,36 @@ main(int argc, char *argv[])
   pladv(0);
 
   /* set gray colormap */
-  for (i=0; i<n_col; i++)
+  for (i=0; i<=n_col; i++)
     rr[i] = gg[i] = bb[i] = i;
 
   plscmap1(rr, gg, bb, n_col);
 
-  plimage(z, XDIM, YDIM, -1., 1., -1., 1., -1., 1., -1., 1.);
-  pladv(0);
-
   /* naive grayscale binary ppm reading. If you know how to, improve it */
-  fp = fopen("lena.pgm","r");
+  fp = fopen("lena.pgm","rb");
   fscanf(fp,"%s\n", ver); /* version */
-  printf("version: %s\n", ver);
-  i = fgetc(fp);
-  ungetc(i, fp);
-  if (i == '#') {
-    fgets(ver, 80, fp); /* comments */
-    printf("%s", ver);
-  }
-  fscanf(fp,"%d%d%d", &width, &height, &num_col); /* width, height num colors */
-  printf("width=%d height=%d num_color=%d\n", width, height, num_col);
+  /* printf("version: %s\n", ver);*/
 
-  img = (char *) malloc(width*height*sizeof(char));
+  while((i=fgetc(fp)) == '#') {
+    fgets(ver, 80, fp); /* comments */
+    /* printf("%s", ver); */
+  }
+  ungetc(i, fp);
+
+  fscanf(fp,"%d%d%d", &width, &height, &num_col); /* width, height num colors */
+  printf("width=%d height=%d\n", width, height);
+
+  img = (unsigned char *) malloc(width*height*sizeof(char));
   img_f = (PLFLT *) malloc(width*height*sizeof(PLFLT));
 
   fread(img, sizeof(char), width*height, fp);
   fclose(fp);
 
-  for (i=0; i<width*height; i++)
-    img_f[i] = 128. - img[i]; /* convert to float and complement ?! */
+  for (i=0; i<width; i++)
+    for (j=0; j<height; j++)
+      img_f[i*height+j] = img[(height-j)*width+i];
 
-  plimage(img_f, width, height, -1., 1., -1., 1., -1, 1., -1., 1.);
+  plimage(img_f, width, height, -1., 1., -1., 1., -1., 1., -1., 1.);
 
   plend();
   exit(0);
