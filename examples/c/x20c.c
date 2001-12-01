360a4827a31e2f207b65b01e1d43b0ce5c6621f0
Joao Cardoso <jcard@users.sourceforge.net>
2001-12-01 01:45:13 +0000

    Remove declaration of plimage, as it is now in plplot.h
    Show the relationship between the xmin, max, ymin, ymax arguments of
    plimage() and the similar arguments of plenv(), as well as the
    
    Also show who to use the Dxmin, Dmax, Dymin, Dymax arguments of
    plimage().
    
    Conditionally enter plxormod() to interatively select and zoom the
    image.
    
    Kind of (!) solves the interation between pladv() and GetCursorCmd().
    This is still a problem! Please try to solve it!
    
    svn path=/trunk/; revision=3346

--- examples/c/x20c.c
+++ examples/c/x20c.c
@@ -10,16 +10,11 @@
 #define XDIM 200
 #define YDIM 300
 
-void
-plimage( PLFLT **data, PLINT nx, PLINT ny, 
-	 PLFLT xmin, PLFLT xmax, PLFLT ymin, PLFLT ymax ,
-	 PLFLT Dxmin, PLFLT Dxmax, PLFLT Dymin, PLFLT Dymax);
-
 int
 main(int argc, char *argv[])
 {
   PLFLT x[XDIM], y[YDIM], **z;
-  int i, j;
+  int i, j, st;
   static PLGraphicsIn gin;
   PLFLT xi, yi, xe, ye, start;
     
@@ -32,14 +27,14 @@ main(int argc, char *argv[])
   PLINT n_col = 255;
   PLINT rr[n_col], gg[n_col], bb[n_col];
 
-/* Parse and process command line arguments */
+  /* Parse and process command line arguments */
 
   plParseOpts(&argc, argv, PL_PARSE_FULL);
 
-/* Initialize plplot */
+  /* Initialize plplot */
 
   plinit();
-  plenv(-1., 1., -1., 1., 1, -2); /* no plot box */
+  plenv(1., (PLFLT) XDIM, 1., (PLFLT) XDIM, 1, -2); /* no plot box */
 
   plAlloc2dGrid(&z, XDIM, YDIM);
 
@@ -57,12 +52,14 @@ main(int argc, char *argv[])
   /*
     for (i=0; i<XDIM; i++) {
     for (j=0; j<YDIM; j++)
-      printf("%d ", (int) z[i][j]);
+    printf("%d ", (int) z[i][j]);
     printf("\n");}
   */
 
   pllab("...around a blue square."," ","Should appear a red border...");
-  plimage(z, XDIM, YDIM, -1., 1., -1., 1., -1., 1., -1., 1.);
+  plimage(z, XDIM, YDIM,
+	  1., (PLFLT) XDIM, 1., (PLFLT) XDIM,
+	  1., (PLFLT) XDIM, 1., (PLFLT) XDIM);
   pladv(0);
 
   plcol0(2); /* draw a yellow plot box, useful for diagnostics! :( */
@@ -116,62 +113,81 @@ main(int argc, char *argv[])
   pllab("Set and drag Button 1 to (re)set selection, Button 3 to finish."," ","Lena...");
   plimage(img_f, width, height, 1., width, 1., height, 1., width, 1., height);
 
-  plxormod(1); /* enter xor mode to draw a selection rectangle */
-  start = 0;
-  xi = ye = 0.;
-  xe = width/2.; yi = height/2.;
+  /* the code bellow should only be executed by an interative device.
+     How to get this info? */
 
-  while(1) {
-    PLFLT sx[5], sy[5];
+  plxormod(1, &st); /* enter xor mode to draw a selection rectangle */
+  if (st) {
+    start = 0;
+    xi = ye = 0.;
+    xe = width/2.; yi = height/2.;
 
-    plGetCursor(&gin);
 
-    if (gin.button == 1) {
-      xi = gin.wX; yi = gin.wY;
-      if (start)
-	plline(5, sx, sy); /* clear previous rectangle */
-      start = 0;
+    while(1) {
+      PLFLT sx[5], sy[5];
 
-      sx[0] = xi; sy[0] = yi;
-      sx[4] = xi; sy[4] = yi;
-    }
+      plGetCursor(&gin);
 
-    if (gin.state == 0x100) {
-      xe = gin.wX; ye = gin.wY;
-      if (start)
-	plline(5, sx, sy); /* clear previous rectangle */
-      start = 1;
+      if (gin.button == 1) {
+	xi = gin.wX; yi = gin.wY;
+	if (start)
+	  plline(5, sx, sy); /* clear previous rectangle */
+	start = 0;
+
+	sx[0] = xi; sy[0] = yi;
+	sx[4] = xi; sy[4] = yi;
+      }
+
+      if (gin.state == 0x100) {
+	xe = gin.wX; ye = gin.wY;
+	if (start)
+	  plline(5, sx, sy); /* clear previous rectangle */
+	start = 1;
   
-      sx[2] = xe; sy[2] = ye;
-      sx[1] = xe; sy[1] = yi;
-      sx[3] = xi; sy[3] = ye;
-      plline(5, sx, sy); /* draw new rectangle */
-    }
+	sx[2] = xe; sy[2] = ye;
+	sx[1] = xe; sy[1] = yi;
+	sx[3] = xi; sy[3] = ye;
+	plline(5, sx, sy); /* draw new rectangle */
+      }
 
-    if (gin.button == 3) {
-      if (start)
-	plline(5, sx, sy); /* clear previous rectangle */
-      printf("end\n");
-      break;
-    }      
+      if (gin.button == 3) {
+	if (start)
+	  plline(5, sx, sy); /* clear previous rectangle */
+	break;
+      }      
+    }
+    plxormod(0, &st); /* leave xor mod */
+  
+    /* 
+       I'm unable to continue, clearing the plot and advancing to the next
+       one, without hiting the enter key, or pressing the button... help!
+
+       Forcing the xwin driver to leave locate mode and destroying the
+       xhairs (in GetCursorCmd()) solves some problems, but I still have
+       to press the enter key or press Button-3 to go to next plot, even
+       if a pladv() is not present!  Using plbop() solves the problem, but
+       it should'nt be needed! 
+    */
+
+    /*  plbop(); */
+
+    /* this also works, plspause(0), pladv(0), plspause(1),
+       but the above question remains.
+       With this approach, the previous pause state is lost,
+       as there is no API call to get it. */
+    plspause(0);
+    pladv(0);
+    plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
+    plspause(1);
+    pladv(0);
+ 
+    plenv(xi, xe, ye, yi, 1, -1);
+    plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
+    pladv(0);
   }
-  plxormod(0); /* leave xor mod */
-
-  /* I'm unable to continue without hiting the enter key, or pressing the button... help!*/
-  //plclear();plflush();
-  pladv(0);
-  
-  plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
-  pladv(0);
-
-  plenv(xi, xe, ye, yi, 1, -1);
-  plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
-  pladv(0);
-
-  plend();
-
   plFree2dGrid(img_f, width, height);
   free(img);
 
+  plend();
   exit(0);
 }
