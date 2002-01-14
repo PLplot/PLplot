75a96710b9c2e4c9b52552973d08dde55b40a48c
Alan W. Irwin <airwin@users.sourceforge.net>
2002-01-14 20:40:01 +0000

    Make this example more consistent with x15c.
    Some rearrangement of code plus actual changes in definition of x and y
    to make them consistent with x15c.
    
    Results were identical to results from x15c, if first pattern of second page
    was dropped from both.  For this pattern there were some small differences
    that could be seen visually between the two results.  I have no idea what
    the source of this inconsistency may be, but it seems to lie with the
    x15.tcl code or the tcl implementation (perhaps the matrix implementation,
    Maurice?).  When I do similar comparisons between the C and yplot results,
    the two 15th examples are identical.
    
    svn path=/trunk/; revision=3448

--- examples/tcl/x15.tcl
+++ examples/tcl/x15.tcl
@@ -6,17 +6,16 @@ proc x15 {{w loopback}} {
   set xpts 35
   set ypts 46
   matrix z f $xpts $ypts
-
-#  $w cmd plscmap0n 3
-#  This changing of cmap2 actually works, but it is not actually used, and it 
-#  unnecessarily changes other tcl and tk examples which *do* use cmap1.
-#  Thus, comment it out.
-#  cmap1_init2 $w
+  # Comment out set up of cmap0 with limited number of colours
+  # $w cmd plscmap0n 3
+  # Comment out set up of cmap1 since actually cmap0 is used, and to
+  # fiddle with this affects other tcl plots bundled with this example.
+  # cmap1_init152 $w
 
   for {set i 0} {$i < $xpts} {incr i} {
-    set x [expr ($i - ($xpts/2.)) / ($xpts/2.)]
+    set x [expr ($i - ($xpts/2)) / double($xpts/2)]
     for {set j 0} {$j < $ypts} {incr j} {
-      set y [expr ($j - .5 * $ypts) / (.5 * $ypts) - 1.]
+      set y [expr ($j - double($ypts/2)) / double($ypts/2) - 1.]
 
       z $i $j = [expr $x*$x - $y*$y + ($x - $y)/($x * $x + $y * $y + .1) ]
     }
@@ -34,7 +33,7 @@ proc x15 {{w loopback}} {
   
 }
 
-proc  cmap1_init2 { w } {
+proc  cmap1_init152 { w } {
 
   matrix i f 4
   matrix h f 4
@@ -70,24 +69,26 @@ proc  cmap1_init2 { w } {
 }
 proc plot151 { w z zmin zmax } {
 
-  $w cmd pladv 0
-  $w cmd plvpor .1 .9 .1 .9
-  $w cmd plwind -1.0 1.0 -1.0 1.0
-
-  set shade_min [expr $zmin + ($zmax - $zmin)*.4 ]
-  set shade_max [expr $zmin + ($zmax - $zmin)*.6 ]
-  set sh_color 7
   set sh_width 2
   set min_color 9
   set max_color 2
   set min_width 2
   set max_width 2
+  # N.B. this flag set to use cmap0
   set sh_cmap 0
+
+  set shade_min [expr $zmin + ($zmax - $zmin)*.4 ]
+  set shade_max [expr $zmin + ($zmax - $zmin)*.6 ]
+  set sh_color 7
+
+  $w cmd pladv 0
+  $w cmd plvpor .1 .9 .1 .9
+  $w cmd plwind -1.0 1.0 -1.0 1.0
 
   $w cmd plpsty 8
   $w cmd plshade z -1. 1. -1. 1. $shade_min $shade_max \
-                  $sh_cmap $sh_color $sh_width \
-                  $min_color $min_width $max_color $max_width 1
+    $sh_cmap $sh_color $sh_width \
+    $min_color $min_width $max_color $max_width 1
   $w cmd plcol0 1
   $w cmd plbox bcnst 0.0 0 bcnstv 0.0 0
   $w cmd plcol0 2
@@ -96,11 +97,13 @@ proc plot151 { w z zmin zmax } {
 
 proc plot152 { w z zmin zmax } {
 
-  set sh_cmap 0
+  set sh_width 2
   set min_color 0
-  set min_width 0
   set max_color 0
+  set min_width 0
   set max_width 0
+  # N.B. this flag set to use cmap0
+  set sh_cmap 0
 
   $w cmd pladv 0
   $w cmd plvpor .1 .9 .1 .9
@@ -109,15 +112,13 @@ proc plot152 { w z zmin zmax } {
     set shade_min [expr $zmin + ($zmax - $zmin)*$i/10.0 ]
     set shade_max [expr $zmin + ($zmax - $zmin)*($i + 1)/10.0 ]
     set sh_color [expr $i + 6]
-    set sh_width 2
     $w cmd plpsty [expr ($i + 2) % 8 + 1]
     $w cmd plshade z -1. 1. -1. 1. $shade_min $shade_max \
-                    $sh_cmap $sh_color $sh_width \
-                    $min_color $min_width $max_color $max_width 1
-
-    $w cmd plcol0 1
-    $w cmd plbox bcnst 0.0 0 bcnstv 0.0 0
-    $w cmd plcol0 2
-    $w cmd pllab "distance" "altitude" "Bogon flux"
+      $sh_cmap $sh_color $sh_width \
+      $min_color $min_width $max_color $max_width 1
   }
+  $w cmd plcol0 1
+  $w cmd plbox bcnst 0.0 0 bcnstv 0.0 0
+  $w cmd plcol0 2
+  $w cmd pllab "distance" "altitude" "Bogon flux"
 }
