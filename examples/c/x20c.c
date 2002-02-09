461ac453c73586c648c4449d43c4857f8622f20e
Joao Cardoso <jcard@users.sourceforge.net>
2002-02-09 03:12:55 +0000

    Code reorganization.
    svn path=/trunk/; revision=3581

--- examples/c/x20c.c
+++ examples/c/x20c.c
@@ -11,35 +11,27 @@
 #define XDIM 160
 #define YDIM 120
 
+void save_plot(void);
+void gray_cmap(void);
+int read_img(char *, PLFLT ***, int *, int *);
+int get_clip(PLFLT *, PLFLT *, PLFLT *, PLFLT *);
+
 int
 main(int argc, char *argv[])
 {
   PLFLT x[XDIM], y[YDIM], **z, **r;
-  int i, j, st;
-  static PLGraphicsIn gin;
-  PLFLT xi, yi, xe, ye, start;
-    
-  FILE *fp;
-  int width, height, num_col;
-  char ver[80];
-  unsigned char *img;
+  PLFLT xi, yi, xe, ye; 
+  int i, j, width, height;
   PLFLT **img_f;
-
-  PLINT n_col = 255;
-  PLINT rr[255], gg[255], bb[255];
 
   /*
     Bugs in plimage():
-     -if the plot window is not totally visible, plimage() fails!
-       (creating an image instead of retrieving the existing one will
-        obliterate any plot already made, so this is no solution)
      -at high magnifications, the left and right edge are ragged, try
         ./x20c -dev xwin -wplt 0.3,0.3,0.6,0.6 -ori 0.5
      
     Bugs in x20c.c:
      -if the window is resized after a selection is made on "lena", when
       making a new selection the old one will re-appear.
-     -sometimes the selected area will be twisted after the selection finishes!?!
   */
 
   /* Parse and process command line arguments */
@@ -51,12 +43,19 @@ main(int argc, char *argv[])
   plinit();
 
   plAlloc2dGrid(&z, XDIM, YDIM);
-  plAlloc2dGrid(&r, XDIM, YDIM);
 
-  if (0) { /* view image border pixels */
-    plenv(1., (PLFLT) XDIM, 1., (PLFLT) YDIM, 1, -2); /* no plot box */
+  if (1) { /* view image border pixels */
+    plenv(1., (PLFLT) XDIM, 1., (PLFLT) YDIM, 1, 1); /* no plot box */
 
-    /* build a square -- diagnostics */
+    /* more diags */
+    /*
+    x[0]=-XDIM/2; y[0]=0;
+    x[1]=XDIM/2; y[1]=0;
+    x[2]=XDIM/2; y[2]=YDIM/2;
+    plfill(3,x,y);
+    */
+    
+    /* build a one pixel square border, for diagnostics */
     for (i=0; i<XDIM; i++)
       z[i][YDIM-1] = 1.; /* right */
     for (i=0; i<XDIM; i++)
@@ -68,42 +67,120 @@ main(int argc, char *argv[])
       z[XDIM-1][i] = 1.; /* botton */
 
     pllab("...around a blue square."," ","A red border should appear...");
+
     plimage(z, XDIM, YDIM,
 	    1., (PLFLT) XDIM, 1., (PLFLT) YDIM,
 	    1., (PLFLT) XDIM, 1., (PLFLT) YDIM);
+
+    /* show how to save a plot */
+    save_plot();
+
+    pladv(0);
+  }
+  /* plend();exit(1); */
+
+  if (1) { /* sombrero-like demo */
+    plAlloc2dGrid(&r, XDIM, YDIM);
+    plcol0(2); /* draw a yellow plot box, useful for diagnostics! :( */
+    plenv(0., 2.*PI, 0, 3.*PI, 1, 0);
+
+    for (i=0; i<XDIM; i++)
+      x[i] = i*2.*PI/(XDIM-1);
+    for (i=0; i<YDIM; i++)
+      y[i] = i*3.*PI/(YDIM-1);
+
+    for (i=0; i<XDIM; i++)
+      for (j=0; j<YDIM; j++) {
+	r[i][j] = sqrt(x[i]*x[i]+y[j]*y[j])+1e-3;
+	z[i][j] = sin(r[i][j]) / (r[i][j]);
+      }
+
+    plimage(z, XDIM, YDIM, 0., 2.*PI, 0, 3.*PI, 0., 2.*PI, 0, 3.*PI); 
+    plFree2dGrid(r, XDIM, YDIM);
     pladv(0);
   }
 
-  plcol0(2); /* draw a yellow plot box, useful for diagnostics! :( */
-  plenv(0., 2.*PI, 0, 3.*PI, 1, 0);
-
-  for (i=0; i<XDIM; i++)
-    x[i] = i*2.*PI/(XDIM-1);
-  for (i=0; i<YDIM; i++)
-    y[i] = i*3.*PI/(YDIM-1);
-
-  for (i=0; i<XDIM; i++)
-    for (j=0; j<YDIM; j++) {
-      r[i][j] = sqrt(x[i]*x[i]+y[j]*y[j])+1e-3;
-      z[i][j] = sin(r[i][j]) / (r[i][j]);
-    }
-
-  plimage(z, XDIM, YDIM, 0., 2.*PI, 0, 3.*PI, 0., 2.*PI, 0, 3.*PI); 
-
-  pladv(0);
   plFree2dGrid(z, XDIM, YDIM);
-  plFree2dGrid(r, XDIM, YDIM);
 
   /* set gray colormap */
-  for (i=0; i<=n_col; i++)
-    rr[i] = gg[i] = bb[i] = i;
+  gray_cmap();
 
-  plscmap1(rr, gg, bb, n_col);
+  /* read image */
+  if (read_img("lena.pgm", &img_f, &width, &height)) {
+    plabort("No such file");
+    plend();
+    exit(1);
+  }
+
+  plenv(1., width, 1., height, 1, -1);
+  pllab("Set and drag Button 1 to (re)set selection, Button 2 to finish."," ","Lena...");
+  plimage(img_f, width, height, 1., width, 1., height, 1., width, 1., height);
+
+  if (1) { /* selection/expansion demo */
+    xi = 200.; xe = 330.;
+    yi = 280.; ye = 220.;
+
+    if (get_clip(&xi, &xe, &yi, &ye)) { /* get selection rectangle */
+      plend();
+      exit(0);
+    }
+  
+    /* 
+       I'm unable to continue, clearing the plot and advancing to the next
+       one, without hiting the enter key, or pressing the button... help!
+
+       Forcing the xwin driver to leave locate mode and destroying the
+       xhairs (in GetCursorCmd()) solves some problems, but I still have
+       to press the enter key or press Button-2 to go to next plot, even
+       if a pladv() is not present!  Using plbop() solves the problem, but
+       it shouldn't be needed! 
+    */
+
+    /* plbop(); */
+
+    /* 
+       plspause(0), pladv(0), plspause(1), also works,
+       but the above question remains.
+       With this approach, the previous pause state is lost,
+       as there is no API call to get its current state.
+    */
+
+    plspause(0);
+    pladv(0);
+
+    plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
+
+    plspause(1);
+    pladv(0);
+
+    plenv(xi, xe, ye, yi, 1, -1);
+    plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
+    pladv(0);
+  }
+
+  plFree2dGrid(img_f, width, height);
+
+  plend();
+  exit(0);
+}
+
+int read_img(char *fname, PLFLT ***img_f, int *width, int *height)
+{
+  FILE *fp;
+  unsigned char *img;
+  char ver[80];
+  int num_col, i, j, w, h;
+  PLFLT **imf;
 
   /* naive grayscale binary ppm reading. If you know how to, improve it */
-  fp = fopen("lena.pgm","rb");
+  if ((fp = fopen(fname,"rb")) == NULL)
+    return 1;
+
   fscanf(fp,"%s\n", ver); /* version */
   /* printf("version: %s\n", ver);*/
+
+  if (strcmp(ver, "P5")) /* I only understand this! */
+    return 1;
 
   while((i=fgetc(fp)) == '#') {
     fgets(ver, 80, fp); /* comments */
@@ -111,30 +188,53 @@ main(int argc, char *argv[])
   }
   ungetc(i, fp);
 
-  fscanf(fp,"%d%d%d", &width, &height, &num_col); /* width, height num colors */
-  /* printf("width=%d height=%d\n", width, height); */
+  fscanf(fp,"%d%d%d", &w, &h, &num_col); /* width, height num colors */
+  /* printf("width=%d height=%d\n", w, h); */
 
-  img = (unsigned char *) malloc(width*height*sizeof(char));
-  plAlloc2dGrid(&img_f, width, height);
+  img = (unsigned char *) malloc(w*h*sizeof(char));
+  plAlloc2dGrid(&imf, w, h);
 
-  fread(img, sizeof(char), width*height, fp);
+  fread(img, sizeof(char), w*h, fp);
   fclose(fp);
 
-  for (i=0; i<width; i++)
-    for (j=0; j<height; j++)
-      img_f[i][j] = img[(height-j)*width+i];
+  for (i=0; i<w; i++)
+    for (j=0; j<h; j++)
+      imf[i][j] = img[(h-j)*w+i];
 
-  plenv(1., width, 1., height, 1, -1);
-  pllab("Set and drag Button 1 to (re)set selection, Button 2 to finish."," ","Lena...");
-  plimage(img_f, width, height, 1., width, 1., height, 1., width, 1., height);
+  free(img);
 
-  xi = 200.; xe = 330.;
-  yi = 280.; ye = 220.;
+  *width = w;
+  *height = h;
+  *img_f = imf;
+  return 0;
+}
+
+void save_plot()   /* save plot */
+{
+  int cur_strm, new_strm; 
+
+  plgstrm(&cur_strm); /* get current stream */
+  plmkstrm(&new_strm); /* create a new one */ 
+    
+  plsfnam("foo.ps"); /* file name */
+  plsdev("psc"); /* device type */
+
+  plcpstrm(cur_strm, 0); /* copy old stream parameters to new stream */
+  plreplot();	/* do the save */
+  plend1();
+
+  plsstrm(cur_strm);	/* return to previous stream */
+}
+
+int get_clip(PLFLT *xi, PLFLT *xe, PLFLT *yi, PLFLT *ye)
+{
+  PLGraphicsIn gin;
+  PLFLT xxi=*xi, yyi=*yi, xxe=*xe, yye=*ye, t;
+  int st, start = 0;
 
   plxormod(1, &st); /* enter xor mode to draw a selection rectangle */
-  if (st) { /* driver has xormod capability */
-    start = 0;
 
+  if (st) { /* driver has xormod capability */
     while(1) {
       PLFLT sx[5], sy[5];
 
@@ -143,26 +243,26 @@ main(int argc, char *argv[])
       plxormod(1, &st);
 
       if (gin.button == 1) {
-	xi = gin.wX; yi = gin.wY;
+	xxi = gin.wX; yyi = gin.wY;
 	if (start)
 	  plline(5, sx, sy); /* clear previous rectangle */
 
 	start = 0;
 
-	sx[0] = xi; sy[0] = yi;
-	sx[4] = xi; sy[4] = yi;
+	sx[0] = xxi; sy[0] = yyi;
+	sx[4] = xxi; sy[4] = yyi;
       }
 
       if (gin.state && 0x100) {
-	xe = gin.wX; ye = gin.wY;
+	xxe = gin.wX; yye = gin.wY;
 	if (start)
 	  plline(5, sx, sy); /* clear previous rectangle */
 
 	start = 1;
   
-	sx[2] = xe; sy[2] = ye;
-	sx[1] = xe; sy[1] = yi;
-	sx[3] = xi; sy[3] = ye;
+	sx[2] = xxe; sy[2] = yye;
+	sx[1] = xxe; sy[1] = yyi;
+	sx[3] = xxi; sy[3] = yye;
 	plline(5, sx, sy); /* draw new rectangle */
       }
 
@@ -173,55 +273,30 @@ main(int argc, char *argv[])
       }      
     }
     plxormod(0, &st); /* leave xor mod */
+  }
 
-    if (xe < xi) {
-      PLFLT t;
-      t=xi; xi=xe; xe=t;
-    }
+  if (xxe < xxi) {
+    t=xxi; xxi=xxe; xxe=t;}
 
-    if (yi < ye) {
-      PLFLT t;
-      t=yi; yi=ye; ye=t;
-    }
+  if (yyi < yye) {
+    t=yyi; yyi=yye; yye=t;}
 
-    if (gin.keysym == 'Q')
-      exit(0);
-  }  
-  /* 
-     I'm unable to continue, clearing the plot and advancing to the next
-     one, without hiting the enter key, or pressing the button... help!
+  *xe = xxe; *xi = xxi;
+  *ye = yye; *yi = yyi;
 
-     Forcing the xwin driver to leave locate mode and destroying the
-     xhairs (in GetCursorCmd()) solves some problems, but I still have
-     to press the enter key or press Button-2 to go to next plot, even
-     if a pladv() is not present!  Using plbop() solves the problem, but
-     it shouldn't be needed! 
-  */
+  return (gin.keysym == 'Q');
+}
 
-  /* plbop(); */
-
-  /* 
-     plspause(0), pladv(0), plspause(1), also works,
-     but the above question remains.
-     With this approach, the previous pause state is lost,
-     as there is no API call to get its current state.
-  */
-
-  plspause(0);
-  pladv(0);
-
-  plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
-
-  plspause(1);
-  pladv(0);
-
-  plenv(xi, xe, ye, yi, 1, -1);
-  plimage(img_f, width, height, 1., width, 1., height, xi, xe, ye, yi);
-  pladv(0);
-
-  plFree2dGrid(img_f, width, height);
-  free(img);
-
-  plend();
-  exit(0);
+void gray_cmap(void)
+{
+  PLFLT r[2], g[2], b[2], pos[2];
+    
+  r[0] = g[0] = b[0] = 0.0;
+  r[1] = g[1] = b[1] = 1.0;
+    
+  pos[0] = 0.0;
+  pos[1] = 1.0;
+    
+  plscmap1n(256);
+  plscmap1l(1, 2, pos, r, g, b, NULL);
 }
