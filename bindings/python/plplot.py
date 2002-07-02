20089c1a44aaf30ff2b0491677a89beaaec3e4ef
Alan W. Irwin <airwin@users.sourceforge.net>
2002-07-02 02:28:46 +0000

    plcont: put in check for correct number of arguments
    plshades: interface is completely redone to do fairly rigorous
    self-consistency checks on the argument list as well as to provide many more
    variations of arguments than before.  Also, I did fairly extensive checks of
    the new interface using example 16 (which exercises plshades).  Not only the
    old version of xw16.py, but also the new version (which takes advantage of
    the defaults allowed here) produced identical results to the tcl results for
    example 16.
    
    svn path=/trunk/; revision=3743

--- bindings/python/plplot.py
+++ bindings/python/plplot.py
@@ -21,34 +21,34 @@ from plplotc import *
 import types
 import Numeric
 
-#  Redefine plcont to have the user-friendly interface
-#  Allowable syntaxes:
+# Redefine plcont to have the user-friendly interface
+# Allowable syntaxes:
 
-#  plcont( z, [kx, lx, ky, ly], clev, [pltr, [pltr_data] or [xg, yg, [wrap]])
-#  N.B. Brackets represent options here and not python lists!
+# plcont( z, [kx, lx, ky, ly], clev, [pltr, [pltr_data] or [xg, yg, [wrap]]])
+# N.B. Brackets represent options here and not python lists!
 
-#  All unbracketed arguments within brackets must all be present or all be
-#  missing.  Furthermore, z must be a 2D array, kx, lx, ky, ly must all be
-#  integers, clev must be a 1D array, pltr can be a function reference or
-#  string, pltr_data is an optional arbitrary data object, xg and yg are 
-#  optional 1D or 2D arrays and wrap (which only works if xg and yg
-#  are specified) is 0, 1, or 2.
+# All unbracketed arguments within brackets must all be present or all be
+# missing.  Furthermore, z must be a 2D array, kx, lx, ky, ly must all be
+# integers, clev must be a 1D array, pltr can be a function reference or
+# string, pltr_data is an optional arbitrary data object, xg and yg are 
+# optional 1D or 2D arrays and wrap (which only works if xg and yg
+# are specified) is 0, 1, or 2.
 
-#  If pltr is a string it must be either "pltr0", "pltr1", or "pltr2" to
-#  refer to those built-in transformation functions.  Alternatively, the
-#  function names pltr0, pltr1, or pltr2 may be specified to refer to
-#  the built-in transformation functions or an arbitrary name for a
-#  user-defined transformation function may be specified.  Such functions
-#  must have x, y, and optional pltr_data arguments and return arbitrarily
-#  transformed x' and y' in a tuple.  The built-in pltr's such as pltr1 and
-#  pltr2 use pltr_data = tuple(xg, yg), and for this oft-used case (and any
-#  other user-defined pltr which uses a tuple of two arrays for pltr_data),
-#  we also provide optional xg and yg arguments separately as an alternative
-#  to the tuple method of providing these data. Note, that pltr_data cannot
-#  be in the argument list if xg and yg are there, and vice versa. Also note
-#  that the built-in pltr0 and some user-defined transformation functions
-#  ignore the auxiliary pltr_data (or the alternative xg and yg) in which
-#  case neither pltr_data nor xg and yg need to be specified.
+# If pltr is a string it must be either "pltr0", "pltr1", or "pltr2" to
+# refer to those built-in transformation functions.  Alternatively, the
+# function names pltr0, pltr1, or pltr2 may be specified to refer to
+# the built-in transformation functions or an arbitrary name for a
+# user-defined transformation function may be specified.  Such functions
+# must have x, y, and optional pltr_data arguments and return arbitrarily
+# transformed x' and y' in a tuple.  The built-in pltr's such as pltr1 and
+# pltr2 use pltr_data = tuple(xg, yg), and for this oft-used case (and any
+# other user-defined pltr which uses a tuple of two arrays for pltr_data),
+# we also provide optional xg and yg arguments separately as an alternative
+# to the tuple method of providing these data. Note, that pltr_data cannot
+# be in the argument list if xg and yg are there, and vice versa. Also note
+# that the built-in pltr0 and some user-defined transformation functions
+# ignore the auxiliary pltr_data (or the alternative xg and yg) in which
+# case neither pltr_data nor xg and yg need to be specified.
 
 _plcont = plcont
 def plcont(z, *args):
@@ -104,6 +104,7 @@ def plcont(z, *args):
 	elif len(args) == 1:
 	    #Must be pltr_data
 	    pltr_data = args[0]
+	    args = args[1:]
 	elif len(args) == 2 or len(args) == 3:
 	    xg = Numeric.asarray(args[0])
 	    if len(xg.shape) < 1 or len(xg.shape) > 2:
@@ -111,10 +112,12 @@ def plcont(z, *args):
 	    yg = Numeric.asarray(args[1])
 	    if len(yg.shape) < 1 or len(yg.shape) > 2:
 		raise ValueError, 'yg must be 1D or 2D array'
+	    args = args[2:]
 	    # wrap only relevant if xg and yg specified.
-	    if len(args) == 3:
-	     if type(args[-1]) == types.IntType:
-	      wrap = args[-1]
+	    if len(args) > 0:
+	     if type(args[0]) == types.IntType:
+	      wrap = args[0]
+	      args = args[1:]
 	      if len(xg.shape) == 2 and len(yg.shape) == 2 and  \
 	      z.shape == xg.shape and z.shape == yg.shape:
 		# handle wrap
@@ -142,6 +145,8 @@ def plcont(z, *args):
 	# default is identity transformation
 	pltr = pltr0
 	pltr_data = None
+    if len(args) > 0:
+	raise ValueError, 'Too many arguments for plcont'
     if ifdefault_range:
 	# Default is to take full range (still using fortran convention
 	# for indices which is embedded in the PLplot library API)
@@ -152,55 +157,141 @@ def plcont(z, *args):
     _plcont(z, kx, lx, ky, ly, clev, pltr, pltr_data)
 plcont.__doc__ = _plcont.__doc__
   
+# Redefine plshades to have the user-friendly interface
+# Allowable syntaxes:
+
+# plshades(z,  [xmin, xmax, ymin, ymax,] clev, \
+# fill_width, [cont_color, cont_width,], rect, \
+# [pltr, [pltr_data] or [xg, yg, [wrap]]])
+ 
 _plshades = plshades
-def plshades(z, xmin, xmax, ymin, ymax, clevel, fill_width, cont_color, cont_width, rect,
-             *args):
-  pltr = pltr0
-  xg = None
-  yg = None
-  if len(args) >= 3:
-    pltr, xg, yg = args[0:3]
-    args = args[3:]
+def plshades(z, *args):
+    z = Numeric.asarray(z)
+    if len(z.shape) != 2:
+	raise ValueError, 'Expected 2D z array'
 
-  wrap = 0
-  if len(args) == 1:
-    wrap = args[0]
-  elif len(args) != 0:
-    raise 'ValueError', 'too many arguments'
-
-  if wrap == 1:
-    z = Numeric.resize(z, (z.shape[0]+1, z.shape[1]))
-    if xg:
-      xg = Numeric.resize(xg, (xg.shape[0]+1, xg.shape[1]))
-    if yg:
-      yg = Numeric.resize(yg, (yg.shape[0]+1, yg.shape[1]))
-  elif wrap == 2:
-    z = Numeric.transpose(Numeric.resize(Numeric.transpose(z), (z.shape[1]+1, z.shape[0])))
-    if xg:
-      xg =  Numeric.transpose(Numeric.resize(Numeric.transpose(xg), (xg.shape[1]+1, xg.shape[0])))
-    if yg:
-      yg =  Numeric.transpose(Numeric.resize(Numeric.transpose(yg), (yg.shape[1]+1, yg.shape[0])))
-  elif wrap != 0:
-    raise ValueError, "Invalid wrap specifier, must be 0, 1 or 2."
-
-  # handle the string names for the callbacks though the function works fine too
-  if type(pltr) == types.StringType:
-    if pltr == 'pltr0':
-      pltr = pltr0
-    elif pltr == 'pltr1':
-      pltr = pltr1
-    elif pltr == 'pltr2':
-      pltr = pltr2
+    if len(args) > 4 and \
+    (type(args[0]) == types.FloatType or type(args[0]) == types.IntType) and \
+    (type(args[1]) == types.FloatType or type(args[1]) == types.IntType) and \
+    (type(args[2]) == types.FloatType or type(args[2]) == types.IntType) and \
+    (type(args[3]) == types.FloatType or type(args[3]) == types.IntType):
+	# These 4 args are xmin, xmax, ymin, ymax
+	xmin, xmax, ymin, ymax = args[0:4]
+	args = args[4:]
     else:
-      raise ValueError, 'pltr is unrecognized'
+	# These values are ignored if pltr and pltr_data are defined in any case.
+	# So pick some convenient defaults that work for the pltr0, None case
+	xmin = -1.
+	xmax = 1.
+	ymin = -1.
+	ymax = 1.
 
-  pltr_data = None
-  if xg and yg:
-    pltr_data = (xg, yg)
-  else:
-    pltr_data = None
+    # clev must be present.
+    if len(args) > 0:
+	clev = Numeric.asarray(args[0])
+	if len(clev.shape) !=1:
+	    raise ValueError, 'Expected 1D clev array'
+	args = args[1:]
+    else:
+	raise ValueError, 'Missing clev argument'
 
-  _plshades(z,  xmin, xmax, ymin, ymax, clevel, fill_width, cont_color, cont_width,
-            rect, pltr, pltr_data)
+    # fill_width must be present
+    if len(args) > 0 and type(args[0]) == types.IntType:
+	fill_width = args[0]
+	args = args[1:]
+    else:
+	raise ValueError, 'Missing fill_width argument'
+
+    # cont_color and cont_width are optional.
+    if len(args) > 2 and \
+    type(args[0]) == types.IntType and \
+    type(args[1]) == types.IntType:
+	# These 2 args are 
+	cont_color, cont_width = args[0:2]
+	args = args[2:]
+    else:
+	# Turn off contouring.
+	cont_color, cont_width = (0,0)
+
+    # rect must be present.
+    if len(args) > 0 and type(args[0]) == types.IntType:
+	rect = args[0]
+	args = args[1:]
+    else:
+	raise ValueError, 'Missing rect argument'
+
+    if len(args) > 0 and ( \
+    type(args[0]) == types.StringType or \
+    type(args[0]) == types.FunctionType or \
+    type(args[0]) == types.BuiltinFunctionType):
+	pltr = args[0]
+	# Handle the string names for the callbacks though specifying the
+	# built-in function name directly (without the surrounding quotes) 
+	# or specifying any user-defined transformation function 
+	# (following above rules) works fine too.
+	if type(pltr) == types.StringType:
+	    if pltr == 'pltr0':
+		pltr = pltr0
+	    elif pltr == 'pltr1':
+		pltr = pltr1
+	    elif pltr == 'pltr2':
+		pltr = pltr2
+	    else:
+		raise ValueError, 'pltr string is unrecognized'
+
+	args = args[1:]
+	# Handle pltr_data or separate xg, yg, [wrap]
+	if len(args) == 0:
+	    # Default pltr_data
+	    pltr_data = None
+	elif len(args) == 1:
+	    #Must be pltr_data
+	    pltr_data = args[0]
+	    args = args[1:]
+	elif len(args) == 2 or len(args) == 3:
+	    xg = Numeric.asarray(args[0])
+	    if len(xg.shape) < 1 or len(xg.shape) > 2:
+		raise ValueError, 'xg must be 1D or 2D array'
+	    yg = Numeric.asarray(args[1])
+	    if len(yg.shape) < 1 or len(yg.shape) > 2:
+		raise ValueError, 'yg must be 1D or 2D array'
+	    args = args[2:]
+	    # wrap only relevant if xg and yg specified.
+	    if len(args) > 0:
+	     if type(args[0]) == types.IntType:
+	      wrap = args[0]
+	      args = args[1:]
+	      if len(xg.shape) == 2 and len(yg.shape) == 2 and  \
+	      z.shape == xg.shape and z.shape == yg.shape:
+		# handle wrap
+		if wrap == 1:
+		    z = Numeric.resize(z, (z.shape[0]+1, z.shape[1]))
+		    xg = Numeric.resize(xg, (xg.shape[0]+1, xg.shape[1]))
+		    yg = Numeric.resize(yg, (yg.shape[0]+1, yg.shape[1]))
+		elif wrap == 2:
+		    z = Numeric.transpose(Numeric.resize( \
+		    Numeric.transpose(z), (z.shape[1]+1, z.shape[0])))
+		    xg = Numeric.transpose(Numeric.resize( \
+		    Numeric.transpose(xg), (xg.shape[1]+1, xg.shape[0])))
+		    yg = Numeric.transpose(Numeric.resize( \
+		    Numeric.transpose(yg), (yg.shape[1]+1, yg.shape[0])))
+		elif wrap != 0:
+		    raise ValueError, "Invalid wrap specifier, must be 0, 1 or 2."
+	      elif wrap != 0:
+		  raise ValueError, 'Non-zero wrap specified and xg and yg are not 2D arrays'
+	     else:
+		 raise ValueError, 'Specified wrap is not an integer'
+	    pltr_data = (xg, yg)
+	else:
+	    raise ValueError, 'Must have 0-3 arguments after pltr'
+    else:
+	# default is identity transformation
+	pltr = pltr0
+	pltr_data = None
+    if len(args) > 0:
+	raise ValueError, 'Too many arguments for plshades'
+
+    _plshades(z,  xmin, xmax, ymin, ymax, clev, \
+    fill_width, cont_color, cont_width, rect, pltr, pltr_data)
 plshades.__doc__ = _plshades.__doc__
   
