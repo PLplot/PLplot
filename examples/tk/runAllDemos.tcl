49d8e9e9d28cb51d8955888c7a7fb620c7c93cb1
Vince Darley <vincentdarley@users.sourceforge.net>
2002-07-02 09:51:29 +0000

    run all tk demos from one nice gui
    svn path=/trunk/; revision=3750

---
+++ examples/tk/runAllDemos.tcl
@@ -0,0 +1,64 @@
+#----------------------------------------------------------------------------
+# Source this file into a working Tk interpreter to run all the Tcl demos
+# in a nice window with buttons for each demo you'd like to run.
+# 
+# Vince Darley
+# vince@santafe.edu
+# 
+#----------------------------------------------------------------------------
+
+cd [file join [file dirname [info script]] .. tcl]
+lappend auto_path [pwd]
+catch {package require Plplotter}
+plframe .p
+grid .p -columnspan 5 -sticky news
+grid rowconfigure . 0 -weight 1
+for {set i 0} {$i < 5} {incr i} {
+    grid columnconfigure . $i -weight 1
+}
+
+# turn on pauses
+.p cmd plspause 1
+
+button .cexit -text "Quit" -command exit
+button .cshell -text "Shell" -command "console show"
+button .bnextpage -text "Page" -command ".p nextpage"
+
+set buttons [list .cexit .cshell .bnextpage]
+
+proc run {demo} {
+    .p configure -eopcmd [list .bnextpage configure -state normal]
+    .l configure -text "Starting $demo"
+    setButtonState disabled
+    update idletasks
+    .p cmd plbop
+    $demo .p
+    .p cmd pleop
+    focus .p
+    .l configure -text "$demo complete"
+    setButtonState normal
+    .bnextpage configure -state disabled
+}
+
+proc setButtonState {state} {
+    foreach b [info commands .b*] {
+	$b configure -state $state
+    }
+}
+
+for {set i 1} {$i <= [llength [glob x*.tcl]]} {incr i} {
+    set demo x[format "%02d" $i]
+    button .b$i -text "Demo $i" -command [list run $demo]
+    lappend buttons .b$i
+    if {[llength $buttons] == 5} {
+	eval grid $buttons -sticky ew
+	set buttons {}
+    }
+}
+
+if {[llength $buttons]} {
+    eval grid $buttons -sticky ew
+}
+
+label .l
+grid .l -sticky ew -columnspan 5
