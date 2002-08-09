98812d399fa6ba9877d4b787a749b568b998f730
Alan W. Irwin <airwin@users.sourceforge.net>
2002-08-09 14:44:08 +0000

    AWI for Olof Svensson.  Apply patch to facilitate using new swing-based python interface.
    svn path=/trunk/; revision=3859

--- sys/win32/msdev/src/setup.py
+++ sys/win32/msdev/src/setup.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 
-setup( name = "pl",
-       version = "5.1.0",
-       description = "PLplot Python extension module",
-       author = "Geoffrey Furnish",
-       author_email = "furnish@actel.com",
+include_dirs       = ['.', '..\\tmp','c:\\Python21\\Include\\Numeric']
+extra_compile_args = ['/DWIN32']
+library_dirs       = ['..\\lib']
+libraries          = ['plplot', 'kernel32', 'user32', 'gdi32', 'winspool', 'comdlg32', 'advapi32', 'shell32', 'ole32', 'oleaut32', 'uuid', 'odbc32', 'odbccp32']
+
+
+module1 = Extension( "plplotcmodule", 
+		     extra_compile_args = extra_compile_args,
+                     include_dirs = include_dirs,
+		     library_dirs = library_dirs,
+		     libraries = libraries,
+		     sources = ["plplotcmodule.c"]
+		     )
+
+module2 = Extension( "plplot_widgetmodule", 
+		     extra_compile_args = extra_compile_args,
+                     include_dirs = include_dirs,
+		     library_dirs = library_dirs,
+		     libraries = libraries,
+		     sources = ["plplot_widgetmodule.c"]
+		     )
+
+setup( name = "plplot",
+       version = "5.1",
+       description = "PLplot Python extension modules",
+       author = "Alan W. Irwin",
+       author_email = "irwin@beluga.phys.uvic.ca",
        url = "http://www.plplot.org",
-
-       extra_path = 'PLplot',
-       #py_modules = ["Plframe", "TclSup" ],
-
-
-       ext_modules = [( 'pl',
-                        {'sources' : ['plmodule.c',
-                                      ],
-                         'include_dirs' : ['.', 
-'..\\tmp','c:\\Python21\\Include\\Numeric'],
-                         'library_dirs' : ['..\\lib'],
-                         'libraries' : ['plplot', 'kernel32', 'user32',
-'gdi32', 'winspool', 'comdlg32', 'advapi32', 'shell32', 'ole32',
-'oleaut32', 'uuid', 'odbc32', 'odbccp32'],
-                         'extra_compile_args' : ['/DWIN32'],
-                         }
-                       ),
-                      ( 'pyqt_pl',
-                        {'sources' : ['pyqt_plmodule.c',
-                                     ],
-                         'include_dirs' : ['.',
-'..\\tmp','c:\\Python21\\Include\\Numeric'],
-                         'library_dirs' : ['..\\lib'],
-                         'libraries' : ['plplot', 'kernel32', 'user32',
-'gdi32', 'winspool', 'comdlg32', 'advapi32', 'shell32', 'ole32',
-'oleaut32', 'uuid', 'odbc32', 'odbccp32'],
-                         'extra_compile_args' : ['/DWIN32'],
-                         }
-                       )                                  
-                      
-                      ],
+       license = "LGPL",
+       ext_modules = [module1,module2]
        )
-
