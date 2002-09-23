2b9e956893c7fd327affefec27ad46ff6150444a
Joao Cardoso <jcard@users.sourceforge.net>
2002-09-23 18:43:01 +0000

    Use the new rubber-banding "plrb" facility. Update the zoomed plot automaticaly if Octave automatic_replot variable is set.
    svn path=/trunk/; revision=3948

--- bindings/octave/PLplot/zoom.m
+++ bindings/octave/PLplot/zoom.m
@@ -14,11 +14,8 @@
 
 ## zoom(arg)
 ##
-## No, it is just a frontend to set axis :(
-## Select left botton and right top area for setting axis.
-## Clicking with right mouse button or arg == "off" set axis "autoscale"
-## Due to a bug in ginput, the cross-hair remains in the plot after zoom terminate
-## Use the tk driver for zooming and panning.
+## Iteratively select an area for setting the axis.
+## If 'arg' == "off" set axis to "autoscale"
 
 function zoom(arg)
   
@@ -32,27 +29,14 @@ function zoom(arg)
     endif
   endif
 
-  [x1, y1, b1] = ginput(1);
-  if (b1 == 3)
-    axis;
-    return;
-  endif
-  
-  [x2, y2, b2] = ginput(1);
+  [x1, y1, x2, y2] = plrb;
 
-  if (b2 == 3)
-    axis;
-    return
-  else
-    ## FIXME: this is buggy
-    if (__pl.type == 11 || __pl.type == 31)
-      x1 = 10^x1; x1 = 10^x2;
-    endif
-    if (__pl.type == 21 || __pl.type == 31)
-      y1 = 10^y1; y2 = 10^y2;
-    endif
-    axis([min(x1, x2), max(x1, x2), min(y1, y2), max(y1, y2)]);
+  if (__pl.type == 11 || __pl.type == 31)
+    x1 = 10^x1; x1 = 10^x2;
   endif
-
+  if (__pl.type == 21 || __pl.type == 31)
+    y1 = 10^y1; y2 = 10^y2;
+  endif
+  axis([min(x1, x2), max(x1, x2), min(y1, y2), max(y1, y2)]);
 
 endfunction
