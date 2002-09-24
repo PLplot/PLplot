72099988af5d80d40b4bb7cebe9613d0e83bbb3b
Maurice LeBrun <mlebrun@users.sourceforge.net>
2002-09-24 10:40:35 +0000

    Fixes for the install image under Unix: set the driver name correctly, give preference to the double precision version (the "d" has nothing to do with debug), and ensure the tcl subdir is added to auto_path.
    svn path=/trunk/; revision=3961

--- bindings/tk-x-plat/pkgIndex.tcl
+++ bindings/tk-x-plat/pkgIndex.tcl
@@ -1,9 +1,12 @@
 
 if {$tcl_platform(platform) == "unix"} {
-    if {[info exists tcl_platform(debug)]} {
-	set file [file join $dir .. drivers libtkwind[info sharedlibextension]]
+    set file_d [file join $dir drivers tkwind_drv[info sharedlibextension]]
+    set file_s [file join $dir drivers tkwin_drv[info sharedlibextension]]
+# give preference to double precision driver, if it exists.
+    if [file exists $file_d] {
+	set file $file_d
     } else {
-	set file [file join $dir .. drivers libtkwin[info sharedlibextension]]
+	set file $file_s
     }
 } else {
     if {[info exists tcl_platform(debug)]} {
@@ -21,6 +24,8 @@ proc loadPlplot {dir file} {
     set pllibrary $dir
     load $file Plplotter
     rename loadPlplot {}
+# put core tcl scripts in path
+    lappend auto_path $dir/tcl
 }
 
 package ifneeded Plplotter 5.1.0 [list loadPlplot $dir $file]
