038feed4edb2da032affb0c24a7b769a4255443a
nobody <nobody@localhost>
2002-09-29 09:47:52 +0000

    This commit was manufactured by cvs2svn to create branch 'enhance_3dplots'.
    svn path=/branches/enhance_3dplots/; revision=3982

--- test/test_python.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Test suite for Python demos.  
-# This is called from plplot-test.sh with $pythondir, $device, $dsuffix, 
-# and $options defined.
-$pythondir/pythondemos.py -dev $device -o pythondemos.$dsuffix $options
