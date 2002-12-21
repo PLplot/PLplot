88816fd58e50f51258756981140ec7744693928c
Joao Cardoso <jcard@users.sourceforge.net>
2002-12-21 00:50:25 +0000

    Keep compatibility with Octave-2.0.16
    svn path=/trunk/; revision=4175

--- bindings/octave/PLplot/bgr.m
+++ bindings/octave/PLplot/bgr.m
@@ -1,3 +1,17 @@
+## Copyright (C) 2002 Joao Cardoso.
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 2 of the License, or (at your
+## option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## This file is part of plplot_octave.
+
 ## map = bgr(n)
 ##
 ## return a colormap of size 'n' going from blue to green to red.
@@ -13,24 +27,24 @@ function map = bgr(n)
   i = fix(linspace (1, n, 5));
 
   rg = i(1):i(2);
-  r(rg) = 0;
-  g(rg) = linspace(0,1,length(rg));
-  b(rg) = 1;
+  r(1,rg) = 0;
+  g(1,rg) = linspace(0,1,length(rg));
+  b(1,rg) = 1;
   
   rg = i(2):i(3);
-  r(rg) = 0;
-  g(rg) = 1;
-  b(rg) = linspace(1,0,length(rg));
+  r(1,rg) = 0;
+  g(1,rg) = 1;
+  b(1,rg) = linspace(1,0,length(rg));
   
   rg = i(3):i(4);
-  r(rg) = linspace(0,1,length(rg));
-  g(rg) = 1;
-  b(rg) = 0;
+  r(1,rg) = linspace(0,1,length(rg));
+  g(1,rg) = 1;
+  b(1,rg) = 0;
 
   rg = i(4):i(5);
-  r(rg) = 1;
-  g(rg) = linspace(1,0,length(rg));
-  b(rg) = 0;
+  r(1,rg) = 1;
+  g(1,rg) = linspace(1,0,length(rg));
+  b(1,rg) = 0;
 
   map = [r', g', b'];
 endfunction
