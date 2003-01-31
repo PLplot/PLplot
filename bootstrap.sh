e194447c42415f4a845d31eeec1556ca23ca6206
Alan W. Irwin <airwin@users.sourceforge.net>
2003-01-31 02:21:30 +0000

    Make sure you start with zero-length config.h.in file each time ./bootstrap.sh is invoked.
    svn path=/trunk/; revision=4293

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 aclocal \
   && touch config.h.in \
+  && rm config.h.in \
+  && touch config.h.in \
   && libtoolize --copy --ltdl --automake\
   && automake --add-missing --copy\
   && autoheader \
