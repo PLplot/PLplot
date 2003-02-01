50849413ae358e033f71b1bd64c8fab456a5106b
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-02-01 14:33:40 +0000

    Issue messages at stdout about which program is launched, including version information.  This is accomplished through the defined shell function "run". Since this apparently does not work with vanilla Bourne shells, I made this script run /bin/bash.  This is a mild constraint, since the developers that are going to use the GNU AutoTools, almost certainly have GNU bash installed in their systems.
    svn path=/trunk/; revision=4302

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 
-aclocal \
-  && libtoolize --copy --ltdl --automake \
-  && autoheader \
-  && automake --add-missing --copy \
-  && autoconf
+function run {
+  echo -n Running `$1 --version | sed q`...
+  $*
+  echo " done"
+}
+  
+run aclocal \
+  && run libtoolize --copy --ltdl --automake \
+  && run autoheader \
+  && run automake --add-missing --copy \
+  && run autoconf
