d5626df59f330ca6129eb00b1f42c7a9ef3672ea
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-02-06 08:05:10 +0000

    This changes were necessary for me to get Libtool 1.4.3 working properly with Autoconf 2.57 and Automake 1.7.2.  Apparently, something changed between the version 1.4.2a and 1.4.3 of Libtool such that its aclocal.m4 does get not installed at all.  This is why the option -I is given to aclocal now.
    If launching simply "./bootstrap.sh" does not work for you, please give it
    the arguments:
    
        ./bootstrap.sh -I <libltdl_inc_dir>
    
    where <libltdl_inc_dir> is the location where the Libtool's aclocal.m4 is
    found (it defaults to "/usr/share/libtool/libltdl").  In regular Linux
    installations (like Debian) there is no need to specify arguments to
    bootstrap.sh.  This is particularly necessary if you have built and
    installed Libtool by hand.
    
    I also made other two changes in order to get things working: option
    --force-missing has been added to the automake call and the order of
    invocation of automake and libtoolize are reversed.
    
    The addition of the argument to bootstrap.sh needs to be documented.  I will
    do that when I will have some more time.
    
    svn path=/trunk/; revision=4319

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,12 @@ function run {
   $*
   echo " done"
 }
-  
-run aclocal \
-  && run libtoolize --copy --ltdl --automake \
+
+aclocal_opts="$1"
+aclocal_opts=${aclocal_opts:="-I /usr/share/libtool/libltdl"}
+
+run aclocal $aclocal_opts \
   && run autoheader \
-  && run automake --add-missing --copy \
+  && run automake --force-missing --add-missing --copy \
+  && run libtoolize --copy --ltdl --automake \
   && run autoconf
