ba723a1c90b3d77eb935f46c9a65d9a2cd6c26c8
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-02-13 23:26:24 +0000

    * Get right pattern for parsing AM_INIT_AUTOMAKE in configure.ac. * Accept option -u, which restore the canonical version number in   configure.ac.
    svn path=/trunk/; revision=4357

--- debian/make-cvs-release.sh
+++ debian/make-cvs-release.sh
@@ -6,15 +6,22 @@
 # This will prepare a PLplot source tree for a release <version>.cvs.<date>
 # of Debian packages.  Run it from the top dir.
 
-today=`date '+%Y%m%d'`
+if test "$1" = "-u" ; then
+  cvsrel=
+else
+  cvsrel=.cvs.`date '+%Y%m%d'`
+fi
 
 echo -n "Patching configure.ac ... "
 perl -pi -e \
-  's/^(AM_INIT_AUTOMAKE\(plplot, )(\d\.\d\.\d)([^)]+\))/$1$2'.cvs.$today')/' \
+  's/^(AM_INIT_AUTOMAKE\(plplot, )(\d\.\d\.\d)([^)]*\))/$1$2'$cvsrel')/' \
   configure.ac
 echo done
 
-./bootstrap.sh
-debian/rules config
-make dist
-mv plplot-$version.tar.gz ../plplot_$version.orig.tar.gz
+if test "$1" != "-u" ; then
+  ./bootstrap.sh
+  debian/rules config
+  make dist
+  version=`debian/get-upstream-version.pl < configure.ac`
+  mv plplot-$version.tar.gz ../plplot_$version.orig.tar.gz
+fi
