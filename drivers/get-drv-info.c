eca94c59d30da90890980ad7efc34008775cf7ff
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-02-13 02:02:11 +0000

    Abort if call to lt_dlopenext is not successful.
    svn path=/trunk/; revision=4345

--- drivers/get-drv-info.c
+++ drivers/get-drv-info.c
@@ -2,28 +2,28 @@
  *
  * Copyright (C) 2003  Rafael Laboissiere
  * This file is part of PLplot.
- *	 
+ *
  * PLplot is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Library General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
- *    
+ *
  * PLplot is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
  * General Public License for more details.
-		  
+
  * You should have received a copy of the GNU Library General Public License
  * along with the GNU C Library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
- * MA 02111-1307, USA.  
+ * MA 02111-1307, USA.
  */
-   
+
 #include <ltdl.h>
 #include <stdio.h>
 
 int
-main (int argc, char* argv[]) 
+main (int argc, char* argv[])
 {
   lt_dlhandle dlhand;
   char sym[300];
@@ -33,6 +33,10 @@ main (int argc, char* argv[])
 
   lt_dlinit ();
   dlhand = lt_dlopenext (drvnam);
+  if (dlhand == NULL) {
+    fprintf (stderr, "Could not open driver module %s\n", drvnam);
+    return 1;
+  }
   sprintf (sym, "plD_DEVICE_INFO_%s", drvnam);
   info = (char **) lt_dlsym (dlhand, sym);
   if (info != NULL) {
