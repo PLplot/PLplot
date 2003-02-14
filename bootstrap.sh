dbee7245aab25ab5d92d12a81f383633ab927c78
Alan W. Irwin <airwin@users.sourceforge.net>
2003-02-14 03:43:38 +0000

    Drop --force-missing as an option to the run automake command.  It was incorrectly overwriting the PLplot INSTALL file which is under cvs control with the generic, boring FSF INSTALL file.
    svn path=/trunk/; revision=4359

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,6 @@ aclocal_opts=${aclocal_opts:="-I /usr/share/libtool/libltdl"}
 
 run aclocal $aclocal_opts \
   && run autoheader \
-  && run automake --force-missing --add-missing --copy \
+  && run automake --add-missing --copy \
   && run libtoolize --copy --ltdl --automake \
   && run autoconf
