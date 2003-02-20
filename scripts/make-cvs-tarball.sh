9d7c659caedce0e40e4f6f61d32312bd2bde69a4
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-02-20 09:16:14 +0000

    This is a first try on a script to make automatic nightly candidate-release tarballs from CVS HEAD.  For now, it only generates the tarball, no transfer to the SF web site (or wherever) is done.
    Run it like:
    
        USER=<your_SF_user_name> make-cvs-tarball.sh
    
    And a file plplot-<version>.cvs.<date>.tar.gz will be created in the current
    directory.
    
    Of course, this tarball does not work properly for "./configure; make",
    because "make dist" does not produces a totally functional tarball yet.
    
    svn path=/trunk/; revision=4400

---
+++ scripts/make-cvs-tarball.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright (C) 2003  Rafael Laboissiere
+# This script is in the Public Domain
+
+# This will prepare a tdistribution tarbal directly from the CVS sources of
+# the PLplot project
+
+SF_USER=${USER:-rlaboiss}
+SF_CVSROOT=cvs.plplot.sourceforge.net:/cvsroot/plplot
+
+CVSTMPDIR=plplot-cvs-tarball
+
+rm -rf $CVSTMPDIR
+cvs -d${SF_USER}@$SF_CVSROOT co -d$CVSTMPDIR  plplot
+cd $CVSTMPDIR
+
+CVSREL=.cvs.`date '+%Y%m%d'`
+
+echo -n "Patching configure.ac ... "
+perl -pi -e \
+  's/^(AM_INIT_AUTOMAKE\(plplot, )(\d\.\d\.\d)([^)]*\))/$1$2'$CVSREL')/' \
+  configure.ac
+echo done
+
+./bootstrap.sh
+./configure
+make dist
+TARBALL=`ls plplot-*.tar.gz`
+mv $TARBALL ..
+cd ..
+rm -rf $CVSTMPDIR
+echo "CVS Distribution tarball: $TARBALL"
