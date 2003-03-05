dd40218f2ca308dd7e88bdc0a41c7d72b82c776a
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-03-05 08:35:31 +0000

    - More robust parsing of configure.ac to extract the version number.  My cvs   snapshot version with ".cvs.<date>" was screwing up the script. - Exclude CVS files from the check test.
    svn path=/trunk/; revision=4523

--- scripts/check-tarball-cvs.sh
+++ scripts/check-tarball-cvs.sh
@@ -5,12 +5,12 @@
 # send to stdout a list of files that are present in a freshly checked out
 # CVS tree and absent from the tarball.
 #
-# Run it from the top directory.
+# Run it from the top_builddir directory.
 #
 # Written by Rafael Laboissiere on 2003-31-01
 
 VERSION=`perl -ne \
-           'if(/AM_INIT_AUTOMAKE\(plplot, ([0-9.]+)/){print "$1\n"; last}'\
+           'if(/AM_INIT_AUTOMAKE\(plplot, ([0-9.cvs]+)/){print "$1\n"; last}'\
            < configure.ac`
 TOPDIR=plplot-$VERSION
 TARBALL=$TOPDIR.tar.gz
@@ -18,7 +18,8 @@ TARBALL=$TOPDIR.tar.gz
 make dist > /dev/null
 cvs co plplot > /dev/null
 
-for f in `find plplot -type f | sed 's|^plplot/||'` ; do
+for f in `find plplot -type f | sed 's|^plplot/||' \
+          | fgrep -v CVS | fgrep -v .cvsignore` ; do
   if [ -z "`tar tfz $TARBALL $TOPDIR/$f 2>/dev/null`" ] ; then
     echo $f
   fi
