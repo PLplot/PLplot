9c8926ac3ea9ad8eedd264a322e792a5fb50c07c
Joao Cardoso <jcard@users.sourceforge.net>
2003-03-18 20:37:46 +0000

    Change format of stored data support for drawnow.m/stopdraw.m support to mesh/surf uses __pl_logplotit move min/max determination to __pl_store.m make pl.type per stream support new axis.m options support "no legend" string specification
    svn path=/trunk/; revision=4634

--- bindings/octave/PLplot/support/__pl_plotit.m
+++ bindings/octave/PLplot/support/__pl_plotit.m
@@ -1,4 +1,4 @@
-## Copyright (C) 1998, 1999, 2000 Joao Cardoso.
+## Copyright (C) 1998-2003 Joao Cardoso.
 ## 
 ## This program is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by the
@@ -16,87 +16,40 @@ function __pl_plotit
 
   ## __pl.type is the type of plot (polar, logx, etc.),
   ## __pl.items the number of triples (x, y, fmt) in the plot argument
-  ## __pl.[x|y|items] stores the triples, as .x_i, .y_i, fmt_i,
+  ## __pl.[x|y|fmt] stores the triples, as .x_i, .y_i, fmt_i,
   ## were _i varies from 1 to __pl_items
+  ## now the format is __pl.x{i}_j, where i is the item and j the figure
 
   global __pl
+
+  strm = __pl_init;
+  if (__pl.stopdraw(strm))
+    return
+  endif
 
   old_empty_list_elements_ok = empty_list_elements_ok;
   empty_list_elements_ok = 1;
 
-  __pl_strm = __pl_init;
+  if (__pl.type(strm) >= 100 && __pl.type(strm) < 200)
+    __pl_meshplotit;
+    return;
+  endif
 
-  ## character height, to correctly position labels
-
-  [a, ch_height] = plgchr;
-  ch_height = ch_height/100; ## why 100? it works with me... docs say its units are mm
-
-  ## plplot has no notion of log plots. What to plot for values <= 0?
-  ## this is a kludge,  FIXME no matrix allowed!
-
-  i=0;	
-  while (i++ < __pl.items)
-    if (__pl.type == 11 || __pl.type == 21 || __pl.type == 31)
-      
-      x = eval(sprintf("__pl.x%d;", i));
-      y = eval(sprintf("__pl.y%d;", i));
-      
-      if (__pl.type == 11)
-	if (any(x <= 0))
-	  expr = "ix = find(x > 0);";
-	else
-	  break;
-	endif
-	
-      elseif (__pl.type == 21)
-	if (any(y <= 0))
-	  expr = "ix = find(y > 0);";
-	else
-	  break;
-	endif
-      elseif (__pl.type == 31)
-	if (any(y <= 0) || any(x <= 0))
-	  expr = "ix = find(y > 0 & x > 0);";
-	else
-	  break
-	endif
-      endif
-      try    
-	k = __pl.items;
-	while(!isempty(eval(expr)))
-	  ixx = ix(find(ix == (min(ix):rows(ix)+min(ix)-1)'));
-	  eval(sprintf("__pl.y%d = y(ixx,:);", k));
-	  eval(sprintf("__pl.x%d = x(ixx,:);", k));
-	  eval(sprintf("__pl.fmt%d = __pl.fmt%d;", k, i));
-	  x(ixx,:) = y(ixx,:) = []; k++;
-	endwhile
-	k--;
-	eval(sprintf("__pl.y%d = __pl.y%d;", i, k));
-	eval(sprintf("__pl.x%d = __pl.x%d;", i, k));
-	eval(sprintf("__pl.fmt%d = __pl.fmt%d;", i, k));		    
-	__pl.items = k;
-      catch
-	empty_list_elements_ok = old_empty_list_elements_ok;
-	error("__pl_plotit: FIXME. Logplot of matrices with negative elements is in some cases not allowed.\n");
-      end
-      
-    endif
-  endwhile
+  ## for log plots one has to calculate the log of the data,
+  ## but first remove the negative data.
+  if (__pl.type(strm) == 10 || __pl.type(strm) == 20 || __pl.type(strm) == 30)
+    __pl_logplotit;
+  endif
   
-  ## in plplot one must define a viewport before plotting.
-  ## In hold mode, the viewport will not be calculated for the current plot, and
-  ## the last viewport (when not in hold mode) will be used, so plots made in
-  ## hold mode can/will be clipped.
-
-  if (1|| ishold == 0) # change: always start from the beginning!
-
+  ## define a viewport before plotting.
+  if(0) # moved to __pl_store
     __pl_lxM = __pl_lyM = -realmax;
     __pl_lxm = __pl_lym = realmax;
-    
-    for i=1:__pl.items-1
+
+    for i=0:__pl.items(strm)-1
       
-      x = eval(sprintf("__pl.x%d;", i));
-      y = eval(sprintf("__pl.y%d;", i));
+      x = eval(sprintf("__pl.x%d_%d;", i, strm));
+      y = eval(sprintf("__pl.y%d_%d;", i, strm));
       
       __pl_lxm = min([__pl_lxm, min(min(x))]);
       __pl_lxM = max([__pl_lxM, max(max(x))]);
@@ -104,73 +57,84 @@ function __pl_plotit
       __pl_lym = min([__pl_lym, min(min(y))]);
       __pl_lyM = max([__pl_lyM, max(max(y))]);
       
-      ## It seems that a viewport of (x, x, y, y) doesn't work
-      
-      if (__pl_lxm == __pl_lxM)		
-	__pl_lxm = __pl_lxm - 10*eps; __pl_lxM = __pl_lxM + 10*eps;
-      endif
-      if (__pl_lym == __pl_lyM)
-	__pl_lym = __pl_lym - 10*eps; __pl_lyM = __pl_lyM + 10*eps;
-      endif
     endfor
-
-    ## now check for user supplied axis ranges
-    ## FIXME: possible polar plot inconsistency. User must specify axis in (x,y)
-    ## coordinates, not (r,t). Advise user to use zoom() or ginput() to get (x,y)
-    
-    if (__pl.axis_st(__pl_strm))
-      __pl_lxm = __pl.axis(__pl_strm,1); __pl_lxM = __pl.axis(__pl_strm,2);
-      if (length(__pl.axis(__pl_strm,:)) >= 4)
-	__pl_lym = __pl.axis(__pl_strm,3); __pl_lyM = __pl.axis(__pl_strm,4);
-      else
-	__pl.axis(__pl_strm,3) = __pl_lym; __pl.axis(__pl_strm,4) = __pl_lyM;
-      endif
-    else	## make axis() return current axis
-      __pl.axis(__pl_strm,1) = __pl_lxm; __pl.axis(__pl_strm,2) = __pl_lxM;
-      __pl.axis(__pl_strm,3) = __pl_lym; __pl.axis(__pl_strm,4) = __pl_lyM;
-    endif
-
-    ## for log plots,
-
-    if (__pl.type == 11 || __pl.type == 31)
-      __pl_lxm = log10(__pl_lxm); __pl_lxM = log10(__pl_lxM);
-    endif
-    if (__pl.type == 21 || __pl.type == 31)		
-      __pl_lym = log10(__pl_lym); __pl_lyM = log10(__pl_lyM);
-    endif
-    
-    ## start from the beginning
-    
-    __pl.plcol(__pl_strm) = 1;
-    __pl.pllsty(__pl_strm) = 1;
-    __pl.lab_pos(__pl_strm) = 1;
-    __pl.lab_sym(__pl_strm,:) = 0;
-    __pl.line_count(__pl_strm) = 1;
-    __pl.lab_str = "";
-    
-    plcol(15); pllsty(1);
-    if (__pl.type == -1)	# polar plot is special
-      __pl_plenv(__pl_lxm, __pl_lxM, __pl_lym, __pl_lyM, 1, __pl.type);
-      if (__pl.grid(__pl_strm) == 1)
-	__pl_polargrid(__pl_lxM, __pl_lyM, __pl_lxm, __pl_lym)
-      endif
-    else
-      __pl_plenv(__pl_lxm, __pl_lxM, __pl_lym, __pl_lyM, 0, __pl.type + __pl.grid(__pl_strm));
-    endif			    
   endif
 
-  ## get the plots to be done, previously stored in the __pl_struct structure
+  ## It seems that a viewport of (x, x, y, y) doesn't work
+  __pl_lxm = __pl.lxm(strm); __pl_lxM = __pl.lxM(strm);
+  __pl_lym = __pl.lym(strm); __pl_lyM = __pl.lyM(strm);
 
-  for items=1:__pl.items-1
+  if (__pl_lxm == __pl_lxM)		
+    __pl_lxm = __pl_lxm - 10*eps; __pl_lxM = __pl_lxM + 10*eps;
+  endif
+  if (__pl_lym == __pl_lyM)
+    __pl_lym = __pl_lym - 10*eps; __pl_lyM = __pl_lyM + 10*eps;
+  endif
 
-    x = eval(sprintf("__pl.x%d;", items));
-    y = eval(sprintf("__pl.y%d;", items));
-    fmt = eval(sprintf("__pl.fmt%d;", items));
+  ## now check for user supplied axis ranges
+  ## FIXME: possible polar plot inconsistency. User must specify axis in (x,y)
+  ## coordinates, not (r,t). Advise user to use zoom() or ginput() to get (x,y)
     
-    if (__pl.type == 11 || __pl.type == 31)
+  if (__pl.axis_st(strm))
+    __pl_lxm = __pl.axis(strm,1); __pl_lxM = __pl.axis(strm,2);
+    if (length(__pl.axis(strm,:)) >= 4)
+      __pl_lym = __pl.axis(strm,3); __pl_lyM = __pl.axis(strm,4);
+    else
+      __pl.axis(strm,3) = __pl_lym; __pl.axis(strm,4) = __pl_lyM;
+    endif
+  else	## make axis() return current axis
+    __pl.axis(strm,1) = __pl_lxm; __pl.axis(strm,2) = __pl_lxM;
+    __pl.axis(strm,3) = __pl_lym; __pl.axis(strm,4) = __pl_lyM;
+  endif
+
+  ## for log plots,
+
+  if (__pl.type(strm) == 10 || __pl.type(strm) == 30)
+    __pl_lxm = log10(__pl_lxm); __pl_lxM = log10(__pl_lxM);
+  endif
+  if (__pl.type(strm) == 20 || __pl.type(strm) == 30)		
+    __pl_lym = log10(__pl_lym); __pl_lyM = log10(__pl_lyM);
+  endif
+    
+  ## start from the beginning
+    
+  if (__pl.items(strm) != 0)
+    __pl.plcol(strm) = 1;
+    __pl.pllsty(strm) = 1;
+    __pl.lab_pos(strm) = 1;
+    __pl.lab_sym(strm,:) = 0;
+    __pl.line_count(strm) = 1;
+    __pl.lab_str = "";
+  endif
+  
+  ## set plot limits, plot box, grid and axis type  
+  plcol(15); pllsty(1);
+  if (__pl.type(strm) == -1)  # polar plot is special
+    __pl_plenv(__pl_lxm, __pl_lxM, __pl_lym, __pl_lyM, 1, __pl.type(strm));
+    if (__pl.grid(strm) == 1)
+      __pl_polargrid(__pl_lxM, __pl_lyM, __pl_lxm, __pl_lym)
+    endif
+  else
+    t = 0;
+    if (__pl.axisxy(strm))
+      t = __pl.grid(strm) + __pl.axisxy(strm);
+    elseif (__pl.grid(strm))
+      t = 1 + __pl.grid(strm);
+    endif
+    __pl_plenv(__pl_lxm, __pl_lxM, __pl_lym, __pl_lyM, 0, __pl.type(strm) + t);
+  endif			    
+
+  ## get the plots to be done, previously stored in the __pl_struct structure
+  for items=0:__pl.items(strm)-1
+
+    x = eval(sprintf("__pl.x%d_%d;", items, strm));
+    y = eval(sprintf("__pl.y%d_%d;", items, strm));
+    fmt = eval(sprintf("__pl.fmt%d_%d;", items, strm));
+    
+    if (__pl.type(strm) == 10 || __pl.type(strm) == 30)
       x = log10(x); 
     endif
-    if (__pl.type == 21 || __pl.type == 31)
+    if (__pl.type(strm) == 20 || __pl.type(strm) == 30)
       y = log10(y);
     endif
     
@@ -182,7 +146,7 @@ function __pl_plotit
     endif
 
     if (color != 20)
-      __pl.plcol(__pl_strm) = color;
+      __pl.plcol(strm) = color;
     endif
     
     xc = columns(x); yc = columns(y);
@@ -190,7 +154,7 @@ function __pl_plotit
     if (is_matrix(x) && !is_vector(x) && is_matrix(y) && !is_vector(y)) 
       if (style != 9 && (xc != yc || rows(x) != rows(y)))
 	empty_list_elements_ok = old_empty_list_elements_ok;
-	error ("__pl_plotit: matrix dimensions must match");
+	error ("__pl_plotit: matrix dimensions must match.");
       endif
       range = "i;";	
     elseif (is_vector(x) || is_vector(y))
@@ -207,46 +171,57 @@ function __pl_plotit
 	  ## else
 	  [style, color, symbol, key_title] = __pl_opt(fmt(rem(j-1,rows(fmt))+1,:));
 	  if (color != 20)
-	    __pl.plcol(__pl_strm) = color;
+	    __pl.plcol(strm) = color;
 	  endif
-	  ##				endif
+	  ## endif
 	endif
 	
 	if (isempty(key_title))
 	  if (!is_vector(x) || !is_vector(y))
-	    lab = sprintf("line %d.%d", __pl.line_count(__pl_strm), (i-1)*yc+j);
-	    if (i==xc && j==max(eval(range))); __pl.line_count(__pl_strm)=__pl.line_count(__pl_strm)+1; endif	# nothing more to do, hein?
+	    lab = sprintf("line %d.%d", __pl.line_count(strm), (i-1)*yc+j);
+	    if (i==xc && j==max(eval(range)))
+	      __pl.line_count(strm)=__pl.line_count(strm)+1;
+	    endif	# nothing more to do, hein?
 	  else
-	    lab = sprintf("line %d", __pl.line_count(__pl_strm));
-	    __pl.line_count(__pl_strm) = __pl.line_count(__pl_strm)+1;
+	    lab = sprintf("line %d", __pl.line_count(strm));
+	    __pl.line_count(strm) = __pl.line_count(strm)+1;
 	  endif
-	else
+	elseif (length(key_title) != 1 || key_title != " ")
 	  if ((!is_vector(x) || !is_vector(y)) && is_strvector(fmt))
 	    lab = sprintf("%s %d", key_title, (i-1)*yc+j);
 	  else
 	    lab = sprintf("%s", key_title);
 	  endif
+	else
+	  lab = " ";
 	endif
-	
-	if (__pl.legend(__pl_strm))
-	  __pl.lab_str = [__pl.lab_str; lab];
-	  __pl.lab_col(__pl_strm, __pl.lab_pos(__pl_strm)) = __pl.plcol(__pl_strm);
-	  __pl.lab_lsty(__pl_strm, __pl.lab_pos(__pl_strm)) = __pl.pllsty(__pl_strm);
-	  __pl.lab_pos(__pl_strm) = __pl.lab_pos(__pl_strm)+1;
-	  if (style == 1 || style == 5)
-	    __pl.lab_sym(__pl_strm, __pl.lab_pos(__pl_strm)-1) = symbol;
+
+	if (__pl.legend(strm))
+	  if (lab == "") ## log plots may have a CTRL-A as the label plot
+	    __pl.lab_str = [__pl.lab_str; " "];
 	  else
-	    __pl.lab_sym(__pl_strm, __pl.lab_pos(__pl_strm)-1) = 0;
+	    __pl.lab_str = [__pl.lab_str; lab];
+	  endif
+	  __pl.lab_col(strm, __pl.lab_pos(strm)) = __pl.plcol(strm);
+	  __pl.lab_lsty(strm, __pl.lab_pos(strm)) = __pl.pllsty(strm);
+	  __pl.lab_pos(strm) = __pl.lab_pos(strm)+1;
+
+	  if (style == 1 || style == 5)
+	    __pl.lab_sym(strm, __pl.lab_pos(strm)-1) = symbol;
+	  else
+	    __pl.lab_sym(strm, __pl.lab_pos(strm)-1) = 0;
 	  endif
 	endif
 	
-	plcol(__pl.plcol(__pl_strm));
-	__pl.plcol(__pl_strm) = rem(__pl.plcol(__pl_strm), 15)+1;
-	pllsty(__pl.pllsty(__pl_strm));
-	if  (__pl.lstlyle(__pl_strm))
-	  __pl.pllsty(__pl_strm) = rem(__pl.pllsty(__pl_strm), 8)+1;
+	if (lab != "") ## log plots may have a CTRL-A as the label plot
+	  plcol(__pl.plcol(strm));
+	  __pl.plcol(strm) = rem(__pl.plcol(strm), 15)+1;
+	  pllsty(__pl.pllsty(strm));
+	  if  (__pl.line_style(strm))
+	    __pl.pllsty(strm) = rem(__pl.pllsty(strm), 8)+1;
+	  endif
 	endif
-	
+
 	switch (style)
 	  case (1)	## linespoints
 	    plline(x(:,i),y(:,j));
@@ -287,6 +262,7 @@ function __pl_plotit
 		  and a bar is plotted from x-dx to x+dx;\n\
 		  if it has three columns, it is interpreted as (x, xmin, xmax)");			
 	    endif
+
 	    pllsty(1); # solid bars
 	    if (xc == 2)
 	      xm = x(:,1) .- x(:,2); xM = x(:,1) .+ x(:,2);
@@ -306,29 +282,27 @@ function __pl_plotit
 	    if (yc != 1)
 	      plerry( x(:,1), ym, yM);
 	    endif
-	    pllsty(__pl.pllsty(__pl_strm)); # recover linestyle
+	    pllsty(__pl.pllsty(strm)); # recover linestyle
 	    
 	  otherwise
 	    plline(x(:,i),y(:,j));
 	    warning("__pl_plotit: FIXME: format '%s' not implemented",fmt);
 	  endswitch
-	  if (style == 9);break;endif		## errorbars
+
+	  if (style == 9) 	## errorbars
+	    break;
+	  endif
 	endfor ## j=eval(range)
     endfor ## i=1:xc
-  endfor ## i=1:__pl.items-1
+  endfor ## i=1:__pl.items
 
-  if (__pl.legend(__pl_strm))
+  if (__pl.legend(strm))
     __pl_draw_legend
   endif
 
   plcol(15);
-  pllab(tdeblank(__pl.xlabel(__pl_strm,:)), tdeblank(__pl.ylabel(__pl_strm,:)), tdeblank(__pl.tlabel(__pl_strm,:)));
+  pllab(tdeblank(__pl.xlabel(strm,:)), tdeblank(__pl.ylabel(strm,:)), tdeblank(__pl.tlabel(strm,:)));
   plflush;
-
-  ## change: 
-  ## if (!ishold)
-  ##__pl.items=1;
-  ## endif
 
   empty_list_elements_ok = old_empty_list_elements_ok;
 
