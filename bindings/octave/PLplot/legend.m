a79adb018301e86bb0a05b3d7e543c3e119a7df6
Joao Cardoso <jcard@users.sourceforge.net>
2003-03-19 19:14:31 +0000

    To disable an entry, enter one blank, e.g. legend("set","; ;") Use strm instead of __pl_strm, making the code clearer
    svn path=/trunk/; revision=4658

--- bindings/octave/PLplot/legend.m
+++ bindings/octave/PLplot/legend.m
@@ -1,4 +1,4 @@
-## Copyright (C) 1998-2002 Joao Cardoso.
+## Copyright (C) 1998-2003 Joao Cardoso.
 ## 
 ## This program is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by the
@@ -23,39 +23,41 @@
 ##
 ## legend ("set", "legend 1", "legend 2", ...)
 ##   sets the legend strings, where the string must be equal to the
-##   format used in the plot, e.g., "r+;legend;"
+##   format used in the plot, e.g., "r+;legend;".
+##   To disable an entry, enter one blank, e.g. legend("set","; ;")
 
 function st = legend (x, xpos, ...)
 
   global __pl
-  __pl_strm = __pl_init;
+  strm = __pl_init;
 
   if (nargin == 0)
-    ##__pl.legend(__pl_strm) = 2;
-    ##__pl.legend_xpos(__pl_strm) = __pl.legend_ypos(__pl_strm) = 1;
-    st = __pl.legend(__pl_strm);
+    ##__pl.legend(strm) = 2;
+    ##__pl.legend_xpos(strm) = __pl.legend_ypos(strm) = 1;
+    st = __pl.legend(strm);
 
   elseif (nargin == 1 && is_scalar(x) && x >=0 && x <= 2)
-    __pl.legend(__pl_strm) = x;
+    __pl.legend(strm) = x;
 
   elseif (nargin == 2 && !isstr(x) && !isstr(xpos))
-    __pl.legend_xpos(__pl_strm) = x;
-    __pl.legend_ypos(__pl_strm) = xpos;
+    __pl.legend_xpos(strm) = x;
+    __pl.legend_ypos(strm) = xpos;
 
   elseif (nargin >= 1 && isstr (x))
     if (strcmp ("off", x))
-      __pl.legend(__pl_strm) = 0;
+      __pl.legend(strm) = 0;
     elseif (strcmp ("on", x) || strcmp ("transparent", x))
-      __pl.legend(__pl_strm) = 1;
+      __pl.legend(strm) = 1;
     elseif (strcmp ("opaque", x))
-      __pl.legend(__pl_strm) = 2;
+      __pl.legend(strm) = 2;
 
     elseif (strcmp ("set", x))
       if (nargin >= 2 && isstr(xpos))
-	__pl.fmt1 = xpos;
-	va_start; i=2;
+	__pl.fmt0 = xpos;
+	va_start;
+	i=1;
 	while (--nargin > 1)
-	  eval(sprintf("__pl.fmt%d=va_arg;",i++))
+	  eval(sprintf("__pl.fmt%d=va_arg;",i++));
 	endwhile
       endif
     else
