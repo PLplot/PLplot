d872c7dce5e271428bd2b6327f5a9e31c4e9d00b
Joao Cardoso <jcard@users.sourceforge.net>
2003-03-19 19:21:32 +0000

    Add arguments to show only a region of the image.
    svn path=/trunk/; revision=4663

--- bindings/octave/PLplot/plimage.m
+++ bindings/octave/PLplot/plimage.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2000-2002 Joao Cardoso.
+## Copyright (C) 2000-2003 Joao Cardoso.
 ## 
 ## This program is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by the
@@ -12,28 +12,58 @@
 ##
 ## This file is part of plplot_octave.
 
-## Assumes that img is in octave image format, where each
-## matrix element is an index in the colormap. Slow.
+## plimage(img, xi, xe, yi, ye)
+##
+## Plot an image, assuming that it is in octave image format, where each
+##   matrix element is an index in the colormap.
+##   if xi, xe, yi and ye exists, and are smaller than the image dimension
+##   in pixels, than only that portion of the image will be shown.
+##   No smoothing will be performed!
+## Preliminary! see p17.m
 ##
 ## ex:
-##  [img, map] = loadimage("default.img"); # "lena.img" can also be used
+##   [img, map] = loadimage("default.img"); # "lena.img" can also be used
 ##   colormap(map);
 ##   plimage(img);
-##
-## 
 
-
-function plimage(img)
+function plimage(img, x1, x2, y1, y2)
 
   global __pl
-  __pl_strm = __pl_init;
+  strm = __pl_init;
   
-  plenv(0, 1, 0, 1, 1, -2);
-  pplimage (fliplr(img'), 0, 1, 0, 1,
-	    min(min(img)), max(max(img)), 0, 1, 0, 1);
-  pllab(tdeblank(__pl.xlabel(__pl_strm,:)),
-	tdeblank(__pl.ylabel(__pl_strm,:)),
-	tdeblank(__pl.tlabel(__pl_strm,:)));
+  [nr, nc] = size(img);
+  if (nargin == 1)
+    xi = yi = 1;
+    xf = nc;
+    yf = nr;
+  elseif (nargin == 5)
+    if (x1 < 1 || x1 > nc || x2 < 1 || x2 > nc ||
+	y1 < 1 || y1 > nr || y1 < 1 || y2 > nr)
+      usage "plimage";
+      return;
+    else
+      xi = min([x1, x2]); 
+      xf = max([x1, x2]); 
+      yi = min([y1, y2]); 
+      yf = max([y1, y2]); 
+    endif
+  else
+    usage "plimage";
+    return;
+  endif
+
+  __pl_plenv(xi, xf, yi, yf, 1, -1);
+
+  pplimage (fliplr(img'),
+	    1, nc, 1, nr,
+	    0, 0,
+	    xi, xf, yi, yf);
+
+  pllab(tdeblank(__pl.xlabel(strm,:)),
+	tdeblank(__pl.ylabel(strm,:)),
+	tdeblank(__pl.tlabel(strm,:)));
   plflush;pleop;
+
+  __pl.items(strm) = 0; # for now!
 
 endfunction
