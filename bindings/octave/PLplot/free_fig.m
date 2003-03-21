3cf19248d157465516a5cb97d1ab166ed6e4c174
Joao Cardoso <jcard@users.sourceforge.net>
2003-03-21 18:35:46 +0000

    Don't create a new figure is no one is open or plplot_octave is initialized
    svn path=/trunk/; revision=4692

--- bindings/octave/PLplot/free_fig.m
+++ bindings/octave/PLplot/free_fig.m
@@ -19,7 +19,11 @@
 function n = free_fig
   
   global __pl
-  __pl_init;
+
+  if (!exist("__pl") || !struct_contains (__pl,"inited"))
+    n = 0;
+    return;
+  endif
 
   if (any(__pl.open == 0))
     n = min(find(__pl.open == 0))-1;
