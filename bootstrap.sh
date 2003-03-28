5a422e83997f07ec8fbc5f6baa39f4b9582b3892
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-03-28 21:57:26 +0000

    Reversed the order of invokation of libtoolize and automake.  With this change, the ltmain.sh script should be included into the distribution tarball now.
    svn path=/trunk/; revision=4738

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,6 +76,6 @@ aclocal_opts=${aclocal_opts:="-I /usr/share/libtool/libltdl"}
 
 run aclocal $aclocal_opts \
   && run autoheader \
-  && run automake --add-missing --copy \
   && run libtoolize --copy --ltdl --automake \
+  && run automake --add-missing --copy \
   && run autoconf
