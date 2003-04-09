0da0363cbfdd892a5a5adb1d37fed41dce891510
Joao Cardoso <jcard@users.sourceforge.net>
2003-04-09 01:17:52 +0000

    Finish implementation.
    svn path=/trunk/; revision=4748

--- bindings/octave/PLplot/griddata.m
+++ bindings/octave/PLplot/griddata.m
@@ -26,9 +26,12 @@
 ##         GRID_CSA: Bivariate Cubic Spline approximation
 ##         GRID_DTLI: Delaunay Triangulation Linear Interpolation 
 ##         GRID_NNI: Natural Neighbors Interpolation 
-##         GRID_NNIDW: Nearest Neighbors Inverse Distance Weighted (default)
+##         GRID_NNIDW: Nearest Neighbors Inverse Distance Weighted
 ##         GRID_NNLI: Nearest Neighbors Linear Interpolation 
-##         GRID_NNAIDW:  Nearest Neighbors Around Inverse Distance Weighted 
+##         GRID_NNAIDW:  Nearest Neighbors Around Inverse Distance Weighted (default) 
+##
+##   Some algorithms can generate NaNs, and as PLplot is not NaN aware the
+##   returned data must be treated before being plotted, e.g., zg(isnan (zg)) = 0;
 ##
 ##   (*) Some algorithms might not be present in your system, depending
 ##    on the availability of the QHull library.
@@ -37,28 +40,52 @@
 
 function  zg = griddata(x, y, z, xg, yg, method, arg)
 
+  __pl_init;
+
+  global GRID_CSA GRID_DTLI GRID_NNI GRID_NNIDW GRID_NNLI GRID_NNAIDW
+
   if (nargin < 5)
     help griddata
-    return
+    return;
   endif
 
   if (is_vector(x) && is_vector(y) && is_vector(z) &&
       is_vector(xg) && is_vector(yg))
     
-    [nr, nc] = size(x);
-    len = nc;
-    if (nc == 1 && nr > 1)
-      x = x';
-      len = nr;
+    [err, x, y, z] = common_size(x, y, z);
+    if (err)
+      help griddata
+      return;
     endif
 
-    [r, c] = size(y);
-    if (c == 1 && r > 1)
-      y = y';
-      if ( 
-      len = nc;
+    if (nargin == 5)
+      method = GRID_NNAIDW;
+      arg = 15;
+    elseif (nargin == 6)
+      arg = 0;
     endif
 
+    if (! any(method == [GRID_CSA GRID_DTLI GRID_NNI GRID_NNIDW GRID_NNLI GRID_NNAIDW]))
+      help griddata
+      return
+    endif
 
+    if (rows(xg) == 1)
+      xg = xg';
+    endif
+
+    if (rows(yg) == 1)
+      yg = yg';
+    endif
+
+    zg = plgriddata(x, y, z, xg, yg, method, arg);
+    ##if (any(any(isnan(zg))))
+    ##  warning("griddata: NaNs returned.");
+    ##endif
+
+  else
+    help griddata
+    return
   endif 
+
 endfunction
