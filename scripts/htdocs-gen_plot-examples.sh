a346ec3f4bbcd4afeb2c99ee240f8acdeabd8945
Joao Cardoso <jcard@users.sourceforge.net>
2003-04-15 19:15:36 +0000

    Generate the x21 demo plots. Adapt to the new directory structure.
    svn path=/trunk/; revision=4774

--- scripts/htdocs-gen_plot-examples.sh
+++ scripts/htdocs-gen_plot-examples.sh
@@ -7,36 +7,44 @@
 # i.e. 'htdocs' must appear when a 'ls' is issued.
 # 
 # The tarball can be copied to sf as, e.g.,
-#    scp tmp/htdocs_plot_examples.tgz USER@login.sf.net:/home/groups/p/pl/plplot/htdocs_plot_examples.tgz
+#    scp htdocs_plot_examples.tgz \
+#        USER@login.sf.net:/home/groups/p/pl/plplot/htdocs_plot_examples.tgz
 #
-
 # This script must be run in the top plplot source directory as:
 #     scripts/htdocs-gen_plot-examples.sh
 
 # a fresh make, with minimum bindings
-make clean
-make configure
 
-./configure --enable-dyndrivers --prefix=/usr/local --with-double --with-freetype \
---disable-cxx --disable-python --disable-java --disable-itcl --disable-octave --enable-f77
+build=true
 
-make
-make cdemos
+if $build="true"; then
+    make clean
+    make configure
 
-cd tmp
+    ./configure --disable-static --enable-dyndrivers --prefix=/tmp/plplot \
+    --disable-cxx --disable-python --disable-java --disable-itcl \
+    --disable-octave --enable-f77
+
+    make
+    make install
+fi
 
 # remove htdocs directory, left from a previous script execution
 rm -rf htdocs
 
 # for all C demos executables:
+(cd examples/c; make check)
 
-for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20; do
+# hack, x20c needs lena in the current directory
+cp examples/c/lena.pgm .
+
+for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20 21; do
 
     echo Working in example ${exe}
 
     # generate standard and preview size images
-    ./x${exe}c -dev png -o x${exe} -fam -fflen 2;
-    ./x${exe}c -dev png -o prev-x${exe} -fam -fflen 2 -geometry 200x150;
+    examples/c/x${exe}c -dev png -o x${exe} -fam -fflen 2;
+    examples/c/x${exe}c -dev png -o prev-x${exe} -fam -fflen 2 -geometry 200x150;
 
     # give png extension
     for i in `ls x${exe}.?? prev-x${exe}.??`; do
@@ -47,9 +55,9 @@ for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20; do
     echo populatting www directory demo${exe}
     mkdir -p htdocs/examples/demo${exe}
     mv *${exe}.??.png htdocs/examples/demo${exe}/
-    cp x${exe}c.c ../examples/tcl/x${exe}.tcl ../examples/java/x${exe}.java \
-	x${exe}f.f ../bindings/octave/demos/x${exe}c.m ../examples/python/xw${exe}.py \
-	htdocs/examples/demo${exe}/
+    cp examples/c/x${exe}c.c examples/tcl/x${exe}.tcl examples/java/x${exe}.java \
+	examples/f77/x${exe}f.f bindings/octave/demos/x${exe}c.m \
+	examples/python/xw${exe}.py htdocs/examples/demo${exe}
 
     # rename executables, to avoid browsers trying to execute files instead of showing them.
     (cd  htdocs/examples/demo${exe};
@@ -60,6 +68,10 @@ for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20; do
 
 done
 
+# remove hack
+rm -f lena.pgm
+
 # create the tarball
+find htdocs -size 0 -exec rm {} \;
 tar cvzf htdocs_plot_examples.tgz htdocs
 rm -rf htdocs
