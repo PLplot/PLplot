77b189ed2caead62defdba19ae8f4f582cce52d3
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-04-16 18:18:09 +0000

    Changed the license terms to LGPL (with Wesley Ebisuzaki'a authorization) and added copyright notices for the people that contributed in the past to the code in this file.  This makes a nice example of how to change the licence and copyright notices in PLplot files.
    svn path=/trunk/; revision=4781

--- src/plmap.c
+++ src/plmap.c
@@ -11,10 +11,32 @@
 	the background has been drawn, one can use a contour routine or a
 	symbol plotter to finish off the plot.
 
-	Copyright 1991,1993,1994  Wesley Ebisuzaki
+	Copyright (C) 1991, 1993, 1994  Wesley Ebisuzaki
+        Copyright (C) 1994, 2000, 2001  Maurice LeBrun
+        Copyright (C) 1999  Geoffrey Furnish
+        Copyright (C) 2000, 2001, 2002  Alan W. Irwin
+        Copyright (C) 2001  Andrew Roach
+        Copyright (C) 2001  Rafael Laboissiere
+        Copyright (C) 2002  Vincent Darley
+        Copyright (C) 2003  Joao Cardoso
 
-	This program can be freely distributed, sale except for media
-	costs is prohibited.
+        This file is part of PLplot.
+
+	PLplot is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Library General Public License
+	as published by the Free Software Foundation; version 2 of the
+	License.
+
+	PLplot is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+        USA
+
 */
 
 #include "plplotP.h"
