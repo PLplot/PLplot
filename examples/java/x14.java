6352d6eba606ebf87e5734bcb7218b86ee32734c
Alan W. Irwin <airwin@users.sourceforge.net>
2003-09-02 00:43:30 +0000

    Put in copyright and copyright conditions (LGPL).
    svn path=/trunk/; revision=4849

--- examples/java/x14.java
+++ examples/java/x14.java
@@ -1,6 +1,26 @@
 //---------------------------------------------------------------------------//
 // $Id$
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+// Copyright (C) 2001  Geoffrey Furnish
+// This file is part of PLplot.
 //
+// This file is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the file; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 // Implementation of PLplot example 14 in Java.
 //---------------------------------------------------------------------------//
 
