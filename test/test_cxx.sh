b66d3c52fc8f419bfa186e2862e92db2391a5dfb
Alan W. Irwin <airwin@users.sourceforge.net>
2003-09-02 01:39:50 +0000

    Take advantage of command-line options that are now available for C++. Also, do all the new C++ examples.
    svn path=/trunk/; revision=4856

--- test/test_cxx.sh
+++ test/test_cxx.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 # Test suite for c++ examples.  
-# This is called from plplot-test.sh with $cxxdir, $device, and 
-# $dsuffix defined.
+# This is called from plplot-test.sh with $cxxdir, $device, $dsuffix,
+# and $options defined.
 
 # To build the c++ examples before running this script do the following:
 # pushd $cxxdir; make; popd 
 
-# Eventually, argument parsing will be added to the c++ api, and this
-# file will be greatly simplified after that while allowing additional $options
-# (such as familying for the png device) to be enabled.
-$cxxdir/x01cc <<EOF >/dev/null
-$device
-x01cc.$dsuffix
-EOF
+$cxxdir/x01cc -dev $device -o x01cc.$dsuffix $options
+$cxxdir/x01 -dev $device -o x01cxx.$dsuffix $options
+$cxxdir/x02 -dev $device -o x02cxx.$dsuffix $options
+$cxxdir/x03 -dev $device -o x03cxx.$dsuffix $options
+$cxxdir/x04 -dev $device -o x04cxx.$dsuffix $options
+$cxxdir/x05 -dev $device -o x05cxx.$dsuffix $options
+$cxxdir/x06 -dev $device -o x06cxx.$dsuffix $options
+$cxxdir/x07 -dev $device -o x07cxx.$dsuffix $options
+$cxxdir/x08 -dev $device -o x08cxx.$dsuffix $options
+$cxxdir/x09 -dev $device -o x09cxx.$dsuffix $options
+$cxxdir/x10 -dev $device -o x10cxx.$dsuffix $options
+$cxxdir/x11 -dev $device -o x11cxx.$dsuffix $options
+$cxxdir/x12 -dev $device -o x12cxx.$dsuffix $options
+$cxxdir/x13 -dev $device -o x13cxx.$dsuffix $options
+#skip x14 since uses tk device.
+$cxxdir/x15 -dev $device -o x15cxx.$dsuffix $options
+$cxxdir/x16 -dev $device -o x16cxx.$dsuffix $options
+#skip x17 which is only meant for interactive execution
+$cxxdir/x18 -dev $device -o x18cxx.$dsuffix $options
+$cxxdir/x19 -dev $device -o x19cxx.$dsuffix $options
+#skip x20 which is only meant for interactive execution
+$cxxdir/x21 -dev $device -o x21cxx.$dsuffix $options
