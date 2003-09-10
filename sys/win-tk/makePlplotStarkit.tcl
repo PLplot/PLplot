8618f41a8edc8ebca7a7ebc7fd31e0b7b9ed50d8
Vince Darley <vincentdarley@users.sourceforge.net>
2003-09-10 14:34:26 +0000

    updated build for current plplot config
    svn path=/trunk/; revision=4869

--- sys/win-tk/makePlplotStarkit.tcl
+++ sys/win-tk/makePlplotStarkit.tcl
@@ -1,10 +1,16 @@
 puts stdout "Making plplotter starkit" ; update
+catch {
+    console show
+    wm withdraw .
+    update
+}
+set vers "5.2.1"
 cd [file dirname [info script]]
 catch {file delete plplotter.kit}
 file delete -force plplotter.vfs
 file mkdir plplotter.vfs
 file mkdir plplotter.vfs/lib
-file mkdir plplotter.vfs/lib/plplotter5.1.1
+file mkdir plplotter.vfs/lib/plplotter${vers}
 file mkdir plplotter.vfs/examples
 file mkdir plplotter.vfs/examples/tcl
 file mkdir plplotter.vfs/examples/tk
@@ -16,19 +22,19 @@ foreach pat {*.tcl tclIndex *.dat *.log} {
 foreach f [glob -dir ../../examples/tk *.tcl] {
     file copy $f plplotter.vfs/examples/tk
 }
-foreach f [glob -dir ../../lib *.{map,fnt}] {
-    file copy $f plplotter.vfs/lib/plplotter5.1.1
+foreach f [glob -dir ../../data *.{map,fnt}] {
+    file copy $f plplotter.vfs/lib/plplotter${vers}
 }
 foreach f [glob -dir ../../bindings/tk pl{color,plot,defaults,tools,widget}.tcl] {
-    file copy $f plplotter.vfs/lib/plplotter5.1.1
+    file copy $f plplotter.vfs/lib/plplotter${vers}
 }
 foreach f [glob -dir ../../bindings/tk-x-plat *.tcl] {
-    file copy $f plplotter.vfs/lib/plplotter5.1.1
+    file copy $f plplotter.vfs/lib/plplotter${vers}
 }
-file copy pkgIndex.tcl plplotter.vfs/lib/plplotter5.1.1
-file copy tclIndex plplotter.vfs/lib/plplotter5.1.1
+file copy pkgIndex.tcl plplotter.vfs/lib/plplotter${vers}
+file copy tclIndex plplotter.vfs/lib/plplotter${vers}
 foreach f [glob *.dll] {
-    file copy $f plplotter.vfs/lib/plplotter5.1.1
+    file copy $f plplotter.vfs/lib/plplotter${vers}
 }
 
 # Make our 'main.tcl'
