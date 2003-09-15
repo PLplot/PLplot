486c5938681e8abe5dbeff5ee50be021b7bc6b5b
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-09-15 19:08:05 +0000

    Added --force option to the call of libtoolize, forcing the file ltmain.sh to be overwritten.  This is crucial when running bootstrap.sh with a new version of Libtool in a non-fresh build tree.
    svn path=/trunk/; revision=4871

--- bootstrap.sh
+++ bootstrap.sh
@@ -95,7 +95,7 @@ aclocal_opts=${aclocal_opts:="-I /usr/share/libtool/libltdl"}
 
 run aclocal $aclocal_opts \
   && run autoheader \
-  && run libtoolize --copy --ltdl --automake \
+  && run libtoolize --force --copy --ltdl --automake \
   && run automake --add-missing --copy \
   && run autoconf \
   && ( echo -n "Regenerating libltdl/configure..."; \
