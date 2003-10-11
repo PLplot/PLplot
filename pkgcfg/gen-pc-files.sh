ae060f71c14761090b8e9550ff5d541d59422035
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-10-11 13:40:37 +0000

    Total redesign of the pkg-config support for PLplot
    Read the pkgcfg/README file for details.
    
    svn path=/trunk/; revision=4952

---
+++ pkgcfg/gen-pc-files.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+FILE_INFO="\
+    ::: \
+    -c++:C++:C++_bindings,_:cxx \
+    -f77:F77:Fortran77_bindings,_:f77 \
+    -tcl:Tcl/Tk:Tcl/Tk_bindings,_:tcltk"
+
+MASTER=plplot-master-pc
+
+for f in $FILE_INFO ; do
+    pcext=`echo $f | cut -d: -f1`
+    name=`echo $f | cut -d: -f2`
+    desc=`echo $f | cut -d: -f3 | sed "s/_/ /g`
+    lib=`echo $f | cut -d: -f4`
+    sed "s:#NAME#:PLplot $name:" < $MASTER \
+        | sed "s:#DESC#:Scientific plotting library (${desc}single precision):" \
+	| sed "s:#LIB#:plplot$lib:" > plplot$pcext.pc
+    sed "s:#NAME#:PLplot $name:" < $MASTER \
+        | sed "s:#DESC#:Scientific plotting library (${desc}double precision):" \
+	| sed "s:#LIB#:plplot${lib}d:" > plplotd$pcext.pc
+done
