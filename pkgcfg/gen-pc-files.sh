8bd2a13f039471a623de0521f5138ef64cb45d11
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-10-15 21:31:30 +0000

    Added some output messages
    In the for loop, messages like "Generated plplot(...).pc" are now echoed.
    This will help to diagnose problems in the future.
    
    Also, some small reformatting of the code was done.
    
    svn path=/trunk/; revision=4955

--- pkgcfg/gen-pc-files.sh
+++ pkgcfg/gen-pc-files.sh
@@ -9,14 +9,20 @@ FILE_INFO="\
 MASTER=plplot-master-pc
 
 for f in $FILE_INFO ; do
+
     pcext=`echo $f | cut -d: -f1`
     name=`echo $f | cut -d: -f2`
     desc=`echo $f | cut -d: -f3 | sed "s/_/ /g"`
     lib=`echo $f | cut -d: -f4`
+
     sed "s:#NAME#:PLplot $name:" < $MASTER \
         | sed "s:#DESC#:Scientific plotting library (${desc}single precision):" \
 	| sed "s:#LIB#:plplot$lib:" > plplot$pcext.pc
+    echo "Generated plplot$pcext.pc"
+
     sed "s:#NAME#:PLplot $name:" < $MASTER \
         | sed "s:#DESC#:Scientific plotting library (${desc}double precision):" \
 	| sed "s:#LIB#:plplot${lib}d:" > plplotd$pcext.pc
+    echo "Generated plplotd$pcext.pc"
+
 done
