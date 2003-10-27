3e27d222beb0f1967d3e3999b79a4bd1bc3153df
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2003-10-27 07:46:56 +0000

    Made make-cvs-tarball script more robust to premature exit.
    Added a cleanup function that is called for the trapped signals 0, HUP,
    QUIT, INT, TERM, and PIPE.  This insures that the temporary directory
    $CVSTMPDIR is removed at exit.  Also, a caveat note is added at the comments
    on the top of the file.
    
    svn path=/trunk/; revision=4974

--- scripts/make-cvs-tarball.sh
+++ scripts/make-cvs-tarball.sh
@@ -3,24 +3,30 @@
 # Copyright (C) 2003  Rafael Laboissiere
 # This script is in the Public Domain
 
-# This will prepare a tdistribution tarbal directly from the CVS sources of
-# the PLplot project
+# This will prepare a distribution tarbal directly from the CVS sources of
+# the PLplot project.
+# CAVEAT: this script silently and completely deletes a directory called 
+# plplot-cvs-tarball existent in the current working directory.
 
 SF_USER=${USER:-rlaboiss}
 SF_CVSROOT=cvs.plplot.sourceforge.net:/cvsroot/plplot
-
+VERSION=${1:+--version=$1}
 CVSTMPDIR=plplot-cvs-tarball
 
-rm -rf $CVSTMPDIR
-cvs -d${SF_USER}@$SF_CVSROOT export -d$CVSTMPDIR -D now plplot
-cd $CVSTMPDIR
+cleanup ( ) {
+    rm -rf $CVSTMPDIR
+}
 
-VERSION=${1:+--version=$1}
-./bootstrap.sh ${VERSION:---date-version}
-./configure --enable-builddoc
-make dist
-TARBALL=`ls plplot-*.tar.gz`
-mv $TARBALL ..
-cd ..
-rm -rf $CVSTMPDIR
-echo "CVS distribution tarball: $TARBALL"
+trap "cleanup" 0 HUP INT QUIT PIPE TERM
+
+cleanup
+
+cvs -d${SF_USER}@$SF_CVSROOT export -d$CVSTMPDIR -D now plplot \
+  && cd $CVSTMPDIR \
+  && ./bootstrap.sh ${VERSION:---date-version} \
+  && ./configure --enable-builddoc \
+  && make dist \
+  && TARBALL=`ls plplot-*.tar.gz` \
+  && mv $TARBALL .. \
+  && cd .. \
+  && echo "CVS distribution tarball: $TARBALL"
