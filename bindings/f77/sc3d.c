cd77eb3d0321e2ef51df4fee73620b8048701f35
Alan W. Irwin <airwin@users.sourceforge.net>
2003-12-27 22:04:15 +0000

    * Implement PLMESHC, PLOT3DC (actually reorganize so PLMESH, PLMESHC, and PLOT3D all call PLOT3DC in analogy with code in plot3d.c) for the fortran interface.
    * implement PLSURF3D for the fortran interface.
    
    * Change eighth fortran example to produce identical file results to the
    corresponding C example.  This tests the implementation of PLSURF3D.
    
    svn path=/trunk/; revision=5036

--- bindings/f77/sc3d.c
+++ bindings/f77/sc3d.c
@@ -6,8 +6,9 @@
 #include "plstubs.h"
 
 void
-PLMESH(PLFLT *x, PLFLT *y, PLFLT *z,
-       PLINT *nx, PLINT *ny, PLINT *opt, PLINT *lx)
+PLOT3DC(PLFLT *x, PLFLT *y, PLFLT *z,
+       PLINT *nx, PLINT *ny, PLINT *opt,
+       PLFLT *clevel, PLINT *nlevel, PLINT *lx)
 {
     int i, j;
     PLFLT **temp;
@@ -18,7 +19,7 @@ PLMESH(PLFLT *x, PLFLT *y, PLFLT *z,
        direction. */
 
     if ( ! (temp = (PLFLT **) malloc((size_t) * nx * sizeof(PLFLT *)))) {
-	plabort("PLMESH: Out of memory");
+	plabort("PLOT3DC: Out of memory");
 	return;
     }
 
@@ -29,7 +30,7 @@ PLMESH(PLFLT *x, PLFLT *y, PLFLT *z,
 	    for (ii = 0; ii < i-1; ii++)
 		free((void *) temp[i]);
 	    free((void *) temp);
-	    plabort("PLMESH: Out of memory");
+	    plabort("PLOT3DC: Out of memory");
 	    return;
 	}
     }
@@ -38,12 +39,75 @@ PLMESH(PLFLT *x, PLFLT *y, PLFLT *z,
 	for (j = 0; j < *ny; j++)
 	    temp[i][j] = *(z + j * *lx + i);
 
-    c_plmesh(x, y, temp, *nx, *ny, *opt);
+    c_plot3dc(x, y, temp, *nx, *ny, *opt, clevel, *nlevel);
 
     for (i = 0; i < *nx; i++)
 	free((void *) temp[i]);
 
     free((void *) temp);
+}
+
+void
+PLSURF3D(PLFLT *x, PLFLT *y, PLFLT *z,
+       PLINT *nx, PLINT *ny, PLINT *opt,
+       PLFLT *clevel, PLINT *nlevel, PLINT *lx)
+{
+    int i, j;
+    PLFLT **temp;
+
+    /* Create the vectored C matrix from the Fortran matrix */
+    /* To make things easy we save a temporary copy of the transpose of the
+       Fortran matrix, so that the first dimension of z corresponds to the x
+       direction. */
+
+    if ( ! (temp = (PLFLT **) malloc((size_t) * nx * sizeof(PLFLT *)))) {
+	plabort("PLSURF3D: Out of memory");
+	return;
+    }
+
+    for (i = 0; i < *nx; i++) {
+	if ( ! (temp[i] = (PLFLT *) malloc((size_t) * ny * sizeof(PLFLT)))) {
+	    int ii;
+
+	    for (ii = 0; ii < i-1; ii++)
+		free((void *) temp[i]);
+	    free((void *) temp);
+	    plabort("PLSURF3D: Out of memory");
+	    return;
+	}
+    }
+
+    for (i = 0; i < *nx; i++)
+	for (j = 0; j < *ny; j++)
+	    temp[i][j] = *(z + j * *lx + i);
+
+    c_plsurf3d(x, y, temp, *nx, *ny, *opt, clevel, *nlevel);
+
+    for (i = 0; i < *nx; i++)
+	free((void *) temp[i]);
+
+    free((void *) temp);
+}
+
+void
+PLMESH(PLFLT *x, PLFLT *y, PLFLT *z,
+       PLINT *nx, PLINT *ny, PLINT *opt, PLINT *lx)
+{
+   PLINT optlocal, nlevel = 0;
+   PLFLT clevel = 0.;
+   
+   optlocal = *opt | MESH;
+   PLOT3DC(x, y, z, nx, ny, &optlocal, &clevel, &nlevel, lx);
+}
+
+void
+PLMESHC(PLFLT *x, PLFLT *y, PLFLT *z,
+       PLINT *nx, PLINT *ny, PLINT *opt, 
+       PLFLT *clevel, PLINT *nlevel, PLINT *lx)
+{
+   PLINT optlocal;
+   optlocal = *opt | MESH;
+   PLOT3DC(x, y, z, nx, ny, &optlocal, clevel, nlevel, lx);
 }
 
 
@@ -51,39 +115,10 @@ void
 PLOT3D(PLFLT *x, PLFLT *y, PLFLT *z,
        PLINT *nx, PLINT *ny, PLINT *opt, PLINT *side, PLINT *lx)
 {
-    int i, j;
-    PLFLT **temp;
-
-    /* Create the vectored C matrix from the Fortran matrix */
-    /* To make things easy we save a temporary copy of the transpose of the
-       Fortran matrix, so that the first dimension of z corresponds to the x
-       direction. */
-
-    if ( ! (temp = (PLFLT **) malloc((size_t) * nx * sizeof(PLFLT *)))) {
-	plabort("PLOT3D: Out of memory");
-	return;
-    }
-
-    for (i = 0; i < *nx; i++) {
-	if ( ! (temp[i] = (PLFLT *) malloc((size_t) * ny * sizeof(PLFLT)))) {
-	    int ii;
-
-	    for (ii = 0; ii < i-1; ii++)
-		free((void *) temp[i]);
-	    free((void *) temp);
-	    plabort("PLOT3D: Out of memory");
-	    return;
-	}
-    }
-
-    for (i = 0; i < *nx; i++)
-	for (j = 0; j < *ny; j++)
-	    temp[i][j] = *(z + j * *lx + i);
-
-    c_plot3d(x, y, temp, *nx, *ny, *opt, *side);
-
-    for (i = 0; i < *nx; i++)
-	free((void *) temp[i]);
-
-    free((void *) temp);
+   PLINT optlocal, nlevel = 0;
+   PLFLT clevel = 0.;
+   
+   optlocal = *opt | (*side == 1 ? DRAW_SIDES : 0);
+   PLOT3DC(x, y, z, nx, ny, &optlocal, &clevel, &nlevel, lx);
 }
+
