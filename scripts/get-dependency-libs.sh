43843f9cfd1c1b77bec790e853c277b7fafd8e0e
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2004-01-16 08:25:14 +0000

    Only include -L$path/.libs conditionally on the existence of the $path/.libs directory.
    svn path=/trunk/; revision=5151

--- scripts/get-dependency-libs.sh
+++ scripts/get-dependency-libs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if test $# != 1 ; then
-  echo 2>1 "Usage: $0 path/to/libfoo.la"
-  exit 1
+    echo 2>1 "Usage: $0 path/to/libfoo.la"
+    exit 1
 fi
 
 result=
@@ -10,20 +10,22 @@ result=
 for lib in `grep ^dependency_libs= $1 \
                | sed 's/^dependency_libs=//' | sed "s/'//g"` ; do
 
-    case $lib in 
+    case $lib in
 
         -l* | -L* )
             result="$result $lib"
-	;;
-	    
-        *.la)
-	    path=`echo $lib | sed 's:/*lib[^/]*.la::'`
-	    libname=`echo $lib | sed 's:^.*lib::' | sed 's/\.la//'`
-	    result="$result -L$path -L$path/.libs -l$libname"
         ;;
-	    
+
+        *.la)
+            path=`echo $lib | sed 's:/*lib[^/]*.la::'`
+            libname=`echo $lib | sed 's:^.*lib::' | sed 's/\.la//'`
+            result="$result -L$path"
+            test -d $path/.libs && result="$result -L$path/.libs"
+            result="$result -l$libname"
+        ;;
+
     esac
-    
+
 done
 
 echo $result
