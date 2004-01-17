476a504dd35e7bba8cb583836049b48e24c05a4b
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2004-01-17 15:38:10 +0000

    make-cvs-tarball.sh accepts now a third argument which must be a CVS tag. This allows to build CVS tarballs from a specific CVS branch.
    svn path=/trunk/; revision=5158

--- scripts/make-cvs-tarball.sh
+++ scripts/make-cvs-tarball.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-# Copyright (C) 2003  Rafael Laboissiere
-# This script is in the Public Domain
-
 # This will prepare a distribution tarbal directly from the CVS sources of
 # the PLplot project.
-# CAVEAT: this script silently and completely deletes a directory called 
+# CAVEAT: this script silently and completely deletes a directory called
 # plplot-cvs-tarball existent in the current working directory.
+
+# Copyright (C) 2003, 2004  Rafael Laboissiere
+#
+# This file is part of PLplot.
+#
+# PLplot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Library Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PLplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with PLplot; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 SF_USER=${USER:-rlaboiss}
 SF_CVSROOT=cvs.plplot.sourceforge.net:/cvsroot/plplot
 VERSION=${1:+--version=$1}
+BRANCH=${2:+-r $2}
+BRANCH=${BRANCH:--D now}
 CVSTMPDIR=plplot-cvs-tarball
 
 cleanup ( ) {
