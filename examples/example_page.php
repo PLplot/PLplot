5fe98cf0afed0b13087dc8920b876eba07e80487
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2004-02-06 11:39:16 +0000

    Use a PHP function instead of copy-and-paste code repeated in all demo*/index.php files.  This will simplify maintenance in teh future, since the code is now concentrated in a single place.
    The results should be identical to what we had before.  The index.php files
    look now like this (e.g. for demo 16):
    
        <?php
        include ("../example_page.php");
        demo_page (16, 5, $lbind);
        ?>
    
    The first two arguments of the demo_page function (defined in the new PHP
    file examples/example_page.php) are the demo number and the number of plot
    images (x*.*.png files).  The third argument must always be "$lbind".  That
    is ugly and I will find a better solution later.  Please notice that demo20
    produces a wrong result.  I will fix this eventually.
    
    Also, the demo files will be at the htdocs root directory instead of in the
    examples directory.
    
    svn path=/www/trunk/; revision=7552

---
+++ examples/example_page.php
@@ -0,0 +1,90 @@
+<?php
+
+function demo_page ($demo, $nfigs, $language) {
+
+  include ("example_head.html");
+
+  $demo_str = sprintf ("%02d", $demo);
+  $demo_dir = "../../demo/demo$demo_str";
+
+  print <<<END
+<div ALIGN="CENTER"><b>Select to see the example $demo source code</b></div>
+
+<div ALIGN="CENTER">
+<form ACTION="index.php" METHOD="GET">
+
+<input TYPE="submit" NAME="lbind" VALUE="C">
+<input TYPE="submit" NAME="lbind" VALUE="F77">
+<input TYPE="submit" NAME="lbind" VALUE="Tcl">
+<input TYPE="submit" NAME="lbind" VALUE="Java">
+<input TYPE="submit" NAME="lbind" VALUE="Python">
+<input TYPE="submit" NAME="lbind" VALUE="Octave">
+
+</form>
+</div>
+<br>
+END;
+
+  switch ($language) {
+  case ("C"):
+    $fname = "x" . $demo_str . "c.c-";
+    break;
+  case ("F77"):
+    $fname = "x" . $demo_str . "f.f-";
+    break;
+  case ("Tcl"):
+    $fname = "x" . $demo_str . ".tcl-";
+    break;
+  case ("Java"):
+    $fname = "x" . $demo_str . ".java-";
+    break;
+  case ("Python"):
+    $fname = "xw" . $demo_str . ".py-";
+    break;
+  case ("Octave"):
+    $fname = "x" . $demo_str . "c.m-";
+    break;
+  default:
+    $fname = "x" . $demo_str . "c.c-";
+  }
+
+  echo '<OBJECT TYPE="text/plain" DATA="' . "$demo_dir/$fname"
+    . '" WIDTH=600 height=200></object>';
+
+  print <<<END
+<hr>
+<br>
+
+<div ALIGN="CENTER">
+<b>Select to view full-size image</b><br>
+<table>
+  <tbody>
+    <tr>
+
+END;
+
+  for ($ndx = 1; $ndx <= $nfigs; ++$ndx) {
+    if (($ndx > 1) and ($ndx % 3 == 1)) {
+      print <<<END
+    </tr>
+    <tr>
+END;
+    }
+    $ndx_str = sprintf ("%02d", $ndx);
+    echo "      <td> <A HREF=\"$demo_dir/x$demo_str.$ndx_str.png\">"
+      . "<IMG SRC=\"$demo_dir/prev-x$demo_str.$ndx_str.png\">"
+      . "</a> </td>\n";
+  }
+
+  print <<<END
+    </tr>
+  </tbody>
+</table>
+</div>
+END;
+
+  include ("example_tail.html");
+
+}
+
+?>
