d7a4c32a10f8b22fec22f4a7d29158469fa52ee5
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2004-11-12 15:02:46 +0000

    Added the lbind argument for the links to the other examples in the footer of the page.  This prevents the return to the default C binding when navigating through those links.  Also, dropped x17 from the list of examples.
    svn path=/www/trunk/; revision=7604

--- examples/example_page.php
+++ examples/example_page.php
@@ -7,7 +7,7 @@ function demo_page ($demo, $nfigs, $language) {
   $demo_str = sprintf ("%02d", $demo);
   $demo_dir = "../examples-data/demo$demo_str";
   $demos = array (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-                  15, 16, 17, 18, 19, 20, 21, 22);
+                  15, 16, 18, 19, 20, 21, 22);
 
   print <<<END
 <div ALIGN="CENTER"><b>Select to see the example $demo source code</b></div>
@@ -98,7 +98,7 @@ END;
   foreach ($demos as $i) {
     $demo_str = sprintf ("%02d", $i);
     if ($i != $demo) {
-      echo "<A HREF=\"demo$demo_str.php\">x$demo_str</A>\n";
+      echo "<A HREF=\"demo$demo_str.php?lbind=$language\">x$demo_str</A>\n";
     } else {
       echo "<b>x$demo_str\n</b>";
     }
