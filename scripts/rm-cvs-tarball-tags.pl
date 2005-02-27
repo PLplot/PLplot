988b52151af01fa8aed8a71ac7058c6600e07946
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2005-02-27 00:41:21 +0000

    Cosmetic changes.  Not yet fully tested.
    svn path=/trunk/; revision=6026

--- scripts/rm-cvs-tarball-tags.pl
+++ scripts/rm-cvs-tarball-tags.pl
@@ -8,20 +8,24 @@
 my $cf = "configure.ac";
 my $tag_pattern = 'cvs-tarball_\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d';
 
+sub remove_tag {
+  my $tag = shift;
+  print "Removing tag $tag\n";
+  system "cvs rtag -d $_ plplot";
+}
+
 -f $cf
   or die "E: $cf not found.  Are you at the PLplot top_srcdir?\n";
 
 (my $ac_init = `grep "AC_INIT(plplot" $cf`) =~ /plplot/
   or die "E: Current directory does not seem to have the PLplot sources.\n";
 
-open (CVS, "cvs -f log $cf |");
+open (LOG, "cvs -f log $cf |");
 
-my @tags = map {
+remove_tag ($_)
+  foreach map {
     /($tag_pattern)/;
     $1;
   } grep {
       /^\s*($tag_pattern):/;
-    } <CVS>;
-
-system "cvs rtag -d $_ plplot"
-  foreach @tags;
+    } <LOG>;
