f3d4df33a70c70b741bb557c0675c463ee32b3a2
Hazen Babcock <hbabcock@users.sourceforge.net>
2005-04-17 23:06:39 +0000

    Expanded font list to include the 30 expected fonts
    svn path=/trunk/; revision=6194

--- drivers/aqt.c
+++ drivers/aqt.c
@@ -101,14 +101,38 @@ static int windowYSize = 0;
 
 /* AquaTerm font look-up table */
 
-#define AQT_N_Type1Lookup 6
+#define AQT_N_Type1Lookup 30
 const FCI_to_FontName_Table AQT_Type1Lookup[AQT_N_Type1Lookup] = {
-     {0x10000000, "Helvetica"},
-     {0x10000001, "Times-Roman"},
-     {0x10000002, "Courier"},
-     {0x10000003, "Times-Roman"},
-     {0x10000004, "Symbol"},
-     {0x10000011, "Times-Italic"},
+	{0x10000000, "Helvetica"},
+	{0x10000001, "Times-Roman"},
+	{0x10000002, "Courier"},
+	{0x10000003, "Times-Roman"},
+	{0x10000004, "Symbol"},
+	{0x10000010, "Helvetica-Oblique"},
+	{0x10000011, "Times-Italic"},
+	{0x10000012, "Courier-Oblique"},
+	{0x10000013, "Times-Italic"},
+	{0x10000014, "Symbol"},
+	{0x10000020, "Helvetica-Oblique"},
+	{0x10000021, "Times-Italic"},
+	{0x10000022, "Courier-Oblique"},
+	{0x10000023, "Times-Italic"},
+	{0x10000024, "Symbol"},
+	{0x10000100, "Helvetica-Bold"},
+	{0x10000101, "Times-Bold"},
+	{0x10000102, "Courier-Bold"},
+	{0x10000103, "Times-Bold"},
+	{0x10000104, "Symbol"},
+	{0x10000110, "Helvetica-BoldOblique"},
+	{0x10000111, "Times-BoldItalic"},
+	{0x10000112, "Courier-BoldOblique"},
+	{0x10000113, "Times-BoldItalic"},
+	{0x10000114, "Symbol"},
+	{0x10000120, "Helvetica-BoldOblique"},
+	{0x10000121, "Times-BoldItalic"},
+	{0x10000122, "Courier-BoldOblique"},
+	{0x10000123, "Times-BoldItalic"},
+	{0x10000124, "Symbol"}
 };
 
 //---------------------------------------------------------------------
@@ -437,8 +461,6 @@ void proc_str (PLStream *pls, EscText *args)
                 angle:angle 
            shearAngle:shear 
                 align:(jst | ref)];
-
-//	[adapter addLabel:str atPoint:NSMakePoint((float)args->x*SCALE, (float)args->y*SCALE) angle:angle align:(jst | ref)];
 
     [str release];
 }
