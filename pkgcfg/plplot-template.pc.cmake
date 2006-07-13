e3c8b229813ab18b8720ad795cf6209e7bf24b05
Alan W. Irwin <airwin@users.sourceforge.net>
2006-07-13 07:32:43 +0000

    Initial commit of file that serves as the template for all CMake-generated *.pc files.
    svn path=/trunk/; revision=6761

---
+++ pkgcfg/plplot-template.pc.cmake
@@ -0,0 +1,6 @@
+Name: PLplot ${PC_SHORT_NAME}
+Description: Scientific plotting library (${PC_LONG_NAME}, ${PC_PRECISION} precision)
+Requires: ${PC_REQUIRES}
+Version: ${VERSION}
+Libs: ${PC_LINK_FLAGS}
+Cflags: ${PC_COMPILE_FLAGS} 
