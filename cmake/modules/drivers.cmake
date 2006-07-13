2d64b2a58403f94a7b7a871c0d8045c39c2e566d
Alan W. Irwin <airwin@users.sourceforge.net>
2006-07-13 07:25:24 +0000

    Tweak commentary.
    svn path=/trunk/; revision=6759

--- cmake/modules/drivers.cmake
+++ cmake/modules/drivers.cmake
@@ -25,12 +25,11 @@
 # ENABLE_DYNDRIVERS (ON or OFF): whether to dynamically load device drivers.
 # PLD_devicename (ON or OFF): whether each PLplot-related device is enabled
 #   or not.  devicename is png, jpeg, etc.
-# PLD_devicename_INCLUDE_DIR: compile (e.g., -I) options for each individual 
+# devicename_COMPILE_FLAGS: compile (e.g., -I) options for each individual 
 #   device.
-# PLD_devicename_LIBRARIES: link options (e.g., -L  and -l) for each
+# devicename_LINK_FLAGS: link options (e.g., -L  and -l) for each
 #   individual device.
-# DRIVERS_INCLUDE_DIR: concatanated compile options for all devices
-# DRIVERS_LIBRARIES: concatanated link options for all devices
+# DRIVERS_LINK_FLAGS: concatanated link options for all devices
 # DEVICES_LIST: list of devices (e.g. png, jpeg),where PLD_devicename is ON
 # DRIVERS_LIST: list of device drivers (e.g., gd for the png and jpeg devices)
 #   where at least one of the associated devices is enabled.
@@ -47,7 +46,7 @@ endif(ENABLE_DYNDRIVERS AND NOT BUILD_SHARED_LIBS)
 # Initialize device options (e.g., PLD_png is set to ON or OFF).
 include(drivers-init)
 
-# Find *_INC_DIR and *_LIBRARIES resources for device drivers that need them,
+# Find *_COMPILE_FLAGS and *_LINK_FLAGS resources for device drivers that need them,
 # and set appropriate PLD_devicename to OFF if the required resources are 
 # not available.
 #include(cgm)
