6c48e8dd53d25cc61f7adc3b75b8ba1e81ae8a99
Alan W. Irwin <airwin@users.sourceforge.net>
2006-07-17 18:27:36 +0000

    Add libdir, includedir, and drvdir definitions. Change over to "@ONLY" form.
    svn path=/trunk/; revision=6791

--- pkgcfg/plplot-template.pc.cmake
+++ pkgcfg/plplot-template.pc.cmake
@@ -1,6 +1,10 @@
-Name: PLplot ${PC_SHORT_NAME}
-Description: Scientific plotting library (${PC_LONG_NAME}, ${PC_PRECISION} precision)
-Requires: ${PC_REQUIRES}
-Version: ${VERSION}
-Libs: ${PC_LINK_FLAGS}
-Cflags: ${PC_COMPILE_FLAGS} 
+libdir=@LIB_DIR@
+includedir=@INCLUDE_DIR@
+drvdir=@LIB_DIR@/plplot@VERSION@/drivers@LIB_TAG@
+
+Name: PLplot @PC_SHORT_NAME@
+Description: Scientific plotting library (@PC_LONG_NAME@@PC_PRECISION@ precision)
+Requires: @PC_REQUIRES@
+Version: @VERSION@
+Libs: -L${libdir} @PC_LINK_FLAGS@
+Cflags: -I${includedir} @PC_COMPILE_FLAGS@ 
