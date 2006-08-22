59b33963cfc3824902bc6e77d3afd48794276317
Arjen Markus <arjenmarkus@users.sourceforge.net>
2006-08-22 05:44:55 +0000

    Added _MSC_VER for detecting Wnndows compiler
    svn path=/trunk/; revision=7020

--- include/pldll.h
+++ include/pldll.h
@@ -3,7 +3,7 @@
 
 #if defined(WIN32)
   /* Visual C/C++, Borland, MinGW and Watcom */
-  #if defined(__VISUALC__) || defined(__BORLANDC__) || defined(__GNUC__) || defined(__WATCOMC__)
+  #if defined(__VISUALC__) || defined(_MSC_VER) || defined(__BORLANDC__) || defined(__GNUC__) || defined(__WATCOMC__)
     #define PLDLLEXPORT __declspec(dllexport)
     #define PLDLLIMPORT __declspec(dllimport)
   #else
