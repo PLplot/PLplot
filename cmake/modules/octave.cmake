4aed0a764faab6b965033964b132cec276b07324
Alan W. Irwin <airwin@users.sourceforge.net>
2006-09-12 23:16:41 +0000

    Finish determining CMake variables needed to control octave interface build and install. These results look fine on the one system (Debian stable) tested so far, but are untested for an actual build since the octave interface build is not implemented yet.
    Use -DENABLE_octave=ON (the default is currently OFF) if you want to test
    what CMake octave control variables are set for your particular octave
    configuration, but that is all that option does at the current time.
    
    svn path=/trunk/; revision=7056

--- cmake/modules/octave.cmake
+++ cmake/modules/octave.cmake
@@ -29,28 +29,31 @@ if(ENABLE_octave AND NOT BUILD_SHARED_LIBS)
 endif(ENABLE_octave AND NOT BUILD_SHARED_LIBS)
 
 if(ENABLE_octave)
-#  find_program(OCTAVE octave)
-  find_program(OCTAVE octave2.0)
-  if(NOT OCTAVE)
+  find_program(OCTAVE octave)
+  if(OCTAVE)
+    message(STATUS "OCTAVE = ${OCTAVE}")
+  else(OCTAVE)
     message(STATUS "WARNING: "
     "octave not found. Disabling octave bindings")
     set(ENABLE_octave OFF CACHE BOOL "Enable Octave bindings" FORCE)
-  endif(NOT OCTAVE)
+  endif(OCTAVE)
 endif(ENABLE_octave)
 
 if(ENABLE_octave)
   find_program(MKOCTFILE mkoctfile)
-  if(NOT MKOCTFILE)
+  if(MKOCTFILE)
+    message(STATUS "MKOCTFILE = ${MKOCTFILE}")
+  else(MKOCTFILE)
     message(STATUS "WARNING: "
     "mkoctfile not found. Disabling octave bindings")
     set(ENABLE_octave OFF CACHE BOOL "Enable Octave bindings" FORCE)
-  endif(NOT MKOCTFILE)
+  endif(MKOCTFILE)
 endif(ENABLE_octave)
 
 if(ENABLE_octave)
   #MATWRAP is the path+filename of the matwrap script.
   set(MATWRAP "${CMAKE_CURRENT_SOURCE_DIR}/bindings/octave/matwrap/matwrap")
-  message(STATUS "Using built-in ${MATWRAP}")
+  message(STATUS "MATWRAP = ${MATWRAP}")
 
   #OCTAVE_VERSION is the (dotted triplet) octave version.
   execute_process(
@@ -68,13 +71,13 @@ if(ENABLE_octave)
   # PLPLOT_OCTAVE_DIR is the directory for installation of the PLplot_Octave
   # specific m files
   set(PLPLOT_OCTAVE_DIR ${CMAKE_INSTALL_PREFIX}/share/plplot_octave)
+  message(STATUS "PLPLOT_OCTAVE_DIR = ${PLPLOT_OCTAVE_DIR}")
   
   # OCTAVE_PREFIX is the prefix where octave was installed.
   # N.B. this file method is really clunky, but we are forced to use
   # this method because as far as I know there is no method
   # of invoking octave scripts from the octave command line other than
   # with a file.
-  
   file(WRITE ${CMAKE_BINARY_DIR}/octave_command
   "printf(octave_config_info(\"prefix\"));"
   )
@@ -83,23 +86,9 @@ if(ENABLE_octave)
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   OUTPUT_VARIABLE OCTAVE_PREFIX
   )
-  message(STATUS "OCTAVE_PREFIX = ${OCTAVE_PREFIX}")
+  #message(STATUS "OCTAVE_PREFIX = ${OCTAVE_PREFIX}")
   
-  #OCTAVE_M_DIR
-  # N.B. I have found that localfcnfiledir works fine for Debian stable
-  # versions of both octave 2.0.x and 2.1.x.  Therefore, unless somebody
-  # complains will keep this simple logic rather than doing more complicated
-  # localfcnfilepath logic for octave 2.0.x that occurs for ABS system.
-  file(WRITE ${CMAKE_BINARY_DIR}/octave_command
-  "printf(octave_config_info(\"localfcnfiledir\"));"
-  )
-  execute_process(
-  COMMAND ${OCTAVE} -q -f octave_command
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-  OUTPUT_VARIABLE OCTAVE_M_DIR
-  )
-  message(STATUS "OCTAVE_M_DIR = ${OCTAVE_M_DIR}")
-
+  # Logic that depends on octave version
   set(OCTAVE_TESTING_MINIMUM_VERSION "2.1.0")
   transform_version(
   NUMERICAL_OCTAVE_TESTING_MINIMUM_VERSION
@@ -111,11 +100,88 @@ if(ENABLE_octave)
    LESS
    "${NUMERICAL_OCTAVE_TESTING_MINIMUM_VERSION}"
    )
-    # Further processing of OCTAVE_M_DIR required if octave 2.0.x
+    # octave-2.0 logic.
+    #_OCTAVE_M_DIR
+    file(WRITE ${CMAKE_BINARY_DIR}/octave_command
+    "printf(octave_config_info(\"localfcnfilepath\"));"
+    )
+    execute_process(
+    COMMAND ${OCTAVE} -q -f octave_command
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    OUTPUT_VARIABLE __OCTAVE_M_DIR
+    )
+    #message(STATUS "__OCTAVE_M_DIR = ${__OCTAVE_M_DIR}")    
+    string(REGEX REPLACE
+    "^.*:([^:][^:]*)//$"
+    "\\1"
+    _OCTAVE_M_DIR
+    ${__OCTAVE_M_DIR}
+    )
+    #message(STATUS "_OCTAVE_M_DIR = ${_OCTAVE_M_DIR}")    
+    #OCTAVE_OCT_DIR
+    file(WRITE ${CMAKE_BINARY_DIR}/octave_command
+    "printf(octave_config_info(\"localoctfilepath\"));"
+    )
+    execute_process(
+    COMMAND ${OCTAVE} -q -f octave_command
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    OUTPUT_VARIABLE _OCTAVE_OCT_DIR
+    )
+    #message(STATUS "_OCTAVE_OCT_DIR = ${_OCTAVE_OCT_DIR}")    
+    string(REGEX REPLACE
+    "^.*:([^:][^:]*)//$"
+    "\\1"
+    OCTAVE_OCT_DIR
+    ${_OCTAVE_OCT_DIR}
+    )
+    #message(STATUS "OCTAVE_OCT_DIR = ${OCTAVE_OCT_DIR}")    
+  else(
+   NUMERICAL_OCTAVE_VERSION
+   LESS
+   "${NUMERICAL_OCTAVE_TESTING_MINIMUM_VERSION}"
+   )
+    # octave-2.1 (or higher) logic.
+    #_OCTAVE_M_DIR
+    file(WRITE ${CMAKE_BINARY_DIR}/octave_command
+    "printf(octave_config_info(\"localfcnfiledir\"));"
+    )
+    execute_process(
+    COMMAND ${OCTAVE} -q -f octave_command
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    OUTPUT_VARIABLE _OCTAVE_M_DIR
+    )
+    #OCTAVE_OCT_DIR
+    file(WRITE ${CMAKE_BINARY_DIR}/octave_command
+    "printf(octave_config_info(\"localoctfiledir\"));"
+    )
+    execute_process(
+    COMMAND ${OCTAVE} -q -f octave_command
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    OUTPUT_VARIABLE OCTAVE_OCT_DIR
+    )
   endif(
    NUMERICAL_OCTAVE_VERSION
    LESS
    "${NUMERICAL_OCTAVE_TESTING_MINIMUM_VERSION}"
    )
-#  message(FATAL_ERROR "")
+  # Replace the OCTAVE_PREFIX with the PLplot prefix in OCTAVE_M_DIR
+  string(REGEX REPLACE
+  "${OCTAVE_PREFIX}" 
+  "${CMAKE_INSTALL_PREFIX}"
+  OCTAVE_M_DIR
+  ${_OCTAVE_M_DIR}
+  )
+  message(STATUS "OCTAVE_M_DIR = ${OCTAVE_M_DIR}")
+  
+  # Transform OCTAVE_OCT_DIR if prefixes not the same.
+  if(NOT CMAKE_INSTALL_PREFIX STREQUAL "${OCTAVE_PREFIX}")
+    set(OCTAVE_OCT_DIR ${PLPLOT_OCTAVE_DIR})
+  endif(NOT CMAKE_INSTALL_PREFIX STREQUAL "${OCTAVE_PREFIX}") 
+  message(STATUS "OCTAVE_OCT_DIR = ${OCTAVE_OCT_DIR}")
+  
+  # Use DEFINE_PL_DOUBLE in bindings/octave/plplot_octave.h.in
+  # (I [AWI] presume this is to simplify the #include's).
+  if(PL_DOUBLE)
+    set(DEFINE_PL_DOUBLE "#define PL_DOUBLE")
+  endif(PL_DOUBLE)
 endif(ENABLE_octave)
