68df7f1467f806d96ace6e10c0dbe2d0668999d6
Werner Smekal <smekal@users.sourceforge.net>
2006-10-27 18:19:29 +0000

    TestNaNAware.c works now also for Visual C, Borland and all other compilers which have NaN correctly defined in lib/csa/nan.h. nan.h from lib/csa is now included to get the definitions right. Old code removed.
    svn path=/trunk/; revision=7116

--- cmake/modules/TestNaNAware.c
+++ cmake/modules/TestNaNAware.c
@@ -1,22 +1,6 @@
 /* Test for NaN awareness. */
 #include <stdio.h>
-#if defined(__GNUC__)
-static const double NaN = 0.0 / 0.0;
-#elif defined(BIG_ENDIAN) || defined(_BIG_ENDIAN)
-static const long long lNaN = 0x7fffffffffffffff;
-# define NaN (*(double*)&lNaN)
-#else
-#if !defined(_MSC_VER)
-static const long long lNaN = 0xfff8000000000000;
-# define NaN (*(double*)&lNaN)
-#else
-#include <float.h>
-#include <ymath.h>
-#undef NaN
-#define NaN _Nan._D
-#define isnan _isnan
-#endif
-#endif
+#include "nan.h"
 
 int
 main()
@@ -24,7 +8,7 @@ main()
    double x = NaN;
    /* N.B. CMake run-time tests need return of true (1) or false (0) not
     * the usual status code of 0 for success. */
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) | defined(__BORLANDC__)
    if (isnan(x)) return 1;
 #endif
 
