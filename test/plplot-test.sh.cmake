0b082dffa2ed4c1de3a2f7cec3086cdc7ba719f8
Alan W. Irwin <airwin@users.sourceforge.net>
2006-11-03 02:21:39 +0000

    Remove all instances of "//" from octavedir going slightly beyond the recommendations from Orion Poplawski and Rafael.  The reason for this change apparently is some screwup in the recursive directory support for octave-2.9.x.  Result gives a good build directory test (ctest) and a good installed examples test for our octave interface to PLplot for octave-2.1.69 (Debian stable) but needs further testing on other platforms (especially octave-2.9.x).
    svn path=/trunk/; revision=7144

--- test/plplot-test.sh.cmake
+++ test/plplot-test.sh.cmake
@@ -147,14 +147,16 @@ perldir=$SRC_EXAMPLES_DIR/perl
 javadir=$EXAMPLES_DIR/java
 octave=@OCTAVE@
 octavedir=\
-$EXAMPLES_DIR/../bindings/octave//:\
-$SRC_EXAMPLES_DIR/../bindings/octave/PLplot//:\
-$SRC_EXAMPLES_DIR/../bindings/octave/demos//:\
-$SRC_EXAMPLES_DIR/../bindings/octave/misc//:\
-$SRC_EXAMPLES_DIR/octave//:\
-@PLPLOT_OCTAVE_DIR@//:\
-@OCTAVE_M_DIR@//:\
-@OCTAVE_OCT_DIR@//:
+$EXAMPLES_DIR/../bindings/octave:\
+$SRC_EXAMPLES_DIR/../bindings/octave/PLplot:\
+$SRC_EXAMPLES_DIR/../bindings/octave/PLplot/support:\
+$SRC_EXAMPLES_DIR/../bindings/octave/demos:\
+$SRC_EXAMPLES_DIR/../bindings/octave/misc:\
+$SRC_EXAMPLES_DIR/octave:\
+@PLPLOT_OCTAVE_DIR@:\
+@PLPLOT_OCTAVE_DIR@/support:\
+@OCTAVE_M_DIR@/PLplot:\
+@OCTAVE_OCT_DIR@:
 PATH=$EXAMPLES_DIR/../utils:@exec_prefix@/bin:$PATH
 export cdir cxxdir f77dir f95dir pythondir javadir octave octavedir tcldir perldir PATH
 
