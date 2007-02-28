1527d222fd7dd4cabccba47fd55e2717a874b21f
Alan W. Irwin <airwin@users.sourceforge.net>
2007-02-28 19:26:02 +0000

    try_compile no longer works with new add_executable for Ada since there is no way to specify the target name (which is used to construct the source file name).  Therefore, replace try_compile with simple test of gnatmake using execute_process.
    svn path=/trunk/; revision=7391

--- cmake/modules/CMakeTestAdaCompiler.cmake
+++ cmake/modules/CMakeTestAdaCompiler.cmake
@@ -1,36 +1,46 @@
 # This file is used by EnableLanguage in cmGlobalGenerator to
-# determine that that selected Ada compiler can actually compile
+# determine that that the Ada builder gnatmake can actually compile
 # and link the most basic of programs.   If not, a fatal error
 # is set and cmake stops processing commands and will not generate
 # any makefiles or projects.
 IF(NOT CMAKE_Ada_COMPILER_WORKS)
-  MESSAGE(STATUS "Check for working Ada compiler: ${CMAKE_Ada_COMPILER}")
+  MESSAGE(STATUS "Check for working Ada builder: gnatmake")
   FILE(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testadacompiler.adb
   "procedure testAdaCompiler is\n"
   "     begin\n"
   "       null;\n"
   "     end;\n"
   )
-  TRY_COMPILE(CMAKE_Ada_COMPILER_WORKS ${CMAKE_BINARY_DIR} 
-    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testadacompiler.adb
-    OUTPUT_VARIABLE OUTPUT) 
+  execute_process(
+  COMMAND gnatmake testadacompiler.adb
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
+  RESULT_VARIABLE CMAKE_Ada_COMPILER_WORKS
+  OUTPUT_VARIABLE OUTPUT
+  ERROR_VARIABLE OUTPUT
+  )
+  if(NOT CMAKE_Ada_COMPILER_WORKS)
+    set(CMAKE_Ada_COMPILER_WORKS ON)
+  else(NOT CMAKE_Ada_COMPILER_WORKS)
+    set(CMAKE_Ada_COMPILER_WORKS OFF)
+  endif(NOT CMAKE_Ada_COMPILER_WORKS)
+
   SET(ADA_TEST_WAS_RUN 1)
 ENDIF(NOT CMAKE_Ada_COMPILER_WORKS)
 
 IF(NOT CMAKE_Ada_COMPILER_WORKS)
-  MESSAGE(STATUS "Check for working Ada compiler: ${CMAKE_Ada_COMPILER} -- broken")
+  MESSAGE(STATUS "Check for working Ada builder: gnatmake -- broken")
   FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
-    "Determining if the Ada compiler works failed with "
+    "Determining if the Ada builder works failed with "
     "the following output:\n${OUTPUT}\n\n")
-  MESSAGE(FATAL_ERROR "The Ada compiler \"${CMAKE_Ada_COMPILER}\" "
-    "is not able to compile a simple test program.\nIt fails "
+  MESSAGE(FATAL_ERROR "The Ada builder \"gnatmake\" "
+    "is not able to compile, bind, and link a simple test program.\nIt fails "
     "with the following output:\n ${OUTPUT}\n\n"
     "CMake will not be able to correctly generate this project.")
 ELSE(NOT CMAKE_Ada_COMPILER_WORKS)
   IF(ADA_TEST_WAS_RUN)
-    MESSAGE(STATUS "Check for working Ada compiler: ${CMAKE_Ada_COMPILER} -- works")
+    MESSAGE(STATUS "Check for working Ada builder: gnatmake -- works")
     FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
-      "Determining if the Ada compiler works passed with "
+      "Determining if the Ada builder works passed with "
       "the following output:\n${OUTPUT}\n\n") 
   ENDIF(ADA_TEST_WAS_RUN)
   SET(CMAKE_Ada_COMPILER_WORKS 1 CACHE INTERNAL "")
