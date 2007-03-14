00b574fdb31230741eeb0f58a44130a9a012c9f8
Alan W. Irwin <airwin@users.sourceforge.net>
2007-03-14 03:42:15 +0000

    White-space changes, add plptex3 demonstration.
    svn path=/trunk/; revision=7418

--- examples/c/x28c.c
+++ examples/c/x28c.c
@@ -1,6 +1,6 @@
 /* $Id$
 
-	plmtex3 demo.
+	plmtex3, plptex3 demo.
 */
 
 #include "plcdemos.h"
@@ -16,24 +16,28 @@ int main(int argc, char *argv[])
 {
 /* Parse and process command line arguments */
    
-       (void) plparseopts(&argc, argv, PL_PARSE_FULL);
+   (void) plparseopts(&argc, argv, PL_PARSE_FULL);
    
-	plinit();
-	pladv(0);
-	plvpor(0.0, 1.0, 0.0, 0.9);
-	plwind(-1.0, 1.0, -0.9, 1.1);
-	plw3d(1.0, 1.0, 1.0, -1.5, 1.5, -0.5, 1.5, 0.0, 2.0, 40.0, 60.0+270.0);
-
-    	plcol0(1);
-	plbox3(	"bnst", "", 0.0, 0,
-			"bnst", "", 0.0, 0,
-	     	"bcdmnstv", "", 0.0, 0);
-	plmtex3("xpv", 4.5, 0.5, 0.5, "one");
-	plmtex3("xs", 4.5, 0.5, 0.5, "two");
-	plmtex3("ypv", 4.5, 0.5, 0.5, "three");
-	plmtex3("ys", 4.5, 0.5, 0.5, "------- four ------");
-	plmtex3("zpv", 4.5, 0.5, 0.5, "five");
-	plmtex3("zs", 4.5, 0.5, 0.5, "six");
-
-	plend();
+   plinit();
+   pladv(0);
+   plvpor(0.0, 1.0, 0.0, 0.9);
+   plwind(-1.0, 1.0, -0.9, 1.1);
+   plw3d(1.0, 1.0, 1.0, -1.5, 1.5, -0.5, 1.5, 0.0, 2.0, 40.0, 60.0 + 270.0);
+   
+   plcol0(1);
+   plbox3("bnst", "", 0.0, 0,
+	  "bnst", "", 0.0, 0,
+	  "bcdmnstv", "", 0.0, 0);
+   plmtex3("xpv", 4.5, 0.5, 0.5, "one");
+   plmtex3("xs", 4.5, 0.5, 0.5, "two");
+   plmtex3("ypv", 4.5, 0.5, 0.5, "three");
+   plmtex3("ys", 4.5, 0.5, 0.5, "------- four ------");
+   plmtex3("zpv", 4.5, 0.5, 0.5, "five");
+   plmtex3("zs", 4.5, 0.5, 0.5, "six");
+   plptex3(-1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, 0.0,  0.0,
+	   "hello world");
+   plptex3(-1.0, 1.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, 1.0,  0.0,
+	   "hello world");
+   
+   plend();
 }
