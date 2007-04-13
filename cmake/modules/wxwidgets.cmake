aaee4c899d09271ae24ed7ff86644981d5fb90a5
Alan W. Irwin <airwin@users.sourceforge.net>
2007-04-13 17:39:43 +0000

    Warning message when wxWidgets not found.
    svn path=/trunk/; revision=7675

--- cmake/modules/wxwidgets.cmake
+++ cmake/modules/wxwidgets.cmake
@@ -31,6 +31,10 @@ SET(wxWidgets_USE_LIBS core base)
 find_package(wxWidgets QUIET)
 
 if( NOT wxWidgets_FOUND )
+  message(STATUS
+  "WARNING: wxWidgets not found so "
+  "setting PLD_wxwidgets to OFF."
+  )
   set(PLD_wxwidgets OFF CACHE BOOL "Enable wxwidgets device" FORCE)
 endif( NOT wxWidgets_FOUND )  
 
