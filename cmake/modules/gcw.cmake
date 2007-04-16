879c4ac62ad245102a01db0218248b43fa80f368
Alan W. Irwin <airwin@users.sourceforge.net>
2007-04-16 06:33:47 +0000

    Tweak documentation.
    pkgconfig ==> pkg_check_pkgconfig (which implies cflags must be converted
    to blank delimited string, and only use linkflags to decide whether there
    was success or not)
    
    exec_prog ==> execute_process
    
    svn path=/trunk/; revision=7680

--- cmake/modules/gcw.cmake
+++ cmake/modules/gcw.cmake
@@ -21,57 +21,58 @@
 # device driver.
 # The following variables are set/modified:
 # PLD_gcw		  - ON means the gcw device is enabled.
-# gcw_COMPILE_FLAGS	  - individual COMPILE_FLAGS required to compile gcw
-# 			    device.
-# gcw_LINK_FLAGS	  - individual LINK_FLAGS for dynamic gcw device.
+# gcw_COMPILE_FLAGS	  - blank-separated COMPILE_FLAGS required to 
+#			    compile gcw device and gnome2 bindings.
+# gcw_LINK_FLAGS	  - list of LINK_FLAGS for dynamic gcw device and
+#			    gnome2 bindings.
 # gcw_TARGETS		  - list of targets which the gcw dynamic device
-# 			    depends on.
+#			    depends on.
 # DRIVERS_LINK_FLAGS	  - list of LINK_FLAGS for case when 
-# 			    ENABLE_DYNDRIVERS OFF.  (gcw_TARGETS not included
+#			    ENABLE_DYNDRIVERS OFF.  (gcw_TARGETS not included
 #			    since source code used instead of this target
 #			    for ENABLE_DYNDRIVERS OFF.)
 # gcw_SOURCE		  - list of source files other than gcw.c
+# PYGCW_CFLAGS		  - blank-separated COMPILE_FLAGS required to 
+#			    compile pygcw bindings.
+# PYGCW_LIBS		  - list of LINK_FLAGS for pygcw bindings.
 if(PLD_gcw)
-  if(NOT PKGCONFIG_EXECUTABLE)
+  if(NOT PKG_CONFIG_EXECUTABLE)
     message(STATUS 
     "WARNING: pkg-config not found. Setting PLD_gcw to OFF."
     )
     set(PLD_gcw OFF CACHE BOOL "Enable gcw device" FORCE)
-  endif(NOT PKGCONFIG_EXECUTABLE)
+  endif(NOT PKG_CONFIG_EXECUTABLE)
 endif(PLD_gcw)
 if(PLD_gcw)
 # linkflags and cflags ignored because not used in autotools side and
 # there is a question whether all that ton of extra potentially interfering
 # libraries might be causing a segfault that is observed for the
 # dynamic devices case.
-#  pkgconfig(libgnomeui-2.0 includedir libdir linkflags cflags)
-#  if(linkflags AND cflags)
-    pkgconfig(libgnomeprintui-2.2 includedir libdir linkflags1 cflags1)
-    if(linkflags1 AND cflags1)
-      # N.B. pkgconfig returns blank-delimited strings so must concatanate
-      # results the same way.
-      #set(gcw_COMPILE_FLAGS "${cflags} ${cflags1}")
-      #set(gcw_LINK_FLAGS "${linkflags} ${linkflags1}")
-      set(gcw_COMPILE_FLAGS "${cflags1}")
+#  pkg_check_pkgconfig(libgnomeui-2.0 includedir libdir linkflags cflags _GCW1)
+#  if(linkflags)
+    pkg_check_pkgconfig(libgnomeprintui-2.2 includedir libdir linkflags1 cflags1 _GCW2)
+    if(linkflags1)
+      # Requires blank-delimited list.
+      string(REGEX REPLACE ";" " " gcw_COMPILE_FLAGS "${cflags1}")
       set(gcw_LINK_FLAGS "${linkflags1}")
-    else(linkflags1 AND cflags1)
+    else(linkflags1)
       message(STATUS
       "WARNING: libgnomeprintui-2.2 not found.  Setting PLD_gcw "
       "to OFF."
       )
       set(PLD_gcw OFF CACHE BOOL "Enable gcw device" FORCE)
-    endif(linkflags1 AND cflags1)
-#  else(linkflags AND cflags)
+    endif(linkflags1)
+#  else(linkflags)
 #    message(STATUS "WARNING: libgnomeui-2.0 not found.  "
 #    "Setting PLD_gcw to OFF."
 #    )
 #    set(PLD_gcw OFF CACHE BOOL "Enable gcw device" FORCE)
-#  endif(linkflags AND cflags)
+#  endif(linkflags)
 endif(PLD_gcw)
 if(PLD_gcw)
 #  Check for gthread-2.0 done in cf/gcw.ac, but result not actually used
 #  anywhere in the autotools configuration so we comment it out here.
-#  pkgconfig(gthread-2.0 includedir libdir linkflags cflags)
+#  pkg_check_pkgconfig(gthread-2.0 includedir libdir linkflags cflags _GCW3)
   set(include_files_to_check
   "math.h"
   "glib.h"
@@ -165,25 +166,29 @@ if(ENABLE_pygcw AND NOT ENABLE_python)
 endif(ENABLE_pygcw AND NOT ENABLE_python)
 
 if(ENABLE_pygcw)
-  pkgconfig(pygtk-2.0 includedir libdir linkflags cflags)
-  if(linkflags AND cflags)
-    pkgconfig(gnome-python-2.0 includedir libdir linkflags1 cflags1)
+  pkg_check_pkgconfig(pygtk-2.0 includedir libdir linkflags cflags _GCW4)
+  if(linkflags)
     # This logic needs review.  It was copied right out of cf/gcw.ac
     # which checks for the existence of gnome-python-2.0 but uses no
     # compile or link flag returned by that check.  That made sense
     # at the time because the returned values were blank, but now (2006-11-27)
     # Ubuntu dapper drake, for example, returns an interesting value
     # "-I/usr/include/gnome-python-2.0" for --cflags1.
-    if(linkflags1 AND cflags1)
-      set(PYGCW_CFLAGS "${cflags}")
+    #pkg_check_pkgconfig(gnome-python-2.0 includedir libdir linkflags1 cflags1 _GCW5)
+#    if(linkflags1)
+      # Requires blank-delimited list.
+      string(REGEX REPLACE ";" " " PYGCW_CFLAGS "${cflags}")
       set(PYGCW_LIBS "${linkflags}")
       set(CMAKE_REQUIRED_FLAGS "${PYGCW_CFLAGS}")
       check_include_files(glib-object.h pygcw_HEADERS)
       set(CMAKE_REQUIRED_FLAGS)
       if(pygcw_HEADERS)
-        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE}
-	ARGS pygtk-2.0 --variable=codegendir
+        execute_process(
+	COMMAND
+	${PKG_CONFIG_EXECUTABLE}
+	pygtk-2.0 --variable=codegendir
 	OUTPUT_VARIABLE codegen
+	OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 	if(NOT codegen)
           message(STATUS "WARNING: pygtk/codegen not found.  "
@@ -197,16 +202,16 @@ if(ENABLE_pygcw)
 	)
         set(ENABLE_pygcw OFF CACHE BOOL "Enable Python Gnome2 bindings" FORCE)
       endif(pygcw_HEADERS)
-    else(linkflags1 AND cflags1)
-      message(STATUS "WARNING: gnome-python-2.0 not found. "
-      "Setting ENABLE_pygcw to OFF."
-      )
-      set(ENABLE_pygcw OFF CACHE BOOL "Enable Python Gnome2 bindings" FORCE)
-    endif(linkflags1 AND cflags1)
-  else(linkflags AND cflags)
+#    else(linkflags1)
+#      message(STATUS "WARNING: gnome-python-2.0 not found. "
+#      "Setting ENABLE_pygcw to OFF."
+#      )
+#      set(ENABLE_pygcw OFF CACHE BOOL "Enable Python Gnome2 bindings" FORCE)
+#    endif(linkflags1)
+  else(linkflags)
     message(STATUS "WARNING: pygtk-2.0 not found.  "
     "Setting ENABLE_pygcw to OFF"
     )
     set(ENABLE_pygcw OFF)
-  endif(linkflags AND cflags)
+  endif(linkflags)
 endif(ENABLE_pygcw)
