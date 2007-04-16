53d546698bdccf674733b84b1825ea4731a09751
Alan W. Irwin <airwin@users.sourceforge.net>
2007-04-16 02:29:28 +0000

    Use correct variables names in documentation of macros. _pkgconfig_unset now done with correct variable names.
    svn path=/trunk/; revision=7676

--- cmake/modules/FindPkgConfig.cmake
+++ cmake/modules/FindPkgConfig.cmake
@@ -22,10 +22,10 @@
 #   <XPREFIX>_LIBRARIES      ... only the libraries (w/o the '-l')
 #   <XPREFIX>_LIBRARY_DIRS   ... the paths of the libraries (w/o the '-L')
 #   <XPREFIX>_LDFLAGS        ... all required linker flags
-#   <XPREFIX>_LDFLAGS_OTHERS ... all other linker flags
+#   <XPREFIX>_LDFLAGS_OTHER ... all other linker flags
 #   <XPREFIX>_INCLUDE_DIRS   ... the '-I' preprocessor flags (w/o the '-I')
 #   <XPREFIX>_CFLAGS         ... all required cflags
-#   <XPREFIX>_CFLAGS_OTHERS  ... the other compiler flags
+#   <XPREFIX>_CFLAGS_OTHER  ... the other compiler flags
 #
 #   <XPREFIX> = <PREFIX>        for common case
 #   <XPREFIX> = <PREFIX>_STATIC for static linking
@@ -169,20 +169,19 @@ macro(_pkg_check_modules_internal _is_required _is_silent _prefix)
   _pkgconfig_unset(${_prefix}_PREFIX)
   _pkgconfig_unset(${_prefix}_INCLUDEDIR)
   _pkgconfig_unset(${_prefix}_LIBDIR)
-  _pkgconfig_unset(${_prefix}_LIBS)
-  _pkgconfig_unset(${_prefix}_LIBS_L)
-  _pkgconfig_unset(${_prefix}_LIBS_PATHS)
-  _pkgconfig_unset(${_prefix}_LIBS_OTHER)
+  _pkgconfig_unset(${_prefix}_LIBRARIES)
+  _pkgconfig_unset(${_prefix}_LIBRARY_DIRS)
+  _pkgconfig_unset(${_prefix}_LDFLAGS)
+  _pkgconfig_unset(${_prefix}_LDFLAGS_OTHER)
+  _pkgconfig_unset(${_prefix}_INCLUDE_DIRS)
   _pkgconfig_unset(${_prefix}_CFLAGS)
-  _pkgconfig_unset(${_prefix}_CFLAGS_I)
   _pkgconfig_unset(${_prefix}_CFLAGS_OTHER)
-  _pkgconfig_unset(${_prefix}_STATIC_LIBDIR)
-  _pkgconfig_unset(${_prefix}_STATIC_LIBS)
-  _pkgconfig_unset(${_prefix}_STATIC_LIBS_L)
-  _pkgconfig_unset(${_prefix}_STATIC_LIBS_PATHS)
-  _pkgconfig_unset(${_prefix}_STATIC_LIBS_OTHER)
+  _pkgconfig_unset(${_prefix}_STATIC_LIBRARIES)
+  _pkgconfig_unset(${_prefix}_STATIC_LIBRARY_DIRS)
+  _pkgconfig_unset(${_prefix}_STATIC_LDFLAGS)
+  _pkgconfig_unset(${_prefix}_STATIC_LDFLAGS_OTHER)
+  _pkgconfig_unset(${_prefix}_STATIC_INCLUDE_DIRS)
   _pkgconfig_unset(${_prefix}_STATIC_CFLAGS)
-  _pkgconfig_unset(${_prefix}_STATIC_CFLAGS_I)
   _pkgconfig_unset(${_prefix}_STATIC_CFLAGS_OTHER)
 
   # create a better addressable variable of the modules and calculate its size
