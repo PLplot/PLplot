87f252fdf297cd6b35d0ed089de0173f59122897
Alan W. Irwin <airwin@users.sourceforge.net>
2007-09-11 19:58:18 +0000

    Create grid at Z=0. to give better perspective on results. Adjust plmtex3 spacing to make more visually appealing result. Expand plptex3 calls to demonstrate results of different shearing.
    svn path=/trunk/; revision=7859

--- examples/c/x28c.c
+++ examples/c/x28c.c
@@ -4,6 +4,9 @@
 */
 
 #include "plcdemos.h"
+/* Choose these values to correspond to tick marks. */
+#define XPTS 7
+#define YPTS 5
 
 /*--------------------------------------------------------------------------*\
  * main
@@ -14,30 +17,62 @@
 
 int main(int argc, char *argv[])
 {
-/* Parse and process command line arguments */
+   PLFLT *x, *y, **z, xmin=-1.5, xmax=1.5, ymin=-0.5, 
+     ymax=1.5, zmin=0., zmax = 2.;
+   int i,j;
+   /* Parse and process command line arguments */
    
    (void) plparseopts(&argc, argv, PL_PARSE_FULL);
    
    plinit();
+   x = (PLFLT *) calloc(XPTS, sizeof(PLFLT));
+   y = (PLFLT *) calloc(YPTS, sizeof(PLFLT));
+
+   plAlloc2dGrid(&z, XPTS, YPTS);
+   for (i = 0; i < XPTS; i++) {
+      x[i] = xmin + (double) i * (xmax-xmin)/ (double) (XPTS-1);
+   }
+
+   for (j = 0; j < YPTS; j++)
+     y[j] = ymin + (double) j * (ymax-ymin)/ (double) (YPTS-1);
+
+   for (i = 0; i < XPTS; i++) {
+      for (j = 0; j < YPTS; j++) {
+	 z[i][j] = 0.;
+      }
+   }
+
    pladv(0);
    plvpor(0.0, 1.0, 0.0, 0.9);
-   plwind(-1.0, 1.0, -0.9, 1.1);
-   plw3d(1.0, 1.0, 1.0, -1.5, 1.5, -0.5, 1.5, 0.0, 2.0, 40.0, 60.0 + 270.0);
+   plwind(-1.2, 1.2, -0.8, 1.5);
+   plw3d(1.0, 1.0, 1.0, xmin, xmax, ymin, ymax, zmin, zmax, 
+	 40.0, 60.0 + 270.0);
    
-   plcol0(1);
-   plbox3("bnst", "", 0.0, 0,
-	  "bnst", "", 0.0, 0,
-	  "bcdmnstv", "", 0.0, 0);
-   plmtex3("xpv", 4.5, 0.5, 0.5, "one");
-   plmtex3("xs", 4.5, 0.5, 0.5, "two");
-   plmtex3("ypv", 4.5, 0.5, 0.5, "three");
-   plmtex3("ys", 4.5, 0.5, 0.5, "------- four ------");
-   plmtex3("zpv", 4.5, 0.5, 0.5, "five");
-   plmtex3("zs", 4.5, 0.5, 0.5, "six");
+   plcol0(2);
+   plbox3("bnt", "", 0.5, 0,
+	  "bnt", "", 0.0, 0,
+	  "bcdmntv", "", 0.0, 0);
+   plmtex3("xpv", 2.5, 0.5, 1.0, "one");
+   plmtex3("xs", -1.0, 0.5, 0.5, "two");
+   plmtex3("ypv", -0.5, 0.5, 0.0, "three");
+   plmtex3("ys", 3.0, 0.5, 0.5, "------- four ------");
+   plmtex3("zpv", 2.5, 0.5, 1.0, "five");
+   plmtex3("zs", 3.0, 0.5, 0.5, "six");
    plptex3(-1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, 0.0,  0.0,
-	   "hello world");
-   plptex3(-1.0, 1.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, 1.0,  0.0,
-	   "hello world");
+	   "seven");
+   plptex3(-1.0, 0.0, 1.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  0.0,
+	   "eight");
+   plptex3(-1.0, 0.0, 2.0,  1.0, 0.0, 0.0,  2.0, 0.0, 0.0,  0.0,
+	   "nine");
+   plptex3(-1.0, 1.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, 0.0,  0.0,
+	   "ten");
+   plptex3(-1.0, 1.0, 1.0,  1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0,
+	   "eleven");
+   plptex3(-1.0, 1.0, 2.0,  1.0, 0.0, 0.0,  0.0, 2.0, 0.0,  0.0,
+	   "twelve");
+   /* Draw 3D grid (where the grid lines correspond to tick marks because
+    * of XPTS, YPTS choice) at Z = 0. */
+   plmesh(x, y, z, XPTS, YPTS, DRAW_LINEXY);
    
    plend();
 }
