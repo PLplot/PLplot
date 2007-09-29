d9506335018037f2dae6b56c258becfc490524e3
Alan W. Irwin <airwin@users.sourceforge.net>
2007-09-29 21:17:19 +0000

    Copyright.
    svn path=/trunk/; revision=7905

--- examples/c++/x28.cc
+++ examples/c++/x28.cc
@@ -2,6 +2,25 @@
 //
 //	plmtex3, plptex3 demo.
 //
+// Copyright (C) 2007  Alan W. Irwin
+// Copyright (C) 2007  Andrew Ross 
+//
+// This file is part of PLplot.
+//
+// PLplot is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// PLplot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with PLplot; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+//---------------------------------------------------------------------------//
 
 #include "plc++demos.h"
 
