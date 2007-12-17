cd2826a559c16f4d8c8d53ad011ea2e1103c5449
Alan W. Irwin <airwin@users.sourceforge.net>
2007-12-17 22:07:45 +0000

    Tweak alphabetization by driver (not device).
    svn path=/trunk/; revision=8108

--- cmake/modules/drivers-init.cmake
+++ cmake/modules/drivers-init.cmake
@@ -108,9 +108,9 @@ set(DRIVERS_DEVICE_LIST
 # (psttf and pscairo) to get modern fonts for postscript results.
 "pstex:pstex:OFF"
 "psttf:psttf:ON"
+"svg:svg:ON"
 "xterm:tek:OFF" "tek4010:tek:OFF" "tek4107:tek:OFF" "mskermit:tek:OFF"
 "versaterm:tek:OFF" "vlt:tek:OFF" "conex:tek:OFF" "tek4010f:tek:OFF"
-"svg:svg:ON"
 "tek4107f:tek:OFF"
 "tk:tk:ON"
 "tkwin:tkwin:ON"
