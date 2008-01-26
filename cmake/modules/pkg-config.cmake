ac7656a34f730a88a6ce3fcb3d629e4103c7413f
Alan W. Irwin <airwin@users.sourceforge.net>
2008-01-26 21:36:04 +0000

    Make cmake_link_flags macro much more robust. pkg_check_pkgconfig macro processes its link_flags with cmake_link_flags macro.
    svn path=/trunk/; revision=8177

--- cmake/modules/pkg-config.cmake
+++ cmake/modules/pkg-config.cmake
@@ -62,7 +62,8 @@ endif(PKG_CONFIG_EXECUTABLE)
 macro(pkg_check_pkgconfig _package _include_DIR _link_DIR _link_FLAGS _cflags)
   # Similar to legacy pkgconfig only these results are derived
   # from pkg_check_modules and therefore are returned as lists rather than
-  # blank-delimited strings.
+  # blank-delimited strings.  Also, the _link_FLAGS value is converted
+  # to the preferred CMake form via the cmake_link_flags macro.
 
   # N.B. if using this macro in more than one context, cache clashes will
   # occur unless optional trailing prefix argument is specified to distinguish
@@ -76,7 +77,7 @@ macro(pkg_check_pkgconfig _package _include_DIR _link_DIR _link_FLAGS _cflags)
   if (${_prefix}_FOUND)
     set(${_include_DIR} ${${_prefix}_INCLUDE_DIRS})
     set(${_link_DIR}    ${${_prefix}_LIBRARY_DIRS})
-    set(${_link_FLAGS}  ${${_prefix}_LDFLAGS})
+    cmake_link_flags(${_link_FLAGS}  "${${_prefix}_LDFLAGS}")
     set(${_cflags}      ${${_prefix}_CFLAGS})
     set(_return_VALUE 0)
   else(${_prefix}_FOUND)
@@ -150,58 +151,73 @@ macro(cmake_link_flags _link_flags_out _link_flags_in)
   # may not be the case for windows.
   # N.B. ${_link_flags_in} must be a string and not a list.
 
-  #message("(original link flags) = ${_link_flags_in}")
-  # Convert link flags to a list.
-  string(REGEX REPLACE " " ";" _link_flags_list "${_link_flags_in}")
-  # Extract list of directories from -L options.
-  list(LENGTH _link_flags_list _link_flags_length)
-  math(EXPR _link_flags_length "${_link_flags_length} - 1")
-  set(_index_list)
-  set(_link_directory_list)
-  foreach(_list_index RANGE ${_link_flags_length})
-    list(GET _link_flags_list ${_list_index} _list_element)
-    string(REGEX REPLACE "^-L" "" _list_element1 ${_list_element})
-    if(_list_element STREQUAL "-L${_list_element1}")
-      list(APPEND _index_list ${_list_index})
-      list(APPEND _link_directory_list ${_list_element1})
-    endif(_list_element STREQUAL "-L${_list_element1}")
-  endforeach(_list_index RANGE ${_link_flags_length})
-  # Remove -L options from list.
-  list(REMOVE_AT _link_flags_list ${_index_list})
-  #message("_link_directory_list = ${_link_directory_list}")
-  #message("_link_flags_list (without -L options) = ${_link_flags_list}")
-
-  # Derive ${_link_flags_out} from _link_flags_list with -l options 
-  # replaced by complete pathname of library.
-  list(LENGTH _link_flags_list _link_flags_length)
-  math(EXPR _link_flags_length "${_link_flags_length} - 1")
-  set(${_link_flags_out})
-  foreach(_list_index RANGE ${_link_flags_length})
-    list(GET _link_flags_list ${_list_index} _list_element)
-    string(REGEX REPLACE "^-l" "" _list_element1 ${_list_element})
-    if(_list_element STREQUAL "-l${_list_element1}")
-      set(_library_pathname "_library_pathname-NOTFOUND")
-      find_library(
-       _library_pathname 
-       ${_list_element1}
-       PATHS ${_link_directory_list}
-       NO_DEFAULT_PATH
-      )
-      if(NOT _library_pathname)
-        message(
-	"Cannot find library corresponding to linker option ${_list_element}"
-	)
-        message(
-	"original link flags delivered by pkg-config = ${_link_flags_in}"
-	)
-        message(FATAL_ERROR "FATAL ERROR in cmake_link_flags macro")
-      endif(NOT _library_pathname)
-      list(APPEND ${_link_flags_out} ${_library_pathname})
-    else(_list_element STREQUAL "-L${_list_element1}")
-      # link options that are not -L or -l passed through in correct order
-      # in ${_link_flags_out}.
-      list(APPEND ${_link_flags_out} ${_list_element})
-    endif(_list_element STREQUAL "-l${_list_element1}")
-  endforeach(_list_index RANGE ${_link_flags_length})
-  #message("${_link_flags_out} = ${${_link_flags_out}}")
+  if("${_link_flags_in}" STREQUAL "")
+    set(${_link_flags_out})
+  else("${_link_flags_in}" STREQUAL ""})
+    #message("(original link flags) = ${_link_flags_in}")
+    # Convert link flags to a list.
+    string(REGEX REPLACE " " ";" _link_flags_list "${_link_flags_in}")
+    # Extract list of directories from -L options.
+    list(LENGTH _link_flags_list _link_flags_length)
+    math(EXPR _link_flags_length "${_link_flags_length} - 1")
+    set(_index_list)
+    set(_link_directory_list)
+    foreach(_list_index RANGE ${_link_flags_length})
+      list(GET _link_flags_list ${_list_index} _list_element)
+      string(REGEX REPLACE "^-L" "" _list_element1 ${_list_element})
+      if(_list_element STREQUAL "-L${_list_element1}")
+        list(APPEND _index_list ${_list_index})
+        list(APPEND _link_directory_list ${_list_element1})
+      endif(_list_element STREQUAL "-L${_list_element1}")
+    endforeach(_list_index RANGE ${_link_flags_length})
+    #message("_index_list = ${_index_list}")
+    if("${_index_list}" STREQUAL "")
+    else("${_index_list}" STREQUAL "")
+      # Remove -L options from list.
+      list(REMOVE_AT _link_flags_list ${_index_list})
+    endif("${_index_list}" STREQUAL "")
+    #message("_link_directory_list = ${_link_directory_list}")
+    #message("_link_flags_list (without -L options) = ${_link_flags_list}")
+  
+    # Derive ${_link_flags_out} from _link_flags_list with -l options 
+    # replaced by complete pathname of library.
+    list(LENGTH _link_flags_list _link_flags_length)
+    math(EXPR _link_flags_length "${_link_flags_length} - 1")
+    set(${_link_flags_out})
+    foreach(_list_index RANGE ${_link_flags_length})
+      list(GET _link_flags_list ${_list_index} _list_element)
+      string(REGEX REPLACE "^-l" "" _list_element1 ${_list_element})
+      if(_list_element STREQUAL "-l${_list_element1}")
+        set(_library_pathname "_library_pathname-NOTFOUND")
+        find_library(
+         _library_pathname 
+         ${_list_element1}
+         PATHS ${_link_directory_list}
+         NO_DEFAULT_PATH
+        )
+	# Try second time (without NO_DEFAULT_PATH) just in case pkg-config
+	# specified some system libraries without corresponding -L options.
+        find_library(
+         _library_pathname 
+         ${_list_element1}
+         PATHS ${_link_directory_list}
+        )
+        if(NOT _library_pathname)
+          message(
+  	"Cannot find library corresponding to linker option ${_list_element}"
+  	)
+          message(
+  	"original link flags delivered by pkg-config = ${_link_flags_in}"
+  	)
+          message(FATAL_ERROR "FATAL ERROR in cmake_link_flags macro")
+        endif(NOT _library_pathname)
+        list(APPEND ${_link_flags_out} ${_library_pathname})
+      else(_list_element STREQUAL "-L${_list_element1}")
+        # link options that are not -L or -l passed through in correct order
+        # in ${_link_flags_out}.
+        list(APPEND ${_link_flags_out} ${_list_element})
+      endif(_list_element STREQUAL "-l${_list_element1}")
+    endforeach(_list_index RANGE ${_link_flags_length})
+    #message("${_link_flags_out} = ${${_link_flags_out}}")
+  endif("${_link_flags_in}" STREQUAL "")
 endmacro(cmake_link_flags)
