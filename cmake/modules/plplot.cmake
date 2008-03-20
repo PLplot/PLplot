5c509766a73139687a6c48f20c830344ab090e30
Alan W. Irwin <airwin@users.sourceforge.net>
2008-03-20 19:45:31 +0000

    Find MATH_LIB rather than assuming it is available.  This also has the added benefit of using the full pathname for MATH_LIB which the cvs version of CMake prefers.
    svn path=/trunk/; revision=8303

--- cmake/modules/plplot.cmake
+++ cmake/modules/plplot.cmake
@@ -299,7 +299,10 @@ include(csiro)
 # On windows systems the math library is not separated so do not specify
 # it unless you are on a non-windows system.
 if(NOT WIN32)
-  set(MATH_LIB m)
+  find_library(MATH_LIB NAMES m PATHS /usr/local/lib /usr/lib)
+  if(NOT MATH_LIB)
+    message(FATAL_ERROR "Cannot find required math library")
+  endif(NOT MATH_LIB)
 endif(NOT WIN32)
 
 # =======================================================================
