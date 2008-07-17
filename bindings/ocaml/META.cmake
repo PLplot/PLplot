a2f44bce1ab140fd405d3de4f77986263f0d534f
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2008-07-17 15:07:26 +0000

    Tag Debian release plplot_5.9.0-8
    svn path=/tags/debian/5.9.0-8/; revision=8521

--- bindings/ocaml/META.cmake
+++
@@ -1,8 +0,0 @@
-# findlib META file for ocaml-plplot
-requires = ""
-description = "PLplot library bindings"
-version = "@VERSION@"
-browse_interfaces = " Plplot "
-archive(byte) = "plplot.cma"
-archive(native) = "plplot.cmxa"
-
