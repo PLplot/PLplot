0482de41d4b7e7689fe86f226cd0fdfe978ef9f3
Andrew Ross <andrewross@users.sourceforge.net>
2008-07-23 02:28:54 +0000

    Fix f77 bindings so plgriddata works correctly. Code to copy data back from C to fortran arrays was missing.
    svn path=/trunk/; revision=8559

--- bindings/f77/scstubs.c
+++ bindings/f77/scstubs.c
@@ -322,11 +322,26 @@ PLGRA(void)
 void
 PLGRIDDATA(PLFLT *x, PLFLT *y, PLFLT *z, PLINT *npts,
 	   PLFLT *xg, PLINT *nptsx, PLFLT *yg,  PLINT *nptsy,
-	   PLFLT **zg, PLINT *type, PLFLT *data)
+	   PLFLT *zg, PLINT *type, PLFLT *data, PLINT *lx)
 {
+    PLFLT **a;
+    int i,j;
+
+    plAlloc2dGrid(&a, *nptsx, *nptsy);
+
     c_plgriddata(x, y, z, *npts,
 		 xg, *nptsx, yg, *nptsy,
-		 zg, *type, *data);
+		 a, *type, *data);
+
+    for (i = 0; i < *nptsx; i++) {
+       for (j = 0; j < *nptsy; j++) {
+           zg[i +j * *lx] = a[i][j];
+       }
+    }
+
+/* Clean up memory allocated for a */
+   plFree2dGrid(a, *nptsx, *nptsy);
+
 }
 
 void
