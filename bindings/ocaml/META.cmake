fee0729b2038666428958879d52dd88eba203c82
Alan W. Irwin <airwin@users.sourceforge.net>
2008-07-30 21:28:06 +0000

    AWI for Hezekiah M. Carty.  Ease linking when using ocamlfind.  This should allow a user to compile their OCaml program using ocamlfind without having to specify any extra linking directories.
    svn path=/trunk/; revision=8598

--- bindings/ocaml/META.cmake
+++ bindings/ocaml/META.cmake
@@ -3,6 +3,7 @@ requires = ""
 description = "PLplot library bindings"
 version = "@VERSION@"
 browse_interfaces = " Plplot "
+linkopts = "-ccopt \"-L@SHLIB_DIR@\""
 archive(byte) = "plplot.cma"
 archive(native) = "plplot.cmxa"
 
