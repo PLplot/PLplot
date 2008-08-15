eb31d4e1b743c95c992ace83c37bd47496694cd2
Alan W. Irwin <airwin@users.sourceforge.net>
2008-08-15 00:28:15 +0000

    AWI for Hezekiah M. Carty.  Update the 11th standard example so the results agree exactly with the C version.
    svn path=/trunk/; revision=8659

--- examples/ocaml/x11.ml
+++ examples/ocaml/x11.ml
@@ -1,48 +1,57 @@
-(*
-Copyright 2007  Hezekiah M. Carty
+(* $Id$
 
-This file is part of PLplot.
+        Mesh plot demo.
 
-PLplot is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+   Copyright (C) 2004  Rafael Laboissiere
+   Copyright 2008 Hezekiah M. Carty (OCaml adaptation)
 
-PLplot is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+   This file is part of PLplot.
 
-You should have received a copy of the GNU Lesser General Public License
-along with PLplot.  If not, see <http://www.gnu.org/licenses/>.
+   PLplot is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Library Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   PLplot is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with PLplot; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 *)
 
-(* A more or less direct port of the PLplot example 11 *)
 open Plplot
 
 let xpts = 35 (* Data points in x *)
 let ypts = 46 (* Data points in y *)
-let nlevel = 10 (* Contouring levels *)
+let levels = 10
 
+(* FIXME TODO: The plot style #define statements have not been brought over to
+   the OCaml interface yet.  This example uses the constant values until they
+   are properly carried over. *)
+let opt = [|3; 3|]
 
 let alt = [|33.0; 17.0|]
 let az = [|24.0; 115.0|]
-let opt = [|3; 3|]
-let title = [|"#frPLplot Example 11 - Alt=33, Az=24, Opt=3";
-              "#frPLplot Example 11 - Alt=17, Az=115, Opt=3"|]
+
+let title = [|
+  "#frPLplot Example 11 - Alt=33, Az=24, Opt=3";
+  "#frPLplot Example 11 - Alt=17, Az=115, Opt=3"
+|]
 
 let cmap1_init () =
-  let i = [|0.0; 1.0|] in (* left+right bounds *)
-  let h = [|240.0; 0.0|] in
+  let i = [|0.0; 1.0|] in (* Left and right bounds *)
+  let h = [|240.0; 0.0|] in(* blue -> green -> yellow -> red *)
   let l = [|0.6; 0.6|] in
   let s = [|0.8; 0.8|] in
   plscmap1n 256;
+  (* TODO: The first argument to plscmap1l should be a boolean value *)
   plscmap1l 0 i h l s None;
   ()
 
 (*--------------------------------------------------------------------------*\
- * main
- *
  * Does a series of mesh plots for a given data set, with different
  * viewing options in each plot.
 \*--------------------------------------------------------------------------*)
@@ -54,42 +63,42 @@ let () =
   (* Initialize plplot *)
   plinit ();
 
-  (* Convenience, because I'm lazy *)
-  let (!.) = float_of_int in
-
+  let nlevel = levels in
   let x =
     Array.init xpts (
       fun i ->
-        3. *. (!.i -. (!.xpts /. 2.)) /. (!.xpts /. 2.)
+        3.0 *. float_of_int (i - xpts / 2) /. float_of_int (xpts / 2)
     )
   in
   let y =
     Array.init ypts (
       fun i ->
-        3. *. (!.i -. (!.ypts /. 2.)) /. (!.ypts /. 2.)
+        3.0 *. float_of_int (i - ypts / 2) /. float_of_int (ypts / 2)
     )
   in
 
   let z = Array.make_matrix xpts ypts 0.0 in
-
   for i = 0 to xpts - 1 do
     let xx = x.(i) in
     for j = 0 to ypts - 1 do
       let yy = y.(j) in
       z.(i).(j) <-
-        3. *. (1. -. xx) *. (1. -. xx) *.
-        exp (-.(xx *. xx) -. (yy +. 1.) *. (yy +. 1.)) -.
-        10. *. (xx /. 5. -. xx**3. -. yy**5.) *.
-        exp (-.(xx *. xx) -. (yy *. yy)) -.
-        1. /. 3. *. exp (-.(xx +. 1.) *. (xx +. 1.) -. (yy *. yy));
+        3.0 *. (1.0 -. xx) *. (1.0 -. xx) *.
+        exp (~-.(xx *. xx) -. (yy +. 1.0) *. (yy +. 1.0)) -.
+        10.0 *. (xx /. 5.0 -. xx**3.0 -. yy**5.0) *.
+        exp (~-.xx *. xx -. yy *. yy) -.
+        1.0 /. 3.0 *. exp (~-.(xx +. 1.0) *. (xx +. 1.0) -. (yy *. yy));
     done
   done;
 
-  let zmin, zmax = plMinMax2dGrid z in
-  let step = (zmax -. zmin) /. !.(nlevel + 1) in
-  let clevel = Array.init nlevel (fun i -> zmin +. step +. step *. !.i) in
+  let zmax, zmin = plMinMax2dGrid z in
+  let step = (zmax -. zmin) /. float_of_int (nlevel + 1) in
+  let clevel =
+    Array.init nlevel (fun i -> zmin +. step +. step *. float_of_int i)
+  in
 
-  cmap1_init();
+  cmap1_init ();
+
   for k = 0 to 1 do
     for i = 0 to 3 do
       pladv 0;
@@ -103,21 +112,20 @@ let () =
 
       plcol0 2;
 
+      (* wireframe plot *)
       let () =
         match i with
             0 ->
-              (* wireframe plot *)
-              plmesh x y z opt.(k);
+              plmesh x y z opt.(k)
           | 1 ->
               (* magnitude colored wireframe plot *)
-              plmesh x y z (opt.(k) lor 4);
+              plmesh x y z (opt.(k) lor 4)
           | 2 ->
               (* magnitude colored wireframe plot with sides *)
-              plot3d x y z (opt.(k) lor 4) 1;
+              plot3d x y z (opt.(k) lor 4) 1
           | 3 ->
               (* magnitude colored wireframe plot with base contour *)
-              plmeshc x y z (opt.(k) lor 4 lor 8)
-                clevel;
+              plmeshc x y z (opt.(k) lor 4 lor 8) clevel
           | _ -> raise (Failure "You let i get too big!")
       in
 
@@ -127,6 +135,5 @@ let () =
   done;
 
   (* Clean up *)
-  
   plend ();
   ()
