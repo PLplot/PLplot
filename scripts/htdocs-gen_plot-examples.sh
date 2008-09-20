cd2ab6890509ef05ee124fcc91b84500bc94ff20
Alan W. Irwin <airwin@users.sourceforge.net>
2008-09-20 18:13:15 +0000

    Get rid of syntax error in continuation (remove trailing space after "\").
    Reformat whitespace using emacs mode.
    
    svn path=/trunk/; revision=8788

--- scripts/htdocs-gen_plot-examples.sh
+++ scripts/htdocs-gen_plot-examples.sh
@@ -37,9 +37,9 @@ if test "$build" = true; then
     cd htdocsgen/build_dir
     # Specifically enable f77 and Ada so that examples will be generated.
     cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../install \
-    -DDEFAULT_NO_BINDINGS=ON -DENABLE_f77=ON -DENABLE_ada=ON\
+	-DDEFAULT_NO_BINDINGS=ON -DENABLE_f77=ON -DENABLE_ada=ON\
     -DDEFAULT_NO_DEVICES=ON -DPLD_pngcairo=ON \
-    ../../
+	../../
     make -j3
     make -j3 install
     cd ../..
@@ -55,31 +55,31 @@ popd
 cp examples/c/lena.pgm .
 
 for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20 21 22 \
-23 24 25 26 27 28 29 30; do
+    23 24 25 26 27 28 29 30; do
 
     if test $exe = 08 -o $exe = 16 -o $exe = 20 -o $exe = 30; then
       # No cairo graphics AA (Yep, 1 turns it off).
-      DRIVEROPT='-drvopt graphics_anti_aliasing=1'
+	DRIVEROPT='-drvopt graphics_anti_aliasing=1'
     else
-      if test $exe = 09 -o $exe = 21; then
+	if test $exe = 09 -o $exe = 21; then
         # Text clipping.
-        DRIVEROPT='-drvopt text_clipping=1'
-      else
+            DRIVEROPT='-drvopt text_clipping=1'
+	else
         # Otherwise use default graphics AA which is full AA 
-        DRIVEROPT=
-      fi
+            DRIVEROPT=
+	fi
     fi
     echo Working on example ${exe} using DRIVEROPT of $DRIVEROPT
 
     # generate standard and preview size images
     $cexamples_dir/x${exe}c -dev pngcairo -o x${exe} -fam -fflen 2 \
-    $DRIVEROPT
+	$DRIVEROPT
     $cexamples_dir/x${exe}c -dev pngcairo -o prev-x${exe} -fam -fflen 2 \
-    $DRIVEROPT -geometry 200x150
+	$DRIVEROPT -geometry 200x150
 
     # give png extension
     for i in `ls x${exe}.?? prev-x${exe}.??`; do
-       mv $i $i.png;
+	mv $i $i.png;
     done;
 
     # move to www directory.
@@ -90,18 +90,18 @@ for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20 21 22 \
     # Note both Ada and fortran 77 examples grabbed from the installed
     # examples.
     for f in \
-	     $cexamples_dir/../ada/x${exe}a.adb \
-             examples/c/x${exe}c.c \
-	     examples/c++/x${exe}.cc \
-	     $cexamples_dir/../f77/x${exe}f.f \
-	     examples/f95/x${exe}f.f90 \
-             examples/java/x${exe}.java \
-             examples/ocaml/x${exe}.ml \
-             bindings/octave/demos/x${exe}c.m \
-             examples/perl/x${exe}.pl \
-             examples/python/xw${exe}.py \
-             examples/tcl/x${exe}.tcl \
-	     ; do
+	$cexamples_dir/../ada/x${exe}a.adb \
+        examples/c/x${exe}c.c \
+	examples/c++/x${exe}.cc \
+	$cexamples_dir/../f77/x${exe}f.f \
+	examples/f95/x${exe}f.f90 \
+        examples/java/x${exe}.java \
+        examples/ocaml/x${exe}.ml \
+        bindings/octave/demos/x${exe}c.m \
+        examples/perl/x${exe}.pl \
+        examples/python/xw${exe}.py \
+        examples/tcl/x${exe}.tcl \
+	; do
         if test -f $f ; then
             cp $f $EXDIR/demo${exe}
         else
@@ -113,22 +113,22 @@ for exe in 01 02 03 04 05 06 07 08 09 10 11 12 13 15 16 18 19 20 21 22 \
     # rename executables, to avoid browsers trying to execute files
     # instead of showing them.
     (cd  $EXDIR/demo${exe};
-    for j in \
-	*.adb \
-        *.c \
-	*.cc \
-	*.f \
-	*.f90 \
-	*.java \
-	*.ml \
-	*.m \
-	*.pl \
-	*.py \
-	*.tcl \ 
-	; do
+	for j in \
+	    *.adb \
+            *.c \
+	    *.cc \
+	    *.f \
+	    *.f90 \
+	    *.java \
+	    *.ml \
+	    *.m \
+	    *.pl \
+	    *.py \
+	    *.tcl \
+	    ; do
 	    mv $j $j-
-    done
-    )
+	done
+	)
 
 done
 
