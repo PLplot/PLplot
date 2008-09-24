b15732901b7a2f82b6c7aa47afef8d87a9afa1df
Alan W. Irwin <airwin@users.sourceforge.net>
2008-09-24 17:46:11 +0000

    Change subversion client style from a sub-section to a simple paragraph and list.
    Add svn to the client list as a powerful command-line client for Linux.
    
    Add section for PLplot Packages for Linux Distributions.
    
    
    svn path=/trunk/; revision=8815

--- www/download.php
+++ www/download.php
@@ -26,13 +26,20 @@
 		  <p>The PLplot Subversion repository can be checked out on the command line through svn
 		     with the following instruction set:</p>
 		  <p><tt>svn co https://plplot.svn.sourceforge.net/svnroot/plplot/trunk plplot</tt></p>
-
-		  <h4>Subversion Clients</h4>
+		  <p>Here is a partial list of subversion clients</p>
 		  <ul class="arrowlist">
-		  <li>SvnX (Mac OS X): <a href="http://www.lachoseinteractive.net/en/community/subversion/svnx/features/">
-		      http://www.lachoseinteract......nx/features/</a></li>
-		  <li>TortoiseSVN (Windows): <a href="http://tortoisesvn.tigris.org/">http://tortoisesvn.tigris.org/</a></li>
-		  <li>KDESvn (Linux): <a href="http://kdesvn.alwins-world.de/wiki">http://kdesvn.alwins-world.de/wiki</a></li>
+		  <li><a href="http://www.lachoseinteractive.net/en/community/subversion/svnx/features/">SvnX (Mac OS X)</a></li>
+		  <li><a href="http://tortoisesvn.tigris.org/">TortoiseSVN (Windows)</a></li>
+		  <li><a href="http://kdesvn.alwins-world.de/wiki">KDESvn (Linux)</a></li>
+		  <li>svn, a powerful command-line subversion client (Linux)</li>
+	
+		  </ul>
+			<h3>PLplot Packages for Linux Distributions</h3>
+		  <ul class="arrowlist">
+<li><a href="http://packages.ubuntu.com/search?keywords=plplot&amp;searchon=names&amp;suite=all&amp;section=all">Ubuntu</a></li>
+<li><a href="http://packages.debian.org/search?searchon=sourcenames&amp;keywords=plplot">Debian</a></li>
+<li><a href="http://rpmfind.net/linux/rpm2html/search.php?query=plplot&amp;submit=Search+...">Rpmfind Results</a></li>
+<li><a href="http://rpm.pbone.net/">http://rpm.pbone.net/</a> (enter "plplot" search term)</li>
 		  </ul>
 		</div>
 
