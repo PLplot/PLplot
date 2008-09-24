5c2557a6f4d585b2caf77d881683999525bde4b9
Werner Smekal <smekal@users.sourceforge.net>
2008-09-24 19:56:35 +0000

    Updated the content of the Credits page. There is now a Developer, Addtional libraries and Tools section where proper credit is given,
    svn path=/trunk/; revision=8817

--- www/credits.php
+++ www/credits.php
@@ -12,15 +12,38 @@
 
 	<div id="contentwrapper">
 		<div id="leftside">
-			<h3>The Authors</h3>
-		  <p>The current version of PLplot is primarily a product of Maurice J. LeBrun
-				 and Geoffrey Furnish.  The conversion of the documentation to DocBook form
-				 and subsequent upgrading of that documentation is primarily the product of
-				 Rafael Laboissiere and Alan W. Irwin.  These four people and Joao Cardoso,
-				 Vince Darley, Arjen Markus, Andrew Roach, Andrew Ross, and David Schleef
-				 (the other members of the
-				 <a href="http://sourceforge.net/project/memberlist.php?group_id=2915">development
-				 team</a>) continue to commit documentation and code to the SVN repository.</p>
+			<h3>Developers</h3>
+		  <p>Several developers contributed to the PLplot library, some of them are not
+				 part of the active development team any more. You can view a list of all
+				 active developers at the <a href=http://sourceforge.net/project/memberlist.php?group_id=2915">
+				 Sourceforge memberpage</a> of the PLplot project.</p>
+				 
+			<p>A more complete list, including inactive developers but very likely
+			   not all of them, can be found at the <a href="http://www.ohloh.net/projects/10094/contributors">
+			   PLplot project page</a> at <a href="http://www.ohloh.net/">Ohloh</a>. In
+			   addition many others contributed by providing patches, finding bugs
+         or requesting features.</p>
+         
+      <h3>Additional libraries</h3>
+      <p>The Plplot library depends on many other open source libraries provided by 
+      the community. These libraries enhance the functionality of the PLplot library
+      considerably.</p>
+      <ul class="arrowlist">
+      <li><a href="http://www.qhull.org/">Qhull</a>: computes the convex hull, Delaunay
+          triangulation, Voronoi diagram, halfspace intersection about a point,
+          furthest-site Delaunay triangulation, and furthest-site Voronoi diagram.</li>
+      <li><a href="http://www.freetype.org/">FreeType</a>: a free, high-quality, and 
+      	  portable font engine.</li>
+      <li><a href="http://www.libharu.org/">libHaru</a>: free, cross platform, open
+      	  source library for generating PDF files.</li>
+      </ul>
+      
+      <h3>Tools</h3>
+      <p>The development of the Plplot project is supported by the use of several
+         open source tools.</p>
+      <ul class="arrowlist">
+      <li><a href="http://www.cmake.org/">CMake</a>: cross-platform, open-source build system.</li>
+      </ul>
 		</div>
 
 		<?php pageSidebar(1); ?>
