82bbb11d28c45135cf2e957a296cffb462684f8b
Alan W. Irwin <airwin@users.sourceforge.net>
2008-10-17 19:13:44 +0000

    Change to new "cmake_working" location.
    svn path=/branches/test_cmake/; revision=8915

--- test_ada/cmake_working/Modules/CMakeTestAdaCompiler.cmake
+++ test_ada/cmake_working/Modules/CMakeTestAdaCompiler.cmake
@@ -18,8 +18,8 @@ end;
   FILE(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/CMakeLists.txt
   "
 # FIXME: This specific location currently needs to be changed whenever the
-# location of the experimental Ada support files is changed.
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake-2.4/Modules)
+# location of the Ada support files are changed.
+set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake_working/Modules)
 set(CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL \"\" FORCE)
 project(test Ada)
 add_executable(testadacompiler testadacompiler.adb)
