473fd3e21ab474938bc69d119133e3fc3d635af1
Alan W. Irwin <airwin@users.sourceforge.net>
2008-10-19 23:03:15 +0000

    Reorganize home page and also rewrite certain sections of it.
    svn path=/trunk/; revision=8932

--- www/index.php
+++ www/index.php
@@ -13,65 +13,65 @@
 	<?php pageMenu("index"); ?>
 
   <!-- the actual content of the page -->
-	<div id="contentwrapper">
-		<div id="leftside">
-			<h3>Introduction</h3>
-			<p>PLplot is a library of functions that are useful for making scientific
-				 plots.</p>
-			<p>PLplot can be used from within compiled languages such as C, C++, D,
-				 Fortran and Java, and interactively from interpreted languages
-				 such as Octave, Python, Perl and Tcl.</p>
-      <p>The PLplot library can be used to create standard x-y plots, semi-log plots,
-				 log-log plots, contour plots, 3D surface plots, mesh plots, bar charts and
-				 pie charts. Multiple graphs (of the same or different sizes) may be
-				 placed on a single page with multiple lines in each graph.</p>
-      <p>A variety of output file devices such as Postscript, png, jpeg
-				 and others, as well as interactive devices such as xwin, tk,
-				 xterm and Tektronics devices are supported. There are also interfaces
-         available to include the PLplot library in your application, e.g.
-         for <a href="http://www.wxwidgets.org">wxWidgets</a>. New devices can be easily
-				 added by writing a small number of device dependent routines.</p>
-      <p>PLplot has core support for Unicode. This means plots can be labelled using
-         the enormous selection of Unicode mathematical symbols and simple text
-         layout (left to right) language scripts for many of our devices. For our
-         cairo-related devices we also support complex text layout (CTL) languages
-         such as Arabic, Hebrew, and Indic and Indic-derived CTL scripts such as
-         Devanagari, Thai, Lao, and Tibetan.</p>
-      <p>Some devices support their own way of dealing with text,
-				 such as the Postscript driver (using postscript fonts), or the png and jpeg drivers
-         (which use the Freetype library).</p>
-			<p>PLplot is free software primarily licensed under the
+  	<div id="contentwrapper">
+	<div id="leftside">
+	<h3>Introduction</h3>
+        <p>PLplot is a cross-platform software package for creating scientific plots.  To help accomplish that task it is organized as a core C library, language bindings for that library, and device drivers which control how the plots are presented in non-interactive and interactive plotting contexts.</p>
+	<p>The PLplot core library can be used to create standard x-y plots, semi-log plots, log-log plots, contour plots, 3D surface plots, mesh plots, bar charts and pie charts. Multiple graphs (of the same or different sizes) may be placed on a single page, and multiple pages are allowed for those device formats that support them.</p>
+	<p>PLplot has core support for Unicode. This means plots can be labelled using the enormous selection of Unicode mathematical symbols for simple text layout (left to right) language scripts using our libfreetype-based devices. For our SVG and libpango/libcairo-based devices we additionally support complex text layout (CTL) languages such as Arabic, Hebrew, and Indic and Indic-derived CTL scripts such as Devanagari, Thai, Lao, and Tibetan.</p>
+        <p>A number of compiled and interpreted languages have access to PLplot, <a href="#languages">see our list of language bindings</a>.</p>
+        <p>PLplot device drivers support <a href="#files">a number of different file formats</a> for non-interactive plotting and <a href="#interactive">a number of different platforms</a> that are suitable for interactive plotting.  It is easy to add new device drivers to PLplot by writing a small number of device dependent routines.</p>
+<p>PLplot is free software primarily licensed under the
 				 <a href="http://www.gnu.org/licenses/lgpl.html">LGPL</a>.</p>
 
 			<h3>Feature Summary</h3>
 			<div class="splitcontentleft">
 				<h4>Cross Platform</h4>
-				 <p>PLplot is currently known to work on following platforms:</p>
+				 <p>PLplot is currently known to work on the following platforms:</p>
 				 <ul class="arrowlist">
-				   <li>Linux and other Unixes</li>
+				   <li>Linux, Mac OS X, and other Unices</li>
 				   <li>Windows (2000, XP and Vista)</li>
-				   <li>Mac OS X</li>
 				   <li>MS-DOS (DJGPP)</li>
 				 </ul>
-				<h4>Output file formats</h4>
-          <p>pdf, svg, postscript, cgm, png, jpeg, gif pbm, xfig and latex</p>
-        <h4>Interactive Platforms</h4>
-          <p>X, Tcl/Tk, wxWidgets, Gnome</p>
-			</div>
-			<div class="splitcontentright">
-				<h4>Language Bindings</h4>
+
+				 <p>With the exception of DJGPP, where we have implemented a custom build system, PLplot is built on the above platforms using our CMake-based build system.</p>
+				<h4><a name="languages">Language Bindings</a></h4>
 				<p>There are many bindings to the PLplot library</p>
 				<ul class="arrowlist">
-				  <li>C/C++</li>
-				  <li>Fortran 77/90</li>
-				  <li>D</li>
-				  <li>Java</li>
-				  <li>Python</li>
-				  <li>Perl</li>
 				  <li>Ada</li>
+				  <li>C/C++/D</li>
+				  <li>Fortran 77/90</li>
+				  <li>Java</li>
 				  <li>OCaml</li>
 				  <li>Octave</li>
+				  <li>Perl</li>
+				  <li>Python</li>
 				  <li>Tcl/Tk</li>
+				  <li>wxWidgets</li>
+				</ul>
+			</div>
+			<div class="splitcontentright">
+				<h4><a name="files">Output file formats</a></h4>
+				<p>PLplot device drivers support a number of plotting file formats.</p>
+				<ul class="arrowlist">
+				  <li>CGM</li>
+				  <li>GIF</li>
+				  <li>JPEG</li>
+				  <li>LaTeX</li>
+				  <li>PBM</li>
+				  <li>PDF</li>
+				  <li>PNG</li>
+				  <li>PostScript</li>
+				  <li>SVG</li>
+				  <li>Xfig</li>
+				</ul>
+				<h4><a name="interactive">Interactive Platforms</a></h4>
+				<p>PLplot device drivers support a number of platforms that are suitable for interactive plotting.</p>
+				<ul class="arrowlist">
+				  <li>X</li>
+				  <li>GNOME</li>
+				  <li>Tcl/Tk</li>
+				  <li>PyQt</li>
 				  <li>wxWidgets</li>
 				</ul>
 			</div>
