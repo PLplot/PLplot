b18d85e7ac7712a50b8979fc5bc0a2dbd8577b2c
Alan W. Irwin <airwin@users.sourceforge.net>
2008-12-15 21:23:03 +0000

    Add prototype of example 31 for testing PLplot set/get functions.
    svn path=/trunk/; revision=9124

---
+++ examples/c/x31c.c
@@ -0,0 +1,38 @@
+/* $Id$
+
+   set/get tester
+*/
+
+#include "plcdemos.h"
+
+/*--------------------------------------------------------------------------*\
+ * main
+ *
+ * Demonstrates absolute positioning of graphs on a page.
+ \*--------------------------------------------------------------------------*/
+
+int
+main(int argc, const char *argv[])
+{
+  PLFLT xmin, xmax, ymin, ymax;
+  /* Parse and process command line arguments */
+
+  (void) plparseopts(&argc, argv, PL_PARSE_FULL);
+
+  /* Initialize plplot */
+
+  plinit();
+
+  pladv(0);
+  plvpor(0.0, 1.0, 0.0, 1.0);
+
+  plwind(0.2, 0.3, 0.4, 0.5);
+  plgvpw(&xmin, &xmax, &ymin, &ymax);
+  if (xmin != 0.2 || xmax != 0.3 || ymin != 0.4 || ymax != 0.5) {
+    fprintf(stderr, "%s\n", "plgvpi test failed");
+    plend();
+    exit(1);
+  }
+  plend();
+  exit(0);
+}
