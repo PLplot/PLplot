5114309f8fb99b78847fac48dddb738a578f9566
Alan W. Irwin <airwin@users.sourceforge.net>
2008-12-16 19:09:56 +0000

    Copyright notice.
    svn path=/trunk/; revision=9146

--- examples/c/x31c.c
+++ examples/c/x31c.c
@@ -1,15 +1,28 @@
 /* $Id$
 
-   set/get tester
+  Copyright (C) 2008 Alan W. Irwin
+
+  set/get tester
+
+  This file is part of PLplot.
+  
+  PLplot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Library Public License as published
+  by the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  PLplot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Library General Public License for more details.
+  
+  You should have received a copy of the GNU Library General Public License
+  along with PLplot; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+  
 */
 
 #include "plcdemos.h"
-
-/*--------------------------------------------------------------------------*\
- * main
- *
- * Demonstrates absolute positioning of graphs on a page.
- \*--------------------------------------------------------------------------*/
 
 int
 main(int argc, const char *argv[])
