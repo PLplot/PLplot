5f6822d6a1432e60ac627ac70738778d08800f05
Alan W. Irwin <airwin@users.sourceforge.net>
2008-12-16 23:40:58 +0000

    Update 31st python example to be standalone.  Also implement some more of the tests (although the list is not completed, yet).
    svn path=/trunk/; revision=9157

--- examples/python/xw31.py
+++ examples/python/xw31.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # $Id$
 
 #  Copyright (C) 2008 Alan W. Irwin
@@ -20,21 +21,85 @@
 #  along with PLplot; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
+# Append to effective python path so that can find plplot modules.
+from plplot_python_start import *
+
+import sys
+from plplot import *
+
+# Parse and process command line arguments
+plparseopts(sys.argv, PL_PARSE_FULL)
+
+# Test setting / getting page size across plinit.
+xp1 = 200.
+yp1 = 200.
+xleng1 = 400
+yleng1 = 200
+xoff1 = 10
+yoff1 = 20
+plspage(xp1, yp1, xleng1, yleng1, xoff1, yoff1)
+
+compression = plgcompression()
+compression1 = compression + 100
+plscompression(compression1)
+compression2 = plgcompression()
+if compression2 != compression1:
+    sys.stderr.write("plgcompression test failed\n")
+    plend()
+    sys.exit(1)
+plscompression(compression)
+
+(fam, num, bmax) = plgfam()
+fam1 = fam + 100
+num1 = num + 100
+bmax1 = bmax + 100
+plsfam(fam1, num1, bmax1)
+(fam2, num2, bmax2) = plgfam()
+if fam2 != fam1  or num2 != num1 or bmax2 != bmax1:
+    sys.stderr.write("plgfam test failed\n")
+    plend()
+    sys.exit(1)
+plsfam(fam, num, bmax)
+          
+# Initialize plplot
+plinit()
+
 from plplot_py_demos import *
 
 def main():
 
+    # Test if device initialization screwed around with any of the
+    # preset page values.
+    (xp2, yp2, xleng2, yleng2, xoff2, yoff2) = plgpage()
+    if xp2 != xp1 or yp2 != yp1 or xleng2 != xleng1 or yleng2 != yleng1 or xoff2 != xoff1 or yoff2 != yoff1:
+        sys.stderr.write("plgpage test failed\n")
+        plend()
+        sys.exit(1)
+
+    # Exercise plscolor, plscol0, plscmap1, and plscmap1a to make sure
+    # they work without any obvious error messages.
+    plscolor(1)
+    plscol0(1, 255, 0, 0)
+    r1 = [0, 255]
+    g1 = [255, 0]
+    b1 = [0, 0]
+    a1 = [1.0, 1.0]
+    plscmap1(r1,g1,b1)
+    plscmap1a(r1,g1,b1,a1)
+
+    level = plglevel()
+    if level != 1:
+        sys.stderr.write("plglevel test failed. Level is %d, but 1 expected.\n" % level)
+        plend();
+        sys.exit(1);
+
     pladv(0)
     plvpor(0.0, 1.0, 0.0, 1.0)
-    plwind(0.0, 1.0, 0.0, 1.0)
-    plbox("bc", 0.0, 0, "bc", 0.0, 0)
-
-    plsvpa(50.0, 150.0, 50.0, 100.0)
-    plwind(0.0, 1.0, 0.0, 1.0)
-    plbox("bc", 0.0, 0, "bc", 0.0, 0)
-    plptex(0.5, 0.5, 1.0, 0.0, 0.5, "BOX at (50,150,50,100)")
+    plwind(0.2, 0.3, 0.4, 0.5)
 
     # Restore defaults
     #plcol0(1)
     
 main()
+# Terminate plplot
+plend()
