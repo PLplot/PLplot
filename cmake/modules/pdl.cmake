ba3ea3bf97320efb652d5340eda8bb916244fe41
Alan W. Irwin <airwin@users.sourceforge.net>
2009-01-07 23:18:30 +0000

    Tweak messages.
    svn path=/trunk/; revision=9276

--- cmake/modules/pdl.cmake
+++ cmake/modules/pdl.cmake
@@ -29,7 +29,7 @@ endif(DEFAULT_NO_BINDINGS)
 if(ENABLE_pdl)
   if(NOT BUILD_SHARED_LIBS)
     message(STATUS "WARNING: "
-    "Disabling Perl/PDL examples because BUILD_SHARED_LIBS=OFF")
+      "Disabling Perl/PDL examples because BUILD_SHARED_LIBS=OFF")
     set(ENABLE_pdl OFF CACHE BOOL "Enable Perl/PDL examples in tests" FORCE)
   endif(NOT BUILD_SHARED_LIBS)
 endif(ENABLE_pdl)
@@ -37,59 +37,60 @@ endif(ENABLE_pdl)
 if(ENABLE_pdl)
   if(PERL_FOUND)
     execute_process(
-    COMMAND ${PERL_EXECUTABLE}
-    -e "use PDL"
-    RESULT_VARIABLE PDL_RETURNCODE
-    OUTPUT_QUIET
-    ERROR_QUIET
-    )
+      COMMAND ${PERL_EXECUTABLE}
+      -e "use PDL"
+      RESULT_VARIABLE PDL_RETURNCODE
+      OUTPUT_QUIET
+      ERROR_QUIET
+      )
     if(NOT PDL_RETURNCODE)
       execute_process(
-      COMMAND ${PERL_EXECUTABLE}
-      "-e use PDL; print \"$PDL::VERSION\""
-      OUTPUT_VARIABLE PDL_VERSION
-      )
-    message("PDL_VERSION = ${PDL_VERSION}")
+	COMMAND ${PERL_EXECUTABLE}
+	"-e use PDL; print \"$PDL::VERSION\""
+	OUTPUT_VARIABLE PDL_VERSION
+	)
+      message(STATUS "PDL_VERSION = ${PDL_VERSION}")
       transform_version(NUMERICAL_PDL_MINIMUM_VERSION "2.4.1")
       transform_version(NUMERICAL_PDL_VERSION "${PDL_VERSION}")
       if(NUMERICAL_PDL_VERSION LESS "${NUMERICAL_PDL_MINIMUM_VERSION}")
         message(STATUS "WARNING: "
-        "perl PDL version < 2.4.1. "
-	"Disabling Perl/PDL examples")
+          "PDL version < 2.4.1. Disabling Perl/PDL examples in tests")
         set(ENABLE_pdl OFF CACHE BOOL "Enable Perl/PDL examples in tests" FORCE)
       else(NUMERICAL_PDL_VERSION LESS "${NUMERICAL_PDL_MINIMUM_VERSION}")
         message(STATUS 
-	   "Perl and PDL are available and PDL passes version test.\n"
-	"   Enable Perl/PDL examples in tests"
-	)
+	  "PDL version >= 2.4.1.  Enabling Perl/PDL examples in tests"
+	  )
         execute_process(
 	  COMMAND ${PERL_EXECUTABLE}
 	  -MPDL::Graphics::PLplot "-e print \"$PDL::Graphics::PLplot::VERSION\""
           OUTPUT_VARIABLE PDL_PLPLOT_VERSION
           ERROR_VARIABLE PDL_PLPLOT_VERSION
           )
-        #message("PDL_PLPLOT_VERSION = ${PDL_PLPLOT_VERSION}")
+        message(STATUS "PDL_PLPLOT_VERSION = ${PDL_PLPLOT_VERSION}")
         transform_version(NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION "0.47.0")
         transform_version(NUMERICAL_PDL_PLPLOT_VERSION "${PDL_PLPLOT_VERSION}.0")
 	#message("NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION = ${NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION}")
 	#message("NUMERICAL_PDL_PLPLOT_VERSION = ${NUMERICAL_PDL_PLPLOT_VERSION}")
         if(NUMERICAL_PDL_PLPLOT_VERSION LESS "${NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION}")
           message(STATUS "WARNING: "
-          "perl PDL PLplot version < 0.47 "
-          "Using more limited set of Perl/PDL examples that work with old versions")
+            "perl PDL PLplot version < 0.47\n"
+            "   Using limited set of Perl/PDL examples that work with this old version")
           set(HAVE_PDL_GRAPHICS_PLPLOT_40 OFF CACHE BOOL "PDL-Graphics-PLplot version is 0.47 or higher" FORCE)
         else(NUMERICAL_PDL_PLPLOT_VERSION LESS "${NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION}")
+          message(STATUS
+            "perl PDL PLplot version >= 0.47\n"
+            "   Using full set of Perl/PDL examples that work with this version")
           set(HAVE_PDL_GRAPHICS_PLPLOT_40 ON CACHE BOOL "PDL-Graphics-PLplot version is 0.47 or higher" FORCE)
         endif(NUMERICAL_PDL_PLPLOT_VERSION LESS "${NUMERICAL_PDL_PLPLOT_MINIMUM_VERSION}")
       endif(NUMERICAL_PDL_VERSION LESS "${NUMERICAL_PDL_MINIMUM_VERSION}")
     else(NOT PDL_RETURNCODE)
       message(STATUS "WARNING: "
-      "perl PDL module not found. Disabling Perl/PDL examples")
+	"perl PDL module not found. Disabling Perl/PDL examples")
       set(ENABLE_pdl OFF CACHE BOOL "Enable Perl/PDL examples in tests" FORCE)
     endif(NOT PDL_RETURNCODE)
   else(PERL_FOUND)
     message(STATUS "WARNING: "
-    "perl not found. Disabling Perl/PDL examples")
+      "perl not found. Disabling Perl/PDL examples")
     set(ENABLE_pdl OFF CACHE BOOL "Enable Perl/PDL examples in tests" FORCE)
   endif(PERL_FOUND)
 endif(ENABLE_pdl)
