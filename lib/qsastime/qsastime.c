59bf7540915f7d82d17704d936dbb51bc50d103c
Andrew Ross <andrewross@users.sourceforge.net>
2009-02-18 09:36:04 +0000

    Make global variables in qsastime.c const as they are constants. This makes it clear that they are thread-safe.
    svn path=/trunk/; revision=9548

--- lib/qsastime/qsastime.c
+++ lib/qsastime/qsastime.c
@@ -53,8 +53,8 @@
 #define MJD_0001J -678577
 #define MJD_0001G -678575
 
-static double SecInDay = 86400; /* we ignore leap seconds */
-static int MJD_1970 = 40587; /* MJD for Jan 01, 1970 00:00:00 */
+static const double SecInDay = 86400; /* we ignore leap seconds */
+static const int MJD_1970 = 40587; /* MJD for Jan 01, 1970 00:00:00 */
 static const int MonthStartDOY[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
 static const int MonthStartDOY_L[] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
 	
