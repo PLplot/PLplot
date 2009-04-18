cb5f305506ac96387b3c38de6b54073082dfa91a
Alan W. Irwin <airwin@users.sourceforge.net>
2009-04-18 00:57:03 +0000

    AWI for Alban Rochel.  Initial commit of the source files for a simple Qt GUI application that accesses the PLplot API using the extqt "device".
    svn path=/trunk/; revision=9817

---
+++ examples/c++/qt_example.cpp
@@ -0,0 +1,36 @@
+/*
+
+  This software is provided under the LGPL in March 2009 by the
+  Cluster Science Centre
+  QSAS team,
+  Imperial College, London
+
+  Copyright (C) 2009  Imperial College, London
+
+  This is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Lesser Public License as published
+  by the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+  This software is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  To received a copy of the GNU Library General Public License
+  write to the Free Software Foundation, Inc., 
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+  
+*/
+
+#include "qt_PlotWindow.h"
+
+int main(int argc, char** argv)
+{
+	QApplication a( argc, argv );
+	PlotWindow* win=new PlotWindow;
+	a.setActiveWindow( win );
+	win->setVisible(true);
+
+	return a.exec();
+}
