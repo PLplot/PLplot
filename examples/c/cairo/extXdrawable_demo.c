702da58438cfe66b61f94ba60465ceea5090a07d
Alan W. Irwin <airwin@users.sourceforge.net>
2009-04-20 18:19:33 +0000

    Change license from GPL to LGPL to be consistent with the rest of PLplot. Jonathan Woithe <jwoithe@physics.adelaide.edu.au>, the original author of this software, has kindly agreed to this change.  To quote his off-list e-mail to me:
    "Sure, I have no problem with relicensing this under the LGPL in the
    interests of consistency with the rest of the plplot library."
    
    
    svn path=/trunk/; revision=9821

--- examples/c/cairo/extXdrawable_demo.c
+++ examples/c/cairo/extXdrawable_demo.c
@@ -1,13 +1,25 @@
 /*
- * A simple GTK application demonstrating the use of the X Drawable
- * functionality of plplot's xcairo driver.
- *
- * Copyright (C) 2008 Jonathan Woithe <jwoithe@physics.adelaide.edu.au>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+  A simple GTK application demonstrating the use of the X Drawable
+  functionality of plplot's xcairo driver.
+
+  Copyright (C) 2008 Jonathan Woithe <jwoithe@physics.adelaide.edu.au>
+
+  This file is part of PLplot.
+
+  PLplot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Library Public License as published
+  by the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  PLplot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with PLplot; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
  */
 
 /* Set to 0 to draw direct to an X Window, 1 to draw via a pixmap */
