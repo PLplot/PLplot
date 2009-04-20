b748ec308e43e478b1728c003b075223945095a4
Arjen Markus <arjenmarkus@users.sourceforge.net>
2009-04-20 04:05:20 +0000

    Use "brute force" to determine the import libraries for gdi32.dll and comdlg32.dll under MSYS.
    svn path=/trunk/; revision=9820

--- cmake/modules/wingcc.cmake
+++ cmake/modules/wingcc.cmake
@@ -23,16 +23,26 @@
 # PLD_wingcc		  - ON means the wingcc device is enabled.
 # wingcc_COMPILE_FLAGS	  - individual COMPILE_FLAGS required to compile wingcc
 # 			    device.
-# wingcc_LINK_FLAGS	  - list of pathnames of libraries for dynamic 
+# wingcc_LINK_FLAGS	  - list of pathnames of libraries for dynamic
 # 			    wingcc device.
 # DRIVERS_LINK_FLAGS	  - list of LINK_FLAGS for all static devices.
 
 if(PLD_wingcc)
   message(STATUS "Looking for gdi32 header and library")
-  find_library(GDI32_LIBRARY gdi32 HINTS ${MINGWLIBPATH} ${BORLANDLIBPATH})
-  if(GDI32_LIBRARY)
-    find_library(COMDLG32_LIBRARY comdlg32 HINTS ${MINGWLIBPATH} ${BORLANDLIBPATH})
-  endif(GDI32_LIBRARY)
+  if(NOT MSYS)
+    find_library(GDI32_LIBRARY gdi32 HINTS ${MINGWLIBPATH} ${BORLANDLIBPATH})
+    if(GDI32_LIBRARY)
+      find_library(COMDLG32_LIBRARY comdlg32 HINTS ${MINGWLIBPATH} ${BORLANDLIBPATH})
+    endif(GDI32_LIBRARY)
+  else(NOT MSYS)
+    # Use fixed names instead
+    # Note: MINGWLIBPATH gave problem: CACHE unknown component
+    get_filename_component(msys_BIN ${CMAKE_C_COMPILER} PATH)
+    set(msys_LIBDIR ${msys_BIN}/../lib)
+    #get_filename_component(msys_LIBDIR ${msys_LIBS} PATH)
+    set(GDI32_LIBRARY ${msys_LIBDIR}/libgdi32.a)
+    set(COMDLG32_LIBRARY ${msys_LIBDIR}/libcomdlg32.a)
+  endif(NOT MSYS)
   if(GDI32_LIBRARY AND COMDLG32_LIBRARY)
     message(STATUS "Looking for gdi32 header and library - found")
     set(wingcc_LINK_FLAGS "${GDI32_LIBRARY};${COMDLG32_LIBRARY}")
@@ -51,6 +61,6 @@ if(PLD_wingcc)
   else(GDI32_LIBRARY AND COMDLG32_LIBRARY)
     message(STATUS "Looking for gdi32 header and library - not found")
     message(STATUS "WARNING: Setting PLD_wingcc to OFF.")
-    set(PLD_wingcc OFF CACHE BOOL "Enable wingcc device" FORCE)    
+    set(PLD_wingcc OFF CACHE BOOL "Enable wingcc device" FORCE)
   endif(GDI32_LIBRARY AND COMDLG32_LIBRARY)
 endif(PLD_wingcc)
