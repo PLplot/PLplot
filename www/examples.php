85110683f42016f20ee65a85220adaeeff687eb9
Hazen Babcock <hbabcock@users.sourceforge.net>
2009-05-03 21:36:57 +0000

    Update web page for 5.9.3
    svn path=/trunk/; revision=9895

--- www/examples.php
+++ www/examples.php
@@ -204,7 +204,7 @@ END;
 		echo <<<END
 			<h3>Examples</h3>
 			<p>These examples were generated with the pngcairo device and
-				 reflect the status of PLplot-5.9.2.</p>
+				 reflect the status of PLplot-5.9.3.</p>
 			<p>Select a thumbnail to view full-size image and source code.</p>
 			<p>
 END;
