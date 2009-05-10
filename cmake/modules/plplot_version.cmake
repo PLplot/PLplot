2e331f2fc927b5ce8d680456d1c66568bc37e322
Hazen Babcock <hbabcock@users.sourceforge.net>
2009-05-10 19:20:10 +0000

    Updated for the 5.9.4 release.
    svn path=/trunk/; revision=9951

--- cmake/modules/plplot_version.cmake
+++ cmake/modules/plplot_version.cmake
@@ -1,15 +1,15 @@
 # For now, must set this by hand before each release.  This value affects
 # at least the documentation build and perhaps other parts of the release.
-set(RELEASE_DATE "2009-05-03")
+set(RELEASE_DATE "2009-05-10")
 
 # Version data that need review and possible modification for each release.
 
-set(VERSION 5.9.3)
+set(VERSION 5.9.4)
 
 # CPack version numbers for release tarball name.
 set(CPACK_PACKAGE_VERSION_MAJOR 5)
 set(CPACK_PACKAGE_VERSION_MINOR 9)
-set(CPACK_PACKAGE_VERSION_PATCH 3)
+set(CPACK_PACKAGE_VERSION_PATCH 4)
 
 # PLplot library version information.
 
