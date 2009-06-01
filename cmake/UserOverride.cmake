a56ced6e29fdec3e9757a1c5d45d4fe2e8af6965
Rafael Laboissiere <rlaboiss@users.sourceforge.net>
2009-06-01 19:53:51 +0000

    Tag Debian release plplot_5.9.2-3
    svn path=/tags/debian/5.9.2-3/; revision=10025

--- cmake/UserOverride.cmake
+++
@@ -1,5 +0,0 @@
-# User overrides of CMake default compiler and linking rules.
-if(APPLE)
-  set(CMAKE_SHARED_LINKER_FLAGS "-single_module"
-  CACHE STRING "Flags used to link a shared library.")
-endif(APPLE)
--- cmake/modules/TestForStdintCXX.cxx
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-typedef uint32_t PLINT;
-
-int main(int, char*[]) {
-  PLINT foo=1;
-  return 0;
-}
