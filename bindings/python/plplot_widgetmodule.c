cbccfdbddd91e7edc8b10db4cfa54e8ba9c9ff6a
Alan W. Irwin <airwin@users.sourceforge.net>
2009-07-06 02:09:13 +0000

    We have an actively maintained pyqt4 interface to PLplot and corresponding example.  Therefore, remove pyqt3 interface to PLplot and pyqt3 example since those are unmaintained and not understood that well.
    svn path=/trunk/; revision=10120

--- bindings/python/plplot_widgetmodule.c
+++ bindings/python/plplot_widgetmodule.c
@@ -14,69 +14,6 @@
 
 #define TRY(E) if(! (E)) return NULL
 
-/* ##############################################################################*/
-static char doc_partialInitXw[]="Partially init a new device (X Window) ";
-
-#ifndef WIN32
-#include "plxwd.h"
-static PyObject * pl_partialInitXw(PyObject *self, PyObject *args)
-{
-    PLINT ipls;
-    PLStream *pls;  
-    TRY (PyArg_ParseTuple(args, ":pl_partialInitXw"));
-    ipls=0;
-
-    plmkstrm(&ipls);
-    printf(" ipls=%d\n",ipls); 
-    plgpls(&pls);   /* pls points directly to a structure PLStream */
-    printf(" pls OK \n"); 
-
-    /* Partially initialize X driver. */
-
-    pllib_init();
-
-    plsdev("xwin");
-    pllib_devinit();
-    plP_esc(PLESC_DEVINIT, NULL);
-
-    printf("devinit  OK \n"); 
-
-    return Py_BuildValue("i", ipls);
-}
-#endif 
-
-
-static char doc_resize[]="resize the window";
-
-static PyObject * pl_resize(PyObject *self, PyObject *args)
-{
-    int width, height ;
-    PLDisplay pldis;
-    TRY (PyArg_ParseTuple(args, "ii:resize", &width, &height));
-
-    pldis.width=width;
-    pldis.height=height;
-    pl_cmd(PLESC_RESIZE, (void *) &(pldis));     
-    pl_cmd(PLESC_EXPOSE, (void *) NULL);     
-
-    Py_INCREF(Py_None);
-    return Py_None;
- 
-}
-
-static char doc_expose[]="expose the whole   window";
-
-static PyObject * pl_expose(PyObject *self, PyObject *args)
-{
-    TRY (PyArg_ParseTuple(args, ":expose"));
-
-    pl_cmd(PLESC_EXPOSE, (void *) NULL);     
-
-    Py_INCREF(Py_None);
-    return Py_None;
- 
-}
-
 #ifdef ENABLE_tk
 static char doc_Pltk_init[] = "Initialize the Pltk Tcl extension.";
 
@@ -121,18 +58,6 @@ static PyObject *pl_Pltk_init(PyObject *self, PyObject *args)
 /* ##############################################################################*/
 
 static PyMethodDef plplot_widget_methods[] = {
-    /* These are a few functions  that I found I needed
-       to call in order to initialise the pyqt widget  */
-
-#ifndef WIN32
-    {"plpartialInitXw",	pl_partialInitXw, 1, doc_partialInitXw},
-#endif
-
-    /* These are a few functions  that I found I needed
-       to filtering events in  pyqt widget  */
-
-    {"plresize",	pl_resize, METH_VARARGS, doc_resize},
-    {"plexpose",	pl_expose, METH_VARARGS, doc_expose},
 
 #ifdef ENABLE_tk
     {"Pltk_init",      pl_Pltk_init, METH_VARARGS, doc_Pltk_init},
