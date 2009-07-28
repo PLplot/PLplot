9588025202ae5d3e394f5bc0ea648c1a02736ac6
Alan W. Irwin <airwin@users.sourceforge.net>
2009-07-28 21:02:56 +0000

    Change TRANSFORM_VERSION from a macro to a function.  Make that function much more robust against version string variations including ignoring everything in the version string after any character that is not in [0-9.] and replacing any patch, minor, or major substring that does not match [0-9]+ with 0.
    svn path=/trunk/; revision=10187

--- cmake/modules/plplot.cmake
+++ cmake/modules/plplot.cmake
@@ -28,23 +28,43 @@ include(CheckFunctionExists)
 include(CheckSymbolExists)
 include(CheckPrototypeExists)
 
-# Useful macros go here.
+# Useful functions go here.
 
-macro(TRANSFORM_VERSION _numerical_result _version)
-# _version must be a period-delimited triplet string of the form
-# "major.minor.patch".
-# This macro transforms that into a numerical result that can be compared.
-if(${_version} MATCHES "^[0-9]*\\.[0-9]*\\.[0-9]*$")
-  string(REGEX REPLACE "^([0-9]*)\\.[0-9]*\\.[0-9]*$" "\\1" _major ${_version})
-  string(REGEX REPLACE "^[0-9]*\\.([0-9]*)\\.[0-9]*$" "\\1" _minor ${_version})
-  string(REGEX REPLACE "^[0-9]*\\.[0-9]*\\.([0-9]*)$" "\\1" _patch ${_version})
-  math(EXPR ${_numerical_result}
-  "${_major}*1000000 + ${_minor}*1000 + ${_patch}
-  ")
-else(${_version} MATCHES "^[0-9]*\\.[0-9]*\\.[0-9]*$")
-  set(${_numerical_result} 0)
-endif(${_version} MATCHES "^[0-9]*\\.[0-9]*\\.[0-9]*$")
-endmacro(TRANSFORM_VERSION)
+function(TRANSFORM_VERSION numerical_result version)
+  # internal_version ignores everything in version after any character that
+  # is not 0-9 or ".".  This should take care of the case when there is
+  # some non-numerical data in the patch version.
+  #message(STATUS "DEBUG: version = ${version}")
+  string(REGEX REPLACE "^([0-9.]+).*$" "\\1" internal_version ${version})
+
+  # internal_version is normally a period-delimited triplet string of the form
+  # "major.minor.patch", but patch and/or minor could be missing.
+  # Transform internal_version into a numerical result that can be compared.
+  string(REGEX REPLACE "^([0-9]*).+$" "\\1" major ${internal_version})
+  string(REGEX REPLACE "^[0-9]*\\.([0-9]*).*$" "\\1" minor ${internal_version})
+  string(REGEX REPLACE "^[0-9]*\\.[0-9]*\\.([0-9]*)$" "\\1" patch ${internal_version})
+
+  if(NOT patch MATCHES "[0-9]+")
+    set(patch 0)
+  endif(NOT patch MATCHES "[0-9]+")
+  
+  if(NOT minor MATCHES "[0-9]+")
+    set(minor 0)
+  endif(NOT minor MATCHES "[0-9]+")
+  
+  if(NOT major MATCHES "[0-9]+")
+    set(major 0)
+  endif(NOT major MATCHES "[0-9]+")
+  #message(STATUS "DEBUG: internal_version = ${internal_version}")
+  #message(STATUS "DEBUG: major = ${major}")
+  #message(STATUS "DEBUG: minor= ${minor}")
+  #message(STATUS "DEBUG: patch = ${patch}")
+  math(EXPR internal_numerical_result
+    "${major}*1000000 + ${minor}*1000 + ${patch}"
+    )
+  #message(STATUS "DEBUG: ${numerical_result} = ${internal_numerical_result}")
+  set(${numerical_result} ${internal_numerical_result} PARENT_SCOPE)
+endfunction(TRANSFORM_VERSION)
 
 # =======================================================================
 # Compilation and build options (PLFLT, install locations, and rpath)
