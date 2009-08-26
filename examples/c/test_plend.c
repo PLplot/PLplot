d95ae78a1d0cd53869403269b064012023a3a97e
Alan W. Irwin <airwin@users.sourceforge.net>
2009-08-26 21:07:02 +0000

    Add small test routine to demonstrate plend or plend1 problems with certain devices.
    svn path=/trunk/; revision=10348

---
+++ examples/c/test_plend.c
@@ -0,0 +1,31 @@
+/* $Id$
+
+	plend and plend1 testing demo.
+*/
+
+#include "plcdemos.h"
+
+/*--------------------------------------------------------------------------*\
+ * main
+ *
+ * Demonstrates absolute positioning of graphs on a page.
+\*--------------------------------------------------------------------------*/
+
+int
+main(int argc, const char *argv[])
+{
+
+/* Parse and process command line arguments */
+
+    (void) plparseopts(&argc, argv, PL_PARSE_FULL);
+
+/* Initialize plplot */
+
+    plinit();
+    plenv(0., 1., 0., 1., 1, 0);
+    plend1();
+    plinit();
+    plenv(0., 10., 0., 10., 1, 0);
+    plend();
+    exit(0);
+}
