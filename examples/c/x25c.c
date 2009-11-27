3498ff0e3bfe66a732a0c53b4d97b940db96bd88
Alan W. Irwin <airwin@users.sourceforge.net>
2009-11-27 01:57:07 +0000

    Add four extra pages to test the experimental new plgradient capability. Because plgradient is experimental with an API that is subject to change do not propagate this C example change to the other languages yet.
    svn path=/trunk/; revision=10631

--- examples/c/x25c.c
+++ examples/c/x25c.c
@@ -14,8 +14,7 @@
 int
 main( int argc, const char *argv[] )
 {
-    int   i;
-    int   j;
+    int   i, j, k;
     int   npts = 0;
     PLFLT xextreme[10][2];
     PLFLT yextreme[10][2];
@@ -41,71 +40,77 @@ main( int argc, const char *argv[] )
     xextreme[7][0] = -80.0; xextreme[7][1] = 80.0; yextreme[7][0] = -80.0; yextreme[7][1] = 80.0;
     xextreme[8][0] = 20.0; xextreme[8][1] = 120.0; yextreme[8][0] = -120.0; yextreme[8][1] = 120.0;
 
-    for ( j = 0; j < 4; j++ )
+    for ( k = 0; k < 2; k++ )
     {
-        if ( j == 0 )
+        for ( j = 0; j < 4; j++ )
         {
+            if ( j == 0 )
+            {
 /* Polygon 1: a diamond */
-            x0[0] = 0; y0[0] = -100;
-            x0[1] = -100; y0[1] = 0;
-            x0[2] = 0; y0[2] = 100;
-            x0[3] = 100; y0[3] = 0;
-            npts  = 4;
-        }
-        if ( j == 1 )
-        {
+                x0[0] = 0; y0[0] = -100;
+                x0[1] = -100; y0[1] = 0;
+                x0[2] = 0; y0[2] = 100;
+                x0[3] = 100; y0[3] = 0;
+                npts  = 4;
+            }
+            if ( j == 1 )
+            {
 /* Polygon 1: a diamond - reverse direction */
-            x0[3] = 0; y0[3] = -100;
-            x0[2] = -100; y0[2] = 0;
-            x0[1] = 0; y0[1] = 100;
-            x0[0] = 100; y0[0] = 0;
-            npts  = 4;
-        }
-        if ( j == 2 )
-        {
+                x0[3] = 0; y0[3] = -100;
+                x0[2] = -100; y0[2] = 0;
+                x0[1] = 0; y0[1] = 100;
+                x0[0] = 100; y0[0] = 0;
+                npts  = 4;
+            }
+            if ( j == 2 )
+            {
 /* Polygon 2: a square with punctures */
-            x0[0] = -100; y0[0] = -100;
-            x0[1] = -100; y0[1] = -80;
-            x0[2] = 80; y0[2] = 0;
-            x0[3] = -100; y0[3] = 80;
-            x0[4] = -100; y0[4] = 100;
-            x0[5] = -80; y0[5] = 100;
-            x0[6] = 0; y0[6] = 80;
-            x0[7] = 80; y0[7] = 100;
-            x0[8] = 100; y0[8] = 100;
-            x0[9] = 100; y0[9] = -100;
-            npts  = 10;
-        }
-        if ( j == 3 )
-        {
+                x0[0] = -100; y0[0] = -100;
+                x0[1] = -100; y0[1] = -80;
+                x0[2] = 80; y0[2] = 0;
+                x0[3] = -100; y0[3] = 80;
+                x0[4] = -100; y0[4] = 100;
+                x0[5] = -80; y0[5] = 100;
+                x0[6] = 0; y0[6] = 80;
+                x0[7] = 80; y0[7] = 100;
+                x0[8] = 100; y0[8] = 100;
+                x0[9] = 100; y0[9] = -100;
+                npts  = 10;
+            }
+            if ( j == 3 )
+            {
 /* Polygon 2: a square with punctures - reversed direction */
-            x0[9] = -100; y0[9] = -100;
-            x0[8] = -100; y0[8] = -80;
-            x0[7] = 80; y0[7] = 0;
-            x0[6] = -100; y0[6] = 80;
-            x0[5] = -100; y0[5] = 100;
-            x0[4] = -80; y0[4] = 100;
-            x0[3] = 0; y0[3] = 80;
-            x0[2] = 80; y0[2] = 100;
-            x0[1] = 100; y0[1] = 100;
-            x0[0] = 100; y0[0] = -100;
-            npts  = 10;
-        }
+                x0[9] = -100; y0[9] = -100;
+                x0[8] = -100; y0[8] = -80;
+                x0[7] = 80; y0[7] = 0;
+                x0[6] = -100; y0[6] = 80;
+                x0[5] = -100; y0[5] = 100;
+                x0[4] = -80; y0[4] = 100;
+                x0[3] = 0; y0[3] = 80;
+                x0[2] = 80; y0[2] = 100;
+                x0[1] = 100; y0[1] = 100;
+                x0[0] = 100; y0[0] = -100;
+                npts  = 10;
+            }
 
-        for ( i = 0; i < 9; i++ )
-        {
-            pladv( 0 );
-            plvsta();
-            plwind( xextreme[i][0], xextreme[i][1], yextreme[i][0], yextreme[i][1] );
+            for ( i = 0; i < 9; i++ )
+            {
+                pladv( 0 );
+                plvsta();
+                plwind( xextreme[i][0], xextreme[i][1], yextreme[i][0], yextreme[i][1] );
 
-            plcol0( 2 );
-            plbox( "bc", 1.0, 0, "bcnv", 10.0, 0 );
-            plcol0( 1 );
-            plpsty( 0 );
-            plfill( npts, x0, y0 );
-            plcol0( 2 );
-            pllsty( 1 );
-            plline( npts, x0, y0 );
+                plcol0( 2 );
+                plbox( "bc", 1.0, 0, "bcnv", 10.0, 0 );
+                plcol0( 1 );
+                plpsty( 0 );
+                if ( k == 0 )
+                    plfill( npts, x0, y0 );
+                else
+                    plgradient( npts, x0, y0, 0., 1., 45. );
+                plcol0( 2 );
+                pllsty( 1 );
+                plline( npts, x0, y0 );
+            }
         }
     }
 
