60813a45d64d0f0dc4164e9dd48febb088badb9f
Alan W. Irwin <airwin@users.sourceforge.net>
2010-02-26 21:03:34 +0000

    Improve script so uses /tmp rather than generating a bunch of files in source tree.
    svn path=/trunk/; revision=10819

--- scripts/test_bisect.sh
+++ scripts/test_bisect.sh
@@ -6,16 +6,22 @@
 # This particular case is used to generate the test_c_tk run-time
 # error in the install-tree.
 
-rm -rf bisect_test_build_dir bisect_test_install bisect_test_install_build_dir
-mkdir bisect_test_build_dir bisect_test_install_build_dir
-cd bisect_test_build_dir
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../bisect_test_install \
+rm -rf \
+/tmp/bisect_test_build_dir \
+/tmp/bisect_test_install \
+/tmp/bisect_test_install_build_dir
+
+mkdir \
+/tmp/bisect_test_build_dir \
+/tmp/bisect_test_install_build_dir
+SOURCE_DIR=`pwd`
+cd /tmp/bisect_test_build_dir
+cmake -DCMAKE_INSTALL_PREFIX=/tmp/bisect_test_install \
 -DDEFAULT_NO_BINDINGS=ON -DENABLE_tcl=ON -DENABLE_tk=ON \
 -DDEFAULT_NO_DEVICES=ON -DPLD_tk=ON -DPLD_xwin=ON \
-.. >& cmake.out
+$SOURCE_DIR >& cmake.out
 make -j4 install >& make_install.out
-cd ../bisect_test_install_build_dir
-cmake ../bisect_test_install/share/plplot5.9.5/examples >& cmake.out
+cd /tmp/bisect_test_install_build_dir
+cmake /tmp/bisect_test_install/share/plplot5.9.5/examples >& cmake.out
 make x01c
 c/x01c -dev tk 
-
