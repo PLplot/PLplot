588cc1a35312a35bef65613c6da51fae513dd299
Alan W. Irwin <airwin@users.sourceforge.net>
2010-02-27 20:29:20 +0000

    Update script to look for extXdrawable regression (which was found by svn-bisect to occur for Hez's revision 10329).
    svn path=/trunk/; revision=10825

--- scripts/test_bisect.sh
+++ scripts/test_bisect.sh
@@ -17,11 +17,25 @@ mkdir \
 SOURCE_DIR=`pwd`
 cd /tmp/bisect_test_build_dir
 cmake -DCMAKE_INSTALL_PREFIX=/tmp/bisect_test_install \
--DDEFAULT_NO_BINDINGS=ON -DENABLE_tcl=ON -DENABLE_tk=ON \
--DDEFAULT_NO_DEVICES=ON -DPLD_tk=ON -DPLD_xwin=ON \
+-DDEFAULT_NO_BINDINGS=ON \
+-DDEFAULT_NO_DEVICES=ON -DBUILD_TEST=ON -DPLD_xcairo=ON \
 $SOURCE_DIR >& cmake.out
-make -j4 install >& make_install.out
-cd /tmp/bisect_test_install_build_dir
-cmake /tmp/bisect_test_install/share/plplot5.9.5/examples >& cmake.out
-make x01c
-c/x01c -dev tk 
+#make -j4 install >& make_install.out
+#cd /tmp/bisect_test_install_build_dir
+#cmake /tmp/bisect_test_install/share/plplot5.9.5/examples >& cmake.out
+#make -j4 test_extXdrawable
+make -j4 extXdrawable_demo
+make -j4 cairo
+examples/c/extXdrawable_demo
+
+result=
+while [ "$result" != "before" -a  "$result" != "after" ]; do
+    echo -n "before/after? "
+    read result
+    if [ "$result" = "before" ]; then
+	exit 0
+    fi
+    if [ "$result" = "after" ]; then
+	exit 1
+    fi
+done
