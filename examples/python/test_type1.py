65c4931152b718deaee40cc0787ed65be522f76e
Alan W. Irwin <airwin@users.sourceforge.net>
2010-04-20 20:52:55 +0000

    Implement an example to display all Type 1 glyphs that are potentially accessible to the ps(c) device in a similar manner to the historical gfontview application.  The ps.c file must be edited to #define PL_TEST_TYPE1 in order to make this example work properly.
    The point of this example is to duplicate the functionality of the gfontview
    application which is no longer being actively developed. Because of this
    app's dependence on freetype1 (as opposed to modern freetype) modern distros
    have been forced to abandon this useful application which is why I have had
    to emulate it with test_type1.py.
    
    If this example (with ps built with PL_TEST_TYPE1 #defined) shows a type1
    glyph that would be useful to PLplot, then it is straightforward to edit the
    unicode to type1 index translation table at include/plunicode-type1.h to
    make that glyph accessible to PLplot by unicode index for ordinary builds of
    ps that do not #define PL_TEST_TYPE1.
    
    
    svn path=/trunk/; revision=10929

---
+++ examples/python/test_type1.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# $Id$
+# Copyright 2010 Alan W. Irwin
+# This file is part of PLplot.
+
+# PLplot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+
+# PLplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+
+# You should have received a copy of the GNU Library General Public License
+# along with the file PLplot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+# Plots all the Type1 fonts that are available with -dev psc.
+# N.B. ps.c must be compiled with -DPL_TEST_TYPE1 (which interprets
+# unicode in the range from 0 to 255 directly as Type1 font indices.)
+
+# Append to effective python path so that can find plplot modules.
+from plplot_python_start import *
+
+import sys
+from plplot_py_demos import *
+
+# Parse and process command line arguments
+plparseopts(sys.argv, PL_PARSE_FULL)
+plsetopt("dev", "ps")
+plsetopt("drvopt", "hrshsym=0")
+
+# Initialize plplot
+plinit()
+
+# main
+#
+# Displays all available Type1 glyphs.
+
+family = (
+"sans-serif",
+"serif",
+"monospace",
+"script",
+"symbol",
+)
+style = (
+"upright",
+"italic",
+"oblique",
+)
+weight = (
+"medium",
+"bold",
+)
+def main():
+
+    
+    FCI_COMBINATIONS = 30
+    plsfont(0, 0, 0)
+    for index_fci in range(0,FCI_COMBINATIONS):
+        family_index = index_fci % 5
+        style_index = (index_fci/5) % 3
+        weight_index = ((index_fci/5)/3) % 2
+        title = "Type 1 glyphs for family = " + family[family_index] + ", style = " + style[style_index] + ", weight = " + weight[weight_index]
+        pladv(0)
+
+# Set up viewport and window
+
+        plcol0(2)
+        plvpor(0.1, 1.0, 0.1, 0.9)
+        plwind(0.0, 1.0, 0.0, 1.0)
+
+# Draw the grid using plbox
+
+        plbox("bcg", 1./16., 0, "bcg", 1./16., 0)
+
+# Write the digits below the frame
+
+        plcol0(15)
+        for i in range(16):
+            plmtex("b", 1.5, ((i+0.5)/16.), 0.5, str(i))
+
+        k = 0
+        plmtex("t", 1.5, 0.5, 0.5, title)
+        for i in range(16):
+
+            # Write the digits to the left of the frame
+
+            plmtex("lv", 1.0, (1.0 - (i+0.5)/16.), 1.0, str(16 * i))
+
+            plsfont(family_index, style_index, weight_index)
+            for j in range(16):
+                x = (j+0.5)/16.
+                y = 1. - (i+0.5)/16
+
+                # Display the Type 1 glyph corresponding to k
+                plptex(x, y, 1., 0., 0.5, unichr(k).encode('utf-8'))
+                k = k + 1
+            plsfont(0, 0, 0)
+
+        
+    # Restore defaults
+    #plcol0(1)
+
+main()
+plend()
