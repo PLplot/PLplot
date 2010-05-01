455e0bdd381ca518859c5e2a215f533692004b36
Alan W. Irwin <airwin@users.sourceforge.net>
2010-05-01 16:16:24 +0000

    Reinstate pltr_data tests for NULL that were removed by revision 10895.  It turns out those tests are essential for the python and tcl bindings to work properly.
    svn path=/trunk/; revision=10959

--- src/plshade.c
+++ src/plshade.c
@@ -514,7 +514,7 @@ plshade_int( PLFLT ( *f2eval )( PLINT, PLINT, PLPointer ),
         return;
     }
 
-    if ( pltr == NULL && plsc->coordinate_transform == NULL )
+    if ( (pltr == NULL && plsc->coordinate_transform == NULL) || pltr_data == NULL )
         rectangular = 1;
 
     int_val    = shade_max - shade_min;
@@ -609,7 +609,7 @@ plshade_int( PLFLT ( *f2eval )( PLINT, PLINT, PLPointer ),
                 y[0] = y[3] = iy;
                 y[1] = y[2] = iy + j;
 
-                if ( pltr )
+                if ( pltr && pltr_data )
                 {
                     for ( i = 0; i < 4; i++ )
                     {
@@ -668,7 +668,7 @@ plshade_int( PLFLT ( *f2eval )( PLINT, PLINT, PLPointer ),
             }
             n += i;
 
-            if ( pltr )
+            if ( pltr && pltr_data )
             {
                 for ( i = 0; i < n; i++ )
                 {
