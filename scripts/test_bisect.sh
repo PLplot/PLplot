19870017c737f026fb950c5ddbe8a04ff08e4ee2
Alan W. Irwin <airwin@users.sourceforge.net>
2010-05-01 15:58:15 +0000

    Update bisect script to help find source of python example 15 rendering regression (and other plshade regressions).
    svn path=/trunk/; revision=10958

--- scripts/test_bisect.sh
+++ scripts/test_bisect.sh
@@ -3,8 +3,8 @@
 # Expect substantial updates to this script for each new error to be
 # bisected.
 #
-# This particular case is used to generate the test_c_tk run-time
-# error in the install-tree.
+# This particular case is used to generate the example 15 python rendering
+# error in the build tree.
 
 rm -rf \
 /tmp/bisect_test_build_dir \
@@ -18,15 +18,20 @@ SOURCE_DIR=`pwd`
 cd /tmp/bisect_test_build_dir
 cmake -DCMAKE_INSTALL_PREFIX=/tmp/bisect_test_install \
 -DDEFAULT_NO_BINDINGS=ON \
--DDEFAULT_NO_DEVICES=ON -DBUILD_TEST=ON -DPLD_xcairo=ON \
+-DDEFAULT_NO_DEVICES=ON -DBUILD_TEST=ON -DENABLE_python=ON -DPLD_xwin=ON \
 $SOURCE_DIR >& cmake.out
 #make -j4 install >& make_install.out
 #cd /tmp/bisect_test_install_build_dir
 #cmake /tmp/bisect_test_install/share/plplot5.9.5/examples >& cmake.out
 #make -j4 test_extXdrawable
-make -j4 x17c
-make -j4 cairo
-examples/c/x17c -dev xcairo -np
+#######
+#make -j4 x17c
+#make -j4 cairo
+#examples/c/x17c -dev xcairo -np
+#######
+make -j4 _plplotcmodule
+make python_examples xwin
+examples/python/x15 -dev xwin
 
 result=
 while [ "$result" != "before" -a  "$result" != "after" ]; do
