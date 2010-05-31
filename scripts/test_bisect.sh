86761b4142b9b0ed3eac342dc41b0249ae27a9d9
Alan W. Irwin <airwin@users.sourceforge.net>
2010-05-31 17:31:25 +0000

    Modify to search for test_tk_standard_examples regression.
    svn path=/trunk/; revision=11044

--- scripts/test_bisect.sh
+++ scripts/test_bisect.sh
@@ -17,8 +17,8 @@ mkdir \
 SOURCE_DIR=`pwd`
 cd /tmp/bisect_test_build_dir
 cmake -DCMAKE_INSTALL_PREFIX=/tmp/bisect_test_install \
--DDEFAULT_NO_BINDINGS=ON \
--DDEFAULT_NO_DEVICES=ON -DBUILD_TEST=ON -DENABLE_python=ON -DPLD_xwin=ON \
+-DDEFAULT_NO_BINDINGS=ON -DDEFAULT_NO_DEVICES=ON -DBUILD_TEST=ON \
+-DENABLE_tcl=ON -DENABLE_tk=ON -DPLD_xwin=ON -DPLD_tk=ON \
 $SOURCE_DIR >& cmake.out
 #make -j4 install >& make_install.out
 #cd /tmp/bisect_test_install_build_dir
@@ -29,9 +29,16 @@ $SOURCE_DIR >& cmake.out
 #make -j4 cairo
 #examples/c/x17c -dev xcairo -np
 #######
-make -j4 _plplotcmodule
-make python_examples xwin
-examples/python/x15 -dev xwin
+#make -j4 _plplotcmodule
+#make python_examples xwin
+#examples/python/x15 -dev xwin
+#######
+# Reduce number of examples run by test_tk_standard_examples to speed everything
+# up.
+cd examples/tk
+patch < /tmp/speedup_patch
+cd ../..
+make -j4 test_tk_standard_examples
 
 result=
 while [ "$result" != "before" -a  "$result" != "after" ]; do
