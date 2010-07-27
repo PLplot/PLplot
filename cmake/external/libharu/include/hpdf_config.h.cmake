994fd313690b94d1bb91eacdf6b418c790b22987
Alan W. Irwin <airwin@users.sourceforge.net>
2010-07-27 22:10:33 +0000

    Initial commit of libharu-2.1.0 added files (to support a CMake-based build system) and changed files to support larger font sizes (required for example 24) and to support gcc visibility so that, e.g,,
    export CFLAGS=-g -fvisibility=hidden'
    
    works correctly on Linux.
    
    svn path=/trunk/; revision=11109

---
+++ cmake/external/libharu/include/hpdf_config.h.cmake
@@ -0,0 +1,13 @@
+/* Necessary header data configured by CMake. */
+
+/* placeholder for debug build */
+#cmakedefine HPDF_DEBUG
+
+/* placeholder for debug trace enabled */
+#cmakedefine HPDF_DEBUG_TRACE
+
+/* libpng is not available */
+#cmakedefine HPDF_NOPNGLIB
+
+/* zlib is not available */
+#cmakedefine HPDF_NOZLIB
