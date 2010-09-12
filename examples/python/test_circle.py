01386a22c55ec24c78709c956fc1221c21090026
Alan W. Irwin <airwin@users.sourceforge.net>
2010-09-12 00:17:06 +0000

    Titles for each of the pages and implement more asterisk variations.
    svn path=/trunk/; revision=11171

--- examples/python/test_circle.py
+++ examples/python/test_circle.py
@@ -9,6 +9,15 @@ from numpy import *
 
 x = [0.5]
 y = [0.5]
+title = [
+  "Large Circle + Light Diagonal Cross with plptex",
+  "Ascii Asterisk with plptex",
+  "Plplot Encoded Unicode Indexed Asterisk with plptex",
+  "Plplot Encoded Hershey Indexed Asterisk with plptex",
+  "Asterisk with plsym",
+  "Asterisk with plpoin",
+]
+  
 ifunicode = True
 
 # Parse and process command line arguments
@@ -16,13 +25,16 @@ plparseopts(sys.argv, PL_PARSE_FULL)
 
 # Initialize plplot
 plinit()
-plcol0(2)
-for kind in range(4):
+for kind in range(6):
   pladv(0)
   plvpor(0.1, 0.9, 0.1, 0.9)
   plwind(0., 1., 0., 1.)
   # Just to show edges of viewport
+  plcol0(1)
   plbox("bc", 0., 0, "bc", 0., 0)
+  plcol0(2)
+  plschr(0., 1.)
+  plmtex("t", 1., 0.5, 0.5, title[kind])
   k=0
   for size in 2.**arange(2,-3,-1):
     k+=1
@@ -40,14 +52,22 @@ for kind in range(4):
         plschr(0., 4.*size)
         plptex(0.5, 0.5, 1., 0., 0.5, "#(907)")
     elif kind ==1:
-      # Print an asterisk using plptex
+      # Print an ascii asterisk using plptex
+      plschr(0., 8.*size)
+      plptex(0.5, 0.5, 1., 0., 0.5, "*")
+    elif kind ==2:
+      # Print a (PLplot encoded) unicode indexed asterisk using plptex
+      plschr(0., 8.*size)
+      plptex(0.5, 0.5, 1., 0., 0.5, "#[0x002a]")
+    elif kind ==3:
+      # Print a (PLplot encoded) Hershey indexed asterisk using plptex
       plschr(0., 8.*size)
       plptex(0.5, 0.5, 1., 0., 0.5, "#(728)")
-    elif kind ==2:
+    elif kind ==4:
       # Print an asterisk using plsym
       plssym(0., 8.*size)
       plsym(x, y, 728)
-    elif kind ==3:
+    elif kind ==5:
       # Print an asterisk using plpoin
       plssym(0., 8.*size)
       plpoin(x, y, 3)
