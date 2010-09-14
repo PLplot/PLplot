4d217706d5f168f23a520ec5a1b9da5b68a9b7d4
Alan W. Irwin <airwin@users.sourceforge.net>
2010-09-14 04:47:14 +0000

    Make sure abi-compliance-checker.pl is on the script user's PATH and has the correct version (currently 1.21.4).  After the reference and working-copy builds and installs are done, run abi-compliance-checker.pl to produce a report for libplplotd.
    svn path=/trunk/; revision=11179

--- scripts/check_backwards_incompatible_api.sh
+++ scripts/check_backwards_incompatible_api.sh
@@ -2,6 +2,22 @@
 # Script to check all our C/C++ API for backwards-incompatible changes
 # using abi-compliance-checker.
 
+# Check that you have abi-compliance-checker.pl on your PATH.
+
+if [ ! -x "$(which abi-compliance-checker.pl)" ] ; then
+    echo "You must put abi-compliance-checker.pl on your PATH"
+    exit 1
+fi
+
+allowed_version=1.21.4
+acc_version=$(abi-compliance-checker.pl -dumpversion)
+if [ "$acc_version" != "$allowed_version" ] ; then
+    echo "
+Detected abi-compliance-checker.pl version = '$acc_version'.
+This script only works with abi-compliance_checker.pl $allowed_version."
+    exit 1
+fi
+
 # Boiler plate to obtain SOURCE_TREE
 ORIGINAL_PATH="$(pwd)"
 cd "$(dirname $0)"
@@ -32,10 +48,6 @@ echo "Configuring, building and installing the working copy of PLplot."
 echo "This may take a while...."
 cmake  "-DCMAKE_INSTALL_PREFIX=/tmp/plplot_wc/install" -DDEFAULT_NO_BINDINGS=ON -DENABLE_cxx=ON -DENABLE_qt=ON -DENABLE_tcl=ON -DENABLE_tk=ON -DENABLE_wxwidgets=ON -DDEFAULT_NO_DEVICES=ON -DPLD_extqt=ON -DPLD_wxwidgets=ON $SOURCE_TREE >& cmake.out
 make -j4 VERBOSE=1 install >& make_install.out
-
-# Remove useless headers that just cause errors for abi-compliance-checker.
-rm -f /tmp/plplot_wc/install/include/plplot/gcw.h
-rm -f /tmp/plplot_wc/install/include/plplot/plplotcanvas.h
 
 # Configure abi-complicance-checker configuration file for working copy install.
 sed -e 's?${VERSION}?'"r${WC_VERSION}?" -e 's?${PREFIX}?'"/tmp/plplot_wc/install?" -e 's?;/?\n/?g' < abi-compliance-checker.xml.template > plplot_wc.xml
@@ -76,10 +88,17 @@ echo "This may take a while...."
 cmake  "-DCMAKE_INSTALL_PREFIX=/tmp/plplot_reference/install" -DDEFAULT_NO_BINDINGS=ON -DENABLE_cxx=ON -DENABLE_qt=ON -DENABLE_tcl=ON -DENABLE_tk=ON -DENABLE_wxwidgets=ON -DDEFAULT_NO_DEVICES=ON -DPLD_extqt=ON -DPLD_wxwidgets=ON $REFERENCE_SOURCE_TREE >& cmake.out
 make -j4 VERBOSE=1 install >& make_install.out
 
-# Remove useless headers that just cause errors for abi-compliance-checker.
-rm -f /tmp/plplot_reference/install/include/plplot/gcw.h
-rm -f /tmp/plplot_reference/install/include/plplot/plplotcanvas.h
-
 # Configure abi-complicance-checker configuration file for reference install.
 sed -e 's?${VERSION}?'"r${REFERENCE_VERSION}?" -e 's?${PREFIX}?'"/tmp/plplot_reference/install?" -e 's?;/?\n/?g' < /tmp/plplot_wc/build/abi-compliance-checker.xml.template > plplot_reference.xml
 
+echo "Running abi-compliance-checker.pl."
+echo "This may take a while...."
+
+rm -rf /tmp/abi-compliance-checker
+mkdir -p /tmp/abi-compliance-checker
+cd /tmp/abi-compliance-checker
+abi-compliance-checker.pl -l plplotd \
+-d1 /tmp/plplot_reference/build/plplot_reference.xml \
+-d2 /tmp/plplot_wc/build/plplot_wc.xml
+
+echo "N.B. the above report file(s) are in the directory $PWD"
