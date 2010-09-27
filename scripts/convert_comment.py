74a0c75ebe8812140bde1a352ea1572b4adb5ec0
Alan W. Irwin <airwin@users.sourceforge.net>
2010-09-27 02:06:17 +0000

    Initial commit of python script to convert from "/*...*/" form of comment (single or multi-line) to "//" form of comment for C/C++ code.
    There are some simplifying assumptions about the form of comments so
    apply this script with a lot of checking and care, but for the one C
    source file where I have tested this script so far, the script seems
    to work.
    
    
    svn path=/trunk/; revision=11222

---
+++ scripts/convert_comment.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# Programme to convert all single-line or multi-line "/* ... */"
+# comments to "//" form in C or C++ code.  Input source code file is
+# stdin, output file is stdout.
+import sys
+import re
+ifsingleline = True
+
+for line in sys.stdin.readlines():
+    end_comment = line.find("*/")
+    # len(line) includes new line character at end.
+    if end_comment >=0 and end_comment !=  len(line) - 3:
+        sys.stderr.write(line)
+        raise RuntimeError, "Trailing garbage after */"
+
+    if ifsingleline :
+        start_comment = line.find("/*")
+        if start_comment <0 and end_comment >=0:
+            sys.stderr.write(line)
+            raise RuntimeError, "Trailing */ for a line which is not a comment"
+
+        # convert single-line comment.
+        line = re.sub(r'^(.*)(/\*)(.*)(\*/)$', "\\1//\\3", line)
+
+        start_comment = line.find("/*")
+	if start_comment >= 0:
+            # Convert first line of multiline comment.
+            line = line.replace("/*", "//")
+            ifsingleline = False
+    else:
+        end_comment = line.find("*/")
+        if end_comment < 0:
+            # Convert multiline comment line that is not start line
+            # or end line of that comment.  Look for leading blank asterisk
+            # standard form of multiline commend line and get rid of it.
+            line = "//" + re.sub(r'^ \*', "", line)
+        else:
+            # Convert last line of multiline comment.
+            line = "//" + re.sub(r'^ *(.*)\*/', "\\1", line)
+            ifsingleline = True
+
+    sys.stdout.write(line)
+sys.exit()
