8a8083cb30035540a909df12a42bb04c14401bf0
Alan W. Irwin <airwin@users.sourceforge.net>
2010-10-27 02:45:33 +0000

    Implement check for parity bits in our non-binary source files.
    utils/parity_bit_check.c is the C code for an app that checks for parity
    bits for the stream of bytes coming from stdin.
    
    scripts/parity_bit_check.sh is a bash script to check all our source
    code files for parity bits being set except for the files listed in
    scripts/parity_bit_check.exclude.  The latter list of files includes,
    e.g., image files and utf8 files which go beyond the ascii subset of
    utf8.
    
    
    svn path=/trunk/; revision=11280

---
+++ scripts/parity_bit_check.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# $Id$
+
+# This script will run parity_bit_check on all files in the PLplot
+# source tree (except those listed below) to discover which of those
+# files have any character with the eighth (parity) bit set.
+
+# Copyright (C) 2010 Alan W. Irwin
+#
+# This file is part of PLplot.
+#
+# PLplot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Library Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PLplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with PLplot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+which parity_bit_check >/dev/null
+parity_bit_check_rc=$?
+if [ "$parity_bit_check_rc" != 0 ] ; then
+    echo "
+This script only works when parity_bit_check is on the PATH.
+To make this so please run
+
+make parity_bit_check
+
+in the build tree and put the utils subdirectory of that build tree
+where parity_bit_check will be built by the above command on your
+PATH."
+    exit 1
+fi
+
+# Find absolute PATH of script without using readlink (since readlink is
+# not available on all platforms).  Followed advice at
+# http://fritzthomas.com/open-source/linux/551-how-to-get-absolute-path-within-shell-script-part2/
+ORIGINAL_PATH="$(pwd)"
+cd "$(dirname $0)"
+# Absolute Path of the script
+SCRIPT_PATH="$(pwd)"
+
+# Assumption: top-level source tree is parent directory of where script
+# is located.
+SOURCE_TREE="$(dirname ${SCRIPT_PATH})"
+cd "${SOURCE_TREE}"
+
+#List of all files in source tree other than .svn ones....
+find -type f >| /tmp/temporary_source_tree_list
+
+for FILE in $(grep -v -f "$SOURCE_TREE"/scripts/parity_bit_check.exclude /tmp/temporary_source_tree_list); do
+    parity_bit_check <$FILE
+    parity_bit_check_rc=$?
+    if [ "$parity_bit_check_rc" -ne 0 ]; then
+	printf "%s %x\n" $FILE $parity_bit_check_rc
+    fi
+done
+rm -f /tmp/temporary_source_tree_list
