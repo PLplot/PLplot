5642f544b697cbe1d0755f87d4f6ad1e671ef66b
Alan W. Irwin <airwin@users.sourceforge.net>
2010-11-10 18:14:32 +0000

    Implement second page which illustrates the effects of nrow, ncolumn, PL_LEGEND_ROW_MAJOR, and position for the same legend data.
    svn path=/trunk/; revision=11317

--- examples/python/xw33.py
+++ examples/python/xw33.py
@@ -49,6 +49,7 @@ PL_LEGEND_LEFT | PL_LEGEND_INSIDE,
 ]
 
 def main():
+    # First page illustrating the 16 standard positions.
     pladv(0)
     plvpor(0.25, 0.75, 0.25, 0.75)
     plwind(0.0, 1.0, 0.0, 1.0)
@@ -98,5 +99,126 @@ def main():
                   1., text_colors, text, box_colors, box_patterns,
                   box_scales, line_colors, line_styles, line_widths,
                   symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    # Secong page illustrating effect of nrow, ncolumn for the same legend
+    # data.
+    pladv(0)
+    plvpor(0.25, 0.75, 0.25, 0.75)
+    plwind(0.0, 1.0, 0.0, 1.0)
+    plbox("bc", 0.0, 0, "bc", 0.0, 0)
+    plmtex("t", 7.5, 0.5, 0.5, "The effect of nrow, ncolumn, PL_LEGEND_ROW_MAJOR,")
+    plmtex("t", 6.0, 0.5, 0.5, "and position for the same legend data") 
+
+    # Set up legend arrays with the correct size, type.
+    nlegend = 7
+    opt_array = zeros(nlegend, "int")
+    text_colors = zeros(nlegend, "int")
+    text = zeros(nlegend, "S200")
+    box_colors = zeros(nlegend, "int")
+    box_patterns = zeros(nlegend, "int")
+    box_scales = zeros(nlegend)
+    line_colors = zeros(nlegend, "int")
+    line_styles = zeros(nlegend, "int")
+    line_widths = zeros(nlegend, "int")
+    symbol_colors = zeros(nlegend, "int")
+    symbol_scales = zeros(nlegend)
+    symbol_numbers = zeros(nlegend, "int")
+    symbols = zeros(nlegend, "S100")
+
+    # Only specify legend data that are required according to the
+    # value of opt_array for that entry.
+    opt_base = PL_LEGEND_BACKGROUND | PL_LEGEND_BOUNDING_BOX
+    for k in range(nlegend):
+        opt_array[k] = PL_LEGEND_LINE | PL_LEGEND_SYMBOL
+        line_styles[k] = 1
+        line_widths[k] = 1
+        symbol_scales[k] = 1.
+        symbol_numbers[k] = 2
+        symbols[k] = "*"
+        text[k] = "%2.2d" % k
+        text_colors[k] = 1 + (k % 8)
+        line_colors[k] = 1 + (k % 8)
+        symbol_colors[k] = 1 + (k % 8)
+
+    # Use monotype fonts so that all legends are the same size.
+    plsfont(PL_FCI_MONO, -1, -1)
+    plscol0a( 15, 32, 32, 32, 0.70 ) 
+
+    opt = opt_base |PL_LEGEND_UPPER|PL_LEGEND_OUTSIDE
+    x = 0.
+    y = 0.1
+    nrow = 1
+    ncolumn = nlegend
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_LOWER|PL_LEGEND_OUTSIDE
+    x = 0.
+    y = 0.1
+    nrow = 1
+    ncolumn = nlegend
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_LEFT|PL_LEGEND_OUTSIDE
+    x = 0.1
+    y = 0.
+    nrow = nlegend
+    ncolumn = 1
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_RIGHT|PL_LEGEND_OUTSIDE
+    x = 0.1
+    y = 0.
+    nrow = nlegend
+    ncolumn = 1
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_LEFT|PL_LEGEND_UPPER|PL_LEGEND_INSIDE
+    x = 0.
+    y = 0.
+    nrow = 6
+    ncolumn = 2
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_RIGHT|PL_LEGEND_UPPER|PL_LEGEND_INSIDE|PL_LEGEND_ROW_MAJOR
+    x = 0.
+    y = 0.
+    nrow = 6
+    ncolumn = 2
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
+
+    opt = opt_base |PL_LEGEND_LOWER|PL_LEGEND_INSIDE|PL_LEGEND_ROW_MAJOR
+    x = 0.
+    y = 0.
+    nrow = 3
+    ncolumn = 3
+    pllegend( opt, x, y,
+              0.05, 15, 1, 1, nrow, ncolumn, opt_array, 1.0, 1.0, 2.0,
+              1., text_colors, text, box_colors, box_patterns,
+              box_scales, line_colors, line_styles, line_widths,
+              symbol_colors, symbol_scales, symbol_numbers, symbols )
 
 main()
