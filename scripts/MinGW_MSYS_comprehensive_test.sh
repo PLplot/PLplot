fc76bf2c4dabcf35b97b71032a12ca96618ac8cd
Alan W. Irwin <airwin@users.sourceforge.net>
2010-12-17 07:55:56 +0000

    Add Tcl.
    If set DEBUG_COMPREHENSIVE_TEST_RESULT into a non-zero length string
    before sourcing this script, then most environment variables will be
    set up for debugging a partially done build-tree result
    other than the following update to PATH.
    
    cd to build tree than
    
    PATH=$(pwd)/dll:$(pwd)/utils:$(pwd)/drivers:$PATH
    
    
    svn path=/trunk/; revision=11376

--- scripts/MinGW_MSYS_comprehensive_test.sh
+++ scripts/MinGW_MSYS_comprehensive_test.sh
@@ -55,9 +55,16 @@ PATH=$SWIG_PREFIX:$PATH
 
 # Give access to Python. (Python installation has no separate bin directory).
 PATH=$PYTHON_PREFIX:$PATH
-CMAKE_LIBRARY_PATH=$PYTHON_PREFIX/libs:$CMAKE_LIBRARY_PATH
 CMAKE_INCLUDE_PATH=$PYTHON_PREFIX/Lib/site-packages/numpy/core/include/numpy:$CMAKE_INCLUDE_PATH
 CMAKE_INCLUDE_PATH=$PYTHON_PREFIX/include:$CMAKE_INCLUDE_PATH
+CMAKE_LIBRARY_PATH=$PYTHON_PREFIX/libs:$CMAKE_LIBRARY_PATH
+
+# Give access to Windows version of Tcl.  N.B. the windows installer for
+# Tcl appears to put $TCL_PREFIX/bin permanently on the PATH so no
+# need to place it on the PATH again.
+#PATH=$TCL_PREFIX/bin:$PATH
+CMAKE_INCLUDE_PATH=$TCL_PREFIX/include:$CMAKE_INCLUDE_PATH
+CMAKE_LIBRARY_PATH=$TCL_PREFIX/lib:$CMAKE_LIBRARY_PATH
 
 # Set MinGW compiler flags for the PLplot build.
 export CFLAGS='-O3'
@@ -68,16 +75,26 @@ export FFLAGS=$CFLAGS
 
 # Must disable Tcl because cmake under wine somehow finds Linux
 # versions of Tcl.  WTF? (1.3.8 last wine version checked).
+# Now no longer disable Tcl because you force it to find the
+# Windows version (see above).
 
 # Must disable f95 because of some gfortran module parsing problem
 # under wine.  (1.3.8 last wine version checked).
 
-bash $PLPLOT_SOURCE_PREFIX/scripts/comprehensive_test.sh \
---prefix $TEST_RESULTS_PREFIX \
---generator_string "MSYS Makefiles" --build_command "make -j4" \
---cmake_added_options "-DENABLE_tcl=OFF -DENABLE_f95=OFF" --do_shared yes --do_nondynamic no --do_static no \
---do_ctest no --do_test_noninteractive yes --do_test_interactive no --do_test_build_tree yes --do_test_install_tree no --do_test_traditional_install_tree no
+# Set DEBUG_COMPREHENSIVE_TEST_RESULT to some value before sourcing
+# this script if you want to set up all environment variables as
+# they are for a normal run of comprehensive_test.sh without
+# actually running that script.
+#	--cmake_added_options "-DENABLE_f95=OFF -DDEFAULT_NO_BINDINGS=ON -DENABLE_tcl=ON -DDEFAULT_NO_DEVICES=ON -DPLD_ps=ON" --do_shared yes --do_nondynamic no --do_static no \
 
-# This file is sourced so restore original directory location and PATH.
-cd $ORIGINAL_DIR
-PATH=$ORIGINAL_PATH
+if [ -z "$DEBUG_COMPREHENSIVE_TEST_RESULT" ] ; then
+    bash $PLPLOT_SOURCE_PREFIX/scripts/comprehensive_test.sh \
+	--prefix $TEST_RESULTS_PREFIX \
+	--generator_string "MSYS Makefiles" --build_command "make -j4" \
+	--cmake_added_options "-DENABLE_f95=OFF" --do_shared yes --do_nondynamic no --do_static no \
+	--do_ctest no --do_test_noninteractive yes --do_test_interactive no --do_test_build_tree yes --do_test_install_tree yes --do_test_traditional_install_tree yes
+
+    # This file is sourced so restore original directory location and PATH.
+    cd $ORIGINAL_DIR
+    PATH=$ORIGINAL_PATH
+fi
