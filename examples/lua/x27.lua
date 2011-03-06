314eca549539543ee4808b9c3a07e8ebe6c84b15
Alan W. Irwin <airwin@users.sourceforge.net>
2011-03-06 00:26:01 +0000

    Add fill pages to the 27th lua example to make the results consistent with the corresponding C example.
    svn path=/trunk/; revision=11600

--- examples/lua/x27.lua
+++ examples/lua/x27.lua
@@ -25,25 +25,38 @@
 dofile("plplot_examples.lua")
 
 
+--------------------------------------------------------------------------
+-- Calculate greatest common divisor following pseudo-code for the
+-- Euclidian algorithm at http://en.wikipedia.org/wiki/Euclidean_algorithm
+
+function gcd (a,  b)
+    a = math.floor(math.abs(a))
+    b = math.floor(math.abs(b))
+    while b~=0 do
+        t = b
+        b = math.mod(a,b)
+        a = t
+    end 
+    return a
+end
+
 function cycloid()
   -- TODO 
 end
 
 
-function spiro( params )
-  NPNT = 20000
+function spiro( params, fill )
+  NPNT = 2000
   xcoord = {}
   ycoord = {}
 
   -- Fill the coordinates 
-  windings = params[4] 
+  -- Proper termination of the angle loop very near the beginning
+  -- point, see
+  -- http://mathforum.org/mathimages/index.php/Hypotrochoid.
+  windings = math.floor(math.abs(params[2])/gcd(params[1], params[2]))
   steps    = math.floor(NPNT/windings)
-  dphi     = 8*math.acos(-1)/steps 
-
-  xmin = 0  -- This initialisation is safe! 
-  xmax = 0 
-  ymin = 0 
-  ymax = 0 
+  dphi     = 2*math.pi/steps 
 
   for i = 1, windings*steps+1 do
     phi       = (i-1) * dphi 
@@ -51,26 +64,31 @@ function spiro( params )
     xcoord[i] = (params[1]-params[2])*math.cos(phi) + params[3]*math.cos(phiw) 
     ycoord[i] = (params[1]-params[2])*math.sin(phi) - params[3]*math.sin(phiw) 
 
+    if i == 1 then
+        xmin = xcoord[i]
+        xmax = xcoord[i]
+        ymin = ycoord[i]
+        ymax = ycoord[i]
+    end
     if xmin>xcoord[i] then xmin = xcoord[i] end 
     if xmax<xcoord[i] then xmax = xcoord[i] end
     if ymin>ycoord[i] then ymin = ycoord[i] end
     if ymax<ycoord[i] then ymax = ycoord[i] end
   end
 
-  if (xmax-xmin)>(ymax-ymin) then
-    scale = xmax - xmin 
-  else
-    scale = ymax - ymin 
-  end
-  xmin = -0.65*scale 
-  xmax =  0.65*scale 
-  ymin = -0.65*scale 
-  ymax =  0.65*scale 
+  xmin = xmin - 0.15*(xmax-xmin)
+  xmax = xmax + 0.15*(xmax-xmin)
+  ymin = ymin - 0.15*(ymax-ymin)
+  ymax = ymax + 0.15*(ymax-ymin)
 
   pl.wind(xmin, xmax, ymin, ymax)
 
   pl.col0(1)
-  pl.line(xcoord, ycoord) 
+  if fill == 1 then
+      pl.fill(xcoord, ycoord)
+  else
+      pl.line(xcoord, ycoord)
+  end
 end
 
 
@@ -83,6 +101,11 @@ end
 ----------------------------------------------------------------------------
 
 -- R, r, p, N 
+-- R and r should be integers to give correct termination of the
+-- angle loop using gcd.
+-- N.B. N is just a place holder since it is no longer used
+-- (because we now have proper termination of the angle loop).
+
 params = {
   { 21,  7,  7,  3 },  -- Deltoid 
   { 21,  7, 10,  3 },
@@ -110,10 +133,11 @@ cycloid()
 
 pl.ssub(3, 3)  -- Three by three window 
 
+fill = 0
 for i = 1, 9 do
   pl.adv(0) 
   pl.vpor(0, 1, 0, 1) 
-  spiro(params[i]) 
+  spiro(params[i], fill) 
 end
 
 pl.adv(0) 
@@ -122,8 +146,18 @@ pl.ssub(1, 1)  -- One window per curve
 for i = 1, 9 do
   pl.adv(0) 
   pl.vpor(0, 1, 0, 1) 
-  spiro(params[i]) 
+  spiro(params[i], fill) 
 end
 
--- Don't forget to call plend() to finish off! 
+-- fill the curves.
+fill = 1
+pl.adv(0) 
+pl.ssub(1, 1)  -- One window per curve 
+
+for i = 1, 9 do
+  pl.adv(0) 
+  pl.vpor(0, 1, 0, 1) 
+  spiro(params[i], fill) 
+end
+
 pl.plend()
