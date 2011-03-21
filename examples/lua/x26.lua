00aade8d3afe97dda37abc5353ef31945ef13073
Alan W. Irwin <airwin@users.sourceforge.net>
2011-03-21 06:27:10 +0000

    Make Lua example 26 consistent with its C counterpart.
    svn path=/trunk/; revision=11668

--- examples/lua/x26.lua
+++ examples/lua/x26.lua
@@ -66,9 +66,26 @@ dofile("plplot_examples.lua")
 x_label = { "Frequency", "Частота" }
 y_label = { "Amplitude (dB)", "Амплитуда (dB)" }
 alty_label = { "Phase shift (degrees)", "Фазовый сдвиг (градусы)" }
+-- Short rearranged versions of y_label and alty_label.
+legend_text = {
+  {"Amplitude", "Phase shift"},
+  {"Амплитуда", "Фазовый сдвиг"}
+  }
 title_label = { "Single Pole Low-Pass Filter", "Однополюсный Низко-Частотный Фильтр" }
 line_label = { "-20 dB/decade", "-20 dB/десяток" }
 
+-- return single bit (for OR)
+function bit(x,b)
+  return (math.mod(x, 2^b) - math.mod(x,2^(b-1)) > 0)
+end
+
+-- logic OR for number values
+
+function lor(x,y)
+  result = 0
+  for p=1,8 do result = result + (((bit(x,p) or bit(y,p)) == true) and 2^(p-1) or 0) end
+  return result
+end
 
 ----------------------------------------------------------------------------
 -- plot1
@@ -76,7 +93,7 @@ line_label = { "-20 dB/decade", "-20 dB/десяток" }
 -- Log-linear plot.
 ----------------------------------------------------------------------------
 
-function plot1(typ, x_label, y_label, alty_label, title_label, line_label)
+function plot1(type, x_label, y_label, alty_label, title_label, line_label, legend_text)
   freql = {}
   ampl = {}
   phase = {}
@@ -98,16 +115,20 @@ function plot1(typ, x_label, y_label, alty_label, title_label, line_label)
 
   -- Try different axis and labelling styles. 
   pl.col0(1)
-  if typ==0 then
+  if type==0 then
     pl.box("bclnst", 0, 0, "bnstv", 0, 0)
-  else
+  elseif type==1 then
     pl.box("bcfghlnst", 0, 0, "bcghnstv", 0, 0)
+  else
+    pl.abort("type must be 0 or 1")
+    pl.plend()
+    os.exit()
   end
 
   -- Plot ampl vs freq 
   pl.col0(2)
   pl.line(freql, ampl)
-  pl.col0(1)
+  pl.col0(2)
   pl.ptex(1.6, -30, 1, -20, 0.5, line_label)
 
   -- Put labels on 
@@ -118,7 +139,7 @@ function plot1(typ, x_label, y_label, alty_label, title_label, line_label)
   pl.mtex("l", 5, 0.5, 0.5, y_label)
 
   -- For the gridless case, put phase vs freq on same plot 
-  if typ==0 then
+  if type==0 then
     pl.col0(1)
     pl.wind(-2, 3, -100, 0)
     pl.box("", 0, 0, "cmstv", 30, 3)
@@ -127,7 +148,74 @@ function plot1(typ, x_label, y_label, alty_label, title_label, line_label)
     pl.string(freql, phase, "*")
     pl.col0(3)
     pl.mtex("r", 5, 0.5, 0.5, alty_label)
+    nlegend = 2
+  else
+    nlegend = 1
   end
+
+  -- Draw a legend.
+  opt_array = {}
+  text_colors = {}
+  text = {}
+  box_colors = {}
+  box_patterns = {}
+  box_scales = {}
+  box_line_widths = {}
+  line_colors = {}
+  line_styles = {}
+  line_widths = {}
+  symbol_colors = {}
+  symbol_scales = {}
+  symbol_numbers = {}
+  symbols = {}
+
+  -- Data for the first legend entry.
+  opt_array[1] = pl.PL_LEGEND_LINE
+  text_colors[1] = 2
+  text[1] = legend_text[1]
+  -- box data unused so initialize to arbitrary values.
+  box_colors[1] = 0
+  box_patterns[1] = 0
+  box_scales[1] = 0
+  box_line_widths[1] = 0
+  line_colors[1] = 2
+  line_styles[1] = 1
+  line_widths[1] = 1
+  -- unused arbitrary data
+  symbol_colors[1]  = 0
+  symbol_scales[1]  = 0
+  symbol_numbers[1] = 0
+  symbols[1]        = ""
+
+  -- Data for the second legend entry.
+  if nlegend > 1 then
+    opt_array[2]      = lor(pl.PL_LEGEND_LINE, pl.PL_LEGEND_SYMBOL)
+    text_colors[2]    = 3
+    text[2]           = legend_text[2]
+  -- box data unused so initialize to arbitrary values.
+    box_colors[2] = 0
+    box_patterns[2] = 0
+    box_scales[2] = 0
+    box_line_widths[2] = 0
+    line_colors[2]    = 3
+    line_styles[2]    = 1
+    line_widths[2]    = 1
+    symbol_colors[2]  = 3
+    symbol_scales[2]  = 1.
+    symbol_numbers[2] = 4
+    symbols[2]        = "*"
+  end
+
+  pl.scol0a( 15, 32, 32, 32, 0.70 )
+
+  legend_width, legend_height = pl.legend(
+    0, lor(pl.PL_LEGEND_BACKGROUND, pl.PL_LEGEND_BOUNDING_BOX), 0.0, 0.0,
+    0.1, 15, 1, 1, 0, 0, opt_array, 1.0, 1.0, 2.0,
+    1., text_colors, text, 
+    box_colors, box_patterns, box_scales, box_line_widths, 
+    line_colors, line_styles, line_widths,
+    symbol_colors, symbol_scales, symbol_numbers, symbols )
+
 end
 
 
@@ -142,11 +230,21 @@ pl.parseopts(arg, pl.PL_PARSE_FULL)
 
 -- Initialize plplot 
 pl.init()
-pl.font(2)
+nlang = # x_label
+if nlang ~= (# y_label) or 
+  nlang ~= (# alty_label) or
+  nlang ~= (# title_label) or
+  nlang ~= (# line_label) or
+   nlang ~= (# legend_text) then
+  pl.abort("All data must be expressed in the same number of languages")
+  pl.plend()
+  os.exit()
+end
 
+pl.font(2)
 -- Make log plots using two different styles. 
-for i = 1, 2 do
-  plot1(0, x_label[i], y_label[i], alty_label[i], title_label[i],  line_label[i])
+for i = 1, nlang do
+  plot1(0, x_label[i], y_label[i], alty_label[i], title_label[i],  line_label[i], legend_text[i])
 end
 
 pl.plend()
