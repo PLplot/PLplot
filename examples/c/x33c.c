9c606d92515c3f20a1a2a2cddbd33be5b3be3d6f
Alan W. Irwin <airwin@users.sourceforge.net>
2011-06-15 16:55:17 +0000

    Adjust positions of colorbars so that text doesn't get clipped.
    svn path=/trunk/; revision=11770

--- examples/c/x33c.c
+++ examples/c/x33c.c
@@ -233,7 +233,7 @@ plcolorbar_example_page( int kind_i, int label_i, int cap_i, PLINT cont_color, P
         plsmaj( 0.0, 0.5 );
         plsmin( 0.0, 0.5 );
 
-        plvpor( 0.1, 0.9, 0.1, 0.9 );
+        plvpor( 0.15, 0.85, 0.15, 0.85 );
         plwind( 0.0, 1.0, 0.0, 1.0 );
         // Set interesting background colour.
         plscol0a( 15, 0, 0, 0, 0.20 );
