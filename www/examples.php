c077445e81a7446463adaa45efbbb0859c364158
Hazen Babcock <hbabcock@users.sourceforge.net>
2011-08-01 18:33:15 +0000

    Update webpage info for the current release.
    svn path=/trunk/; revision=11838

--- www/examples.php
+++ www/examples.php
@@ -216,7 +216,7 @@ END;
 		echo <<<END
 			<h3>Examples</h3>
 			<p>These examples were generated with the pngcairo device and
-				 reflect the status of PLplot-5.9.7.</p>
+				 reflect the status of PLplot-5.9.8.</p>
 			<p>Select a thumbnail to view full-size image and source code.</p>
 			<p>
 END;
