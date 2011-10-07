31cb7b46dabf6a693336d8381de18785a75857a0
Arjen Markus <arjenmarkus@users.sourceforge.net>
2011-10-07 07:20:10 +0000

    Correct a typo in the export definitions for Intel Fortran. Add a missing entry (plsettransform). With these changes PLplot is built smoothly for the combination MSVC/C++ (9.0) and Intel Fortran (11.1)
    svn path=/trunk/; revision=11940

--- bindings/f77/plstubs.h
+++ bindings/f77/plstubs.h
@@ -244,7 +244,7 @@
 #define    PLJOIN                    FNAME( PLJOIN, pljoin )
 #define    PLLAB7                    FNAME( PLLAB7, pllab7 )
 #define    PLLEGEND                  FNAME( PLLEGEND07, pllegend07 )
-#define    PLLEGEND_CNV_TEXT         FNAME( PLLEGEND07_CNVV_TEXT, pllegend07_cnv_text )
+#define    PLLEGEND_CNV_TEXT         FNAME( PLLEGEND07_CNV_TEXT, pllegend07_cnv_text )
 #define    PLLIGHTSOURCE             FNAME( PLLIGHTSOURCE, pllightsource )
 #define    PLLINE                    FNAME( PLLINE, plline )
 #define    PLLINE3                   FNAME( PLLINE3, plline3 )
