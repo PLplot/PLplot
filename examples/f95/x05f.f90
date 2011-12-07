2fb221210262a821fa7461c5dd5b1f1ce2f7438c
Arjen Markus <arjenmarkus@users.sourceforge.net>
2011-12-07 08:56:34 +0000

    Adjusting the formatting of two more examples. Use the auxiliary arange() function in x06f.f90.
    svn path=/trunk/; revision=12097

--- examples/f95/x05f.f90
+++ examples/f95/x05f.f90
@@ -1,48 +1,47 @@
-!      $Id$
-!      Draws a histogram from sample data
+!   $Id$
+!   Draws a histogram from sample data
 !
-!      Copyright (C) 2004  Alan W. Irwin
+!   Copyright (C) 2004  Alan W. Irwin
 !
-!      This file is part of PLplot.
+!   This file is part of PLplot.
 !
-!      PLplot is free software; you can redistribute it and/or modify
-!      it under the terms of the GNU Library General Public License as
-!      published by the Free Software Foundation; either version 2 of the
-!      License, or (at your option) any later version.
+!   PLplot is free software; you can redistribute it and/or modify
+!   it under the terms of the GNU Library General Public License as
+!   published by the Free Software Foundation; either version 2 of the
+!   License, or (at your option) any later version.
 !
-!      PLplot is distributed in the hope that it will be useful,
-!      but WITHOUT ANY WARRANTY; without even the implied warranty of
-!      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!      GNU Library General Public License for more details.
+!   PLplot is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU Library General Public License for more details.
 !
-!      You should have received a copy of the GNU Library General Public
-!      License along with PLplot; if not, write to the Free Software
-!      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+!   You should have received a copy of the GNU Library General Public
+!   License along with PLplot; if not, write to the Free Software
+!   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-      use plplot, PI => PL_PI
-      implicit none
+program x04f95
+    use plplot, PI => PL_PI
+    use plf95demolib
+    implicit none
 
-      integer i
-      integer NPTS
-      parameter ( NPTS = 2047 )
-      real(kind=plflt) data(NPTS), delta
-!      Process command-line arguments
-      call plparseopts(PL_PARSE_FULL)
+    integer, parameter :: NPTS = 2047
+    real(kind=plflt) :: data(NPTS), delta
 
-!      Initialize plplot
-      call plinit()
-!      Fill up data points
+!   Process command-line arguments
+    call plparseopts(PL_PARSE_FULL)
 
-      delta = 2.0_plflt * PI / dble (NPTS)
-      do i=1,NPTS
-        data(i)=sin(delta*dble (i-1))
-      enddo
+!   Initialize plplot
+    call plinit()
 
-      call plcol0(1)
-      call plhist(data, -1.1_plflt, 1.1_plflt, 44, 0)
-      call plcol0(2)
-      call pllab('#frValue','#frFrequency', &
-        '#frPLplot Example 5 - Probability function of Oscillator')
+!   Fill up data points
+    delta = 2.0_plflt * PI / dble (NPTS)
+    data = sin(delta*arange(0, NPTS))
 
-      call plend
-      end
+    call plcol0(1)
+    call plhist(data, -1.1_plflt, 1.1_plflt, 44, 0)
+    call plcol0(2)
+    call pllab('#frValue','#frFrequency', &
+      '#frPLplot Example 5 - Probability function of Oscillator')
+
+    call plend
+end program x04f95
--- examples/f95/x06f.f90
+++ examples/f95/x06f.f90
@@ -1,98 +1,97 @@
-!      $Id$
-!      Displays the plotter symbols for PLPOIN
+!   $Id$
+!   Displays the plotter symbols for PLPOIN
 !
-!      Copyright (C) 2004  Alan W. Irwin
+!   Copyright (C) 2004  Alan W. Irwin
 !
-!      This file is part of PLplot.
+!   This file is part of PLplot.
 !
-!      PLplot is free software; you can redistribute it and/or modify
-!      it under the terms of the GNU Library General Public License as
-!      published by the Free Software Foundation; either version 2 of the
-!      License, or (at your option) any later version.
+!   PLplot is free software; you can redistribute it and/or modify
+!   it under the terms of the GNU Library General Public License as
+!   published by the Free Software Foundation; either version 2 of the
+!   License, or (at your option) any later version.
 !
-!      PLplot is distributed in the hope that it will be useful,
-!      but WITHOUT ANY WARRANTY; without even the implied warranty of
-!      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!      GNU Library General Public License for more details.
+!   PLplot is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU Library General Public License for more details.
 !
-!      You should have received a copy of the GNU Library General Public
-!      License along with PLplot; if not, write to the Free Software
-!      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+!   You should have received a copy of the GNU Library General Public
+!   License along with PLplot; if not, write to the Free Software
+!   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-      use plplot
-      implicit none
-      integer :: i, j, k, font, kind_font, maxfont
-      real(kind=plflt), dimension(1:1) :: x, y
+program x06f95
+    use plplot
+    implicit none
+    integer :: i, j, k, font, kind_font, maxfont
+    real(kind=plflt), dimension(1:1) :: x, y
 
-      character (len=3) :: text
-!      Process command-line arguments
-      call plparseopts(PL_PARSE_FULL)
+    character (len=3) :: text
 
+!   Process command-line arguments
+    call plparseopts(PL_PARSE_FULL)
 
-!      Full sized page for display
+!   Full sized page for display
+    call plinit()
 
-      call plinit()
-
-      do kind_font = 1,2
+    do kind_font = 1,2
         call plfontld(kind_font-1)
-        if (kind_font==1) then
-          maxfont = 1
-        else
-          maxfont = 4
-        endif
+        maxfont = merge( 1, 4, kind_font==1 )
 
         do font=1,maxfont
-          call plfont(font)
+            call plfont(font)
 
-          call pladv(0)
+            call pladv(0)
 
-          call plcol0(2)
+            call plcol0(2)
 
-!      Set up viewport and window
+!           Set up viewport and window
 
-          call plvpor(0.1_plflt, 1.0_plflt, 0.1_plflt, 0.9_plflt)
-          call plwind(0.0_plflt, 1.0_plflt, 0.0_plflt, 1.3_plflt)
+            call plvpor(0.1_plflt, 1.0_plflt, 0.1_plflt, 0.9_plflt)
+            call plwind(0.0_plflt, 1.0_plflt, 0.0_plflt, 1.3_plflt)
 
-!      Draw the grid using plbox
+!           Draw the grid using plbox
 
-          call plbox('bcg', 0.1_plflt, 0, 'bcg', 0.1_plflt, 0)
-          call plcol0(15)
+            call plbox('bcg', 0.1_plflt, 0, 'bcg', 0.1_plflt, 0)
+            call plcol0(15)
 
-!      Write the digits below the frame
+!           Write the digits below the frame
 
-          do i=0,9
-            write (text,'(i1)') i
-            call plmtex('b', 1.5_plflt, (0.1_plflt*i+0.05_plflt), 0.5_plflt, text)
-          enddo
-
-          k=0
-          do i=0,12
-
-!        Write the digits to the left of the frame
-
-            write (text,'(i0)') 10*i
-
-            call plmtex('lv', 1.0_plflt, (1.0_plflt-(2*i+1)/26.0_plflt), &
-              1.0_plflt, text)
-            do j=0,9
-              x=0.1_plflt*j+0.05_plflt
-              y=1.25_plflt-0.1_plflt*i
-
-!          Display the symbols
-
-              if (k.lt.128) call plpoin(x,y,k)
-              k=k+1
+            do i=0,9
+                write (text,'(i1)') i
+                call plmtex('b', 1.5_plflt, (0.1_plflt*i+0.05_plflt), 0.5_plflt, text)
             enddo
-          enddo
-          if (kind_font==1) then
-            call plmtex('t', 1.5_plflt, 0.5_plflt, 0.5_plflt, &
-              'PLplot Example 6 - plpoin symbols (compact)')
-          else
-            call plmtex('t', 1.5_plflt, 0.5_plflt, 0.5_plflt, &
-              'PLplot Example 6 - plpoin symbols (extended)')
-          endif
-        enddo
-      enddo
 
-      call plend
-      end
+            k=0
+            do i=0,12
+
+!               Write the digits to the left of the frame
+
+                write (text,'(i0)') 10*i
+
+                call plmtex('lv', 1.0_plflt, (1.0_plflt-(2*i+1)/26.0_plflt), &
+                   1.0_plflt, text)
+
+                do j=0,9
+                    x=0.1_plflt*j+0.05_plflt
+                    y=1.25_plflt-0.1_plflt*i
+
+!                   Display the symbols
+                    if (k < 128) call plpoin(x,y,k)
+
+                    k=k+1
+
+                enddo
+            enddo
+
+            if (kind_font==1) then
+               call plmtex('t', 1.5_plflt, 0.5_plflt, 0.5_plflt, &
+                  'PLplot Example 6 - plpoin symbols (compact)')
+            else
+               call plmtex('t', 1.5_plflt, 0.5_plflt, 0.5_plflt, &
+                  'PLplot Example 6 - plpoin symbols (extended)')
+            endif
+        enddo
+    enddo
+
+    call plend
+end program x06f95
