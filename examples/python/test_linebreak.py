d4437eb7f72c4f9918b604f381726d4a57de1134
Alan W. Irwin <airwin@users.sourceforge.net>
2012-04-26 23:22:53 +0000

    Add simple test example that demonstrates what happens for line breaks mixed in with unicode text.
    svn path=/trunk/; revision=12191

---
+++ examples/python/test_linebreak.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+# Test whether line breaks work for utf8 "Peace" strings.
+# Append to effective python path so that can find plplot modules.
+from plplot_python_start import *
+
+import sys
+from plplot import *
+from numpy import *
+
+# Taken from http://www.columbia.edu/~fdc/pace/
+
+peace = [
+    "(Mandarin) 和平",
+    "(Hindi) शांति",
+    "(English) Peace",
+    "(Hebrew) שלום",
+    "(Russian) Мир",
+    "(German) Friede",
+    "(Korean) 평화",
+    "(French) Paix",
+    "(Spanish) Paz",
+    "(Arabic) ﺳﻼم",
+    "(Turkish) Barış",
+    "(Kurdish) Hasîtî",
+    ]
+
+# Parse and process command line arguments
+plparseopts(sys.argv, PL_PARSE_FULL)
+
+# Initialize plplot
+plinit()
+plcol0(2)
+pladv(0)
+plvpor(0.1, 0.9, 0.1, 0.9)
+plwind(0., 1., 0., 1.)
+# Just to show edges of viewport
+plbox("bc", 0., 0, "bc", 0., 0)
+plcol0(1)
+plschr(0., 1.5)
+for i in arange(6):
+    plptex(0.05, 0.95 - 0.166*i, 1., 0., 0., peace[2*i] + "\n" + peace[2*i+1])
+plend()
