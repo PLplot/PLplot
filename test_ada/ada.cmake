3a97a90ef9095b3835a0aa00ea2ba1314c89ebd2
Alan W. Irwin <airwin@users.sourceforge.net>
2012-09-07 16:56:47 +0000

    Make this independent software project demonstrating Ada language support more consistent with the well-tested PLplot svn trunk version of the Ada language support.
    svn path=/branches/test_cmake/; revision=12231

--- test_ada/ada.cmake
+++ test_ada/ada.cmake
@@ -1,9 +1,15 @@
-# Find the required gnat library, then enable Ada language support.
+# Find and check Ada compiler
+enable_language(Ada)
+if(NOT CMAKE_Ada_COMPILER_WORKS)
+  message(FATAL_ERROR "Required working Ada compiler not found.")
+endif(NOT CMAKE_Ada_COMPILER_WORKS)
 
-find_library(GNAT_LIB NAMES gnat gnat-4.1 gnat-4.2 gnat-4.3)
+# Find the gnat version used in order to search for the right version of libgnat 
+execute_process(COMMAND ${CMAKE_Ada_COMPILER} --version OUTPUT_VARIABLE ADA_OUTPUT)
+string(REGEX MATCH "gcc [(][^)]*[)] ([0-9]*[.][0-9]*)[.][0-9]" ADA_OUTPUT_TRIM ${ADA_OUTPUT})
+set(GNATVERSION ${CMAKE_MATCH_1})
+message(STATUS "gnat version = ${GNATVERSION}")
+find_library(GNAT_LIB NAMES gnat gnat-${GNATVERSION})
 if(NOT GNAT_LIB)
   message(FATAL_ERROR "Required gnat library not found.")
-else(NOT GNAT_LIB)
-  message(STATUS "FOUND gnat library ${GNAT_LIB}")
 endif(NOT GNAT_LIB)
-enable_language(Ada)
