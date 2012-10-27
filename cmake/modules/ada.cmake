ffaf5cabf8b200dc3326c682c07671ac2e572a85
Alan W. Irwin <airwin@users.sourceforge.net>
2012-10-27 09:24:58 +0000

    Change regex for helping to find gnat version to accommodate ".exe" (or not) after "gcc" in string.
    This change continues to work on Linux, and makes Ada work properly on
    Wine.
    
    svn path=/trunk/; revision=12258

--- cmake/modules/ada.cmake
+++ cmake/modules/ada.cmake
@@ -45,7 +45,7 @@ endif(ENABLE_ada)
 if(ENABLE_ada)
   # Find the gnat version used in order to search for the right version of libgnat 
   execute_process(COMMAND ${CMAKE_Ada_COMPILER} --version OUTPUT_VARIABLE ADA_OUTPUT)
-  string(REGEX MATCH "gcc [(][^)]*[)] ([0-9]*[.][0-9]*)[.][0-9]" ADA_OUTPUT_TRIM ${ADA_OUTPUT})
+  string(REGEX MATCH "gcc.* [(][^)]*[)] ([0-9]*[.][0-9]*)[.][0-9]" ADA_OUTPUT_TRIM ${ADA_OUTPUT})
   set(GNATVERSION ${CMAKE_MATCH_1})
   message(STATUS "gnat version = ${GNATVERSION}")
   find_library(GNAT_LIB NAMES gnat gnat-${GNATVERSION})
