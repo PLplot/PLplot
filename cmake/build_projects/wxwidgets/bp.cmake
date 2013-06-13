bb1f53465b6f2d5e1f4497319d067b3273090849
Alan W. Irwin <airwin@users.sourceforge.net>
2013-06-13 02:53:11 +0000

    Add wxwidgets build configuration.
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> using the
    build_wxwidgets target for the "Ninja" CMake generator both for
    Linux and Wine (git version near 1.6_rc1).
    
    svn path=/trunk/; revision=12382

---
+++ cmake/build_projects/wxwidgets/bp.cmake
@@ -0,0 +1,62 @@
+# wxwidgets/bp.cmake
+# This file should be included directly or indirectly from a top-level
+# CMakeLists.txt file to configure the build of wxwidgets.
+
+# Copyright (C) 2013 Alan W. Irwin
+
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+# The top-level CMakeLists.txt file that includes this file should
+# have run "include(ExternalProject)" and set EP_BASE variable (and
+# corresponding directory property) as well as various BP variables
+# used below that configure how the External_Project functions
+# operate.
+
+set(BP_PACKAGE wxwidgets)
+
+# Data that is related to downloads.
+set(${BP_PACKAGE}_URL http://prdownloads.sourceforge.net/wxwindows/wxWidgets-2.8.12.tar.gz)
+set(${BP_PACKAGE}_URL_MD5 2fa39da14bc06ea86fe902579fedc5b1)
+
+# Data that is related to the PATH that must be used.
+if(MSYS_PLATFORM)
+  #set(BP_PATH_NODLL "${BP_PATH}")
+  #set(BP_PATH "${EP_BASE}/Build/build_${BP_PACKAGE}/dll;${BP_PATH_NODLL}")
+  determine_msys_path(BP_PATH "${BP_PATH}")
+  # Must have all elements of env command in MSYS platform form
+  determine_msys_path(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
+else(MSYS_PLATFORM)
+  set(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
+endif(MSYS_PLATFORM)
+#message(STATUS "modified BP_PATH for ${BP_PACKAGE} = ${BP_PATH}")
+
+# The fno-keep-inline-dllexport flag required to work around
+# the memory exhaustion issue discussed in
+# http://gcc.gnu.org/bugzilla/show_bug.cgi?id=43601
+# Still have memory exhaustion issues if you combine that
+# with -O3 so do not use the -O3 optimization flag.
+ExternalProject_Add(
+  build_${BP_PACKAGE}
+  URL ${${BP_PACKAGE}_URL}
+  URL_MD5 ${${BP_PACKAGE}_URL_MD5}
+  CONFIGURE_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} CXXFLAGS=-fno-keep-inline-dllexport ${source_PATH}/${BP_CONFIGURE_COMMAND} --enable-shared --enable-unicode --enable-debug --enable-debug_gdb
+  BUILD_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_MAKE_COMMAND}
+  INSTALL_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_MAKE_COMMAND} install
+  )
+
+list(APPEND build_target_LIST build_${BP_PACKAGE})
+# Restore BP_PATH to original state.
+set(BP_PATH "${BP_ORIGINAL_NATIVE_PATH}")
+#message(STATUS "${BP_PACKAGE} restored original BP_PATH = ${BP_PATH}")
