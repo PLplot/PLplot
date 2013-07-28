142e5f10c8946a67ccb921a6e04e35971c2b4da7
Alan W. Irwin <airwin@users.sourceforge.net>
2013-07-28 04:20:14 +0000

    First working version that configures template.bp.in to create a number of different */bp.cmake build_package configurations for GTK+ component builds as documented in the README file.
    This version has created a libffi/bp.cmake file that configures the
    libffit build correctly, but glib/bp.cmake needs additional
    configuration to account for a required CFLAGS option.
    
    
    svn path=/trunk/; revision=12438

--- cmake/build_projects/configure_bp.cmake
+++ cmake/build_projects/configure_bp.cmake
@@ -9,22 +9,38 @@ set(remove_indices 0 1 2 3 4 5 6 7 8)
 math(EXPR max_index "(${nlines}/${group_size}) - 1")
 foreach(index RANGE 0 ${max_index})
   list(GET lines 0 package)
-  message("${package}")
   list(GET lines 1 config_type)
-  message("${config_type}")
   list(GET lines 2 config_arguments)
-  message("${config_arguments}")
   list(GET lines 3 download_hash_type)
-  message("${download_hash_type}")
   list(GET lines 4 download_hash)
-  message("${download_hash}")
   list(GET lines 5 download_href)
-  message("${download_href}")
   list(GET lines 6 dependencies)
-  message("${dependencies}")
   list(GET lines 7 suggests)
-  message("${suggests}")
   list(GET lines 8 after)
-  message("${after}")
   list(REMOVE_AT lines ${remove_indices})
+
+  # Turn colon-separated lists into official CMake lists.
+  string(REGEX REPLACE ":" ";" dependencies "${dependencies}")
+  string(REGEX REPLACE ":" ";" suggests "${suggests}")
+  string(REGEX REPLACE ":" ";" after "${after}")
+
+  # For now, ignore "after" and treat "dependencies" and "suggests" the same
+  if(suggests)
+    list(APPEND dependencies "${suggests}")
+  endif(suggests)
+
+  # Configure the package file using the appropriate template file.
+  if(config_type STREQUAL "autotools" OR config_type STREQUAL "tarball" OR config_type STREQUAL "cmake")
+    set(template_file template.bp.in)
+  else(config_type STREQUAL "autotools" OR config_type STREQUAL "tarball" OR config_type STREQUAL "cmake")
+    message(FATAL_ERROR "incorrect config_type = ${config_type}")
+  endif(config_type STREQUAL "autotools" OR config_type STREQUAL "tarball" OR config_type STREQUAL "cmake")
+  
+  if(NOT package)
+    message(FATAL_ERROR "bad package = ${package}")
+  endif(NOT package)
+  
+  file(MAKE_DIRECTORY ${package})
+  configure_file(${template_file} ${package}/bp.cmake @ONLY)
+  
 endforeach(index RANGE 0 ${max_index})
