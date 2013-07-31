58cbcdbbbbec328cb744714f7a3884034a8c9f1c
Alan W. Irwin <airwin@users.sourceforge.net>
2013-07-31 02:14:21 +0000

    Configure additional features for build configurations (typically those from jhbuild build configuration data collected from GTK+) that are generated by configure_bp.cmake.
    Those features include improved processing of dependencies (ignoring
    specific dependencies (mostly dependencies which can be assumed
    to be part of the normal build tools) contained in a list in configure_bp.cmake and dropping
    all dependencies which don't have a build configuration yet) and
    configuring the configure script arguments and the make command
    arguments.
    
    
    svn path=/trunk/; revision=12446

--- cmake/build_projects/configure_bp.cmake
+++ cmake/build_projects/configure_bp.cmake
@@ -1,23 +1,60 @@
 cmake_policy(SET CMP0007 NEW)
+
+# List of dependencies (mostly these are build-tools) which should be
+# ignored.
+set(ignored_dependencies_LIST
+  # build tools
+  pkg-config
+  bison
+  flex
+  python2-devel
+  # dependencies which we want to ignore
+  # (none so far).
+)
+
 file(STRINGS ${FILENAME} lines)
 list(LENGTH lines nlines)
 # There are currently groups of 9 lines for each
 # gtk software package specifying the required
 # build-configuration information.
-set(group_size 9)
-set(remove_indices 0 1 2 3 4 5 6 7 8)
+set(group_size 12)
+set(remove_indices 0 1 2 3 4 5 6 7 8 9 10 11)
 math(EXPR max_index "(${nlines}/${group_size}) - 1")
 foreach(index RANGE 0 ${max_index})
   list(GET lines 0 package)
   list(GET lines 1 config_type)
-  list(GET lines 2 config_arguments)
-  list(GET lines 3 download_hash_type)
-  list(GET lines 4 download_hash)
-  list(GET lines 5 download_href)
-  list(GET lines 6 dependencies)
-  list(GET lines 7 suggests)
-  list(GET lines 8 after)
+  list(GET lines 2 supports_non_srcdir_builds)
+  list(GET lines 3 supports_parallel_builds)
+  list(GET lines 4 config_arguments)
+  list(GET lines 5 make_arguments)
+  list(GET lines 6 download_hash_type)
+  list(GET lines 7 download_hash)
+  list(GET lines 8 download_href)
+  list(GET lines 9 dependencies)
+  list(GET lines 10 suggests)
+  list(GET lines 11 after)
   list(REMOVE_AT lines ${remove_indices})
+
+  if(supports_non_srcdir_builds)
+    set(BUILD_IN_SOURCE OFF)
+  else(supports_non_srcdir_builds)
+    set(BUILD_IN_SOURCE ON)
+  endif(supports_non_srcdir_builds)
+
+  if(supports_parallel_builds)
+    set(BUILD_COMMAND BP_PARALLEL_MAKE_COMMAND)
+  else(supports_parallel_builds)
+    set(BUILD_COMMAND BP_MAKE_COMMAND)
+  endif(supports_parallel_builds)
+
+  # Extra CFLAGS fixups for individual packages.
+  if(package STREQUAL "glib")
+    set(MSYS_EXTRA_CFLAGS "-march=native ")
+    set(UNIX_EXTRA_CFLAGS)
+  else(package STREQUAL "glib")
+    set(MSYS_EXTRA_CFLAGS)
+    set(UNIX_EXTRA_CFLAGS)
+  endif(package STREQUAL "glib")
 
   # Turn colon-separated lists into official CMake lists.
   string(REGEX REPLACE ":" ";" dependencies "${dependencies}")
