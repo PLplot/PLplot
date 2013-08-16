9fefae8422a28a6bcef14bf2862201256c082219
Alan W. Irwin <airwin@users.sourceforge.net>
2013-08-16 19:36:39 +0000

    If perl cannot be found, then do not allow a build of the docbook documentation since all backends for that build require perl.
    svn path=/trunk/; revision=12485

--- cmake/modules/docbook.cmake
+++ cmake/modules/docbook.cmake
@@ -21,12 +21,12 @@
 # Configuration for the building of DocBook documentation 
 
 option(BUILD_DOC "Enable build of DocBook documentation" OFF)
-if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+if(NOT (PERL_FOUND AND CMAKE_SYSTEM_NAME STREQUAL "Linux"))
   set(
   BUILD_DOC OFF CACHE INTERNAL 
   "Enable build of DocBook documentation"
   )
-endif(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(NOT (PERL_FOUND AND CMAKE_SYSTEM_NAME STREQUAL "Linux"))
 
 option(PREBUILT_DOC "Assume documentation is already built and present in doc/docbooks/src, ready to be installed. This option is useful for package maintainers" OFF)
 
