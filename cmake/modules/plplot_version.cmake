abbe90dc84f9fc5a5e5dc78367cba60824e2ca5d
Alan W. Irwin <airwin@users.sourceforge.net>
2013-09-29 08:17:29 +0000

    Update release date to 2013-09-29 and version to 5.9.10 in preparation for the planned release later today.
    Soversion bump for most PLplot libraries in preparation for the
    release because of substantial backwards incompatible API changes
    concerning integer to floating point line width parameters.
    
    
    svn path=/trunk/; revision=12546

--- cmake/modules/plplot_version.cmake
+++ cmake/modules/plplot_version.cmake
@@ -1,15 +1,15 @@
 # For now, must set this by hand before each release.  This value affects
 # at least the documentation build and perhaps other parts of the release.
-set(RELEASE_DATE "2011-10-12")
+set(RELEASE_DATE "2013-09-29")
 
 # Version data that need review and possible modification for each release.
 
-set(VERSION 5.9.9)
+set(VERSION 5.9.10)
 
 # CPack version numbers for release tarball name.
 set(CPACK_PACKAGE_VERSION_MAJOR 5)
 set(CPACK_PACKAGE_VERSION_MINOR 9)
-set(CPACK_PACKAGE_VERSION_PATCH 9)
+set(CPACK_PACKAGE_VERSION_PATCH 10)
 
 # PLplot library version information.
 
@@ -27,8 +27,8 @@ set(CPACK_PACKAGE_VERSION_PATCH 9)
 # (4) If there are no library source code changes at all, then leave all
 #     library version numbers the same for the release.
 
-set(plplotqt_SOVERSION 0)
-set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.1)
+set(plplotqt_SOVERSION 1)
+set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.0)
 
 set(nistcd_SOVERSION 0)
 set(nistcd_VERSION ${nistcd_SOVERSION}.0.1)
@@ -42,20 +42,20 @@ set(csironn_VERSION ${csironn_SOVERSION}.0.1)
 set(qsastime_SOVERSION 0)
 set(qsastime_VERSION ${qsastime_SOVERSION}.0.1)
 
-set(plplot_SOVERSION 11)
+set(plplot_SOVERSION 12)
 set(plplot_VERSION ${plplot_SOVERSION}.0.0)
 
-set(plplotcxx_SOVERSION 10)
+set(plplotcxx_SOVERSION 11)
 set(plplotcxx_VERSION ${plplotcxx_SOVERSION}.0.0)
 
-set(plplotdmd_SOVERSION 0)
+set(plplotdmd_SOVERSION 1)
 set(plplotdmd_VERSION ${plplotdmd_SOVERSION}.0.0)
 
 set(plplotf77_SOVERSION 9)
 set(plplotf77_VERSION ${plplotf77_SOVERSION}.1.1)
 
-set(plplotf95_SOVERSION 9)
-set(plplotf95_VERSION ${plplotf95_SOVERSION}.1.1)
+set(plplotf95_SOVERSION 10)
+set(plplotf95_VERSION ${plplotf95_SOVERSION}.0.0)
 
 set(plplotgnome2_SOVERSION 0)
 set(plplotgnome2_VERSION ${plplotgnome2_SOVERSION}.0.0)
@@ -63,13 +63,13 @@ set(plplotgnome2_VERSION ${plplotgnome2_SOVERSION}.0.0)
 set(tclmatrix_SOVERSION 9)
 set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.2.0)
 
-set(plplottcltk_SOVERSION 9)
-set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.2.0)
+set(plplottcltk_SOVERSION 10)
+set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.0.0)
 
 set(plplotwxwidgets_SOVERSION 0)
 set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.0.0)
 
-set(plplotada_SOVERSION 0)
+set(plplotada_SOVERSION 1)
 set(plplotada_VERSION ${plplotada_SOVERSION}.0.0)
 
 # According to man package, PLPLOT_TCL_VERSION (used in the various
@@ -79,4 +79,3 @@ set(plplotada_VERSION ${plplotada_SOVERSION}.0.0)
 # use the major number from the package version for now.
 
 set(PLPLOT_TCL_VERSION 5)
-
