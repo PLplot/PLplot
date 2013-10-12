9afadf6c8ac234a65a4e606edf33832514a54fe9
Alan W. Irwin <airwin@users.sourceforge.net>
2013-10-12 19:50:43 +0000

    Add libpcre as one of the dependencies for the future swig BUILD_THE_BUILDTOOLS configuration.
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> using
    the cmake -DBUILD_THE_BUILDTOOLS=ON option and the build_libppcre target
    for the build_projects project.
    
    svn path=/trunk/; revision=12592

---
+++ cmake/build_projects/libpcre/bp.cmake
@@ -0,0 +1,96 @@
+# libpcre/bp.cmake
+# N.B. this file is generated so if you edit it you will lose all your
+# changes the next time it is generated (typically by running
+# either/both the update_added_packages.sh or update_pango_packages.sh
+# scripts).  If those scripts do not provide good results, then
+# consider changing their source files (e.g., by editing the files
+# used in those scripts) or add a patch to be run by those scripts).
+
+# This file should be included directly or indirectly from a top-level
+# CMakeLists.txt file to configure the build of libpcre.
+
+# Copyright (C) 2013 Alan W. Irwin
+
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+# The top-level CMakeLists.txt file that includes this file should
+# have run "include(ExternalProject)" and set EP_BASE variable (and
+# corresponding directory property) as well as various BP variables
+# used below that configure how the External_Project functions
+# operate.
+
+# Protect against configuring a build twice in one CMake call
+if(libpcre_configured)
+  return()
+endif(libpcre_configured)
+set(libpcre_configured ON)
+
+# List of dependencies (most of which are build tools) which should be
+# ignored.
+set(BP_ignored_dependencies_LIST pkg-config;bison;flex;python2-devel;libXft ${extra_ignored_dependencies_list})
+
+set(libpcre_dependencies_LIST )
+# Remove dependencies that should be ignored.
+if(libpcre_dependencies_LIST)
+  list(REMOVE_ITEM libpcre_dependencies_LIST ${BP_ignored_dependencies_LIST})
+endif(libpcre_dependencies_LIST)
+
+set(libpcre_dependencies_targets)
+foreach(build_configuration ${libpcre_dependencies_LIST})
+  if(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+    include(${build_configuration}/bp.cmake)
+    list(APPEND libpcre_dependencies_targets build_${build_configuration})
+  else(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+    message(STATUS "Warning: A build_configuration for ${build_configuration} does not exist so it is assumed this dependency of libpcre has been installed another way.")
+  endif(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+endforeach(build_configuration ${libpcre_dependences_LIST})
+
+# This can be safely done only after above includes.
+set(BP_PACKAGE libpcre)
+
+# Data that is related to downloads.
+set(${BP_PACKAGE}_URL http://downloads.sourceforge.net/pcre/pcre-8.33.tar.bz2)
+set(${BP_PACKAGE}_DOWNLOAD_HASH_TYPE MD5)
+set(${BP_PACKAGE}_DOWNLOAD_HASH 21514018448ac10062ece7a668fca949)
+
+# Data that is related to the PATH that must be used.
+if(MSYS_PLATFORM)
+  #set(BP_PATH_NODLL "${BP_PATH}")
+  #set(BP_PATH "${EP_BASE}/Build/build_${BP_PACKAGE}/dll;${BP_PATH_NODLL}")
+  determine_msys_path(BP_PATH "${BP_PATH}")
+  # Must have all elements of env command in MSYS platform form
+  determine_msys_path(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
+  set(${BP_PACKAGE}_SET_CFLAGS "CFLAGS=$ENV{CFLAGS}")
+else(MSYS_PLATFORM)
+  set(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
+  set(${BP_PACKAGE}_SET_CFLAGS "CFLAGS=$ENV{CFLAGS}")
+endif(MSYS_PLATFORM)
+#message(STATUS "modified BP_PATH for ${BP_PACKAGE} = ${BP_PATH}")
+
+ExternalProject_Add(
+  build_${BP_PACKAGE}
+  DEPENDS ${${BP_PACKAGE}_dependencies_targets}
+  URL ${${BP_PACKAGE}_URL}
+  URL_HASH ${${BP_PACKAGE}_DOWNLOAD_HASH_TYPE}=${${BP_PACKAGE}_DOWNLOAD_HASH}
+  CONFIGURE_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${${BP_PACKAGE}_SET_CFLAGS} ${source_PATH}/${BP_CONFIGURE_COMMAND} --enable-unicode-properties --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --disable-static
+  BUILD_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_MAKE_COMMAND} 
+  BUILD_IN_SOURCE OFF
+  INSTALL_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_MAKE_COMMAND}  install
+  )
+
+list(APPEND build_target_LIST build_${BP_PACKAGE})
+# Restore BP_PATH to original state.
+set(BP_PATH "${BP_ORIGINAL_NATIVE_PATH}")
+#message(STATUS "${BP_PACKAGE} restored original BP_PATH = ${BP_PATH}")
