1e5172704e4a65cabe17b9090f37fe04a8b77582
Alan W. Irwin <airwin@users.sourceforge.net>
2013-10-30 17:29:31 +0000

    Change from Fortran 95 "data" form of specifying constants in the BOZ (z'....') form, e.g.,
          integer :: PL_FCI_SANS
          data PL_FCI_SANS / z'0' /
    
    to treating such constants just like integers, e.g.,
    
    integer, parameter :: PL_FCI_SANS = z'0'
    
    The latter form is allowed by the Fortran 2003 standard, and also
    from Arjen's research implemented by most/all Fortran compilers.
    
    
    svn path=/trunk/; revision=12640

--- bindings/f95/plplot_parameters.h
+++ bindings/f95/plplot_parameters.h
@@ -29,26 +29,16 @@
       integer, parameter :: PLESC_HAS_TEXT = 20 ! driver draws text
       integer, parameter :: PLESC_IMAGE = 21 ! handle image
       integer, parameter :: PLESC_IMAGEOPS = 22 ! plimage related operations
-      integer :: DRAW_LINEX ! draw lines parallel to the X axis
-      data DRAW_LINEX / z'01' /
-      integer :: DRAW_LINEY ! draw lines parallel to the Y axis
-      data DRAW_LINEY / z'02' /
-      integer :: DRAW_LINEXY ! draw lines parallel to both the X and Y axes
-      data DRAW_LINEXY / z'03' /
-      integer :: MAG_COLOR ! draw the mesh with a color dependent of the magnitude
-      data MAG_COLOR / z'04' /
-      integer :: BASE_CONT ! draw contour plot at bottom xy plane
-      data BASE_CONT / z'08' /
-      integer :: TOP_CONT ! draw contour plot at top xy plane
-      data TOP_CONT / z'10' /
-      integer :: SURF_CONT ! draw contour plot at surface
-      data SURF_CONT / z'20' /
-      integer :: DRAW_SIDES ! draw sides
-      data DRAW_SIDES / z'40' /
-      integer :: FACETED ! draw outline for each square that makes up the surface
-      data FACETED / z'80' /
-      integer :: MESH ! draw mesh
-      data MESH / z'100' /
+      integer, parameter :: DRAW_LINEX = z'01' ! draw lines parallel to the X axis
+      integer, parameter :: DRAW_LINEY = z'02' ! draw lines parallel to the Y axis
+      integer, parameter :: DRAW_LINEXY = z'03' ! draw lines parallel to both the X and Y axes
+      integer, parameter :: MAG_COLOR = z'04' ! draw the mesh with a color dependent of the magnitude
+      integer, parameter :: BASE_CONT = z'08' ! draw contour plot at bottom xy plane
+      integer, parameter :: TOP_CONT = z'10' ! draw contour plot at top xy plane
+      integer, parameter :: SURF_CONT = z'20' ! draw contour plot at surface
+      integer, parameter :: DRAW_SIDES = z'40' ! draw sides
+      integer, parameter :: FACETED = z'80' ! draw outline for each square that makes up the surface
+      integer, parameter :: MESH = z'100' ! draw mesh
       integer, parameter :: PL_BIN_DEFAULT = 0 
       integer, parameter :: PL_BIN_CENTRED = 1 
       integer, parameter :: PL_BIN_NOEXPAND = 2 
@@ -74,118 +64,65 @@
       integer, parameter :: PL_LEGEND_BACKGROUND = 32 
       integer, parameter :: PL_LEGEND_BOUNDING_BOX = 64 
       integer, parameter :: PL_LEGEND_ROW_MAJOR = 128 
-      integer :: PL_COLORBAR_LABEL_LEFT 
-      data PL_COLORBAR_LABEL_LEFT / z'1' /
-      integer :: PL_COLORBAR_LABEL_RIGHT 
-      data PL_COLORBAR_LABEL_RIGHT / z'2' /
-      integer :: PL_COLORBAR_LABEL_TOP 
-      data PL_COLORBAR_LABEL_TOP / z'4' /
-      integer :: PL_COLORBAR_LABEL_BOTTOM 
-      data PL_COLORBAR_LABEL_BOTTOM / z'8' /
-      integer :: PL_COLORBAR_IMAGE 
-      data PL_COLORBAR_IMAGE / z'10' /
-      integer :: PL_COLORBAR_SHADE 
-      data PL_COLORBAR_SHADE / z'20' /
-      integer :: PL_COLORBAR_GRADIENT 
-      data PL_COLORBAR_GRADIENT / z'40' /
-      integer :: PL_COLORBAR_CAP_NONE 
-      data PL_COLORBAR_CAP_NONE / z'80' /
-      integer :: PL_COLORBAR_CAP_LOW 
-      data PL_COLORBAR_CAP_LOW / z'100' /
-      integer :: PL_COLORBAR_CAP_HIGH 
-      data PL_COLORBAR_CAP_HIGH / z'200' /
-      integer :: PL_COLORBAR_SHADE_LABEL 
-      data PL_COLORBAR_SHADE_LABEL / z'400' /
-      integer :: PL_COLORBAR_ORIENT_RIGHT 
-      data PL_COLORBAR_ORIENT_RIGHT / z'800' /
-      integer :: PL_COLORBAR_ORIENT_TOP 
-      data PL_COLORBAR_ORIENT_TOP / z'1000' /
-      integer :: PL_COLORBAR_ORIENT_LEFT 
-      data PL_COLORBAR_ORIENT_LEFT / z'2000' /
-      integer :: PL_COLORBAR_ORIENT_BOTTOM 
-      data PL_COLORBAR_ORIENT_BOTTOM / z'4000' /
-      integer :: PL_COLORBAR_BACKGROUND 
-      data PL_COLORBAR_BACKGROUND / z'8000' /
-      integer :: PL_COLORBAR_BOUNDING_BOX 
-      data PL_COLORBAR_BOUNDING_BOX / z'10000' /
+      integer, parameter :: PL_COLORBAR_LABEL_LEFT = z'1' 
+      integer, parameter :: PL_COLORBAR_LABEL_RIGHT = z'2' 
+      integer, parameter :: PL_COLORBAR_LABEL_TOP = z'4' 
+      integer, parameter :: PL_COLORBAR_LABEL_BOTTOM = z'8' 
+      integer, parameter :: PL_COLORBAR_IMAGE = z'10' 
+      integer, parameter :: PL_COLORBAR_SHADE = z'20' 
+      integer, parameter :: PL_COLORBAR_GRADIENT = z'40' 
+      integer, parameter :: PL_COLORBAR_CAP_NONE = z'80' 
+      integer, parameter :: PL_COLORBAR_CAP_LOW = z'100' 
+      integer, parameter :: PL_COLORBAR_CAP_HIGH = z'200' 
+      integer, parameter :: PL_COLORBAR_SHADE_LABEL = z'400' 
+      integer, parameter :: PL_COLORBAR_ORIENT_RIGHT = z'800' 
+      integer, parameter :: PL_COLORBAR_ORIENT_TOP = z'1000' 
+      integer, parameter :: PL_COLORBAR_ORIENT_LEFT = z'2000' 
+      integer, parameter :: PL_COLORBAR_ORIENT_BOTTOM = z'4000' 
+      integer, parameter :: PL_COLORBAR_BACKGROUND = z'8000' 
+      integer, parameter :: PL_COLORBAR_BOUNDING_BOX = z'10000' 
       integer, parameter :: PLSWIN_DEVICE = 1 ! device coordinates
       integer, parameter :: PLSWIN_WORLD = 2 ! world coordinates
       integer, parameter :: PL_X_AXIS = 1 ! The x-axis
       integer, parameter :: PL_Y_AXIS = 2 ! The y-axis
       integer, parameter :: PL_Z_AXIS = 3 ! The z-axis
-      integer :: PL_OPT_ENABLED ! Obsolete
-      data PL_OPT_ENABLED / z'0001' /
-      integer :: PL_OPT_ARG ! Option has an argment
-      data PL_OPT_ARG / z'0002' /
-      integer :: PL_OPT_NODELETE ! Don't delete after processing
-      data PL_OPT_NODELETE / z'0004' /
-      integer :: PL_OPT_INVISIBLE ! Make invisible
-      data PL_OPT_INVISIBLE / z'0008' /
-      integer :: PL_OPT_DISABLED ! Processing is disabled
-      data PL_OPT_DISABLED / z'0010' /
-      integer :: PL_OPT_FUNC ! Call handler function
-      data PL_OPT_FUNC / z'0100' /
-      integer :: PL_OPT_BOOL ! Set *var = 1
-      data PL_OPT_BOOL / z'0200' /
-      integer :: PL_OPT_INT ! Set *var = atoi(optarg)
-      data PL_OPT_INT / z'0400' /
-      integer :: PL_OPT_FLOAT ! Set *var = atof(optarg)
-      data PL_OPT_FLOAT / z'0800' /
-      integer :: PL_OPT_STRING ! Set var = optarg
-      data PL_OPT_STRING / z'1000' /
-      integer :: PL_PARSE_PARTIAL ! For backward compatibility
-      data PL_PARSE_PARTIAL / z'0000' /
-      integer :: PL_PARSE_FULL ! Process fully & exit if error
-      data PL_PARSE_FULL / z'0001' /
-      integer :: PL_PARSE_QUIET ! Don't issue messages
-      data PL_PARSE_QUIET / z'0002' /
-      integer :: PL_PARSE_NODELETE ! Don't delete options after
-      data PL_PARSE_NODELETE / z'0004' /
-      integer :: PL_PARSE_SHOWALL ! Show invisible options
-      data PL_PARSE_SHOWALL / z'0008' /
-      integer :: PL_PARSE_OVERRIDE ! Obsolete
-      data PL_PARSE_OVERRIDE / z'0010' /
-      integer :: PL_PARSE_NOPROGRAM ! Program name NOT in *argv[0]..
-      data PL_PARSE_NOPROGRAM / z'0020' /
-      integer :: PL_PARSE_NODASH ! Set if leading dash NOT required
-      data PL_PARSE_NODASH / z'0040' /
-      integer :: PL_PARSE_SKIP ! Skip over unrecognized args
-      data PL_PARSE_SKIP / z'0080' /
+      integer, parameter :: PL_OPT_ENABLED = z'0001' ! Obsolete
+      integer, parameter :: PL_OPT_ARG = z'0002' ! Option has an argment
+      integer, parameter :: PL_OPT_NODELETE = z'0004' ! Don't delete after processing
+      integer, parameter :: PL_OPT_INVISIBLE = z'0008' ! Make invisible
+      integer, parameter :: PL_OPT_DISABLED = z'0010' ! Processing is disabled
+      integer, parameter :: PL_OPT_FUNC = z'0100' ! Call handler function
+      integer, parameter :: PL_OPT_BOOL = z'0200' ! Set *var = 1
+      integer, parameter :: PL_OPT_INT = z'0400' ! Set *var = atoi(optarg)
+      integer, parameter :: PL_OPT_FLOAT = z'0800' ! Set *var = atof(optarg)
+      integer, parameter :: PL_OPT_STRING = z'1000' ! Set var = optarg
+      integer, parameter :: PL_PARSE_PARTIAL = z'0000' ! For backward compatibility
+      integer, parameter :: PL_PARSE_FULL = z'0001' ! Process fully & exit if error
+      integer, parameter :: PL_PARSE_QUIET = z'0002' ! Don't issue messages
+      integer, parameter :: PL_PARSE_NODELETE = z'0004' ! Don't delete options after
+      integer, parameter :: PL_PARSE_SHOWALL = z'0008' ! Show invisible options
+      integer, parameter :: PL_PARSE_OVERRIDE = z'0010' ! Obsolete
+      integer, parameter :: PL_PARSE_NOPROGRAM = z'0020' ! Program name NOT in *argv[0]..
+      integer, parameter :: PL_PARSE_NODASH = z'0040' ! Set if leading dash NOT required
+      integer, parameter :: PL_PARSE_SKIP = z'0080' ! Skip over unrecognized args
       integer, parameter :: PL_FCI_MARK = ishft(1,31) 
-      integer :: PL_FCI_IMPOSSIBLE 
-      data PL_FCI_IMPOSSIBLE / z'00000000' /
-      integer :: PL_FCI_HEXDIGIT_MASK 
-      data PL_FCI_HEXDIGIT_MASK / z'f' /
-      integer :: PL_FCI_HEXPOWER_MASK 
-      data PL_FCI_HEXPOWER_MASK / z'7' /
-      integer :: PL_FCI_HEXPOWER_IMPOSSIBLE 
-      data PL_FCI_HEXPOWER_IMPOSSIBLE / z'f' /
-      integer :: PL_FCI_FAMILY 
-      data PL_FCI_FAMILY / z'0' /
-      integer :: PL_FCI_STYLE 
-      data PL_FCI_STYLE / z'1' /
-      integer :: PL_FCI_WEIGHT 
-      data PL_FCI_WEIGHT / z'2' /
-      integer :: PL_FCI_SANS 
-      data PL_FCI_SANS / z'0' /
-      integer :: PL_FCI_SERIF 
-      data PL_FCI_SERIF / z'1' /
-      integer :: PL_FCI_MONO 
-      data PL_FCI_MONO / z'2' /
-      integer :: PL_FCI_SCRIPT 
-      data PL_FCI_SCRIPT / z'3' /
-      integer :: PL_FCI_SYMBOL 
-      data PL_FCI_SYMBOL / z'4' /
-      integer :: PL_FCI_UPRIGHT 
-      data PL_FCI_UPRIGHT / z'0' /
-      integer :: PL_FCI_ITALIC 
-      data PL_FCI_ITALIC / z'1' /
-      integer :: PL_FCI_OBLIQUE 
-      data PL_FCI_OBLIQUE / z'2' /
-      integer :: PL_FCI_MEDIUM 
-      data PL_FCI_MEDIUM / z'0' /
-      integer :: PL_FCI_BOLD 
-      data PL_FCI_BOLD / z'1' /
+      integer, parameter :: PL_FCI_IMPOSSIBLE = z'00000000' 
+      integer, parameter :: PL_FCI_HEXDIGIT_MASK = z'f' 
+      integer, parameter :: PL_FCI_HEXPOWER_MASK = z'7' 
+      integer, parameter :: PL_FCI_HEXPOWER_IMPOSSIBLE = z'f' 
+      integer, parameter :: PL_FCI_FAMILY = z'0' 
+      integer, parameter :: PL_FCI_STYLE = z'1' 
+      integer, parameter :: PL_FCI_WEIGHT = z'2' 
+      integer, parameter :: PL_FCI_SANS = z'0' 
+      integer, parameter :: PL_FCI_SERIF = z'1' 
+      integer, parameter :: PL_FCI_MONO = z'2' 
+      integer, parameter :: PL_FCI_SCRIPT = z'3' 
+      integer, parameter :: PL_FCI_SYMBOL = z'4' 
+      integer, parameter :: PL_FCI_UPRIGHT = z'0' 
+      integer, parameter :: PL_FCI_ITALIC = z'1' 
+      integer, parameter :: PL_FCI_OBLIQUE = z'2' 
+      integer, parameter :: PL_FCI_MEDIUM = z'0' 
+      integer, parameter :: PL_FCI_BOLD = z'1' 
       integer, parameter :: PL_MAXKEY = 16 
       integer, parameter :: PL_MAXWINDOWS = 64 ! Max number of windows/page tracked
       real(kind=plflt), parameter :: PL_NOTSET = -42.0_plflt
