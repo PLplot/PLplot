2f15faec8373fabf50be500bc12f145bf34f37a0
Alan W. Irwin <airwin@users.sourceforge.net>
2013-11-08 07:52:13 +0000

    I got completely fed up with the broken autoconf-based build system for iwidgets4.0.1 and the inflexible and limited binary distribution approach that was used for iwidgets4.1.0.  So I implemented a CMake-based build system for iwidgets4.0/4.1, deleted the iwidgets4.0 directory that contained the separate autoconf-based build of iwidgets4.0.1, and configured CMake-based builds of both iwidgets4.0 and iwidgets (version 4.1.0 generated from iwidgets4.0 with a patch) in the one remaining iwidgets directory.
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> using the
    -DBUILD_THE_BUILDTOOLS=ON build_projects cmake option and the
    build_iwidgets and build_iwidgets4.0 targets.  The builds (which
    included all the relevant Tcl/Tk8.6 builds and both the version 3 and
    version 4 builds of itcl/itk and the version 4.0 and version 4.1
    builds of iwidgets) proceeded to conclusion without issues.
    
    Some simple testing under wish suceeded with such commands as "package
    require Iwidgets" (which loaded itcl/itk version 4 and iwidgets
    version 4.1.0) and "package require -exact Iwidgets 4.0.1) which
    loaded itcl/itk version 3 and iwidgets version 4.0.1.  I also
    tested the calendar and watch iwidgets demos under both Iwidgets 4.0.1
    and Iwidgets 4.1.0 with no issues.
    
    Additional testing with PLplot should follow.
    
    
    svn path=/trunk/; revision=12670

--- cmake/build_projects/iwidgets/bp.cmake
+++ cmake/build_projects/iwidgets/bp.cmake
@@ -1,5 +1,5 @@
 # iwidgets/bp.cmake
-# CMakeLists.txt file to configure the build of iwidgets.
+# CMakeLists.txt file to configure the build of iwidgets4.0 and iwidgets
 
 # Copyright (C) 2013 Alan W. Irwin
 
@@ -23,9 +23,6 @@
 # used below that configure how the External_Project functions
 # operate.
 
-# These build configuration details for iwidgets adapted from
-# <http://www.mail-archive.com/fink-commits@lists.sourceforge.net/msg113511.html>
-
 # Protect against configuring a build twice in one CMake call
 if(iwidgets_configured)
   return()
@@ -36,57 +33,111 @@ set(iwidgets_configured ON)
 # ignored.
 set(BP_ignored_dependencies_LIST ${extra_ignored_dependencies_list})
 
-set(iwidgets_dependencies_LIST tk tcl itk)
-# Remove dependencies that should be ignored.
-if(iwidgets_dependencies_LIST)
-  list(REMOVE_ITEM iwidgets_dependencies_LIST ${BP_ignored_dependencies_LIST})
-endif(iwidgets_dependencies_LIST)
+set(iwidgets4.0_dependencies_LIST itk3 itcl3 tk tcl)
+set(iwidgets_dependencies_LIST itk tk tcl)
 
-set(iwidgets_dependencies_targets)
-foreach(build_configuration ${iwidgets_dependencies_LIST})
-  if(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
-    include(${build_configuration}/bp.cmake)
-    list(APPEND iwidgets_dependencies_targets build_${build_configuration})
-  else(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
-    message(STATUS "Warning: A build_configuration for ${build_configuration} does not exist so it is assumed this dependency of iwidgets has been installed another way.")
-  endif(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
-endforeach(build_configuration ${iwidgets_dependences_LIST})
+set(tags "4.0" "")
+foreach(tag IN LISTS tags)
+  # Remove dependencies that should be ignored.
+  if(iwidgets${tag}_dependencies_LIST)
+    list(REMOVE_ITEM iwidgets${tag}_dependencies_LIST ${BP_ignored_dependencies_LIST})
+  endif(iwidgets${tag}_dependencies_LIST)
+
+  set(iwidgets${tag}_dependencies_targets)
+  foreach(build_configuration ${iwidgets${tag}_dependencies_LIST})
+    if(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+      include(${build_configuration}/bp.cmake)
+      list(APPEND iwidgets${tag}_dependencies_targets build_${build_configuration})
+    else(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+      message(STATUS "Warning: A build_configuration for ${build_configuration} does not exist so it is assumed this dependency of iwidgets${tag} has been installed another way.")
+    endif(EXISTS ${CMAKE_SOURCE_DIR}/${build_configuration}/bp.cmake)
+  endforeach(build_configuration ${iwidgets${tag}_dependences_LIST})
+endforeach(tag IN LISTS tags)
 
 # This can be safely done only after above includes.
 set(BP_PACKAGE iwidgets)
 
-# Data that is related to downloads.
-set(IWIDGETS_VERSION 41)
-set(IWIDGETS_LIBVERSION 4.1)
-set(${BP_PACKAGE}_URL http://downloads.sourceforge.net/project/kbskit/itk/iwidgets${IWIDGETS_VERSION}.tgz)
-set(${BP_PACKAGE}_DOWNLOAD_HASH_TYPE MD5)
-set(${BP_PACKAGE}_DOWNLOAD_HASH 5a60ec284bd8095c665287215ff2a69f)
+set(${BP_PACKAGE}4.0_VERSION 4.0.1)
+set(${BP_PACKAGE}4.0_LIBVERSION 4.0)
+set(${BP_PACKAGE}4.0_ITCL_VERSION 3.2)
+set(${BP_PACKAGE}4.0_CMAKE_OPTION -DBUILD_IWIDGETS_4.1=OFF)
 
-# Data that is related to the PATH that must be used.
-if(MSYS_PLATFORM)
-  #set(BP_PATH_NODLL "${BP_PATH}")
-  #set(BP_PATH "${EP_BASE}/Build/build_${BP_PACKAGE}/dll;${BP_PATH_NODLL}")
-  determine_msys_path(BP_PATH "${BP_PATH}")
-  # Must have all elements of env command in MSYS platform form
-  determine_msys_path(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
-  set(${BP_PACKAGE}_SET_CFLAGS "CFLAGS=$ENV{CFLAGS}")
-else(MSYS_PLATFORM)
-  set(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}")
-  set(${BP_PACKAGE}_SET_CFLAGS "CFLAGS=$ENV{CFLAGS}")
-endif(MSYS_PLATFORM)
-#message(STATUS "modified BP_PATH for ${BP_PACKAGE} = ${BP_PATH}")
+set(${BP_PACKAGE}_VERSION 4.1.0)
+set(${BP_PACKAGE}_LIBVERSION 4.1)
+set(${BP_PACKAGE}_ITCL_VERSION 4.0)
+set(${BP_PACKAGE}_CMAKE_OPTION -DBUILD_IWIDGETS_4.1=ON)
 
-ExternalProject_Add(
-  build_${BP_PACKAGE}
-  DEPENDS ${${BP_PACKAGE}_dependencies_targets}
-  URL ${${BP_PACKAGE}_URL}
-  URL_HASH ${${BP_PACKAGE}_DOWNLOAD_HASH_TYPE}=${${BP_PACKAGE}_DOWNLOAD_HASH}
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${EP_BASE}/Source/build_${BP_PACKAGE} ${BP_CMAKE_INSTALL_PREFIX}/lib/iwidgets${IWIDGETS_LIBVERSION}
-  )
+foreach(tag IN LISTS tags)
+  # Data that is related to downloads.
+  set(${BP_PACKAGE}${tag}_URL http://downloads.sourceforge.net/project/incrtcl/%5bIncr%20Widgets%5d/${${BP_PACKAGE}4.0_VERSION}/iwidgets${${BP_PACKAGE}4.0_VERSION}.tar.gz)
+  set(${BP_PACKAGE}${tag}_DOWNLOAD_HASH_TYPE MD5)
+  set(${BP_PACKAGE}${tag}_DOWNLOAD_HASH 0e9c140e81ea6015b56130127c7deb03)
 
-list(APPEND build_target_LIST build_${BP_PACKAGE})
-# Restore BP_PATH to original state.
-set(BP_PATH "${BP_ORIGINAL_NATIVE_PATH}")
-#message(STATUS "${BP_PACKAGE} restored original BP_PATH = ${BP_PATH}")
+  # Data that is related to the PATH that must be used.
+  if(MSYS_PLATFORM)
+    #set(BP_PATH_NODLL "${BP_PATH}")
+    #set(BP_PATH "${EP_BASE}/Build/build_${BP_PACKAGE}${tag}/dll;${BP_PATH_NODLL}")
+    determine_msys_path(BP_PATH "${BP_PATH}")
+    # Must have all elements of env command in MSYS platform form
+    determine_msys_path(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}${tag}")
+  else(MSYS_PLATFORM)
+    set(source_PATH "${EP_BASE}/Source/build_${BP_PACKAGE}${tag}")
+  endif(MSYS_PLATFORM)
+  #message(STATUS "modified BP_PATH for ${BP_PACKAGE}${tag} = ${BP_PATH}")
+
+
+  ExternalProject_Add(
+    build_${BP_PACKAGE}${tag}
+    DEPENDS ${${BP_PACKAGE}${tag}_dependencies_targets}
+    URL ${${BP_PACKAGE}${tag}_URL}
+    URL_HASH ${${BP_PACKAGE}${tag}_DOWNLOAD_HASH_TYPE}=${${BP_PACKAGE}${tag}_DOWNLOAD_HASH}
+    PATCH_COMMAND ""
+    CONFIGURE_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_CMAKE_COMMAND} ${${BP_PACKAGE}${tag}_CMAKE_OPTION} ${EP_BASE}/Source/build_${BP_PACKAGE}${tag}
+    BUILD_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_BUILD_COMMAND}
+    INSTALL_COMMAND ${ENV_EXECUTABLE} PATH=${BP_PATH} ${BP_PARALLEL_BUILD_COMMAND} install
+    )
+  if(NOT "${tag}")
+    # Note this logic block executed for iwidget4.1 which has an empty
+    # tag.  For this (4.1) case we do not use the
+    # http://downloads.sourceforge.net/project/kbskit/itk/iwidgets41.tar.gz
+    # tarball because there are some problems (incorrect install
+    # locations and only a subset of what is normally installed) for
+    # that binary distribution of iwidgets4.1.  Instead, we build 4.1
+    # similarly to 4.0 except we apply the following patch which was
+    # derived as follows:
+    #
+    # diff -Naur iwidgets4.0.1/README iwidgets4.1/README >iwidgets4.1.patch
+    # diff -Naur iwidgets4.0.1/license.terms iwidgets4.1/license.terms >>iwidgets4.1.patch
+    # diff -Naur --exclude=pkgIndex.tcl iwidgets4.0.1/generic iwidgets4.1/library >> iwidgets4.1.patch 
+    # diff -Naur iwidgets4.0.1/tests iwidgets4.1/tests >> iwidgets4.1.patch
+    #
+    # N.B. this patch is somewhat larger than
+    # iwidgets4.1/iwidgets.patch because that latter patch is only
+    # concerned with generic subdirectory differences and it also
+    # appears to be taken against iwidgets-4.0.2 (which I cannot find
+    # anywhere on the net) rather than iwidgets-4.0.1.
+    add_custom_command(
+      OUTPUT
+      ${EP_BASE}/Stamp/build_${BP_PACKAGE}${tag}/build_${BP_PACKAGE}${tag}-patch
+      COMMAND echo "Apply iwidgets4.1 source code patch"
+      COMMAND ${PATCH_EXECUTABLE} --directory=${EP_BASE}/Source/build_${BP_PACKAGE}${tag} -p1 < ${CMAKE_SOURCE_DIR}/${BP_PACKAGE}/${BP_PACKAGE}4.1.patch
+      COMMAND echo "Apply patch to support both iwidgets and Iwidgets package names"
+      COMMAND ${PATCH_EXECUTABLE} --directory=${EP_BASE}/Source/build_${BP_PACKAGE}${tag} -p1 < ${CMAKE_SOURCE_DIR}/${BP_PACKAGE}/${BP_PACKAGE}4_case.patch
+      APPEND
+      )
+  endif(NOT "${tag}")
+
+  add_custom_command(
+    OUTPUT
+    ${EP_BASE}/Stamp/build_${BP_PACKAGE}${tag}/build_${BP_PACKAGE}${tag}-patch
+    COMMAND echo "Add CMake-based build system"
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different 
+    ${CMAKE_SOURCE_DIR}/${BP_PACKAGE}/CMakeLists.txt
+    ${EP_BASE}/Source/build_${BP_PACKAGE}${tag}/CMakeLists.txt
+    APPEND
+      )
+  list(APPEND build_target_LIST build_${BP_PACKAGE}${tag})
+  # Restore BP_PATH to original state.
+  set(BP_PATH "${BP_ORIGINAL_NATIVE_PATH}")
+  #message(STATUS "${BP_PACKAGE}${tag} restored original BP_PATH = ${BP_PATH}")
+endforeach(tag IN LISTS tags)
