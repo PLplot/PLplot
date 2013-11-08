aae8847b9a4f229591f44ef1d97355f19a8d8081
Alan W. Irwin <airwin@users.sourceforge.net>
2013-11-08 21:41:30 +0000

    Rationalize and strengthen Itcl, Itk, and Iwidgets detection and consistency checking.  Use temporary files written by CMake in the build tree rather than permanent files in our source tree to do these checks using tclsh.
    svn path=/trunk/; revision=12671

--- cmake/modules/tcl-related.cmake
+++ cmake/modules/tcl-related.cmake
@@ -1,6 +1,6 @@
 # cmake/modules/tcl-related.cmake
 #
-# Copyright (C) 2006  Alan W. Irwin
+# Copyright (C) 2006-2013  Alan W. Irwin
 #
 # This file is part of PLplot.
 #
@@ -24,12 +24,12 @@ if(DEFAULT_NO_BINDINGS)
   option(ENABLE_tcl "Enable Tcl bindings" OFF)
   option(ENABLE_itcl "Enable incr Tcl interface code" OFF)
   option(ENABLE_tk "Enable Tk interface code" OFF)
-  option(ENABLE_itk "Enable incr TK interface code" OFF)
+  option(ENABLE_itk "Enable incr Tk interface code" OFF)
 else(DEFAULT_NO_BINDINGS)
   option(ENABLE_tcl "Enable Tcl bindings" ON)
   option(ENABLE_itcl "Enable incr Tcl interface code" ON)
   option(ENABLE_tk "Enable Tk interface code" ON)
-  option(ENABLE_itk "Enable incr TK interface code" ON)
+  option(ENABLE_itk "Enable incr Tk interface code" ON)
 endif(DEFAULT_NO_BINDINGS)
 
 option(USE_INCRTCL_VERSION_4 "Use [incr Tcl] version 4" OFF)
@@ -64,6 +64,8 @@ if(ENABLE_tcl)
     message(STATUS
       "Looking for include paths and libraries for Tcl/Tk - found"
       )
+    get_filename_component(TCL_LIBRARY_PATH ${TCL_LIBRARY} PATH)
+
     message(STATUS "Looking for tclsh")
     include(FindTclsh)
     if(TCL_TCLSH)
@@ -81,29 +83,29 @@ if(ENABLE_tcl)
     message(STATUS "TCL_INCLUDE_PATH = ${TCL_INCLUDE_PATH}")
     message(STATUS "TCL_LIBRARY = ${TCL_LIBRARY}")
     if(ENABLE_itcl)
+      if(USE_INCRTCL_VERSION_4)
+	set(SUGGESTED_ITCL_VERSION 4)
+      else(USE_INCRTCL_VERSION_4)
+	set(SUGGESTED_ITCL_VERSION 3)
+      endif(USE_INCRTCL_VERSION_4)
+      file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CheckITCL_Available.tcl "puts -nonewline [package require Itcl ${SUGGESTED_ITCL_VERSION}]; exit")
+      # Refine SUGGESTED_ITCL_VERSION to exact value or fail.
       execute_process(
-	COMMAND ${TCL_TCLSH} CheckITCL_Available.tcl
-	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake/modules
-	OUTPUT_VARIABLE AVAILABLE_ITCL)
-      if(AVAILABLE_ITCL)
-        execute_process(
-	  COMMAND ${TCL_TCLSH} GetITCL_Version.tcl
-	  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake/modules
-	  OUTPUT_VARIABLE itcl_library_versions)
+	COMMAND ${TCL_TCLSH} ${CMAKE_CURRENT_BINARY_DIR}/CheckITCL_Available.tcl
+	RESULT_VARIABLE ITCL_RC
+	OUTPUT_VARIABLE ITCL_VERSION
+	ERROR_VARIABLE ITCL_STDERR
+	)
+      if(NOT ITCL_RC)
+	set(ITCL_PACKAGE_NAME "-exact Itcl ${ITCL_VERSION}")
+	message(STATUS "ITCL_PACKAGE_NAME = ${ITCL_PACKAGE_NAME}")
         message(STATUS "Looking for itcl.h")
         find_path(ITCL_INCLUDE_PATH itcl.h ${TCL_INCLUDE_PATH})
         if(ITCL_INCLUDE_PATH)
           message(STATUS "Looking for itcl.h - found")
           message(STATUS "Looking for itcl library")
-          get_filename_component(TCL_LIBRARY_PATH ${TCL_LIBRARY} PATH)
-          set(itcl_library_versions 3.4 3.3 3.2 3.1 3.0 2.1 2.0)
-	  if(USE_INCRTCL_VERSION_4)
-	    set(itcl_library_versions 4.0.0 ${itcl_library_versions})
-	  endif(USE_INCRTCL_VERSION_4)
-          foreach(version ${itcl_library_versions})
-            find_library(ITCL_LIBRARY itcl${version} PATHS ${TCL_LIBRARY_PATH}
-              PATH_SUFFIXES itcl${version})
-          endforeach(version ${itcl_library_versions})
+          find_library(ITCL_LIBRARY itcl${ITCL_VERSION} PATHS ${TCL_LIBRARY_PATH}
+            PATH_SUFFIXES itcl${ITCL_VERSION})
           if(ITCL_LIBRARY)
             message(STATUS "Looking for itcl library - found")
             set(HAVE_ITCL ON)
@@ -118,11 +120,12 @@ if(ENABLE_tcl)
           message(STATUS "WARNING: Disabling incr Tcl interface code")
           set(ENABLE_itcl OFF CACHE BOOL "Enable incr Tcl interface code" FORCE)
         endif(ITCL_INCLUDE_PATH)
-      else(AVAILABLE_ITCL)
+      else(NOT ITCL_RC)
         message(STATUS "Itcl not available or not compatible with current Tcl shell")
+        message(STATUS "WARNING: Disabling incr Tcl interface code")
         set(HAVE_ITCL OFF)
         set(ENABLE_itcl OFF CACHE BOOL "Enable incr Tcl interface code" FORCE)
-      endif(AVAILABLE_ITCL)
+      endif(NOT ITCL_RC)
     endif(ENABLE_itcl)
     if(ENABLE_itcl)
       message(STATUS "ITCL_INCLUDE_PATH = ${ITCL_INCLUDE_PATH}")
@@ -161,33 +164,101 @@ if(ENABLE_tcl)
       set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
     endif(ENABLE_tk)
     if(ENABLE_itk)
-      message(STATUS "Looking for itk.h")
-      find_path(ITK_INCLUDE_PATH itk.h ${TK_INCLUDE_PATH})
-      if(ITK_INCLUDE_PATH)
-        message(STATUS "Looking for itk.h - found")
-        message(STATUS "Looking for itk library")
-        set(itk_library_versions 3.4 3.3 3.2 3.1 3.0 2.1 2.0)
-	if(USE_INCRTCL_VERSION_4)
-	  set(itk_library_versions 4.0.0 ${itk_library_versions})
-	endif(USE_INCRTCL_VERSION_4)
-        foreach(version ${itk_library_versions})
-          find_library(ITK_LIBRARY itk${version} PATHS ${TCL_LIBRARY_PATH}
-            PATH_SUFFIXES itk${version})
-        endforeach(version ${itk_library_versions})
-        if(ITK_LIBRARY)
-          message(STATUS "Looking for itk library - found")
-          set(HAVE_ITK ON)
-        else(ITK_LIBRARY)
-          message(STATUS "Looking for itk library - not found")
+      if(USE_INCRTCL_VERSION_4)
+	set(SUGGESTED_ITK_VERSION 4)
+      else(USE_INCRTCL_VERSION_4)
+	set(SUGGESTED_ITK_VERSION 3)
+      endif(USE_INCRTCL_VERSION_4)
+      file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CheckITK_Available.tcl "puts -nonewline [package require Itk ${SUGGESTED_ITK_VERSION}]; exit")
+      # Refine SUGGESTED_ITK_VERSION to exact value or fail.
+      execute_process(
+	COMMAND ${TCL_TCLSH} ${CMAKE_CURRENT_BINARY_DIR}/CheckITK_Available.tcl
+	RESULT_VARIABLE ITK_RC
+	OUTPUT_VARIABLE ITK_VERSION
+	ERROR_VARIABLE ITK_STDERR
+	)
+      if(NOT ITK_RC)
+	set(ITK_PACKAGE_NAME "-exact Itk ${ITK_VERSION}")
+	message(STATUS "ITK_PACKAGE_NAME = ${ITK_PACKAGE_NAME}")
+	message(STATUS "Looking for itk.h")
+	find_path(ITK_INCLUDE_PATH itk.h ${TK_INCLUDE_PATH})
+	if(ITK_INCLUDE_PATH)
+          message(STATUS "Looking for itk.h - found")
+          message(STATUS "Looking for itk library")
+          find_library(ITK_LIBRARY itk${ITK_VERSION} PATHS ${TCL_LIBRARY_PATH}
+            PATH_SUFFIXES itk${ITK_VERSION})
+          if(ITK_LIBRARY)
+            message(STATUS "Looking for itk library - found")
+            set(HAVE_ITK ON)
+
+	    # Test version consistency between iwidgets, itk, and itcl.
+	    if(USE_INCRTCL_VERSION_4)
+	      set(SUGGESTED_IWIDGETS_VERSION 4.1)
+	      file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/determine_versions.tcl "puts -nonewline \"[package require Iwidgets ${SUGGESTED_IWIDGETS_VERSION}];[package require Itk];[package require Itcl]\";exit")
+	      execute_process(
+		COMMAND ${TCL_TCLSH} ${CMAKE_CURRENT_BINARY_DIR}/determine_versions.tcl
+		RESULT_VARIABLE IWIDGETS_RC
+		OUTPUT_VARIABLE IWIDGETS_VERSIONS_LIST
+		ERROR_VARIABLE IWIDGETS_STDERR
+		)
+	    else(USE_INCRTCL_VERSION_4)
+	      # Find maximum 4.0 version of iwidgets that is
+	      # available.  4.0.1 (released in 2002) is the largest
+	      # version of iwidgets4.0 (as opposed to the recently
+	      # developed iwidgets4.1) I have found on the web in
+	      # tarball form, but I have seen one reference to 4.0.2
+	      # which may have been publicly released somewhere
+	      # inaccessible to my google searches or privately made
+	      # available before all development of iwidgets4.0
+	      # stopped. So add 4.0.2 to the list just in case.
+	      foreach(SUGGESTED_ITK_VERSION 4.0.2 4.0.1 4.0.0)
+		file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/determine_versions.tcl "puts -nonewline \"[package require Iwidgets ${SUGGESTED_IWIDGETS_VERSION}];[package require Itk];[package require Itcl]\";exit")
+		execute_process(
+		  COMMAND ${TCL_TCLSH} ${CMAKE_CURRENT_BINARY_DIR}/determine_versions.tcl
+		  RESULT_VARIABLE IWIDGETS_RC
+		  OUTPUT_VARIABLE IWIDGETS_VERSIONS_LIST
+		  ERROR_VARIABLE IWIDGETS_STDERR
+		  )
+		if(NOT IWIDGETS_RC)
+		  break()
+		endif(NOT IWIDGETS_RC)
+	      endforeach(SUGGESTED_ITK_VERSION 4.0.2 4.0.1 4.0.0)
+	    endif(USE_INCRTCL_VERSION_4)
+	    if(NOT IWIDGETS_RC)
+	      list(GET IWIDGETS_VERSIONS_LIST 0 IWIDGETS_VERSION)
+	      list(GET IWIDGETS_VERSIONS_LIST 1 CONSISTENT_ITK_VERSION)
+	      list(GET IWIDGETS_VERSIONS_LIST 2 CONSISTENT_ITCL_VERSION)
+	      if(CONSISTENT_ITCL_VERSION STREQUAL ITCL_VERSION AND CONSISTENT_ITK_VERSION STREQUAL ITK_VERSION)
+		set(IWIDGETS_PACKAGE_NAME "-exact Iwidgets ${IWIDGETS_VERSION}")
+		message(STATUS "IWIDGETS_PACKAGE_NAME = ${IWIDGETS_PACKAGE_NAME}")
+	      else(CONSISTENT_ITCL_VERSION STREQUAL ITCL_VERSION AND CONSISTENT_ITK_VERSION STREQUAL ITK_VERSION)
+		message(STATUS "WARNING: Consistent combination of Iwidgets, Itk, and Itcl not found so disabling Itcl and Itk")
+		set(ENABLE_itcl OFF CACHE BOOL "Enable incr Tcl interface code" FORCE)
+		set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
+	      endif(CONSISTENT_ITCL_VERSION STREQUAL ITCL_VERSION AND CONSISTENT_ITK_VERSION STREQUAL ITK_VERSION)
+	    else(NOT IWIDGETS_RC)
+	      message(STATUS "WARNING: Iwidgets could not be found so disabling Itcl and Itk")
+	      set(ENABLE_itcl OFF CACHE BOOL "Enable incr Tcl interface code" FORCE)
+	      set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
+	    endif(NOT IWIDGETS_RC)
+          else(ITK_LIBRARY)
+            message(STATUS "Looking for itk library - not found")
+            message(STATUS "WARNING: Disabling incr Tk interface code")
+            set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
+          endif(ITK_LIBRARY)
+	else(ITK_INCLUDE_PATH)
+          message(STATUS "Looking for itk.h - not found")
           message(STATUS "WARNING: Disabling incr Tk interface code")
           set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
-        endif(ITK_LIBRARY)
-      else(ITK_INCLUDE_PATH)
-        message(STATUS "Looking for itk.h - not found")
+	endif(ITK_INCLUDE_PATH)
+      else(NOT ITK_RC)
+        message(STATUS "Itk not available or not compatible with current Tcl shell")
         message(STATUS "WARNING: Disabling incr Tk interface code")
+        set(HAVE_ITK OFF)
         set(ENABLE_itk OFF CACHE BOOL "Enable incr Tk interface code" FORCE)
-      endif(ITK_INCLUDE_PATH)
+      endif(NOT ITK_RC)
     endif(ENABLE_itk)
+
     if(ENABLE_itk)
       message(STATUS "ITK_INCLUDE_PATH = ${ITK_INCLUDE_PATH}")
       message(STATUS "ITK_LIBRARY = ${ITK_LIBRARY}")
