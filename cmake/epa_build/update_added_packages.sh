3c495cb1068de7adb254597260def7dfc766cb1f
Alan W. Irwin <airwin@users.sourceforge.net>
2013-12-02 01:48:01 +0000

    Update scripts for regenerating build configurations, and refer to those scripts in the developer documentation rather than repeating all the commands in those scripts.
    svn path=/trunk/; revision=12799

--- cmake/epa_build/update_added_packages.sh
+++ cmake/epa_build/update_added_packages.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Update build configurations of added packages that are described by
-# gtk_packages_add.xml
+# add_packages.xml
 
-rm -f add_packages.data
-touch add_packages.data
-# Ignore libxml2 since that is sucked in by the libxslt dependency.
-PACKAGE_LIST="libffi intltool pixman gperf libxslt ragel"
+rm -f add_packages.data add_packages.stderr
+touch add_packages.data add_packages.stderr
+# PACKAGE_LIST only contains the package names in add_packages.xml
+# that do not depend on any other package in that file.  Thus,
+# PACKAGE_LIST contains a list of "starting" packages and the
+# dependencies of those should suck in the rest of the packages in
+# add_packages.xml.
+PACKAGE_LIST="libffi intltool pixman gperf swig libxslt ragel"
 for PACKAGE in $PACKAGE_LIST; do
-  ./gtk_transform.py $PACKAGE 1 <gtk_packages_add.xml 1>> add_packages.data
+  ./gtk_transform.py $PACKAGE 1 <add_packages.xml 1>> add_packages.data 2>> add_packages.stderr
 done
-cmake -DFILENAME:FILEPATH=add_packages.data -P configure_bp.cmake
+cmake -DFILENAME:FILEPATH=add_packages.data -P configure_epa.cmake
--- cmake/epa_build/update_pango_packages.sh
+++ cmake/epa_build/update_pango_packages.sh
@@ -2,7 +2,11 @@
 # Update build configurations of pango packages that are described by
 # GTK+ jhbuild build configuration
 
-GTK_VERSION=3.9.4
+# Chose this latest version (as of August 2013) because it probably has
+# improved build and dependency instructions compared to previous versions.
+# For example, the glib version (2.37.4) is known to solve a build issue
+# that I ran into for earlier glib versions.
+export GTK_VERSION=3.9.4
 rm -rf $GTK_VERSION
 mkdir $GTK_VERSION
 cd $GTK_VERSION
@@ -12,10 +16,49 @@ wget -nd  ftp://ftp.acc.umu.se/pub/GNOME/teams/releng/$GTK_VERSION/*
 cd ..
 ./gtk_xml_recursive_process.py \
 $GTK_VERSION/gnome-apps-$GTK_VERSION.modules \
->|gtk_packages_$GTK_VERSION.xml_original
-rm -rf $GTK_VERSION
-cp gtk_packages_$GTK_VERSION.xml_original gtk_packages_$GTK_VERSION.xml 
-patch --no-backup-if-mismatch gtk_packages_$GTK_VERSION.xml patch_gtk_packages.xml
+>| gtk_packages_$GTK_VERSION.xml
 
-./gtk_transform.py pango 1 <gtk_packages_$GTK_VERSION.xml 1>| pango_packages.data
-cmake -DFILENAME:FILEPATH=pango_packages.data -P configure_bp.cmake
+rm -rf $GTK_VERSION
+
+# Patch this result to correct errors I have discovered in the jhbuild
+# configuration or else to use new versions of packages.
+
+patch --no-backup-if-mismatch <patch_gtk_packages.xml
+
+# To create the schema for gtk_packages.xml use
+trang -I xml gtk_packages_$GTK_VERSION.xml gtk_packages_$GTK_VERSION.rnc
+
+# That resulting schema file is quite helpful in figuring out
+# how to process gtk_packages_$GTK_VERSION.xml as below.
+
+# Transform the xml form to a form that can be used by a cmake script.
+# Note, there will be some information messages sent to stderr by this
+# script that typically relate to good packages (ones that can be
+# transformed by this script) and bad packages. Currently the list of
+# bad packages are confined just to those which are completely missing
+# from the jhbuild data for gtk.  The first command-line argument is
+# the starting package name and the second command-line argument is a
+# control variable where the least significant bit is ON for following (hard)
+# "dependencies" dependencies, the next least significant bit is ON
+# for following (soft) "suggests", dependencies, and the next least
+# significant bit is ON for following (would be nice) "after"
+# dependencies.  Currently I use a command variable of 1 to keep
+# the number of packages configured for building pango and
+# (hard) dependencies to a minimum.
+./gtk_transform.py "pango" 1 <gtk_packages_$GTK_VERSION.xml 1>| pango_packages.data 2>|pango_packages.stderr
+
+# Finally to actually generate build configurations for build_packages run
+# the following command.
+
+cmake -DFILENAME:FILEPATH=pango_packages.data -P configure_epa.cmake
+
+# Patch generated configuration files.  This patch file contains
+# additional changes that cannot be done via a patch to the *.xml file.
+# Typically, these changes are hand edits which are tested then committed.
+# So typically the patch is created by rerunning the above cmake
+# command then using "svn diff" >| configured_pango.patch" to generate
+# the reverse form of the patch to change the result created by the
+# above cmake command into the svn committed form which is done with
+# the following patch command.
+
+patch --reverse --no-backup-if-mismatch -p0 <configured_pango.patch
