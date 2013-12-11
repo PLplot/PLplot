e12494d2115eea40f6b0075714e92d44cb6a219d
Andrew Ross <andrewross@users.sourceforge.net>
2013-12-11 01:27:07 +0000

    Add missing delete statement to silence valgrind warning.
    svn path=/trunk/; revision=12842

--- examples/c++/x33.cc
+++ examples/c++/x33.cc
@@ -279,6 +279,8 @@ x33::plcolorbar_example_page( int kind_i, int label_i, int cap_i, PLINT cont_col
     pls->vpor( 0.0, 1.0, 0.0, 1.0 );
     pls->wind( 0.0, 1.0, 0.0, 1.0 );
     pls->ptex( 0.5, 0.5, 0.0, 0.0, 0.5, title );
+
+    delete label;
 }
 
 void
