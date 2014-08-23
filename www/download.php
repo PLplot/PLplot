421a9d6919880229058e96efcce171f5db7294cd
Alan W. Irwin <airwin@users.sourceforge.net>
2014-08-23 08:59:54 -0700

    Change from svn to git wording for our download page.

--- www/download.php
+++ www/download.php
@@ -16,17 +16,11 @@
 		  <p>You can obtain the latest stable version of PLplot from our
 		     <a href="http://sourceforge.net/projects/plplot/files/plplot/">file
 		     release site</a>. Alternatively, you can obtain the cutting-edge version of
-		     PLplot (but with no promises about stability) from SVN via anonymous
-		     access to our <a href="http://svn.code.sf.net/p/plplot/code/trunk/">
-		     SVN repository</a>.  Finally, you can browse our SVN repository
-		     <a href="http://sourceforge.net/p/plplot/code/HEAD/tree/">
-		     here</a>.</p>
-
-			<h3>git Access</h3>
-		  <p>The PLplot git repository can be checked out
-		     with the following instruction on the command line:</p>
+		     PLplot (but with no promises about stability) using</p>
 		  <p><tt>git clone git://git.code.sf.net/p/plplot/plplot plplot.git</tt></p>
-		  <p>Command-line git is so useful/powerful that we do not recommend any git GUI clients.</p>
+		     <p>Finally, you can browse our git repository
+		     <a href="http://sourceforge.net/p/plplot/plplot/ci/master/tree">
+		     here</a>.</p>
 			<h3>PLplot Packages for Linux Distributions</h3>
 		  <ul class="arrowlist">
 <li><a href="http://packages.ubuntu.com/search?keywords=plplot&amp;searchon=names&amp;suite=all&amp;section=all">Ubuntu</a></li>
