d70d3b2e999cb8e120c2a1c2f0b78572dc012d1e
Alan W. Irwin <airwin@users.sourceforge.net>
2014-10-24 12:01:28 -0700

    Java: modify example 8 to make it consistent with recent update to C example 8
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> on Linux using
    the test_diff_psc target.  There were no obvious run-time errors and
    no differences left between the Java and C results.

--- examples/java/x08.java
+++ examples/java/x08.java
@@ -33,18 +33,24 @@ import static plplot.core.plplotjavacConstants.*;
 import java.lang.Math;
 
 class x08 {
+    // These values must be odd, for the middle
+    // of the index range to be an integer, and thus
+    // to correspond to the exact floating point centre
+    // of the sombrero.
+
+
     static final int XPTS = 35;
-    static final int YPTS = 46;
+    static final int YPTS = 45;
 
     PLStream         pls = new PLStream();
 
-    static double    alt[] = { 60.0, 20.0 };
-    static double    az[]  = { 30.0, 60.0 };
+    static double    alt[] = { 60.0, 40.0 };
+    static double    az[]  = { 30.0, -30.0 };
 
     static           String[] title =
     {
         "#frPLplot Example 8 - Alt=60, Az=30",
-        "#frPLplot Example 8 - Alt=20, Az=60",
+        "#frPLplot Example 8 - Alt=40, Az=-30",
     };
     // cmap1_init1
 
@@ -119,10 +125,28 @@ class x08 {
 
         int    ifshade;
 
+        int    indexxmin = 0;
+        int    indexxmax = XPTS;
+        int[] indexymin     = new int[XPTS];
+        int[] indexymax     = new int[XPTS];
+        double[][] zlimited = new double[XPTS][YPTS];
+
+        double dx = 2. / ( XPTS - 1 );
+        double dy = 2. / ( YPTS - 1 );
+
+        // parameters of ellipse (in x, y index coordinates) that limits the data.
+        // x0, y0 correspond to the exact floating point centre of the index
+        // range.
+        double x0 = 0.5 * ( XPTS - 1 );
+        double a  = 0.9 * x0;
+        double y0 = 0.5 * ( YPTS - 1 );
+        double b  = 0.7 * y0;
+        double square_root;
+
         // Parse and process command line arguments.
 
         pls.parseopts( args, PL_PARSE_FULL | PL_PARSE_NOPROGRAM );
-        boolean rosen = true;
+        boolean rosen = false;
 
         // Initialize plplot.
 
@@ -130,14 +154,14 @@ class x08 {
 
         for ( i = 0; i < XPTS; i++ )
         {
-            x[i] = (double) ( i - ( XPTS / 2 ) ) / (double) ( XPTS / 2 );
+            x[i] = -1. + dx * i;
             if ( rosen )
                 x[i] *= 1.5;
         }
 
         for ( j = 0; j < YPTS; j++ )
         {
-            y[j] = (double) ( j - ( YPTS / 2 ) ) / (double) ( YPTS / 2 );
+            y[j] = -1. + dy * j;
             if ( rosen )
                 y[j] += 0.5;
         }
@@ -152,7 +176,7 @@ class x08 {
                 {
                     z[i][j] = Math.pow( 1. - xx, 2. ) + 100 *
                               Math.pow( yy - Math.pow( xx, 2. ), 2. );
-                    // The log argument may be zero for just the right grid.
+                    // The log argument might be zero for just the right grid.
                     if ( z[i][j] > 0. )
                         z[i][j] = Math.log( z[i][j] );
                     else
@@ -170,6 +194,19 @@ class x08 {
             }
         }
 
+        for ( i = indexxmin; i < indexxmax; i++ )
+        {
+            square_root = Math.sqrt( 1. - Math.min( 1., Math.pow( ( i - x0 ) / a, 2. ) ) );
+            // Add 0.5 to find nearest integer and therefore preserve symmetry
+            // with regard to lower and upper bound of y range.
+            indexymin[i] = Math.max( 0, (int) ( 0.5 + y0 - b * square_root ) );
+            // indexymax calculated with the convention that it is 1
+            // greater than highest valid index.
+            indexymax[i] = Math.min( YPTS, 1 + (int) ( 0.5 + y0 + b * square_root ) );
+            for ( j = indexymin[i]; j < indexymax[i]; j++ )
+                zlimited[i][j] = z[i][j];
+        }
+
         double step = ( zmax - zmin ) / ( LEVELS + 1 );
         for ( i = 0; i < LEVELS; i++ )
             clevel[i] = zmin + step * ( i + 1 );
@@ -177,7 +214,7 @@ class x08 {
         pls.lightsource( 1., 1., 1. );
         for ( k = 0; k < 2; k++ )
         {
-            for ( ifshade = 0; ifshade < 4; ifshade++ )
+            for ( ifshade = 0; ifshade < 5; ifshade++ )
             {
                 pls.adv( 0 );
                 pls.vpor( 0.0, 1.0, 0.0, 0.9 );
@@ -197,27 +234,27 @@ class x08 {
 
                 pls.col0( 2 );
 
-                if ( ifshade == 0 ) // diffuse light surface plot
+                switch ( ifshade )
                 {
+                case 0: // diffuse light surface plot
                     cmap1_init( 1 );
-                    // with new interface haven't yet made null work so have
-                    // to put in specific zero-length array.
                     pls.surf3d( x, y, z, 0, clev_null );
-                }
-                else if ( ifshade == 1 )   // magnitude colored plot
-                {
+                    break;
+                case 1: // magnitude colored plot
                     cmap1_init( 0 );
                     pls.surf3d( x, y, z, MAG_COLOR, clev_null );
-                }
-                else if ( ifshade == 2 )   //  magnitude colored plot with faceted squares
-                {
+                    break;
+                case 2: //  magnitude colored plot with faceted squares
                     cmap1_init( 0 );
                     pls.surf3d( x, y, z, MAG_COLOR | FACETED, clev_null );
-                }
-                else                     // magnitude colored plot with contours
-                {
+                    break;
+                case 3: // magnitude colored plot with contours
                     cmap1_init( 0 );
                     pls.surf3d( x, y, z, MAG_COLOR | SURF_CONT | BASE_CONT, clevel );
+                    break;
+                case 4: // magnitude colored plot with contours and index limits.
+                    cmap1_init( 0 );
+                    pls.surf3dl( x, y, zlimited, MAG_COLOR | SURF_CONT | BASE_CONT, clevel, indexxmin, indexymin, indexymax );
                 }
             }
         }
