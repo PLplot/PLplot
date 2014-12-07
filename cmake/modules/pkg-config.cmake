772223c638ecf5dc740c9f3dd7a6883c6d2c83d2
Alan W. Irwin <airwin@users.sourceforge.net>
2014-12-07 09:06:08 -0800

    Adjust for internal CMake-3.1 pkg-config change.
    There is a report from Greg Jung <gvjung@gmail.com> that the
    internal CMake command
    
    _pkg_check_modules_internal(0 0 ${_prefix} "${_package}")
    
    must be changed to
    
    _pkg_check_modules_internal(0 0 0 0 ${_prefix} "${_package}")
    
    for CMake-3.1 in order to build the cairo device properly.  Accordingly, I have made that adjustment.
    
    Tested by Alan W. Irwin <airwin@users> on Linux using CMake-3.0.2
    by building the cairo device.
    
    N.B. currently untested for CMake-3.1.
    
    ToDo:
    
    Extensive tests on CMake-3.1 (once that version is closer to release)
    still need to be done since the change in the pkg-config support by
    CMake may need other adjustments as well.

--- cmake/modules/pkg-config.cmake
+++ cmake/modules/pkg-config.cmake
@@ -1,6 +1,6 @@
 # cmake/modules/pkg-config.cmake
 #
-# Copyright (C) 2006-2014 Alan W. Irwin
+# Copyright (C) 2006-2015 Alan W. Irwin
 #
 # This file is part of PLplot.
 #
@@ -94,7 +94,12 @@ macro(pkg_check_pkgconfig _package _include_DIR _link_DIR _link_FLAGS _cflags _v
     set(_xprefix ${_prefix})
   endif(FORCE_EXTERNAL_STATIC)
   
-  _pkg_check_modules_internal(0 0 ${_prefix} "${_package}")
+  if(CMAKE_VERSION VERSION_LESS "3.1")
+    _pkg_check_modules_internal(0 0 ${_prefix} "${_package}")
+  else(CMAKE_VERSION VERSION_LESS "3.1")
+    _pkg_check_modules_internal(0 0 0 0 ${_prefix} "${_package}")
+  endif(CMAKE_VERSION VERSION_LESS "3.1")
+    
   if(${_prefix}_FOUND)
     cmake_link_flags(${_link_FLAGS} "${${_xprefix}_LDFLAGS}")
     # If libraries cannot be not found, then that is equivalent to whole
