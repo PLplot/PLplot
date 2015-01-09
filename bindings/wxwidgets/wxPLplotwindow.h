73bfb544fd62610219d5531667928a347f852dd3
Phil Rosenberg <philip_rosenberg@yahoo.com>
2015-01-09 14:35:50 +0000

    Changed wxPLplotWindow to a templated class, where the type of wxWidget that you wish to inherit from is passed as the template parameter. This is preferred over the previous method (where wxPLplotWindow inherited from wxWindow) because it no longer requires multiple inheritance. E.g. previously to get a wxPLplotFrame you would inherit from wxPLplotWindow and wxFrame, both of which inherited from wxWindow, (i.e. the diamond problem) which makes working with such objects difficult and tedious. I also removed the event table and replaced it with bind calls. Now to create a wxPLplotWindow we use 2-stage initialisation: wxPLplotWindow<wxSomeWindowType> *myWindow=new wxPLplotWindow<wxSomeWindowType>; myWindow->Create(parent, id, ... ); Easy peasy!

--- bindings/wxwidgets/wxPLplotwindow.h
+++ bindings/wxwidgets/wxPLplotwindow.h
@@ -20,49 +20,189 @@
 #if !defined ( WXPLPLOTWINDOW_H__INCLUDED_ )
 #define WXPLPLOTWINDOW_H__INCLUDED_
 
-#include <wx/window.h>
 #include <wx/dcmemory.h>
+#include <wx/dcclient.h>
 
-#include "wxPLplotstream.h"
+class wxPLplotstream;
 
-
-//! A plot widget which takes care of double buffering and other stuff, but does not
-//  provide an own plot API. You have to get a pointer to wxPLplotstream via the
-//  GetStream() method to gain access to the PLplot API.
-//
-class PLDLLIMPEXP_WX wxPLplotwindow : public wxWindow
+// A plplot wxWindow template. To create an actual plplot wxWindow use
+// the type of wxWindow you wish to inherit from at the template parameter
+// For example to create a plplot wxFrame create a wxPLplotwindow<wxFrame>.
+// You must also create the event table somewhere in your application
+// using the PL_EVENT_TABLE macro below
+template <class WXWINDOW>
+class wxPLplotwindow : public WXWINDOW
 {
 public:
-    wxPLplotwindow( wxWindow * parent, wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition,
-                    const wxSize& size = wxDefaultSize, long style = 0,
-                    int pl_style = wxPLPLOT_NONE );                         //!< Constructor.
-    ~wxPLplotwindow( void );                                                //!< Deconstructor.
+    wxPLplotwindow( int pl_style = wxPLPLOT_NONE );                         //!< Constructor.
+    ~wxPLplotwindow( void );                                                //!< Destructor.
 
     void RenewPlot( void );                                                 //!< Redo plot.
     bool SavePlot( const wxString& driver, const wxString& filename );      //!< Save plot using a different driver.
     wxPLplotstream* GetStream()  { return m_stream; }                       //!< Get pointer to wxPLplotstream of this widget.
     int getBackend()  { return m_backend; }
+	void loadFile( wxString fileName );
 
 protected:
     virtual void OnPaint( wxPaintEvent& event );                //!< Paint event
     virtual void OnErase( wxEraseEvent & WXUNUSED( event ) );   //!< Erase event
     virtual void OnSize( wxSizeEvent & WXUNUSED( event ) );     //!< Size event
+    wxPLplotstream* m_stream;           //!< Pointer to the wxPLplotstream which belongs to this plot widget
 
 private:
     // variables regarding double buffering
-    wxMemoryDC* MemPlotDC;        //!< Pointer to wxMemoryDC, used for double buffering
+    wxMemoryDC m_memPlotDC;        //!< Pointer to wxMemoryDC, used for double buffering
     int       m_width;            //!< Saved width of plot, to find out if size changed.
     int       m_height;           //!< Saved height of plot, to find out if size changed.
-    int       bitmapWidth;        //!< Width of bitmap, only changed if plot gets bigger
-    int       bitmapHeight;       //!< Height of bitmap, only changed if plot gets bigger
-    wxBitmap  * MemPlotDCBitmap;  //!< Pointer to bitmap, used for double buffering.
-    int       m_backend;
-
-protected:
-    wxPLplotstream* m_stream;           //!< Pointer to the wxPLplotstream which belongs to this plot widget
-
-    DECLARE_EVENT_TABLE()
+    int       m_bitmapWidth;        //!< Width of bitmap, only changed if plot gets bigger
+    int       m_bitmapHeight;       //!< Height of bitmap, only changed if plot gets bigger
+    wxBitmap  * m_memPlotDCBitmap;  //!< Pointer to bitmap, used for double buffering.
 };
 
+
+//! Constructor initialises all pointers to NULL and sets sizes to zero, the first resize
+// will initialise these. It also sets up the event handling.
+//
+template<class WXWINDOW>
+wxPLplotwindow<WXWINDOW>::wxPLplotwindow( int pl_style )
+{
+
+
+	m_memPlotDCBitmap = NULL;
+	m_stream = new wxPLplotstream( &m_memPlotDC, 0, 0, pl_style );
+	m_width = 0;
+	m_height = 0;
+	m_bitmapWidth = 0;
+	m_bitmapHeight = 0;
+
+	SetBackgroundStyle( wxBG_STYLE_CUSTOM );
+
+	Bind( wxEVT_SIZE, &wxPLplotwindow<WXWINDOW>::OnSize, this );
+	Bind( wxEVT_PAINT, &wxPLplotwindow<WXWINDOW>::OnPaint, this );
+}
+
+
+//! Deconstructor takes care that all is deleted in the correct order.
+//
+template<class WXWINDOW>
+wxPLplotwindow<WXWINDOW>::~wxPLplotwindow( void )
+{
+	m_memPlotDC.SelectObject( wxNullBitmap );
+
+    if ( m_memPlotDCBitmap )
+        delete m_memPlotDCBitmap;
+
+    if ( m_stream )
+        delete m_stream;
+}
+
+
+//! In the OnPaint Method we check if the Windows was resized (will be moved to OnSize() sometimes
+//  later), we also implement our own double buffering here (since the PLplot wxWidgets driver draws
+//  into a wxMemoryDC)
+//
+template<class WXWINDOW>
+void wxPLplotwindow<WXWINDOW>::OnPaint( wxPaintEvent &WXUNUSED( event ) )
+{
+    wxPaintDC dc( this );
+    dc.Blit( 0, 0, m_width, m_height, &m_memPlotDC, 0, 0 );
+}
+
+
+template<class WXWINDOW>
+void wxPLplotwindow<WXWINDOW>::OnSize( wxSizeEvent& WXUNUSED( event ) )
+{
+    int width, height;
+    GetClientSize( &width, &height );
+
+    // Check if the window was resized
+    if ( ( m_width != width ) || ( m_height != height ) )
+    {
+        if ( ( width > m_bitmapWidth ) || ( height > m_bitmapHeight ) )
+        {
+            m_bitmapWidth  = m_bitmapWidth > width ? m_bitmapWidth : width;
+            m_bitmapHeight = m_bitmapHeight > height ? m_bitmapHeight : height;
+
+            m_memPlotDC.SelectObject( wxNullBitmap );
+            if ( m_memPlotDCBitmap )
+                delete m_memPlotDCBitmap;
+            m_memPlotDCBitmap = new wxBitmap( m_bitmapWidth, m_bitmapHeight, -1 );
+            m_memPlotDC.SelectObject( *m_memPlotDCBitmap );
+        }
+
+        m_stream->SetSize( width, height );
+        m_stream->RenewPlot();
+
+        m_width  = width;
+        m_height = height;
+    }
+    else
+    {
+        m_stream->Update();
+        Refresh( false );
+    }
+}
+
+
+//! Together with "SetBackgroundStyle( wxBG_STYLE_CUSTOM );" in the constructor this method
+//  is responsible that the background is not erased in order to prevent flickering.
+//
+template<class WXWINDOW>
+void wxPLplotwindow<WXWINDOW>::OnErase( wxEraseEvent &WXUNUSED( event ) )
+{
+}
+
+
+//! Redo the whole plot.
+//
+template<class WXWINDOW>
+void wxPLplotwindow<WXWINDOW>::RenewPlot( void )
+{
+    if ( m_stream )
+    {
+        m_stream->RenewPlot();
+        Refresh( false );
+    }
+}
+
+
+//! Save plot.
+//
+template<class WXWINDOW>
+bool wxPLplotwindow<WXWINDOW>::SavePlot( const wxString& devname, const wxString& filename )
+{
+    int  pls, pls_save;
+    FILE *sfile;
+
+    if ( ( sfile = fopen( filename.mb_str(), "wb+" ) ) == NULL )
+    {
+        return false;
+    }
+
+    plgstrm( &pls );
+    plmkstrm( &pls_save );
+    if ( pls_save < 0 )
+    {
+        fclose( sfile );
+        return false;
+    }
+    plsdev( devname.mb_str() );
+    plsfile( sfile );
+
+    plspage( 0., 0., 800, 600, 0, 0 );
+    plcpstrm( pls, 0 );
+    pladv( 0 );
+    plreplot();
+    plend1();
+    plsstrm( pls );
+
+    return true;
+}
+
+template<class WXWINDOW>
+void wxPLplotwindow<WXWINDOW>::loadFile( wxString fileName )
+{
+
+}
 
 #endif // !defined( WXPLPLOTWINDOW_H__INCLUDED_ )
