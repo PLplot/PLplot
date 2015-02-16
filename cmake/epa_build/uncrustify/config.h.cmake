b8627eb5266da00681a1ac5a43aa674da9e8b44b
Alan W. Irwin <airwin@users.sourceforge.net>
2015-02-16 01:26:08 -0800

    epabuild: add uncrustify build
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    by setting all relevant
    environment variables mentioned in cmake_epa_build/README and
    also set by
    
    source cmake/epa_build/setup/setup_linux_makefiles
    
    then running
    
    cmake -DCMAKE_VERBOSE_MAKEFILE=ON -G"$GENERATOR_STRING"
    -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX} \
    -DBUILD_THE_BUILDTOOLS=OFF $EPA_BUILD_SOURCE_PATH >& cmake.out
    
    make build_uncrustify >& build_uncrustify.out
    
    The sourcefile was downloaded, configured by CMake and built
    and installed by make without issues.  Furthermore, the
    resulting uncrustify that was installed in ${INSTALL_PREFIX}/bin
    worked to check the style of all PLplot source.

---
+++ cmake/epa_build/uncrustify/config.h.cmake
@@ -0,0 +1,14 @@
+// Define if inttypes.h can be found 
+#cmakedefine HAVE_INTTYPES_H
+
+// Define if strings.h can be found.
+#cmakedefine HAVE_STRINGS_H
+
+// Define if sys/stat.h can be found.
+#cmakedefine HAVE_SYS_STAT_H
+
+// Define if unistd.h can be found.
+#cmakedefine HAVE_UNISTD_H
+
+// Define if utime.h can be found.
+#cmakedefine HAVE_UTIME_H
