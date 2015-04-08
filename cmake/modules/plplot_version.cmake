b16707a724cb9ef9bf3643c27603098fca1164a5
Alan W. Irwin <airwin@users.sourceforge.net>
2015-04-08 14:46:58 -0700

    Update versions and release date for the forthcoming release
    The release date is now set in doc/docbook/src/CMakeLists.txt since it
    only affects the documentation generation.  The actual value has been
    approximately updated to 4 days from now to reflect my current
    best guess concerning the release date, but that value will likely
    be updated again.
    
    All libraries that used to have the "d" suffix for their name have had
    their SOVERSIONS bumped to reflect that backward-incompatible change
    in the library name.

--- cmake/modules/plplot_version.cmake
+++ cmake/modules/plplot_version.cmake
@@ -1,11 +1,7 @@
-# For now, must set this by hand before each release.  This value affects
-# at least the documentation build and perhaps other parts of the release.
-set(RELEASE_DATE "2014-02-12")
-
 # Version data that need review and possible modification for each release.
 
 set(PLPLOT_VERSION_MAJOR 5)
-set(PLPLOT_VERSION_MINOR 10)
+set(PLPLOT_VERSION_MINOR 11)
 set(PLPLOT_VERSION_PATCH 0)
 
 # Overall PLplot version number.
@@ -32,9 +28,6 @@ set(CPACK_PACKAGE_VERSION_PATCH ${PLPLOT_VERSION_PATCH})
 # (4) If there are no library source code changes at all, then leave all
 #     library version numbers the same for the release.
 
-set(plplotqt_SOVERSION 1)
-set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.0)
-
 set(nistcd_SOVERSION 0)
 set(nistcd_VERSION ${nistcd_SOVERSION}.0.1)
 
@@ -47,32 +40,35 @@ set(csironn_VERSION ${csironn_SOVERSION}.0.1)
 set(qsastime_SOVERSION 0)
 set(qsastime_VERSION ${qsastime_SOVERSION}.0.1)
 
-set(plplot_SOVERSION 12)
+set(plplot_SOVERSION 13)
 set(plplot_VERSION ${plplot_SOVERSION}.0.1)
 
-set(plplotcxx_SOVERSION 11)
+set(plplotcxx_SOVERSION 12)
 set(plplotcxx_VERSION ${plplotcxx_SOVERSION}.0.0)
 
-set(plplotdmd_SOVERSION 1)
+set(plplotdmd_SOVERSION 2)
 set(plplotdmd_VERSION ${plplotdmd_SOVERSION}.0.0)
 
-set(plplotf95_SOVERSION 11)
+set(plplotf95_SOVERSION 12)
 set(plplotf95_VERSION ${plplotf95_SOVERSION}.0.0)
 
-set(plplotgnome2_SOVERSION 0)
+set(plplotqt_SOVERSION 2)
+set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.0)
+
+set(plplotgnome2_SOVERSION 1)
 set(plplotgnome2_VERSION ${plplotgnome2_SOVERSION}.0.0)
 
-set(tclmatrix_SOVERSION 9)
+set(tclmatrix_SOVERSION 10)
 set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.2.0)
 
-set(plplottcltk_SOVERSION 11)
+set(plplottcltk_SOVERSION 12)
 set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.1.0)
 
-set(plplottcltk_Main_SOVERSION 0)
+set(plplottcltk_Main_SOVERSION 1)
 set(plplottcltk_Main_VERSION ${plplottcltk_Main_SOVERSION}.0.0)
 
-set(plplotwxwidgets_SOVERSION 0)
+set(plplotwxwidgets_SOVERSION 1)
 set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.0.0)
 
-set(plplotada_SOVERSION 1)
+set(plplotada_SOVERSION 2)
 set(plplotada_VERSION ${plplotada_SOVERSION}.0.0)
