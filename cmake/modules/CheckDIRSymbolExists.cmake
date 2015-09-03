c051d277a557cb82b826d5f44fd034116c97bd5f
Alan W. Irwin <airwin@users.sourceforge.net>
2015-09-03 13:16:38 -0700

    Build system: style changes for cmake/modules/CheckDIRSymbolExists.cmake
    These style changes mostly consisted of lowercasing the CMake command
    names, but I also took this opportunity to improve the commentary
    and output messages.
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    by running cmake.  The relevant output from that run was
    
    -- Looking for DIR symbol in sys/types.h;dirent.h
    -- Looking for DIR symbol in sys/types.h;dirent.h - found
    
    which is the expected result on that platform.

--- cmake/modules/CheckDIRSymbolExists.cmake
+++ cmake/modules/CheckDIRSymbolExists.cmake
@@ -7,8 +7,8 @@
 # This module is a small but important variation on CheckSymbolExists.cmake.
 # The symbol always searched for is DIR, and the test programme follows
 # the AC_HEADER_DIRENT test programme rather than the CheckSymbolExists.cmake
-# test programme which always fails since DIR tends to be typedef'd
-# rather than #define'd.
+# test programme since that latter programme always fails when the DIR symbol
+# is typedef'd rather than #define'd.
 # 
 # The following variables may be set before calling this macro to
 # modify the way the check is run:
@@ -18,58 +18,61 @@
 #  CMAKE_REQUIRED_INCLUDES = list of include directories
 #  CMAKE_REQUIRED_LIBRARIES = list of libraries to link
 
-MACRO(CHECK_DIRSYMBOL_EXISTS FILES VARIABLE)
-  IF(NOT DEFINED ${VARIABLE})
-    SET(CMAKE_CONFIGURABLE_FILE_CONTENT "/* */\n")
-    SET(MACRO_CHECK_DIRSYMBOL_EXISTS_FLAGS ${CMAKE_REQUIRED_FLAGS})
-    IF(CMAKE_REQUIRED_LIBRARIES)
-      SET(CHECK_DIRSYMBOL_EXISTS_LIBS 
+macro(CHECK_DIRSYMBOL_EXISTS FILES VARIABLE)
+  if(NOT DEFINED ${VARIABLE})
+    set(CMAKE_CONFIGURABLE_FILE_CONTENT "/* */\n")
+    set(MACRO_CHECK_DIRSYMBOL_EXISTS_FLAGS ${CMAKE_REQUIRED_FLAGS})
+
+    if(CMAKE_REQUIRED_LIBRARIES)
+      set(MACRO_CHECK_DIRSYMBOL_EXISTS_LIBS 
         "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
-    ELSE(CMAKE_REQUIRED_LIBRARIES)
-      SET(CHECK_DIRSYMBOL_EXISTS_LIBS)
-    ENDIF(CMAKE_REQUIRED_LIBRARIES)
-    IF(CMAKE_REQUIRED_INCLUDES)
-      SET(CMAKE_DIRSYMBOL_EXISTS_INCLUDES
+    else(CMAKE_REQUIRED_LIBRARIES)
+      set(MACRO_CHECK_DIRSYMBOL_EXISTS_LIBS)
+    endif(CMAKE_REQUIRED_LIBRARIES)
+
+    if(CMAKE_REQUIRED_INCLUDES)
+      set(MACRO_CHECK_DIRSYMBOL_EXISTS_INCLUDES
         "-DINCLUDE_DIRECTORIES:STRING=${CMAKE_REQUIRED_INCLUDES}")
-    ELSE(CMAKE_REQUIRED_INCLUDES)
-      SET(CMAKE_DIRSYMBOL_EXISTS_INCLUDES)
-    ENDIF(CMAKE_REQUIRED_INCLUDES)
-    FOREACH(FILE ${FILES})
-      SET(CMAKE_CONFIGURABLE_FILE_CONTENT
+    else(CMAKE_REQUIRED_INCLUDES)
+      set(MACRO_CHECK_DIRSYMBOL_EXISTS_INCLUDES)
+    endif(CMAKE_REQUIRED_INCLUDES)
+
+    foreach(FILE ${FILES})
+      set(CMAKE_CONFIGURABLE_FILE_CONTENT
         "${CMAKE_CONFIGURABLE_FILE_CONTENT}#include <${FILE}>\n")
-    ENDFOREACH(FILE)
-    SET(CMAKE_CONFIGURABLE_FILE_CONTENT
+    endforeach(FILE)
+    set(CMAKE_CONFIGURABLE_FILE_CONTENT
       "${CMAKE_CONFIGURABLE_FILE_CONTENT}\nint main()\n{if ((DIR *) 0) return 0;}\n")
 
-    CONFIGURE_FILE("${CMAKE_ROOT}/Modules/CMakeConfigurableFile.in"
+    configure_file("${CMAKE_ROOT}/Modules/CMakeConfigurableFile.in"
       "${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/CheckDIRSymbolExists.c" @ONLY)
 
-    MESSAGE(STATUS "Looking for DIR in ${FILES}")
-    TRY_COMPILE(${VARIABLE}
+    message(STATUS "Looking for DIR symbol in ${FILES}")
+    try_compile(${VARIABLE}
       ${CMAKE_BINARY_DIR}
       ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/CheckDIRSymbolExists.c
       COMPILE_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS}
       CMAKE_FLAGS 
       -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_DIRSYMBOL_EXISTS_FLAGS}
-      "${CHECK_DIRSYMBOL_EXISTS_LIBS}"
-      "${CMAKE_DIRSYMBOL_EXISTS_INCLUDES}"
+      "${MACRO_CHECK_DIRSYMBOL_EXISTS_LIBS}"
+      "${MACRO_CHECK_DIRSYMBOL_EXISTS_INCLUDES}"
       OUTPUT_VARIABLE OUTPUT)
-    IF(${VARIABLE})
-      MESSAGE(STATUS "Looking for DIR in ${FILES} - found")
-      SET(${VARIABLE} 1 CACHE INTERNAL "Have symbol DIR")
-      FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeOutput.log 
+    if(${VARIABLE})
+      message(STATUS "Looking for DIR symbol in ${FILES} - found")
+      set(${VARIABLE} 1 CACHE INTERNAL "Have symbol DIR")
+      file(APPEND ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeOutput.log 
         "Determining if the DIR symbol is defined as in AC_HEADER_DIRENT "
         "passed with the following output:\n"
         "${OUTPUT}\nFile ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/CheckDIRSymbolExists.c:\n"
         "${CMAKE_CONFIGURABLE_FILE_CONTENT}\n")
-    ELSE(${VARIABLE})
-      MESSAGE(STATUS "Looking for DIR in ${FILES} - not found.")
-      SET(${VARIABLE} "" CACHE INTERNAL "Have symbol DIR")
-      FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log 
+    else(${VARIABLE})
+      message(STATUS "Looking for DIR symbol in ${FILES} - not found.")
+      set(${VARIABLE} "" CACHE INTERNAL "Have symbol DIR")
+      file(APPEND ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log 
         "Determining if the DIR symbol is defined as in AC_HEADER_DIRENT "
         "failed with the following output:\n"
         "${OUTPUT}\nFile ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/CheckDIRSymbolExists.c:\n"
         "${CMAKE_CONFIGURABLE_FILE_CONTENT}\n")
-    ENDIF(${VARIABLE})
-  ENDIF(NOT DEFINED ${VARIABLE})
-ENDMACRO(CHECK_DIRSYMBOL_EXISTS)
+    endif(${VARIABLE})
+  endif(NOT DEFINED ${VARIABLE})
+endmacro(CHECK_DIRSYMBOL_EXISTS)
