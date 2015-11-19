9009ee86c30745b701760ac964f54a0bc6535b30
Alan W. Irwin <airwin@users.sourceforge.net>
2015-11-19 15:24:39 -0800

    Test Ada language support: move cmake_experimental directory contents to cmake_working

--- cmake/test_ada/cmake_working/Modules/CMakeDetermineAdaCompiler.cmake
+++ cmake/test_ada/cmake_working/Modules/CMakeDetermineAdaCompiler.cmake
@@ -1,61 +1,101 @@
-# Determine the compiler to use for Ada programs
+
+#=============================================================================
+# Copyright 2002-2009 Kitware, Inc.
+# Copyright 2015 Alan W. Irwin
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of Kitware, Inc., the Insight Software Consortium,
+#   nor the names of their contributors may be used to endorse or promote
+#   products derived from this software without specific prior written
+#   permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
+# determine the compiler to use for Ada programs
 # NOTE, a generator may set CMAKE_Ada_COMPILER before
 # loading this file to force a compiler.
-# use environment variable ADA first if defined by user, next use 
+# use environment variable ADA first if defined by user, next use
 # the cmake variable CMAKE_GENERATOR_ADA which can be defined by a generator
 # as a default compiler
+#
+# Sets the following variables:
+#   CMAKE_Ada_COMPILER
+#   CMAKE_COMPILER_IS_GNUADA
+#   CMAKE_AR
+#   CMAKE_RANLIB
+#
 
-IF(NOT CMAKE_Ada_COMPILER)
+include(${CMAKE_ROOT}/Modules/CMakeDetermineCompiler.cmake)
 
-  # prefer the environment variable ADA
-  IF($ENV{ADA} MATCHES ".+")
-    GET_FILENAME_COMPONENT(CMAKE_Ada_COMPILER_INIT $ENV{ADA} PROGRAM PROGRAM_ARGS CMAKE_Ada_FLAGS_ENV_INIT)
-    IF(CMAKE_Ada_FLAGS_ENV_INIT)
-      SET(CMAKE_Ada_COMPILER_ARG1 "${CMAKE_Ada_FLAGS_ENV_INIT}" CACHE STRING "First argument to Ada compiler")
-    ENDIF(CMAKE_Ada_FLAGS_ENV_INIT)
-    IF(EXISTS ${CMAKE_Ada_COMPILER_INIT})
-    ELSE(EXISTS ${CMAKE_Ada_COMPILER_INIT})
-      MESSAGE(FATAL_ERROR "Could not find compiler set in environment variable ADA:\n$ENV{ADA}.") 
-    ENDIF(EXISTS ${CMAKE_Ada_COMPILER_INIT})
-  ENDIF($ENV{ADA} MATCHES ".+")
+# Load system-specific compiler preferences for this language.
+include(Platform/${CMAKE_SYSTEM_NAME}-Ada OPTIONAL)
+if(NOT CMAKE_Ada_COMPILER_NAMES)
+  set(CMAKE_Ada_COMPILER_NAMES gnatgcc)
+endif()
 
-  # next try prefer the compiler specified by the generator
-  IF(CMAKE_GENERATOR_ADA) 
-    IF(NOT CMAKE_Ada_COMPILER_INIT)
-      SET(CMAKE_Ada_COMPILER_INIT ${CMAKE_GENERATOR_ADA})
-    ENDIF(NOT CMAKE_Ada_COMPILER_INIT)
-  ENDIF(CMAKE_GENERATOR_ADA)
+if(${CMAKE_GENERATOR} MATCHES "Visual Studio")
+elseif("${CMAKE_GENERATOR}" MATCHES "Xcode")
+else()
+  if(NOT CMAKE_Ada_COMPILER)
+    set(CMAKE_Ada_COMPILER_INIT NOTFOUND)
 
-  # finally list compilers to try
-  IF(CMAKE_Ada_COMPILER_INIT)
-    SET(CMAKE_Ada_COMPILER_LIST ${CMAKE_Ada_COMPILER_INIT})
-  ELSE(CMAKE_Ada_COMPILER_INIT)
-    SET(CMAKE_Ada_COMPILER_LIST gnatgcc gcc)  
-  ENDIF(CMAKE_Ada_COMPILER_INIT)
+    # prefer the environment variable ADA
+    if(NOT $ENV{ADA} STREQUAL "")
+      get_filename_component(CMAKE_Ada_COMPILER_INIT $ENV{ADA} PROGRAM PROGRAM_ARGS CMAKE_Ada_FLAGS_ENV_INIT)
+      if(CMAKE_Ada_FLAGS_ENV_INIT)
+        set(CMAKE_Ada_COMPILER_ARG1 "${CMAKE_Ada_FLAGS_ENV_INIT}" CACHE STRING "First argument to Ada compiler")
+      endif()
+      if(NOT EXISTS ${CMAKE_Ada_COMPILER_INIT})
+        message(FATAL_ERROR "Could not find compiler set in environment variable ADA:\n$ENV{ADA}.\n${CMAKE_Ada_COMPILER_INIT}")
+      endif()
+    endif()
 
-  # Find the compiler.
-  FIND_PROGRAM(CMAKE_Ada_COMPILER NAMES ${CMAKE_Ada_COMPILER_LIST} DOC "Ada compiler")
-  IF(CMAKE_Ada_COMPILER_INIT AND NOT CMAKE_Ada_COMPILER)
-    SET(CMAKE_Ada_COMPILER "${CMAKE_Ada_COMPILER_INIT}" CACHE FILEPATH "Ada compiler" FORCE)
-  ENDIF(CMAKE_Ada_COMPILER_INIT AND NOT CMAKE_Ada_COMPILER)
-ENDIF(NOT CMAKE_Ada_COMPILER)
-MARK_AS_ADVANCED(CMAKE_Ada_COMPILER)
+    # next prefer the generator specified compiler
+    if(CMAKE_GENERATOR_ADA)
+      if(NOT CMAKE_Ada_COMPILER_INIT)
+        set(CMAKE_Ada_COMPILER_INIT ${CMAKE_GENERATOR_ADA})
+      endif()
+    endif()
 
-GET_FILENAME_COMPONENT(COMPILER_LOCATION "${CMAKE_Ada_COMPILER}"
-  PATH)
+    # finally list compilers to try
+    if(NOT CMAKE_Ada_COMPILER_INIT)
+      set(CMAKE_Ada_COMPILER_LIST gnatgcc gcc)
+    endif()
 
-FIND_PROGRAM(GNAT_EXECUTABLE_BUILDER NAMES gnatmake PATHS ${COMPILER_LOCATION} )
-FIND_PROGRAM(CMAKE_AR NAMES ar PATHS ${COMPILER_LOCATION} )
+    _cmake_find_compiler(Ada)
+  else()
+    _cmake_find_compiler_path(Ada)
+  endif()
+  mark_as_advanced(CMAKE_Ada_COMPILER)
 
-FIND_PROGRAM(CMAKE_RANLIB NAMES ranlib)
-IF(NOT CMAKE_RANLIB)
-   SET(CMAKE_RANLIB : CACHE INTERNAL "noop for ranlib")
-ENDIF(NOT CMAKE_RANLIB)
-MARK_AS_ADVANCED(CMAKE_RANLIB)
+endif()
 
-# configure variables set in this file for fast reload later on
-CONFIGURE_FILE(${CMAKE_MODULE_PATH}/CMakeAdaCompiler.cmake.in
-               "${CMAKE_PLATFORM_INFO_DIR}/CMakeAdaCompiler.cmake" IMMEDIATE)
-MARK_AS_ADVANCED(CMAKE_AR)
+# configure all variables set in this file
+#configure_file(${CMAKE_ROOT}/Modules/CMakeAdaCompiler.cmake.in
+configure_file(${CMAKE_MODULE_PATH}/CMakeAdaCompiler.cmake.in
+  ${CMAKE_PLATFORM_INFO_DIR}/CMakeAdaCompiler.cmake
+  @ONLY
+  )
 
-SET(CMAKE_Ada_COMPILER_ENV_VAR "ADA")
+set(CMAKE_Ada_COMPILER_ENV_VAR "ADA")
--- cmake/test_ada/cmake_working/Modules/CMakeTestAdaCompiler.cmake
+++ cmake/test_ada/cmake_working/Modules/CMakeTestAdaCompiler.cmake
@@ -1,26 +1,76 @@
-# This file is used by EnableLanguage in cmGlobalGenerator to determine that
-# the Ada builder GNAT_EXECUTABLE_BUILDER = gnatmake can actually compile
-# and link the most basic of programs.  If not, a fatal error is set and
-# cmake stops processing commands and will not generate any makefiles or
-# projects.
 
-IF(NOT CMAKE_Ada_COMPILER_WORKS)
-  MESSAGE(STATUS "Check for working Ada builder: ${GNAT_EXECUTABLE_BUILDER}")
-  FILE(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testadacompiler.adb
-  "
+#=============================================================================
+# Copyright 2003-2012 Kitware, Inc.
+# Copyright 2015 Alan W. Irwin
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of Kitware, Inc., the Insight Software Consortium,
+#   nor the names of their contributors may be used to endorse or promote
+#   products derived from this software without specific prior written
+#   permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
+if(CMAKE_Ada_COMPILER_FORCED)
+  # The compiler configuration was forced by the user.
+  # Assume the user has configured all compiler information.
+  set(CMAKE_Ada_COMPILER_WORKS TRUE)
+  return()
+endif()
+
+include(CMakeTestCompilerCommon)
+
+# Remove any cached result from an older CMake version.
+# We now store this in CMakeAdaCompiler.cmake.
+unset(CMAKE_Ada_COMPILER_WORKS CACHE)
+
+# This file is used by EnableLanguage in cmGlobalGenerator to
+# determine that that selected Ada compiler can actually compile
+# and link the most basic of programs.   If not, a fatal error
+# is set and cmake stops processing commands and will not generate
+# any makefiles or projects.
+if(NOT CMAKE_Ada_COMPILER_WORKS)
+  PrintTestCompilerStatus("${GNAT_EXECUTABLE_BUILDER}" "")
+  file(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testadacompiler.adb
+    "
 procedure testAdaCompiler is
 begin
   null;
 end;
   "
-  )
-
+    )
+    
+  # Must use "whole project" form of try_compile because the "compile
+  # file" form is too generic to be used for Ada compilation of an
+  # executable which has special requirements with regard to source
+  # file must be named target.adb.
   FILE(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/CMakeLists.txt
   "
 cmake_minimum_required(VERSION 3.0.2 FATAL_ERROR)
 # FIXME: This specific location currently needs to be changed whenever the
 # location of the Ada support files are changed.
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake_working/Modules)
+set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake_experimental/Modules)
 set(CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL \"\" FORCE)
 project(test Ada)
 add_executable(testadacompiler testadacompiler.adb)
@@ -36,26 +86,42 @@ add_executable(testadacompiler testadacompiler.adb)
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
   projectName
-  OUTPUT_VARIABLE OUTPUT
+  OUTPUT_VARIABLE __CMAKE_Ada_COMPILER_OUTPUT
   )
   SET(ADA_TEST_WAS_RUN 1)
-ENDIF(NOT CMAKE_Ada_COMPILER_WORKS)
 
-IF(NOT CMAKE_Ada_COMPILER_WORKS)
-  MESSAGE(STATUS "Check for working Ada builder: ${GNAT_EXECUTABLE_BUILDER} -- broken")
-  FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
-    "Determining if the Ada builder works failed with "
-    "the following output:\n${OUTPUT}\n\n")
-  MESSAGE(FATAL_ERROR "The Ada builder \"${GNAT_EXECUTABLE_BUILDER}\" "
-    "is not able to compile, bind, and link a simple test program.\nIt fails "
-    "with the following output:\n ${OUTPUT}\n\n"
+  # Move result from cache to normal variable.
+  set(CMAKE_Ada_COMPILER_WORKS ${CMAKE_Ada_COMPILER_WORKS})
+  unset(CMAKE_Ada_COMPILER_WORKS CACHE)
+  set(Ada_TEST_WAS_RUN 1)
+endif()
+
+if(NOT CMAKE_Ada_COMPILER_WORKS)
+  PrintTestCompilerStatus("${GNAT_EXECUTABLE_BUILDER}" " -- broken")
+  file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
+    "Determining if the Ada compiler works failed with "
+    "the following output:\n${__CMAKE_Ada_COMPILER_OUTPUT}\n\n")
+  message(FATAL_ERROR "The Ada compiler \"${CMAKE_Ada_COMPILER}\" "
+    "is not able to compile a simple test program.\nIt fails "
+    "with the following output:\n ${__CMAKE_Ada_COMPILER_OUTPUT}\n\n"
     "CMake will not be able to correctly generate this project.")
-ELSE(NOT CMAKE_Ada_COMPILER_WORKS)
-  IF(ADA_TEST_WAS_RUN)
-    MESSAGE(STATUS "Check for working Ada builder: ${GNAT_EXECUTABLE_BUILDER} -- works")
-    FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
-      "Determining if the Ada builder works passed with "
-      "the following output:\n${OUTPUT}\n\n") 
-  ENDIF(ADA_TEST_WAS_RUN)
-  SET(CMAKE_Ada_COMPILER_WORKS 1 CACHE INTERNAL "")
-ENDIF(NOT CMAKE_Ada_COMPILER_WORKS)
+else()
+  if(ADA_TEST_WAS_RUN)
+    PrintTestCompilerStatus("${GNAT_EXECUTABLE_BUILDER}" " -- works")
+    file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
+      "Determining if the Ada compiler works passed with "
+      "the following output:\n${__CMAKE_Ada_COMPILER_OUTPUT}\n\n")
+  endif()
+
+  # Re-configure to save learned information.
+  configure_file(
+    #${CMAKE_ROOT}/Modules/CMakeAdaCompiler.cmake.in
+    ${CMAKE_MODULE_PATH}/CMakeAdaCompiler.cmake.in
+    ${CMAKE_PLATFORM_INFO_DIR}/CMakeAdaCompiler.cmake
+    @ONLY
+    )
+  include(${CMAKE_PLATFORM_INFO_DIR}/CMakeAdaCompiler.cmake)
+
+endif()
+
+unset(__CMAKE_Ada_COMPILER_OUTPUT)
