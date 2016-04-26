62b5e5290f1836297fb2a411d8dc7dc5a10bd4a0
Alan W. Irwin <airwin@users.sourceforge.net>
2016-04-26 16:35:54 -0700

    libplplot: logic improvements for pl_create_tempfile and pl_create_tempfifo
    Instead of using duplicate code to create the list of directories to
    attempt to use for temporary file and fifo creation,
    implement a static function, get_tmpdir_list, to create the list of
    possible directories.
    
    The list of directories that is tried is changed to be as follows:
    
    The directory names in the environment variables:
    "TMP" [Windows only],
    "TEMP" [Windows only],
    "TMPDIR" [Unix only];
    the value of the P_tmpdir macro as defined in <stdio.h> [Unix only];
    the current working directory; and
    the following hard-coded locations as a last resort:
    c:\\windows\\Temp [Windows only], and
    "/tmp" [Unix only].
    
    The above choices make the following changes in the directory list from
    the previous code.  "TMP" is added for the Windows case; the misspelling
    P_TMPDIR ==> P_tmpdir is corrected and P_tmpdir is now only used for
    the Unix case; and the current working directory is
    added.
    
    Note get_tmpdir_list delivers a list of non-NULL pointers that point
    to NULL-terminated character strings, and it is up to
    pl_create_tempfile and pl_create_tempfifo to use this list one string
    at a time to find and use the first string that is a suitable
    directory location for creating a temporary file or fifo.  This is a
    substantial improvement on the old code which included NULL pointers
    in the list and which only tried the first list item that corresponded
    to a non-NULL pointer.
    
    A pldebug call is now made from both pl_create_tempfile and
    pl_create_tempfifo to make it possible to debug cases where
    there are difficulties in generating temporary files or fifos.
    
    The bugs that were fixed in the previous code were the following:
    
    * The misspelled P_TMPDIR macro mentioned above.
    
    * Errors in the open function flags that are part of the solution when
    mkstemp is not available. The O_BINARY, _O_SHORT_LIVED, and
    _O_TEMPORARY flags are now removed for the Unix case since those flags
    are not defined for that case, but those flags continue to be used for
    the Windows case since they are available for that platform.
    
    * A check for errors was implemented for the mkdtemp call in
    pl_create_tempfifo.  Without such a check the old code segfaulted when
    an attempt was made to create a fifo in an inappropriate directory in
    the list of possibilites.
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian jessie) by building the test_c_tk, and svg targets (to
    satisfy necessary dependencies) and then running both
    
    valgrind examples/c/x00c -dev svg -o test.svg -debug >| debug.out 2>&1
    
    and
    
    valgrind examples/c/x00c -dev tk -debug >| debug.out 2>&1
    
    (The tk case is the only one that currently tests
    pl_create_tempfifo.)
    For the svg case, a perfect valgrind report was obtained (0 errors, no leaks are
    possible) while for the tk case the valgrind report was good (0
    errors, but leaks are possible).  I ascribe that "leaks are possible"
    result to issues with libtcl and libtk.  In both cases the
    appropriate debug messages were obtained as well as the appropriate
    plots.
    
    I repeated the same two tests for the 3 remaining possible combinations of the
    following two local changes to the code:
    
    * replace the preprocessing checks of the PL_HAVE_MKSTEMP macro with
    PL_HAVE_MKSTEMP1 to emulate the case where mkstemp is not available.
    
    * replace some or all of the valid directory possibilities with "whatever" to emulate
    when some or all of the directories on the list are inappropriate.
    
    The same good valgrind report results were obtained for all these three cases
    which validates how these cases are currently handled by the
    pl_create_tempfile and pl_create_tempfifo code.
    
    In sum, from all this testing on a Unix platform I am pretty sure this
    code will work on most Unix platforms, but testing is
    required for the Windows case to make sure I haven't introduced
    any showstopping bugs for that case.

--- src/plstdio.c
+++ src/plstdio.c
@@ -2,6 +2,7 @@
 //
 // Copyright (C) 2006  Jim Dishaw
 // Copyright (C) 2006  Hazen Babcock
+// Copyright (C) 2016  Alan W. Irwin
 //
 // This file is part of PLplot.
 //
@@ -21,15 +22,14 @@
 //
 //
 
+#define DEBUG
+#define NEED_PLDEBUG
 #include "plplotP.h"
 
-#if defined ( MSDOS ) || defined ( WIN32 ) || defined ( _WIN32 )
+// These three are needed for open.
 #include <sys/types.h>
-#include <fcntl.h>
-#endif
-
-// This is needed for mode flags for mkfifo as well as for MSDOS / WIN32
 #include <sys/stat.h>
+#include <fcntl.h>
 
 // For Visual C++ 2005 and later mktemp() and open() are deprecated (see
 // http://msdn.microsoft.com/en-us/library/ms235413.aspx and
@@ -43,6 +43,19 @@
 #define open      _open
 #define fdopen    _fdopen
 #endif
+
+// AM: getcwd has a somewhat strange status on Windows, its proper
+// name is _getcwd, this is a problem in the case of DLLs, like with
+// the Java bindings. The function _getcwd() is
+// declared in direct.h for Visual C++.
+#if defined ( _MSC_VER )
+#  include <direct.h>
+#  define getcwd    _getcwd
+#endif
+
+// Static function declaration
+static PLINT
+get_tmpdir_list( PLINT max_ntmpdir_list, char ** tmpdir_list, PLINT maxcurrdir, char * currdir );
 
 //
 // plio_write()
@@ -151,13 +164,68 @@ plio_fgets( char *buf, int size, FILE *stream )
     }
 }
 
+// Get list of possible directories for creating temporary files.
+// This list of directories is as follows:
+// The directory names in the environment variables:
+// "TMP" [Windows only]
+// "TEMP" [Windows only]
+// "TMPDIR" [Unix only]
+// The value of the P_tmpdir macro as defined in <stdio.h> [Unix only]
+// The current working directory
+// Some specific hard-coded locations that are guessed for the
+// Unix and Windows cases.
+
+static PLINT
+get_tmpdir_list( PLINT max_ntmpdir_list, char ** tmpdir_list, PLINT maxcurrdir, char * currdir )
+{
+    PLINT ntmpdir_list = 0;
+#if defined ( MSDOS ) || defined ( WIN32 )
+    tmpdir_list[ntmpdir_list] = getenv( "TMP" );
+    if ( tmpdir_list[ntmpdir_list] != NULL && ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+
+    tmpdir_list[ntmpdir_list] = getenv( "TEMP" );
+    if ( tmpdir_list[ntmpdir_list] != NULL && ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+#else
+    tmpdir_list[ntmpdir_list] = getenv( "TMPDIR" );
+    if ( tmpdir_list[ntmpdir_list] != NULL && ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+
+// The P_tmpdir macro is defined in stdio.h on many UNIX systems - try that
+#ifdef P_tmpdir
+    tmpdir_list[ntmpdir_list] = P_tmpdir;
+    if ( tmpdir_list[ntmpdir_list] != NULL && ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+#endif
+#endif //#if defined ( MSDOS ) || defined ( WIN32 )
+
+// Add current working directory to list where the currdir char array
+// space should be provided by the calling routine with size of maxcurrdir.
+    if ( getcwd( currdir, maxcurrdir ) == NULL )
+        plexit( "get_tmpdir_list: getcwd error" );
+    tmpdir_list[ntmpdir_list] = currdir;
+    if ( ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+
+#if defined ( MSDOS ) || defined ( WIN32 )
+    tmpdir_list[ntmpdir_list] = "c:\\windows\\Temp";
+#else
+    tmpdir_list[ntmpdir_list] = "/tmp";
+#endif
+    if ( ntmpdir_list < max_ntmpdir_list )
+        ntmpdir_list++;
+    return ntmpdir_list;
+}
+
 //
 // pl_create_tempfile()
 //
 // Securely create a temporary file and return a file handle to it.
 // This provides cross-platform compatibility and also adds some
-// additional functionality over mkstemp in that it honours the TMP /
-// TMPDIR / TEMP environment variables.
+// additional functionality over mkstemp in that it uses the
+// potential temporary directory locations specified in the
+// get_tmpdir_list function.
 //
 // The function returns the file handle.
 //
@@ -172,47 +240,65 @@ FILE *
 pl_create_tempfile( char **fname )
 {
     FILE       *fd;
-    const char *tmpdir;
-    char       *template;
-    const char *tmpname = "plplot_XXXXXX";
-#if !defined PL_HAVE_MKSTEMP
+    char       *tmpdir;
+    char       *template = NULL;
+    const char *tmpname  = "plplot_XXXXXX";
+#ifndef PL_HAVE_MKSTEMP
     int        flags;
 #endif
+    PLINT      ntmpdir_list;
+#define PL_MAXNTMPDIR_LIST    5
+    char       * tmpdir_list[PL_MAXNTMPDIR_LIST];
+    char       currdir[PLPLOT_MAX_PATH];
+    int        itmpdir;
 
-#if defined ( MSDOS ) || defined ( WIN32 )
-    tmpdir = getenv( "TEMP" );
-#else
-    tmpdir = getenv( "TMPDIR" );
-#endif
-
-// The P_TMPDIR macro is defined in stdio.h on many UNIX systems - try that
-#ifdef P_TMPDIR
-    if ( tmpdir == NULL )
-        tmpdir = P_TMPDIR;
-#endif
-
-    if ( tmpdir == NULL )
+    ntmpdir_list = get_tmpdir_list( PL_MAXNTMPDIR_LIST, tmpdir_list, PLPLOT_MAX_PATH, currdir );
+    for ( itmpdir = 0; itmpdir < ntmpdir_list; itmpdir++ )
     {
-#if defined ( MSDOS ) || defined ( WIN32 )
-        tmpdir = "c:\\windows\\Temp";
-#else
-        tmpdir = "/tmp";
-#endif
-    }
+        // Only guarantee is that tmpdir is not NULL and points to a NULL-terminated string.
+        tmpdir = tmpdir_list[itmpdir];
+        pldebug( "pl_create_tempfile", "Attempting to create temporary file in %s directory\n", tmpdir );
 
-    // N.B. Malloc ensures template is long enough so strcpy and strcat
-    // are safe here
-    template = (char *) malloc( sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + 2 ) );
-    strcpy( template, tmpdir );
+        // N.B. realloc ensures template is long enough so strcpy and strcat
+        // are safe here.
+        template = (char *) realloc( template, sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + 2 ) );
+        strcpy( template, tmpdir );
 #if defined ( MSDOS ) || defined ( WIN32 )
-    strcat( template, "\\" );
+        strcat( template, "\\" );
 #else
-    strcat( template, "/" );
+        strcat( template, "/" );
 #endif
-    strcat( template, tmpname );
+        strcat( template, tmpname );
 
 #ifdef PL_HAVE_MKSTEMP
-    fd = fdopen( mkstemp( template ), "wb+" );
+        fd = fdopen( mkstemp( template ), "wb+" );
+#else   //#ifdef PL_HAVE_MKSTEMP
+        // This conditionally compiled code branch is only an insecure last resort
+        // if mkstemp is not available.
+#if !defined ( _S_IREAD )
+#define _S_IREAD     256
+#endif
+#if !defined ( _S_IWRITE )
+#define _S_IWRITE    128
+#endif
+        fd    = NULL;
+        flags = O_RDWR | O_CREAT | O_EXCL;
+#if defined ( MSDOS ) || defined ( WIN32 )
+        // These are flags that are only relevant to Windows open.
+        flags = flags | O_BINARY | _O_SHORT_LIVED;
+        // If we are not returning the file name then add (Windows) flag to automatically
+        // delete file once all file handles are closed.
+        if ( fname == NULL )
+            flags = flags | _O_TEMPORARY;
+#endif
+        mktemp( template );
+        fd = fdopen( open( template, flags, _S_IREAD | _S_IWRITE ), "wb+" );
+#endif  // #ifdef PL_HAVE_MKSTEMP
+        if ( fd == NULL )
+            continue;
+        else
+            break;
+    }
     if ( fd == NULL )
     {
         plwarn( "pl_create_tempfile: Unable to open temporary file - returning" );
@@ -221,29 +307,12 @@ pl_create_tempfile( char **fname )
         free( template );
         return NULL;
     }
+#if defined ( PL_HAVE_MKSTEMP ) && defined ( PL_HAVE_UNLINK )
     // If we are not returning the file name then unlink the file so it is
     // automatically deleted.
-#ifdef PL_HAVE_UNLINK
     if ( fname == NULL )
         unlink( template );
 #endif
-#else
-#if !defined ( _S_IREAD )
-#define _S_IREAD     256
-#endif
-#if !defined ( _S_IWRITE )
-#define _S_IWRITE    128
-#endif
-    fd    = NULL;
-    flags = O_RDWR | O_BINARY | O_CREAT | O_EXCL | _O_SHORT_LIVED;
-    // If we are not returning the file name then add flag to automatically
-    // delete file once all file handles are closed.
-    if ( fname == NULL )
-        flags = flags | _O_TEMPORARY;
-    mktemp( template );
-    fd = fdopen( open( template, flags, _S_IREAD | _S_IWRITE ), "wb+" );
-#endif
-
     if ( fname != NULL )
     {
         *fname = template;
@@ -265,8 +334,9 @@ pl_create_tempfile( char **fname )
 // creates the named fifo in this directory. The combination of
 // a private directory and mkfifo failing if the file name already exists
 // makes this secure against race conditions / DoS attacks. This function
-// includes additional functionality over mkdtemp in that it honours the
-// TMP / TMPDIR / TEMP environment variables.
+// includes additional functionality over mkdtemp in that it uses the
+// potential temporary directory locations specified in the
+// get_tmpdir_list function.
 //
 // The function returns the file name of the fifo.
 //
@@ -278,59 +348,67 @@ pl_create_tempfifo( const char **p_fifoname, const char **p_dirname )
     return NULL;
 #else
     const char *tmpdir;
-    char       *template;
-    char       *dirname;
+    char       *template = NULL;
+    char       *dirname  = NULL;
     const char *tmpname  = "plplot_dir_XXXXXX";
     const char *fifoname = "plplot_fifo";
 
-#if defined ( MSDOS ) || defined ( WIN32 )
-    tmpdir = getenv( "TEMP" );
-#else
-    tmpdir = getenv( "TMPDIR" );
-#endif
+    PLINT      ntmpdir_list;
+#define PL_MAXNTMPDIR_LIST    5
+    char       * tmpdir_list[PL_MAXNTMPDIR_LIST];
+    char       currdir[PLPLOT_MAX_PATH];
+    int        itmpdir;
+    int        mkfifo_rc;
 
-// The P_TMPDIR macro is defined in stdio.h on many UNIX systems - try that
-#ifdef P_TMPDIR
-    if ( tmpdir == NULL )
-        tmpdir = P_TMPDIR;
-#endif
-
-    if ( tmpdir == NULL )
+    ntmpdir_list = get_tmpdir_list( PL_MAXNTMPDIR_LIST, tmpdir_list, PLPLOT_MAX_PATH, currdir );
+    for ( itmpdir = 0; itmpdir < ntmpdir_list; itmpdir++ )
     {
+        // Only guarantee is that tmpdir is not NULL and points to a NULL-terminated string.
+        tmpdir = tmpdir_list[itmpdir];
+        pldebug( "pl_create_tempfifo", "Attempting to create temporary fifo in %s directory\n", tmpdir );
+
+        // N.B. realloc ensures template is long enough so strcpy and strcat
+        // are safe here.
+        dirname = (char *) realloc( dirname, sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + 2 ) );
+        strcpy( dirname, tmpdir );
 #if defined ( MSDOS ) || defined ( WIN32 )
-        tmpdir = "c:\\windows\\Temp";
+        strcat( dirname, "\\" );
 #else
-        tmpdir = "/tmp";
+        strcat( dirname, "/" );
 #endif
+        strcat( dirname, tmpname );
+        // Create the temporary directory
+        dirname = mkdtemp( dirname );
+        if ( dirname == NULL )
+        {
+            // Mark this attempt as a failure.
+            mkfifo_rc = -1;
+            // This is a failed attempt so try other possibilities.
+            continue;
+        }
+        *p_dirname = dirname;
+
+        // Now create the fifo in the directory
+        template = (char *) realloc( template, sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + strlen( fifoname ) + 4 ) );
+        strcpy( template, dirname );
+#if defined ( MSDOS ) || defined ( WIN32 )
+        strcat( template, "\\" );
+#else
+        strcat( template, "/" );
+#endif
+        strcat( template, fifoname );
+        *p_fifoname = template;
+
+        // Check that mkfifo succeeds safely
+        mkfifo_rc = mkfifo( template, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH );
+        if ( mkfifo_rc < 0 )
+            // This is a failed attempt so try other possibilities.
+            continue;
+        else
+            break;
     }
-
-    // N.B. Malloc ensures template is long enough so strcpy and strcat
-    // are safe here
-    dirname = (char *) malloc( sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + 2 ) );
-    strcpy( dirname, tmpdir );
-#if defined ( MSDOS ) || defined ( WIN32 )
-    strcat( dirname, "\\" );
-#else
-    strcat( dirname, "/" );
-#endif
-    strcat( dirname, tmpname );
-    // Create the temporary directory
-    dirname    = mkdtemp( dirname );
-    *p_dirname = dirname;
-
-    // Now create the fifo in the directory
-    template = (char *) malloc( sizeof ( char ) * ( strlen( tmpdir ) + strlen( tmpname ) + strlen( fifoname ) + 4 ) );
-    strcpy( template, dirname );
-#if defined ( MSDOS ) || defined ( WIN32 )
-    strcat( template, "\\" );
-#else
-    strcat( template, "/" );
-#endif
-    strcat( template, fifoname );
-    *p_fifoname = template;
-
-    // Check that mkfifo succeeds safely
-    if ( mkfifo( template, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH ) < 0 )
+    // Check for failure of all attempts in above loop.
+    if ( mkfifo_rc < 0 )
     {
         plwarn( "mkfifo error" );
         free( template );
@@ -343,4 +421,3 @@ pl_create_tempfifo( const char **p_fifoname, const char **p_dirname )
     return template;
 #endif
 }
-
