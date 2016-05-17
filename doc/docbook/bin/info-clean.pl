031aec82a86c0fef6891f5b7558e009039e7dc40
Alan W. Irwin <airwin@users.sourceforge.net>
2016-05-16 20:40:37 -0700

    DocBook documentation:info backend: Do not comment out "part" tags
    This commenting out of "part" tags for the info backend was done long
    ago for unknown reasons, but those tags work fine now for our info
    backend (just like for every other backend) and uncommenting them in
    the info case also allows cross-references to the part tag id's to
    work properly for the info backend rather than generating an error.
    I additionally fixed a file dependency issue for the info target.
    That is, the "info" target now file depends
    on the info-clean.pl perl script used for massaging (e.g., the
    part tag commenting that was just removed) the DocBook source
    code into a form that can be used to generate our info documentation.
    
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> on Linx
    (Debian jessie) by using the -DBUILD_DOC=ON cmake option and
    by building the "info" target without any obvious errors.

--- doc/docbook/bin/info-clean.pl
+++ doc/docbook/bin/info-clean.pl
@@ -5,24 +5,24 @@ $intoc = 0;
 while (<>) {
   if (/(<!DOCTYPE.*)\[(.*)/) {
     print "$1 [$2\n" ; next}
-  if (/(.*)<part(.*)>(.*)/) {
-    $inpart = 1;
-    print "$1<!--<part$2>";
-    $_ = "$3\n";
-    redo;
-  }
-  if ($inpart and m{(.*)</title>(.*)}) {
-    $inpart = 0;
-    print "$1</title>-->";
-    $_ = "$2\n";
-    redo;
-  }
-  if (m{(.*)</part>(.*)}) {
-    $inpart = 1;
-    print "$1<!--</part>-->";
-    $_ = "$2\n";
-    redo;
-  }
+#  if (/(.*)<part(.*)>(.*)/) {
+#    $inpart = 1;
+#    print "$1<!--<part$2>";
+#    $_ = "$3\n";
+#    redo;
+#  }
+#  if ($inpart and m{(.*)</title>(.*)}) {
+#    $inpart = 0;
+#    print "$1</title>-->";
+#    $_ = "$2\n";
+#    redo;
+#  }
+#  if (m{(.*)</part>(.*)}) {
+#    $inpart = 1;
+#    print "$1<!--</part>-->";
+#    $_ = "$2\n";
+#    redo;
+#  }
   if (/(.*)<toc>(.*)/) {
     $intoc = 1;
     print "$1<!--<toc>";
