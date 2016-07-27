71c86fce7ebdce075a26b891a3b19abc49bf3a64
Alan W. Irwin <airwin@users.sourceforge.net>
2016-07-27 13:37:28 -0700

    Implement new test_fortran project to do what its name implies
    This project was inspired by the existing test_ada project.  Similarly
    to that project but for Fortran rather than Ada it creates a library
    with a routine that emits the "hello, world" phrase, and an executable
    that calls that routine.
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian Jessie) with gfortran by following all the steps in
    cmake/test_fortran/README.

---
+++ cmake/test_fortran/src_executable/hello.f90
@@ -0,0 +1,4 @@
+program hello_executable
+    use hello_module
+    call hello
+end program hello_executable
---
+++ cmake/test_fortran/src_lib/hello_1.f90
@@ -0,0 +1,6 @@
+module hello_module
+contains
+    subroutine hello()
+        write(*,"(a)") "hello, world"
+    end subroutine hello
+end module hello_module
