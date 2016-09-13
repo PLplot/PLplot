5b9dd303b50061c8ed3a9f149aea4b2a6817eece
Jerry Bauck <jbauck@users.sourceforge.net>
2016-09-13 02:51:31 -0700

    Restore backward compatiblity for Ada bindings
    Restore backward compatibility for the "standard" Ada binding that was lost when it was renamed from PLplot to PLplot_Standard, with files plplot.adb and plplot.ads renamed to plplot_standard.ads and plplot_standard.adb.
    
    A single new file was added, with the old name, plplot.ads. No .adb file is required. Users that have used the PLplot name need not change their code although a "deprecated" note is added herein.

---
+++ bindings/ada/plplot.ads
@@ -0,0 +1,29 @@
+-- Maintain backward compatibility of the Ada "standard" binding after changing the
+-- name to PLplot_Standard from PLplot. 
+
+-- Copyright (C) 2016 Jerry Bauck
+
+-- This file is part of PLplot.
+
+-- PLplot is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU Library General Public License as published
+-- by the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+
+-- PLplot is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Library General Public License for more details.
+
+-- You should have received a copy of the GNU Library General Public License
+-- along with PLplot; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+-- The preferred name for the "standard" Ada binding is PLplot_Standard. It replaces
+-- the earlier identical package that was named PLplot. This file simply renames that
+-- package so that applications which use the old binding name, PLplot, will still
+-- function as they did before the name change. The name PLplot is thus deprecated
+-- although with this file progams that with-use the PLplot name should always work.
+
+with PLplot_Standard;
+package PLplot renames PLplot_Standard;
