4963a8deac0df7d2c3e064299e5bc5b4c633f07d
Alan W. Irwin <airwin@users.sourceforge.net>
2016-10-04 12:10:32 -0700

    Create script to remove trailing blanks from essentially all text files in our source tree
    This script contains the mass 'sed' command that was recently used to
    clean up trailing blank problems in our source tree.  Thus, this
    script should be available for periodic use in the future to remove
    any trailing blanks that have crept into our source tree since the
    recent cleanup.  The sed command includes a regex to exclude certain
    files (binary files, licensing text, etc.) from the cleanup, and that
    regex may need additional review from time to time.
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian Jessie) by temporarily adding a blank to the end of line for
    one of our files, and running the script.  As expected, the script
    changed that file to remove the trailing blank, and made no other
    changes to our source tree (since the trailing blank problem has
    recently been cleaned up).

---
+++ scripts/remove_trailing_blanks.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script potentially can be quite intrusive since it
+# unconditionally removes trailing blanks from essentially all text files
+# in the source tree.  So after running this script you should use
+# "git diff" to make sure you agree with the source file changes generated
+# by this script before committing these generated changes.
+
+# Copyright (C) 2016 Alan W. Irwin
+#
+# This file is part of PLplot.
+#
+# PLplot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PLplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with PLplot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Find absolute PATH of script without using readlink (since readlink is
+# not available on all platforms).  Followed advice at
+# http://fritzthomas.com/open-source/linux/551-how-to-get-absolute-path-within-shell-script-part2/
+ORIGINAL_PATH="$(pwd)"
+cd "$(dirname $0)"
+# Absolute Path of the script
+SCRIPT_PATH="$(pwd)"
+cd "${ORIGINAL_PATH}"
+
+# Assumption: top-level source tree is parent directory of where script
+# is located.
+SOURCE_TREE="$(dirname ${SCRIPT_PATH})"
+cd $SOURCE_TREE
+
+# Remove trailing blanks on all files in source tree with the following exceptions.
+# * Exclude all files in the .git tree (don't want to mess with our repository).
+# * Exclude all binary files recognized in .gitattributes.
+# * Exclude all files in the debian, rpm, and lib trees (may review lib later).
+# * Exclude all *.patch files.
+# * Exclude libqhull/src/mem.h (may review this later when epa_build is updated).
+# * Exclude COPYING.LIB (since want to preserve exact LGPL licensing text).
+
+sed -i -e 's?  *$??' $(find . -type f |grep -E -v '\.git|\.pgm|\.gif|\.jpg|\.cgm|\.map|\.dbf|\.prj|\.shp|\.shx|\.fnt|debian/|rpm/|lib/|\.patch|libqhull/src/mem.h|COPYING.LIB' | xargs grep -l '  *$')
