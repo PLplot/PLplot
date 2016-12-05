9f90a72c9a8224f03a4c6498f47a084f2c96ef35
Alan W. Irwin <airwin@users.sourceforge.net>
2016-12-04 17:19:21 -0800

    Bug fix for TclSup.py: Allow the possibility that list of file devices is empty
    This bug in TclSup.py was discovered for the case where the cmake options
    
    -DDEFAULT_NO_DEVICES=ON -DPLD_xwin=ON -DDEFAULT_NO_BINDINGS=ON
    -DENABLE_tcl=ON -DENABLE_tk=ON -DENABLE_itcl=ON -DENABLE_itk=ON
    -DENABLE_python=ON
    
    were used and the test_pytkdemo target was built.  (The pytkdemo
    GUI's operation depends on TclSup.py working properly, and before
    this fix an empty list of file devices was causing python errors
    during the GUI startup.)
    
    Tested by Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian Jessie) using the above cmake options and
    building test_pyqtdemo without any build issues or GUI running
    issues (although I did not try the GUI button to
    store results to a file device since there are none of those
    for the above cmake options, so a graceful escape from that
    situation will likely take more programming effort if somebody
    is interested in further developing this GUI).

--- bindings/python/TclSup.py
+++ bindings/python/TclSup.py
@@ -5,28 +5,29 @@ import re;
 
 def TclList2Py(s):
     r = []
-    if s[0] == '{':
-	# List elements eclosed in braces.
-	j = 0
-	itm = ""
-	btwn_items = 1
-	for i in range(len(s)):
-	    if btwn_items:
-		if s[i] == '{':
-		    itm = ""
-		    btwn_items = 0
-		    continue
+    if len(s) > 0:
+        if s[0] == '{':
+            # List elements enclosed in braces.
+            j = 0
+            itm = ""
+            btwn_items = 1
+            for i in range(len(s)):
+                if btwn_items:
+                    if s[i] == '{':
+                        itm = ""
+                        btwn_items = 0
+                        continue
 
-	    if s[i] == '}':
-		# Finishing up an item.
-		r.append( itm )
-		btwn_items = 1
-		continue
-	
-	    itm = itm + s[i]
+                if s[i] == '}':
+                    # Finishing up an item.
+                    r.append( itm )
+                    btwn_items = 1
+                    continue
 
-    else:
-	# List elements delimited by spaces
-	r = re.split( s, ' ')
+                itm = itm + s[i]
+
+        else:
+            # List elements delimited by spaces
+            r = re.split( s, ' ')
 
     return r
