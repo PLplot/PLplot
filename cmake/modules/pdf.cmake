e38681885ac2972e4029f55eb7fc79170f6abe38
Alan W. Irwin <airwin@users.sourceforge.net>
2016-12-15 00:03:49 -0800

    Build system: Implement version check on libharu = libhpdf
    Generally speaking, libharu versions < 2.3.0 have a bug (fixed in
    2.3.0) where they do not support largish fonts such as occur in our
    example 24. Therefore, our build system now implements a version
    check for libharu version >= 2.3.0, and only if that check
    is passed is PLD_pdf allowed to be ON.
    
    Thanks to Hazen for suggesting this version check to deal with the
    several recent dashboard results where test_c_pdf failed on example
    24.
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian Jessie) by attempting to configure against libharu version
    2.2.1 (which was rejected as expected) and then libharu version
    2.3.0 (which was accepted as expected).  For the latter case,
    I built the test_c_pdf target, and build and run went well while
    spot checks using a pdf image viewer, e.g.
    
    gv examples/test_examples_output_dir/x24c.pdf
    
    showed the pdf device was slightly better quality in graphics than our
    psc device and roughly the same quality in text (i.e., extremely
    limited selection of glyphs available).  This was the first time I had
    checked this device linked against version 2.3.0 of libharu. (Previous
    checks were linked against a patched version 2.2.1 of libharu.) All
    seems well with this version of libharu so I feel comfortable with
    forcing those interested in using the pdf device driver
    to use this (latest) version of libharu.

--- cmake/modules/pdf.cmake
+++ cmake/modules/pdf.cmake
@@ -43,6 +43,36 @@ if(PLD_pdf)
     set(pdf_RPATH ${hpdf_LIBRARY_DIRS})
     filter_rpath(pdf_RPATH)
     set(DRIVERS_LINK_FLAGS ${DRIVERS_LINK_FLAGS} ${pdf_LINK_FLAGS})
+    # libharu version must be >= 2.3.0.
+    # Determine that version in cross-platform way.
+    set(check_libharu_version_source "
+#include \"hpdf.h\"
+int main(void)
+{
+// True if version is 2.3.0 or later....
+#if defined(HPDF_MAJOR_VERSION) && defined(HPDF_MINOR_VERSION) && defined(HPDF_BUGFIX_VERSION) \
+&&  (HPDF_BUGFIX_VERSION + 1000*(HPDF_MINOR_VERSION + 1000*HPDF_MAJOR_VERSION)) >= 2003000
+// Return success
+  return 0;
+#else
+// Return failure
+  return 1;
+#endif
+}
+")
+    include(CheckCSourceRuns)
+
+    cmake_push_check_state()
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${hpdf_INCLUDE_DIRS})
+    message(STATUS "Checking whether libharu version >= 2.3.0")
+    check_c_source_runs("${check_libharu_version_source}" LIBHARU_VERSION_LARGE_ENOUGH)
+    cmake_pop_check_state()
+    if(NOT LIBHARU_VERSION_LARGE_ENOUGH)
+      message(STATUS
+	"WARNING: libharu version is less than 2.3.0 (required for handling medium-large fonts and other fixes) so setting PLD_pdf to OFF."
+	)
+      set(PLD_pdf OFF CACHE BOOL "Enable pdf device" FORCE)
+    endif(NOT LIBHARU_VERSION_LARGE_ENOUGH)
   else(hpdf_FOUND)
     message(STATUS "Looking for haru pdf header and library - not found")
     message(STATUS "WARNING: Setting PLD_pdf to OFF.")
