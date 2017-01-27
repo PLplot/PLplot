976b30798085f2d55639155b428728c95c51abab
Alan W. Irwin <airwin@users.sourceforge.net>
2017-01-26 17:49:52 -0800

    Update version information for the release of 5.12.0
    I looked at the backwards-incompatibility comments in our release
    notes as well as "git diff" results for various library source code
    directly locations within our source tree in order to determine the
    correct major=SOVERSION, minor, and patch subfields for our various
    library versions according to the library versioning rules in
    cmake/modules/plplot_version.cmake.

--- cmake/modules/plplot_version.cmake
+++ cmake/modules/plplot_version.cmake
@@ -1,8 +1,8 @@
 # Version data that need review and possible modification for each release.
 
 set(PLPLOT_VERSION_MAJOR 5)
-set(PLPLOT_VERSION_MINOR 11)
-set(PLPLOT_VERSION_PATCH 1)
+set(PLPLOT_VERSION_MINOR 12)
+set(PLPLOT_VERSION_PATCH 0)
 
 # Overall PLplot version number.
 set(PLPLOT_VERSION ${PLPLOT_VERSION_MAJOR}.${PLPLOT_VERSION_MINOR}.${PLPLOT_VERSION_PATCH})
@@ -45,42 +45,44 @@ set(csirocsa_SOVERSION 0)
 set(csirocsa_VERSION ${csirocsa_SOVERSION}.0.1)
 
 set(csironn_SOVERSION 0)
-set(csironn_VERSION ${csironn_SOVERSION}.0.1)
+set(csironn_VERSION ${csironn_SOVERSION}.0.2)
 
 set(qsastime_SOVERSION 0)
 set(qsastime_VERSION ${qsastime_SOVERSION}.0.1)
 
 # Library with source code in the src subdirectory.
-set(plplot_SOVERSION 13)
-set(plplot_VERSION ${plplot_SOVERSION}.1.2)
+set(plplot_SOVERSION 14)
+set(plplot_VERSION ${plplot_SOVERSION}.0.0)
 
 # Libraries with source code in the bindings subdirectory tree.
-set(plplotcxx_SOVERSION 12)
+set(plplotcxx_SOVERSION 13)
 set(plplotcxx_VERSION ${plplotcxx_SOVERSION}.0.0)
 
-set(plplotdmd_SOVERSION 2)
+set(plplotdmd_SOVERSION 3)
 set(plplotdmd_VERSION ${plplotdmd_SOVERSION}.0.0)
 
-set(plplotf95c_SOVERSION 12)
-set(plplotf95c_VERSION ${plplotf95c_SOVERSION}.0.0)
-
-set(plplotf95_SOVERSION 12)
-set(plplotf95_VERSION ${plplotf95_SOVERSION}.1.0)
+set(plplotf95_SOVERSION 13)
+set(plplotf95_VERSION ${plplotf95_SOVERSION}.0.0)
 
 set(plplotqt_SOVERSION 2)
-set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.1)
+set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.2)
 
 set(tclmatrix_SOVERSION 10)
-set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.2.0)
+set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.3.0)
 
-set(plplottcltk_SOVERSION 12)
-set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.1.1)
+set(plplottcltk_SOVERSION 13)
+set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.0.0)
 
 set(plplottcltk_Main_SOVERSION 1)
 set(plplottcltk_Main_VERSION ${plplottcltk_Main_SOVERSION}.0.0)
 
 set(plplotwxwidgets_SOVERSION 1)
-set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.1.0)
+set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.2.0)
 
 set(plplotada_SOVERSION 2)
-set(plplotada_VERSION ${plplotada_SOVERSION}.0.0)
+set(plplotada_VERSION ${plplotada_SOVERSION}.1.0)
+
+# Fixed from now on since this is a frozen library that
+# is only used if PL_DEPRECATED_f95 is ON.
+set(plplotf95c_SOVERSION 12)
+set(plplotf95c_VERSION ${plplotf95c_SOVERSION}.0.0)
--- www/examples.php
+++ www/examples.php
@@ -234,7 +234,7 @@ END;
 		echo <<<END
 			<h3>Examples</h3>
 			<p>These examples were generated with the pngcairo device and
-				 reflect the status of PLplot-5.11.1.</p>
+				 reflect the status of PLplot-5.12.0.</p>
 			<p>Select a thumbnail to view full-size image and source code.</p>
 			<p>
 END;
