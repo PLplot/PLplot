28ffa1e843d6be445e1fc3ebdb76ede5f3358ea6
Alan W. Irwin <airwin@users.sourceforge.net>
2020-11-20 14:46:32 -0800

    Cache *_SOVERSION and *_VERSION library version strings
    My thanks to Nicolas Boulenguez and Rafael Laboissière for suggesting
    this change which allows packagers to override the *_SOVERSION and
    *_VERSION version strings for any plplot library in a convenient way
    on the cmake command line whenever necessary for packaging (e.g., when
    there is an ABI change for a particular language).
    
    Tested by: Alan W. Irwin <airwin@users.sourceforge.net> on Linux
    (Debian Stable = Buster) by configuring PLplot and checking the cached
    results of that configuration which are stored in CMakeCache.txt.

--- cmake/modules/plplot_version.cmake
+++ cmake/modules/plplot_version.cmake
@@ -35,55 +35,57 @@ set(CPACK_PACKAGE_VERSION_PATCH ${PLPLOT_VERSION_PATCH})
 # (4) If there are no library source code changes at all, then leave all
 #     library version numbers the same for the release.
 
-# N.B. all these variables must include the exact library name
-# so that the configure_library_build function works correctly.
+# N.B. all these variables must include the exact library name so that
+# the configure_library_build function works correctly.  All these
+# variables are cached to allow software distribution packagers to
+# override them (say if there is an ABI change) if necessary.
 
 # Libraries with source code in the lib subdirectory tree.
-set(nistcd_SOVERSION 0)
-set(nistcd_VERSION ${nistcd_SOVERSION}.0.1)
+set(nistcd_SOVERSION 0 CACHE STRING "linistcdb shared object version")
+set(nistcd_VERSION ${nistcd_SOVERSION}.0.1 CACHE STRING "libnistcd shared version")
 
-set(csirocsa_SOVERSION 0)
-set(csirocsa_VERSION ${csirocsa_SOVERSION}.0.1)
+set(csirocsa_SOVERSION 0 CACHE STRING "libcsirocsa shared object version")
+set(csirocsa_VERSION ${csirocsa_SOVERSION}.0.1 CACHE STRING "libcsirocsa shared version")
 
-set(csironn_SOVERSION 0)
-set(csironn_VERSION ${csironn_SOVERSION}.0.2)
+set(csironn_SOVERSION 0 CACHE STRING "libcsironn shared object version")
+set(csironn_VERSION ${csironn_SOVERSION}.0.2 CACHE STRING "libcsironn shared version")
 
-set(qsastime_SOVERSION 0)
-set(qsastime_VERSION ${qsastime_SOVERSION}.0.1)
+set(qsastime_SOVERSION 0 CACHE STRING "libqsastime shared object version")
+set(qsastime_VERSION ${qsastime_SOVERSION}.0.1 CACHE STRING "libqsastime shared version")
 
 # Library with source code in the src subdirectory and headers in the include subdirectory.
 
-set(plplot_SOVERSION 17)
-set(plplot_VERSION ${plplot_SOVERSION}.0.0)
+set(plplot_SOVERSION 17 CACHE STRING "libplplot shared object version")
+set(plplot_VERSION ${plplot_SOVERSION}.0.0 CACHE STRING "libplplot shared version")
 
 # Libraries with source code in the bindings subdirectory tree.
 
-set(plplotada_SOVERSION 4)
-set(plplotada_VERSION ${plplotada_SOVERSION}.0.0)
+set(plplotada_SOVERSION 4 CACHE STRING "libplplotada shared object version")
+set(plplotada_VERSION ${plplotada_SOVERSION}.0.0 CACHE STRING "libplplotada shared version")
 
-set(plplotcxx_SOVERSION 15)
-set(plplotcxx_VERSION ${plplotcxx_SOVERSION}.0.0)
+set(plplotcxx_SOVERSION 15 CACHE STRING "libplplotcxx shared object version")
+set(plplotcxx_VERSION ${plplotcxx_SOVERSION}.0.0 CACHE STRING "libplplotcxx shared version")
 
-set(plplotdmd_SOVERSION 4)
-set(plplotdmd_VERSION ${plplotdmd_SOVERSION}.0.0)
+set(plplotdmd_SOVERSION 4 CACHE STRING "libplplotdmd shared object version")
+set(plplotdmd_VERSION ${plplotdmd_SOVERSION}.0.0 CACHE STRING "libplplotdmd shared version")
 
-set(plplotfortran_SOVERSION 0)
-set(plplotfortran_VERSION ${plplotfortran_SOVERSION}.2.0)
+set(plplotfortran_SOVERSION 0 CACHE STRING "libplplotfortran shared object version")
+set(plplotfortran_VERSION ${plplotfortran_SOVERSION}.2.0 CACHE STRING "libplplotfortran shared version")
 
-set(plplotqt_SOVERSION 2)
-set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.3)
+set(plplotqt_SOVERSION 2 CACHE STRING "libplplotqt shared object version")
+set(plplotqt_VERSION ${plplotqt_SOVERSION}.0.3 CACHE STRING "libplplotqt shared version")
 
-set(tclmatrix_SOVERSION 10)
-set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.3.0)
+set(tclmatrix_SOVERSION 10 CACHE STRING "libtclmatrix shared object version")
+set(tclmatrix_VERSION ${tclmatrix_SOVERSION}.3.0 CACHE STRING "libtclmatrix shared version")
 
-set(plplottcltk_SOVERSION 14)
-set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.1.0)
+set(plplottcltk_SOVERSION 14 CACHE STRING "libplplottcltk shared object version")
+set(plplottcltk_VERSION ${plplottcltk_SOVERSION}.1.0 CACHE STRING "libplplottcltk shared version")
 
-set(plplottcltk_Main_SOVERSION 1)
-set(plplottcltk_Main_VERSION ${plplottcltk_Main_SOVERSION}.0.1)
+set(plplottcltk_Main_SOVERSION 1 CACHE STRING "libplplottcltk_Main shared object version")
+set(plplottcltk_Main_VERSION ${plplottcltk_Main_SOVERSION}.0.1 CACHE STRING "libplplottcltk_Main shared version")
 
-set(plplotwxwidgets_SOVERSION 1)
-set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.2.2)
+set(plplotwxwidgets_SOVERSION 1 CACHE STRING "libplplotwxwidgets shared object version")
+set(plplotwxwidgets_VERSION ${plplotwxwidgets_SOVERSION}.2.2 CACHE STRING "libplplotwxwidgets shared version")
 
-set(smokeplplotqt_SOVERSION 0)
-set(smokeplplotqt_VERSION ${smokeplplotqt_SOVERSION}.0.0)
+set(smokeplplotqt_SOVERSION 0 CACHE STRING "libsmokeplplotqt shared object version")
+set(smokeplplotqt_VERSION ${smokeplplotqt_SOVERSION}.0.0 CACHE STRING "libsmokeplplotqt shared version")
